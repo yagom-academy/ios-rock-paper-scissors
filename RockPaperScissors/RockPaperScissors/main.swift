6d672e94a2b757e8cea00709c8448dafde1fe6bc
yeon <jryoun1@naver.com>
2020-11-03 23:09:26 +0900

    add(main.swift 0.1 / jake) : 최초 실행시 출력해주는 함수

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,10 @@
 
 import Foundation
 
-print("Hello, World!")
-
+/*
+게임 시작할 때 최초로 출력해주는 함수
+code by jake
+*/
+func printRockPaperScissors () {
+    print("가위(1).바위(2).보(3)! <종료 : 0> : ", terminator:"")
+}
