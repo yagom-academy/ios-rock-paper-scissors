0deb4391bfb635c090fcc4b7718460b4d9c00703
zziro95 <opers7878@gmail.com>
2020-11-04 22:13:39 +0900

    [#2]리뷰후 코드수정 if문과 switch문 간결화

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -13,10 +13,8 @@ func printComputerPick(_ computerPick : Int) {
         print("컴퓨터 : 가위")
     case 2:
         print("컴퓨터 : 바위")
-    case 3:
-        print("컴퓨터 : 보")
     default:
-        print("1...3이 아닌 다른값이 들어올 경우가 있을까??")
+        print("컴퓨터 : 보")
     }
 }
 
@@ -35,37 +33,23 @@ func comparePick(_ usersPick : String, _ computerPick : Int){
         switch computerPick {
         case 2:
             print("졌습니다.")
-            break
-        case 3:
-            print("이겼습니다.")
-            break
         default:
-            print("default case code")
+            print("이겼습니다.")
         }
     } else if usersPick == "2" {
         switch computerPick {
         case 1:
             print("이겼습니다.")
-            break
-        case 3:
-            print("졌습니다.")
-            break
         default:
-            print("default case code")
+            print("졌습니다.")
         }
-    } else if usersPick == "3" {
+    } else  {
         switch computerPick {
         case 1:
             print("졌습니다.")
-            break
-        case 2:
-            print("이겼습니다.")
-            break
         default:
-            print("default case code")
+            print("이겼습니다.")
         }
-    } else {
-        print("1...3이 아닌 다른값이 들어올 경우가 있을까??")
     }
 }
 
@@ -73,8 +57,8 @@ func rockPaperScissors() {
     print("가위(1),바위(2),보(3)! <종료 : 0> : ", terminator : "")
     guard let usersPick = readLine() else { return }
     let computerPick = Int.random(in: 1...3)
-//    유저에게 입력받은 usersPick이 String타입임을 확인
-//    print(type(of: usersPick))
+//유저에게 입력받은 usersPick이 String타입임을 확인
+//print(type(of: usersPick))
     switch usersPick {
     case "0" :
         print("가위바위보 프로그램이 종료됩니다.")
@@ -93,7 +77,7 @@ func rockPaperScissors() {
         comparePick(usersPick, computerPick)
     default:
         print("잘못된 입력입니다. 다시 시도해 주세요.")
-        //한줄안띄워주면 눈이 너무 아파서 한줄개행
+//한줄안띄워주면 눈이 너무 아파서 한줄개행
         print()
         rockPaperScissors()
     }
