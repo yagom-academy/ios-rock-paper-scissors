6701f37fcb546f55e3e721ee829549f22bd06e4a
odong-Tree <ss12357@naver.com>
2020-11-04 14:31:11 +0900

    [#1] 유저픽과 컴퓨터의 픽 입력값 받기

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,16 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
 
-import Foundation
 
-print("Hello, World!")
+
+while true {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    guard let input = readLine() else { break }
+    guard var userPick: Int = Int(input) else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        break
+    }
+    var comPick = Int.random(in: 1...3)
+    
+    
+    
+}
 
