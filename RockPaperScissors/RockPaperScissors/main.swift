af9af7c8f5ef14454badaf8f3288ca5a28073226
iluxsm <be9.smile@gmail.com>
2020-11-04 22:01:58 +0900

    [#2] 가위바위보 구현하기

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,12 +6,23 @@
 
 import Foundation
 
+var victory: [(Int, UInt32)] = [(1, 3), (2, 1), (3, 2)]
+
 while(true) {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
     guard let input = readLine() else { continue }
     
     if input == "1" || input == "2" || input == "3" {
-        print("가위바위보 프로세스")
+        let randomNumber: UInt32 = arc4random_uniform(3) + 1
+        if victory.contains(where: { $0 == (Int(input), randomNumber) }) {
+            print("이겼습니다!")
+            break
+        } else if UInt32(input) == randomNumber {
+            print("비겼습니다!")
+        } else {
+            print("졌습니다!")
+            break
+        }
     } else if input == "0" {
         break
     } else {
