71b92be3350b1a477e25acc33c41d12a8f0b1d93
GREENOVER <humains29@gmail.com>
2020-11-04 10:49:33 +0900

    PR 테스트입니다.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,9 +2,29 @@
 //  RockPaperScissors - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//  2020.11.04 Green written
 
 import Foundation
 
-print("Hello, World!")
+func rockPaperScissors () {
+    print("가위(1), 바위(2), 보(3)!<종료 : 0> : ", terminator: " ")
+    
+    guard let hand: String = read.Line() else {return}
+    
+    switch hand {
+    case "0":
+        print("게임 종료")
+        break
+    case "1":
+    
+    case "2":
+        
+    case "3":
+        
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        rockPaperScissors()
+    }
+
+}
 
