9873f77638976e46722a6c165761a9d3b0a12114
odong-Tree <ss12357@naver.com>
2020-11-04 15:24:12 +0900

    [#2] 가위바위보 기능 구현, 입력값 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,14 +6,27 @@ while true {
     guard let input = readLine() else { break }
 
     var userPick = 0
-    let comPick = Int.random(in: 1...3)
+//    let comPick = Int.random(in: 1...3)
+    let comPick = 2
     
     switch input {
-    case "0"..."4" :
+    case "0","1","2","3" :
         userPick = Int(input)!
     default :
         print("잘못된 입력값입니다. 다시 시도해주세요.")
     }
 
+    switch userPick {
+    case 0:
+        break
+    case comPick :
+        print("비겼습니다!")
+    case comPick + 1, comPick - 2 :
+        print("이겼습니다!")
+        break
+    default:
+        print("졌습니다!")
+        break
+    }
 }
 
