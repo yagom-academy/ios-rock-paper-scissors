a6a250285e2049f29a1f2fde63369a169f718689
odong-Tree <ss12357@naver.com>
2020-11-04 14:54:35 +0900

    [#1-1] 유저픽, 컴퓨터픽 입력값 받기 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,13 +4,16 @@
 while true {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
     guard let input = readLine() else { break }
-    guard var userPick: Int = Int(input) else {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        break
+
+    var userPick = 0
+    let comPick = Int.random(in: 1...3)
+    
+    switch input {
+    case "0"..."4" :
+        userPick = Int(input)!
+    default :
+        print("잘못된 입력값입니다. 다시 시도해주세요.")
     }
-    var comPick = Int.random(in: 1...3)
-    
-    
-    
+
 }
 
