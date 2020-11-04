dbe3f643df9334b6dc0efd4d10859563f5b72eeb
zziro95 <opers7878@gmail.com>
2020-11-04 15:06:57 +0900

    [#1]Readme.md파일 수정, 유저에게 입력받고 입력값에 따라 케이스 구분하기

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,25 @@
 
 import Foundation
 
-print("Hello, World!")
+func rockPaperScissors() {
+    print("가위(1),바위(2),보(3)! <종료 : 0> : ", terminator : "")
+    guard let usersPick = readLine() else { return }
+//    유저에게 입력받은 usersPick이 String타입임을 확인
+//    print(type(of: usersPick))
+    switch usersPick {
+    case "0" :
+        print("가위바위보 프로그램이 종료됩니다.")
+        break
+    case "1" :
+        print("사용자 : 가위")
+    case "2" :
+        print("사용자 : 바위")
+    case "3" :
+        print("사용자 : 보")
+    default:
+        print("잘못된 입력입니다. 다시 시도해 주세요.")
+        rockPaperScissors()
+    }
+}
 
+rockPaperScissors()
