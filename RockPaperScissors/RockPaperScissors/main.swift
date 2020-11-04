828d0f0e54487b970fc9b50789eb4fdf89c3c73d
iluxsm <be9.smile@gmail.com>
2020-11-04 21:00:54 +0900

    [#1] 콘솔 출력 및 사용 입력 받기

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,15 @@
 
 import Foundation
 
-print("Hello, World!")
-
+while(true) {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    guard let input = readLine() else { continue }
+    
+    if input == "1" || input == "2" || input == "3" {
+        print("가위바위보 프로세스")
+    } else if input == "0" {
+        break
+    } else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
