f1580f0c42630685f510af22d2712e66e29b04ab
jae57 <ggapsong@naver.com>
2020-11-05 02:38:05 +0900

    #1 gawibawibo base

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,37 @@
 
 import Foundation
 
-print("Hello, World!")
+func gawibawibo(user: String, computer: String) {
+    guard let userNum = Int(user), let computerNum = Int(computer) else { return }
+    
+    if userNum > computerNum {
+        print("이겼습니다!")
+    } else if userNum < computerNum {
+        print("졌습니다!")
+    } else {
+        print("비겼습니다!")
+    }
+}
+
+var flag = true
+while(flag) {
+    guard let computerPick = ["1", "2", "3"].randomElement() else { break }
+    
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+    guard let userPick = readLine() else { break }
+    
+    switch userPick {
+    case "0":
+        flag = false
+    case "1", "2", "3":
+        gawibawibo(user: userPick, computer: computerPick)
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
+
+
+
+
+
 
