9849eeb62b267650652431e28399dbfc1b2e0c41
odong-Tree <ss12357@naver.com>
2020-11-05 19:14:10 +0900

    [#4] 묵찌빠 함수, 값 입력받기

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -3,6 +3,7 @@ func rockPaperScissors() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
     guard let input = readLine() else {
         print("잘못된 입력값입니다. 다시 시도해주세요.")
+        rockPaperScissors()
         return
     }
 
@@ -20,16 +21,44 @@ func rockPaperScissors() {
     switch userPick {
     case 0:
         break
-    case computerPick :
+    case computerPick:
         print("비겼습니다!")
         rockPaperScissors()
-    case computerPick + 1, computerPick - 2 :
+    case computerPick + 1, computerPick - 2:
         print("이겼습니다!")
-    default :
+    default:
         print("졌습니다!")
     }
 }
 
-rockPaperScissors()
 
+func mukzzipa(_ userPicked: Int, _ computerPicked: Int) {
+    var mukzzipaUserPick = userPicked
+    var mukzzipaComputerPick = computerPicked
+    
+    switch mukzzipaUserPick {
+    case mukzzipaComputerPick + 1, mukzzipaComputerPick - 2 :
+        print("[사용자 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
+    default :
+        print("[컴퓨터 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
+    // 가위바위보 결과, 이기고 진 경우에만 들어갈 함수이기 때문에 2가지 case만 필요
+    }
+    
+    guard let mukzzipaInput = readLine() else {
+        print("잘못된 입력값입니다. 다시 시도해주세요.")
+        mukzzipa(mukzzipaUserPick, mukzzipaComputerPick)
+        return
+    }
+
+    mukzzipaComputerPick = Int.random(in: 1...3)
+    
+    switch mukzzipaInput {
+    case "0", "1", "2", "3" :
+        mukzzipaUserPick = Int(mukzzipaInput)!
+    default :
+        print("잘못된 입력값입니다. 다시 시도해주세요.")
+        mukzzipa(mukzzipaUserPick, mukzzipaComputerPick)
+    }
+    
+}
 
