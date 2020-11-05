e1a0deee0778c1031fe36882524f53f4c3da7a84
llim <llim@student.42seoul.kr>
2020-11-05 17:06:44 +0900

    add func rspWinnerChecker

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,19 @@
 
 import Foundation
 
-print("Hello, World!")
-
+// 가위(1), 바위(2), 보(3)
+// return 값: 비긴경우 0, 사람이 이긴경우 1, 컴퓨터가 이긴경우 2
+func rspWinnerChecker(_ player: Int) -> Int {
+    let computer = Int.random(in: 1...3)
+    
+    // 비긴경우
+    if player == computer { return 0 }
+    // 사용자가 보 일때 판단
+    if player == 3 {
+        if computer == 2 { return 1 }
+        else { return 2 }
+    }
+    // 그 외
+    if (player - computer) == -1 { return 2 }
+    return 1
+}
