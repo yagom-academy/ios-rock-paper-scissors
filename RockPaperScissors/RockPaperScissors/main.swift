d90576db0e68764c4479366d1847856129326185
odong-Tree <ss12357@naver.com>
2020-11-05 18:46:10 +0900

    [#3] STEP1에서 띄어쓰기, input 받는 구문에 오류 메세지 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,13 +1,16 @@
 
 func rockPaperScissors() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-    guard let input = readLine() else { return }
+    guard let input = readLine() else {
+        print("잘못된 입력값입니다. 다시 시도해주세요.")
+        return
+    }
 
     var userPick = 0
     let computerPick = Int.random(in: 1...3)
     
     switch input {
-    case "0","1","2","3" :
+    case "0", "1", "2", "3" :
         userPick = Int(input)!
     default :
         print("잘못된 입력값입니다. 다시 시도해주세요.")
@@ -22,9 +25,11 @@ func rockPaperScissors() {
         rockPaperScissors()
     case computerPick + 1, computerPick - 2 :
         print("이겼습니다!")
-    default:
+    default :
         print("졌습니다!")
     }
 }
 
 rockPaperScissors()
+
+
