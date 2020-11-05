32dacdfa02fa7e30ed5744badd185f3dc2b423a5
wonhee <wonhee010@gmail.com>
2020-11-05 20:43:53 +0900

    add input logic

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,28 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
-print("Hello, World!")
+// step 1의 고정 출력문
+let order = "가위(1), 바위(2), 보(3)! <종료: 0> : "
+// step 2의 고정 출력문
+
+/*
+ * step 1
+ */
+// 게임을 지속할지에 대한 변수
+var continueGame = false
+repeat {
+    if let number = readLine() {
+        
+    }
+    // 입력 실패
+    else {
+        continueGame = false
+    }
+}
+while(continueGame)
 
