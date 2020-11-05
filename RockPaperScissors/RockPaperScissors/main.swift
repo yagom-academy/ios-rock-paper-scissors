84a0968a19d29dbe313a9e183f907ed7a66662e6
llim <llim@student.42seoul.kr>
2020-11-05 17:11:11 +0900

    add func rockScissorPaperGame

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -22,3 +22,41 @@ func rspWinnerChecker(_ player: Int) -> Int {
     if (player - computer) == -1 { return 2 }
     return 1
 }
+
+// return 값: 에러 -1, 사용자 승리 1, 컴퓨터 승리 2 -> 묵찌빠에서 사용하기 위해
+func rockScissorPaperGame() -> Int {
+    print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
+    
+    var condition: Int = 3 // rspWinnerChecker에서 받아오는 값
+    guard let input = readLine() else { return -1 }
+    
+    switch input {
+    case "0":
+        print("게임 종료")
+        return -1
+    case "1":
+        condition = rspWinnerChecker(1)
+    case "2":
+        condition = rspWinnerChecker(2)
+    case "3":
+        condition = rspWinnerChecker(3)
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        rockScissorPaperGame()
+    }
+    
+    switch condition {
+    case 0:
+        print("비겼습니다!")
+    case 1:
+        print("이겼습니다!")
+    case 2:
+        print("졌습니다!")
+    default:
+        rockScissorPaperGame()
+    }
+    if condition == 0 { condition = rockScissorPaperGame() }
+    return condition
+}
+
+rockScissorPaperGame()
