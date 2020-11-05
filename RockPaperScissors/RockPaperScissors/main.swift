c593f03d708b3801c80c4b99c9da9f999a89bd75
zziro95 <opers7878@gmail.com>
2020-11-05 10:43:11 +0900

    [#1]함수명,변수명 변경, 스위치문 불필요한 break문 삭제

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,32 +1,30 @@
 
-func galba() {
+func rockPaperScissors() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
     guard let input = readLine() else { return }
 
     var userPick = 0
-    let comPick = Int.random(in: 1...3)
+    let computerPick = Int.random(in: 1...3)
     
     switch input {
     case "0","1","2","3" :
         userPick = Int(input)!
     default :
         print("잘못된 입력값입니다. 다시 시도해주세요.")
-        galba()
+        rockPaperScissors()
     }
 
     switch userPick {
     case 0:
         break
-    case comPick :
+    case computerPick :
         print("비겼습니다!")
-        galba()
-    case comPick + 1, comPick - 2 :
+        rockPaperScissors()
+    case computerPick + 1, computerPick - 2 :
         print("이겼습니다!")
-        break
     default:
         print("졌습니다!")
-        break
     }
 }
 
-galba()
+rockPaperScissors()
