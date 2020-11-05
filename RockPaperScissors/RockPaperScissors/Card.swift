e345eab76b4bf578eea7d69bde442a49ef863511
innieminnie <rkddlsl1001@gmail.com>
2020-11-05 18:11:25 +0900

    [#11]chores: 변수명 일부 수정 및 주석 해설 달기

--- RockPaperScissors/RockPaperScissors/Card.swift
+++ RockPaperScissors/RockPaperScissors/Card.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct Card{
-    var cardIdx: Int
-    var winSet = [2,3,1]
+    var cardIdx: Int //카드패
+    let checkWinOrLose = [2,3,1] // 승패결정확인용 배열
     
     init() {
         self.cardIdx = 0
@@ -17,12 +17,25 @@ struct Card{
     
     //승패결정
     func playGame(_ idx1: Int, _ idx2: Int) -> Bool{
-        if winSet[idx1-1] == idx2{
+        if checkWinOrLose[idx1-1] == idx2{
             return false
         }else{
             return true
         }
     }
+    /*
+     해당 함수로 가위바위보 실행시, 호출한 구조체Card 본인의 입장에서 idx1에 본인의 idx, idx2에 상대방의 idx를 넣으면
+     이겼을 때 true를 리턴, 졌을 때 false를 리턴한다.
+     
+     해당 함수로 묵찌빠 실행시, 호출한 구조체Card 본인의 입장에서 idx1에 상대방의 idx, idx2에 본인의 idx를 넣으면
+     이겼을 때 true를 리턴, 졌을 때 false를 리턴한다.
+     
+     가위바위보,묵찌빠의 승패상관관계를 고려하여 하나의 게임실행함수로 만들고자 했고,
+     게임이 어떤 것이냐에 따라 파라미터가 (나의 idx, 상대방의 idx) 혹은 (상대방의 idx, 나의 idx) 가 들어갈 수 있기에
+     파라미터명은 idx1,idx2로 만들었다.
+     
+     승패를 확인하기 위해서 winOrLose 배열을 사용하고 배열의 index와 idx1을 매칭시키고, 배열의 value와 idx2를 매칭시켜 승패상관관계를 확인한다.
+     */
 }
 
 
--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,13 +6,13 @@
 
 import Foundation
 
-var myCard = Card()
-var partnerCard = Card()
+//player와 computer의 패(카드) 생성
+var playerCard = Card()
+var computerCard = Card()
 var currentTurn = Turn.사용자
 
-// 가위바위보 게임
-
-func getUserInput()->Int{
+// 가위바위보 or 묵찌빠 게임 시작 시, 입력값에 따른 처리
+func getPlayerInput()->Int{
     let inputNum = Int(readLine()!) ?? -1
     
     guard inputNum != 0 else {
@@ -33,32 +33,32 @@ func playRoundOne(){
     while(true){
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ",terminator:"")
         
-        let input = getUserInput()
+        let input = getPlayerInput()
         
+        //입력값 받는 함수의 return값에 따른 상황 처리
         switch input{
         case -1:
             continue
         case 0:
            return
         default:
-            myCard.cardIdx = input
+            playerCard.cardIdx = input // 사용자의 카드패 설정
+            computerCard.cardIdx = Int.random(in: 1...3) // 컴퓨터의 카드패 설정
         }
         
-        partnerCard.cardIdx = Int.random(in: 1...3)
+        print(playerCard.cardIdx, computerCard.cardIdx) // 랜덤값 뭐들어갔는지 확인용
         
-        print(myCard.cardIdx, partnerCard.cardIdx)
-        
-        if myCard.cardIdx != partnerCard.cardIdx{
-            if myCard.playGame(myCard.cardIdx,partnerCard.cardIdx){
-                currentTurn = Turn.사용자
+        if playerCard.cardIdx != computerCard.cardIdx{ //승패가 결정되는 경우 (묵찌빠 게임으로 이동)
+            if playerCard.playGame(playerCard.cardIdx,computerCard.cardIdx){ //true면 player가 이김
+                currentTurn = Turn.사용자 //사용자 차례로 묵찌빠 시작
                 print("이겼습니다!")
-            }else{
-                currentTurn = Turn.컴퓨터
+            }else{//false면 computer가 이김
+                currentTurn = Turn.컴퓨터 //컴퓨터 차례로 묵찌빠 시작
                 print("졌습니다!")
             }
-            playRoundTwo()
-            return
-        }else{
+            playRoundTwo() //묵찌빠 게임하러
+            return //묵찌빠끝나면 아예 종료
+        }else{ // 비길 경우 게임 계속 진행
             print("비겼습니다!")
         }
     }
@@ -68,9 +68,9 @@ func playRoundTwo(){
     print("묵찌빠 게임 시작")
     
     while(true){
-        print("[\(currentTurn) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ",terminator:"")
+        print("[\(currentTurn) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ",terminator:"") //currentTurn이 게임 진행에 따라 계속 업데이트됨
         
-        let input = getUserInput()
+        let input = getPlayerInput() // 가위바위보의 입력값 처리 과정과 동일
         
         switch input{
         case -1:
@@ -78,23 +78,22 @@ func playRoundTwo(){
         case 0:
             exit(0)
         default:
-            myCard.cardIdx = input
+            playerCard.cardIdx = input
+            computerCard.cardIdx = Int.random(in: 1...3)
         }
         
-        partnerCard.cardIdx = Int.random(in: 1...3)
+        print(playerCard.cardIdx, computerCard.cardIdx) // 랜덤값 뭐들어갔는지 확인용
         
-        print(myCard.cardIdx, partnerCard.cardIdx)
-        
-        if myCard.cardIdx != partnerCard.cardIdx{
-            currentTurn = myCard.playGame(partnerCard.cardIdx, myCard.cardIdx) ? Turn.사용자 : Turn.컴퓨터
-        }else{
+        if playerCard.cardIdx != computerCard.cardIdx{ // 패가 다를 경우, 게임계속진행되고 turn은 게임결과에따라 바뀜
+            currentTurn = playerCard.playGame(computerCard.cardIdx, playerCard.cardIdx) ? Turn.사용자 : Turn.컴퓨터 //true면 사용자이김 false면 컴퓨터이김
+        }else{ //패가 같은 경우 currentTurn이 이기고 게임종료
             print("\(currentTurn)의 승리!")
             return
         }
     }
 }
 
-playRoundOne()
+playRoundOne() //가위바위보 실행
 
 
 
