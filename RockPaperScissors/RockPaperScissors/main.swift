9f53d08d0a178da7076208837f67725eaef7bc9b
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-11-05 18:35:14 +0900

    주석 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -48,27 +48,32 @@ func scissorsRockPaper() {
     while true {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
         
-        // 사용자 입력 받기
+        // 유저 입력 받기
         guard let input = readLine(), let inputNumber = Int(input) else {
             print("잘못된 입력입니다. 다시 시도해주세요.")
             continue
         }
         
+        // '0'입력시 게임 종료
         guard inputNumber != 0 else {
             print("게임 종료")
             break
         }
         
+        // 가위바위보 내기
         guard let myHand = Hand(rawValue: inputNumber),
             let computerHand = Hand(rawValue: Int.random(in: 1...3)) else {
             break
         }
         
+        // 결과 판독
         let gameResult = scissorsRockPaperResult(myHand: myHand, otherHand: computerHand)
         
+        // 결과 출력
         print("\(myHand)(나) vs \(computerHand)(컴) : " , terminator: "")
         printResultMessage(gameResult: gameResult)
         
+        // 비겼으면 게임 종료, 아니면 다시 처음부터
         if gameResult != GameResult.draw {
             break
         }
