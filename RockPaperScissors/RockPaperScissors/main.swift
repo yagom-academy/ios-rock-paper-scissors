c282f379871371677897ef28102e5d39f3b08c32
innieminnie <rkddlsl1001@gmail.com>
2020-11-05 12:34:47 +0900

    [#5]feat: 묵찌빠게임 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -41,7 +41,7 @@ func playRoundOne(){
                 partnerCard.isMyTurn = true
                 print("졌습니다!")
             }
-            //playRoundTwo()
+            playRoundTwo()
             return
         }else{
             print("비겼습니다!")
@@ -49,6 +49,51 @@ func playRoundOne(){
     }
 }
 
+func playRoundTwo(){
+    print("묵찌빠 게임 시작")
+  
+    while(true){
+        if myCard.isMyTurn{
+            print("[사용자턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :",terminator:"")
+        }else{
+            print("[컴퓨터턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :",terminator:"")
+        }
+        
+        let myCardIdx = Int(readLine()!) ?? -1
+        
+        guard myCardIdx != 0 else {
+            print("게임 종료")
+            return
+        }
+        
+        if myCardIdx == -1 {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            continue
+        }
+        
+        myCard.cardIdx = myCardIdx
+        partnerCard.cardIdx = Int.random(in: 1...3)
+        
+        print(myCard.cardIdx, partnerCard.cardIdx)
+        
+        if myCard.cardIdx != partnerCard.cardIdx{
+            if myCard.playGame(partnerCard.cardIdx,myCard.cardIdx){
+                myCard.isMyTurn = true
+                partnerCard.isMyTurn = false
+            }else{
+                myCard.isMyTurn = false
+                partnerCard.isMyTurn = true
+            }
+        }else{
+            if myCard.isMyTurn{
+                print("사용자의 승리!")
+            }else{
+                print("컴퓨터의 승리!")
+            }
+            return
+        }
+    }
+}
 //
 playRoundOne()
 
