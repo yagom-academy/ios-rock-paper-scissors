f1407d40d08154e49e1c28dc31b46c4091be1568
llim <llim@student.42seoul.kr>
2020-11-06 10:41:00 +0900

    새 방식을 사용하여 가위바위보 게임 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,53 +6,80 @@
 
 import Foundation
 
-enum PlayState {
-    case win
-    case lose
+// 플레이어 승리 상태
+enum GameStateIs: String {
+    case win = "이겼습니다!"
+    case lose = "졌습니다!"
+    case draw = "비겼습니다!"
 }
 
-enum Hand: String {
-    case sisssors = "1"
-    case rock = "2"
-    case paper = "3"
+// 가위, 바위, 보
+enum Hand: Int {
+    case scissors = 1
+    case rock = 2
+    case paper = 3
+    case empty
 }
 
-// return 값: 에러 -1, 사용자 승리 1, 컴퓨터 승리 2 -> 묵찌빠에서 사용하기 위해
-func rockScissorPaperGame() -> Int {
+// 플레이어 상태에 대한 전역변수
+var playerIs: GameStateIs = .draw
+
+// 입력 에러 출력 및 게임 재시작
+func printInputErrorAndResartGame() {
+    print("잘못된 입력입니다. 다시 시도해주세요.")
+    PlayrockPaperSsissors()
+}
+
+// 누가 이겼는지 확인
+func checkWhoIsWinner(_ playerHand: Hand, vs computerHand: Hand) {
+    playerIs = .lose
+    if playerHand == computerHand { playerIs = .draw }
+    switch playerHand {
+    case .scissors:
+        if computerHand == .paper { playerIs = .win }
+    case .rock:
+        if computerHand == .scissors { playerIs = .win }
+    case .paper:
+        if computerHand == .rock { playerIs = .win }
+    default:
+        printInputErrorAndResartGame()
+    }
+}
+
+// 컴퓨터의 패를 결정하고, 사용자의 패를 입력받은 후 비교하고 출력
+func PlayrockPaperSsissors() {
     print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
     
-    var condition: Int = 3 // rspWinnerChecker에서 받아오는 값
-    guard let input = readLine() else { return -1 }
+    var player: Hand = .empty
+    guard let computer: Hand = Hand(rawValue: Int.random(in: 1...3)) else { return }
+    guard let input = readLine() else {
+        printInputErrorAndResartGame()
+        return
+    }
     
-    switch input {
-    case "0":
+    if input == "0" {
         print("게임 종료")
-        return -1
-    case "1":
-        condition = rspWinnerChecker(1)
-    case "2":
-        condition = rspWinnerChecker(2)
-    case "3":
-        condition = rspWinnerChecker(3)
-    default:
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        rockScissorPaperGame()
+        return
+    }
+    if let inputNumber = Int(input) {
+        if inputNumber >= 1 && inputNumber <= 3 {
+            player = Hand(rawValue: inputNumber)!
+        }
+    } else {
+        printInputErrorAndResartGame()
+        return
     }
     
-    switch condition {
-    case 0:
-        print("비겼습니다!")
-    case 1:
-        print("이겼습니다!")
-    case 2:
-        print("졌습니다!")
-    default:
-        rockScissorPaperGame()
-    }
-    if condition == 0 { condition = rockScissorPaperGame() }
-    return condition
+    checkWhoIsWinner(player, vs: computer)
+    print(computer)
+    print(playerIs.rawValue)
+    // 비긴 경우 새로 시작
+    if playerIs == .draw { PlayrockPaperSsissors() }
 }
 
+PlayrockPaperSsissors()
+
+/*
 func mukchiba() {
     // winner에 승리자가 player이면 1, computer이면 2
     var winner: Int = rockScissorPaperGame()
@@ -94,3 +121,4 @@ func mukchiba() {
 }
 
 mukchiba()
+*/
