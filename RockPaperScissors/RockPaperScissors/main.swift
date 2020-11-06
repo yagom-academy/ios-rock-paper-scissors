5e9cc1efeba8f078cd49c6ef41cc88dd7e2fffb1
llim <llim@student.42seoul.kr>
2020-11-06 19:26:31 +0900

    함수 이름 수정, 전역변수 제거 등

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -3,13 +3,23 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
+
+// 전역변수
+//1. 유지보수가 어렵다
+//2. 여기저기서 접근할 수 있어서 보안에 어려움이 있음
+//3. 의도치 않게 값이 변경될 수도있고...
+//4. 메모리
+
+// 전역변수, 함수에 and 사용, checkWhoIsWinner 이름 변경, 가위바위보 반환값 없애기
+
 import Foundation
 
 // 플레이어 승리 상태
-enum PlayerIs: String {
+enum GameState: String {
     case win = "이겼습니다!"
     case lose = "졌습니다!"
     case draw = "비겼습니다!"
+    case exit
 }
 
 // 가위, 바위, 보
@@ -21,113 +31,107 @@ enum Hand: Int {
 }
 
 // 누가 승리했는지
-enum WinnerIs: String {
+enum WinnerState: String {
     case player = "사용자"
     case computer = "컴퓨터"
     case empty
 }
 
-// 플레이어 상태에 대한 전역변수
-var playerIs: PlayerIs = .draw
-
-// 입력 에러 출력 및 가위바위보 게임 재시작
-func printInputErrorAndResartRPSGame() {
-    print("잘못된 입력입니다. 다시 시도해주세요.")
-    playRockPaperScissorsGame()
+// input에러 출력
+func printInputError() {
+    print("잘못된 입력입니다. 다시 입력해주세요.")
 }
 
 // 누가 이겼는지 확인
-func checkWhoIsWinner(_ playerHand: Hand, vs computerHand: Hand) {
-    playerIs = .lose
-    if playerHand == computerHand { playerIs = .draw }
+func determineGameState(playerHand: Hand, computerHand: Hand) -> GameState {
+    var gameState:GameState = .lose
+    if playerHand == computerHand { gameState = .draw }
     switch playerHand {
     case .scissors:
-        if computerHand == .paper { playerIs = .win }
+        if computerHand == .paper { gameState = .win }
     case .rock:
-        if computerHand == .scissors { playerIs = .win }
+        if computerHand == .scissors { gameState = .win }
     case .paper:
-        if computerHand == .rock { playerIs = .win }
+        if computerHand == .rock { gameState = .win }
     default:
-        printInputErrorAndResartRPSGame()
+        printInputError()
+        playRockPaperScissorsGame()
     }
+    return gameState
 }
 
 // 컴퓨터의 패를 결정하고, 사용자의 패를 입력받은 후 비교하고 출력
-func playRockPaperScissorsGame() -> Bool {
+func playRockPaperScissorsGame() {
     print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
-    
+
+    var gameState: GameState = .draw
     var player: Hand = .empty
-    guard let computer: Hand = Hand(rawValue: Int.random(in: 1...3)) else { 
+    guard let computer: Hand = Hand(rawValue: Int.random(in: 1...3)) else {
+        printInputError()
         playRockPaperScissorsGame()
-        return true // 1. 여기를 false로 해야하는 건지.. true로 해야하는 건지 모르겠어요
+        return
     }
     guard let input = readLine() else {
-        printInputErrorAndResartRPSGame()
-        return true // 2. 여기랑
+        printInputError()
+        playRockPaperScissorsGame()
+        return
     }
     
     if input == "0" {
         print("게임 종료")
-        return false
+        return
     }
     if let inputNumber = Int(input) {
-        if inputNumber >= 1 && inputNumber <= 3 {
-            player = Hand(rawValue: inputNumber)!
-        }
+        if inputNumber >= 1 && inputNumber <= 3 { player = Hand(rawValue: inputNumber)! }
     } else {
-        printInputErrorAndResartRPSGame()
-        return true // 3. 여기도요! 다시 palyRockPaperScissorGame을 부르면, 순환? 함수같은 느낌인데, 
-        // 마지막 true값이 다음 함수에 전달되는건지, 중간에 true가 전달되는건지 모르겠어요. 하지만 마지막 값이 전달된다고 생각하고 짰습니다!
+        printInputError()
+        playRockPaperScissorsGame()
+        return
     }
-    
-    print("player: \(player) vs computer: \(computer)") // 값 확인용 출력, 삭제 예정
-    checkWhoIsWinner(player, vs: computer)
-    print(playerIs.rawValue)
+
+    gameState = determineGameState(playerHand: player, computerHand: computer)
+    print(gameState.rawValue)
     // 비긴 경우 새로 시작
-    if playerIs == .draw { playRockPaperScissorsGame() }
-    return true
+    if gameState == .draw { playRockPaperScissorsGame() }
+    else { playMukchibaGame(rpsState: gameState) }
+    return
 }
 
-func palyMukchibaGame() {
-    if !playRockPaperScissorsGame() { return }
-    var winnerIs: WinnerIs = .empty
-    if playerIs == .win { winnerIs = .player }
-    else if playerIs == .lose { winnerIs = .computer }
+func playMukchibaGame(rpsState: GameState) {
+    var gameState: GameState = rpsState
+    var winnerState: WinnerState = .empty
+    if gameState == .win { winnerState = .player }
+    else if gameState == .lose { winnerState = .computer }
     
     while true {
-        print("[\(winnerIs.rawValue) 턴] 묵(1), 찌(2), 빠(3)! <종료: 0> : ", terminator: "")
-        var player: Hand = .empty
+        print("[\(winnerState.rawValue) 턴] 묵(1), 찌(2), 빠(3)! <종료: 0> : ", terminator: "")
+        var player: Hand
         guard let computer: Hand = Hand(rawValue: Int.random(in: 1...3)) else { return }
         guard let input = readLine() else {
-            print("잘못된 입력입니다. 다시 입력해주세요.")
+            printInputError()
             continue
         }
         
-        switch input {
-        case "0":
+        if input == "0" {
             print("게임 종료")
             return
-        case "1":
-            player = .rock
-        case "2":
-            player = .scissors
-        case "3":
-            player = .paper
-        default:
-            print("잘못된 입력입니다. 다시 입력해주세요.")
+        } else if input == "1" { player = .rock }
+        else if input == "2" { player = .scissors }
+        else if input == "3" { player = .paper }
+        else {
+            printInputError()
             continue
         }
         
-        print("player: \(player) vs computer: \(computer)") // 값 확인용 출력, 삭제 예정
-        checkWhoIsWinner(player, vs: computer)
-        if playerIs == .draw {
-            print("\(winnerIs.rawValue)의 승리!")
+        gameState = determineGameState(playerHand: player, computerHand: computer)
+        if gameState == .draw {
+            print("\(winnerState.rawValue)의 승리!")
             break
         }
-        if playerIs == .win { winnerIs = .player }
-        else if playerIs == .lose { winnerIs = .computer }
-        print("\(winnerIs.rawValue)턴 입니다.")
+        if gameState == .win { winnerState = .player }
+        else if gameState == .lose { winnerState = .computer }
+        print("\(winnerState.rawValue)턴 입니다.")
     }
 }
 
-palyMukchibaGame()
+playRockPaperScissorsGame()
