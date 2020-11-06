76aa9f807a675b6f9b9161d324a344aac2123955
llim <llim@student.42seoul.kr>
2020-11-06 11:20:09 +0900

    새 방식을 사용하여 묵찌빠 게임 재구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,13 +1,13 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
 // 플레이어 승리 상태
-enum GameStateIs: String {
+enum PlayerIs: String {
     case win = "이겼습니다!"
     case lose = "졌습니다!"
     case draw = "비겼습니다!"
@@ -21,13 +21,20 @@ enum Hand: Int {
     case empty
 }
 
-// 플레이어 상태에 대한 전역변수
-var playerIs: GameStateIs = .draw
+// 누가 승리했는지
+enum WinnerIs: String {
+    case player = "사용자"
+    case computer = "컴퓨터"
+    case empty
+}
 
-// 입력 에러 출력 및 게임 재시작
-func printInputErrorAndResartGame() {
+// 플레이어 상태에 대한 전역변수
+var playerIs: PlayerIs = .draw
+
+// 입력 에러 출력 및 가위바위보 게임 재시작
+func printInputErrorAndResartRPSGame() {
     print("잘못된 입력입니다. 다시 시도해주세요.")
-    PlayrockPaperSsissors()
+    playRockPaperSsissorsGame()
 }
 
 // 누가 이겼는지 확인
@@ -42,83 +49,82 @@ func checkWhoIsWinner(_ playerHand: Hand, vs computerHand: Hand) {
     case .paper:
         if computerHand == .rock { playerIs = .win }
     default:
-        printInputErrorAndResartGame()
+        printInputErrorAndResartRPSGame()
     }
 }
 
 // 컴퓨터의 패를 결정하고, 사용자의 패를 입력받은 후 비교하고 출력
-func PlayrockPaperSsissors() {
+func playRockPaperSsissorsGame() -> Bool {
     print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
     
     var player: Hand = .empty
-    guard let computer: Hand = Hand(rawValue: Int.random(in: 1...3)) else { return }
+    guard let computer: Hand = Hand(rawValue: Int.random(in: 1...3)) else { return true }
     guard let input = readLine() else {
-        printInputErrorAndResartGame()
-        return
+        printInputErrorAndResartRPSGame()
+        return true
     }
     
     if input == "0" {
         print("게임 종료")
-        return
+        return false
     }
     if let inputNumber = Int(input) {
         if inputNumber >= 1 && inputNumber <= 3 {
             player = Hand(rawValue: inputNumber)!
         }
     } else {
-        printInputErrorAndResartGame()
-        return
+        printInputErrorAndResartRPSGame()
+        return true
     }
     
+    print("player: \(player) vs computer: \(computer)")
     checkWhoIsWinner(player, vs: computer)
-    print(computer)
     print(playerIs.rawValue)
     // 비긴 경우 새로 시작
-    if playerIs == .draw { PlayrockPaperSsissors() }
+    if playerIs == .draw { playRockPaperSsissorsGame() }
+    return true
 }
 
-PlayrockPaperSsissors()
-
-/*
-func mukchiba() {
-    // winner에 승리자가 player이면 1, computer이면 2
-    var winner: Int = rockScissorPaperGame()
-    var condition: Int = 3 // rspWinnerChecker에서 받아오는 값
+func palyMukchibaGame() {
+    if !playRockPaperSsissorsGame() { return }
+    var winnerIs: WinnerIs = .empty
+    if playerIs == .win { winnerIs = .player }
+    else if playerIs == .lose { winnerIs = .computer }
     
     while true {
-        if winner == 1 { print("[사용자 턴] ", terminator: "") }
-        else { print("[컴퓨터 턴] ", terminator: "") }
-        print("묵(1), 찌(2), 빠(3)! <종료: 0> : ", terminator: "")
-        
-        guard let input = readLine() else { return }
+        print("[\(winnerIs.rawValue) 턴] 묵(1), 찌(2), 빠(3)! <종료: 0> : ", terminator: "")
+        var player: Hand = .empty
+        guard let computer: Hand = Hand(rawValue: Int.random(in: 1...3)) else { return }
+        guard let input = readLine() else {
+            print("잘못된 입력입니다. 다시 입력해주세요.")
+            continue
+        }
         
         switch input {
         case "0":
             print("게임 종료")
-            break
+            return
         case "1":
-            condition = mcbWinnerChecker(1)
+            player = .rock
         case "2":
-            condition = mcbWinnerChecker(2)
+            player = .scissors
         case "3":
-            condition = mcbWinnerChecker(3)
+            player = .paper
         default:
-            print("잘못된 입력입니다. 다시 시도해주세요.")
+            print("잘못된 입력입니다. 다시 입력해주세요.")
+            continue
         }
         
-        switch condition {
-        case 0:
-            if winner == 1 { print("사용자의 ", terminator: "") }
-            else { print("컴퓨터의 ", terminator: "") }
-            print("승리!")
-            return
-        case 1:
-            winner = 1
-        default:
-            winner = 2
+        print("player: \(player) vs computer: \(computer)")
+        checkWhoIsWinner(player, vs: computer)
+        if playerIs == .draw {
+            print("\(winnerIs.rawValue)의 승리!")
+            break
         }
+        print("\(winnerIs.rawValue)턴 입니다.")
+        if playerIs == .win { winnerIs = .player }
+        else if playerIs == .lose { winnerIs = .computer }
     }
 }
 
-mukchiba()
-*/
+palyMukchibaGame()
