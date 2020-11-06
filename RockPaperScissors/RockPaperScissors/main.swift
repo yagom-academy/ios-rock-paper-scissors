4b20aeeeabd846a32cb741648cd8fe27320925fd
wonhee <wonhee010@gmail.com>
2020-11-06 13:52:54 +0900

    edit code after review
    enum을 통해 type 분류, return string func를 통해 메시지 조립

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,210 +6,229 @@
 
 import Foundation
 
-// step 1의 고정 출력문
-let order = "가위(1), 바위(2), 보(3)! <종료: 0> : "
-// step 2의 고정 출력문
-let orderRPS = "묵(1), 찌(2), 빠(3)! <종료: 0> : "
+// 고정 출력문
+let order: [String : String] = [
+    "SRP" : "가위(1), 바위(2), 보(3)! <종료: 0> : ",
+    "RSP" : "묵(1), 찌(2), 빠(3)! <종료: 0> : "
+]
 
-let userString = "[사용자 턴] "
-let computerString = "[컴퓨터 턴] "
+enum Gamer : String {
+    case user = "사용자"
+    case computer = "컴퓨터"
+    // 비겼음
+    case draw
+}
 
-let userWin = "사용자의 승리!"
-let computerWin = "컴퓨터의 승리!"
+// 손 상태
+enum Hand {
+    case rock, scissor, paper, empty
+}
 
-let errorInput = "잘못된 입력입니다. 다시 시도해주세요."
+// error 메시지
+enum Msg : String {
+    case errorPrint = "시스템 에러로 인해 다시 시도해주세요."
+    case errorInput = "잘못된 입력입니다. 다시 시도해주세요."
+    case exit = "게임을 종료합니다."
+}
 
-/*
- * step 1: 1(가위), 2(바위), 3(보)
- * step 2: 1(바위), 2(가위), 3(보)
- */
-// 컴퓨터의 손 상태
-var computerHand: Int
-// 사용자의 손 상태
-var userHand: Int
+var computerHand: Hand = .empty
+var userHand: Hand = .empty
+// 게임 턴
+var gameTurn: Gamer = .draw
 
-// 묵찌빠 턴 (1: 사용자, -1: 컴퓨터, 0: 불가)
-var gameTurn = 0
+func turnMessage(_ turn: Gamer) -> String {
+    switch turn {
+    case .computer:
+        return "[컴퓨터 턴] "
+    case .user:
+        return "[사용자 턴] "
+    default:
+        return ""
+    }
+}
 
-/*
- * 가위바위보 (step1) 게임 함수
- * return 0 -> 비김
- * return 1 -> 사용자 이김
- * return -1 -> 컴퓨터 이김
- */
-func gameScissorRockPaper(user: Int, computer: Int) -> Int {
+func winMessage(_ turn: Gamer) -> String {
+    switch turn {
+    case .computer:
+        return "컴퓨터의 승리!"
+    case .user:
+        return "사용자의 승리!"
+    default:
+        return ""
+    }
+}
+
+func nextTurnMessage(_ nextTurn: Gamer) -> String {
+    switch nextTurn {
+    case .computer:
+        return "컴퓨터의 턴입니다"
+    case .user:
+        return "사용자의 턴입니다"
+    default:
+        return ""
+    }
+}
+
+// 게임에서 이긴 Gamer return
+func game(user: Hand, computer: Hand) -> Gamer {
     // 비겼음
     if user == computer {
-        print("비겼습니다!")
-        return 0
-    }
-    
-    var result: Int
-    switch user {
-    case 1:
-        if computer == 2 {
-            result =  -1
-        }
-        else {
-            result = 1
-        }
-    case 2:
-        if computer == 3 {
-            result =  -1
-        }
-        else {
-            result = 1
-        }
-    case 3:
-        if computer == 1 {
-            result =  -1
-        }
-        else {
-            result = 1
-        }
-    default:
-        return 0
-    }
-    
-    if result == 1 {
-        print("이겼습니다!")
-    }
-    else {
-        print("졌습니다!")
-    }
-    
-    return result
-}
-
-func convertHand(_ origin: Int) -> Int {
-    switch origin {
-    case 1:
-        return 2
-    case 2:
-        return 1
-    case 3:
-        return 3
-    default:
-        return 0
-    }
-}
-
-/*
- * 묵찌빠 (step2) 게임 함수
- * return 0 -> 패가 같음
- * return 1 -> 사용자 턴
- * return -1 -> 컴퓨터 턴
- */
-func gameRockPaperScissor(user: Int, computer: Int) -> Int {
-    // 패가 같음 -> 현재 턴인 사람이 승리
-    if user == computer {
-        return 0
+        return Gamer.draw
     }
     
     switch user {
-    case 1:
-        if computer == 3 {
-            return -1
+    case .rock:
+        if computer == .paper {
+            return Gamer.computer
         }
-        return 1
-    case 2:
-        if computer == 1 {
-            return -1
+        return Gamer.user
+    case .scissor:
+        if computer == .rock {
+            return Gamer.computer
         }
-        return 1
-    case 3:
-        if computer == 2 {
-            return -1
+        return Gamer.user
+    case .paper:
+        if computer == .scissor {
+            return Gamer.computer
         }
-        return 1
-    default:
-        return 0
+        return Gamer.user
+    case .empty:
+        return Gamer.draw
     }
 }
 
-/*
- * step 1
- */
+func SRPNumberToHand(_ number: Int) -> Hand {
+    switch number {
+    case 1:
+        return Hand.scissor
+    case 2:
+        return Hand.rock
+    case 3:
+        return Hand.paper
+    default:
+        return Hand.empty
+    }
+}
+
+func RSPNumberToHand(_ number: Int) -> Hand {
+    switch number {
+    case 1:
+        return Hand.rock
+    case 2:
+        return Hand.paper
+    case 3:
+        return Hand.scissor
+    default:
+        return Hand.empty
+    }
+}
+
 // 게임 종료
 var exit = false
 
-while !exit {
-    print(order)
-    
-    guard let input = readLine() else {
-        exit = true
-        break
-    }
-    
-    if let number = Int(input),
-       number >= 0 && number <= 3 {
-        // 게임 종료
-        if number == 0 {
-            exit = true
-            break
+// 가위바위보 게임
+func gameSRP() {
+    while !exit {
+        guard let order = order["SRP"] else {
+            print(Msg.errorPrint.rawValue)
+            continue
         }
+        print(order)
         
-        userHand = number
-        computerHand = Int.random(in: 1...3)
-        
-        // 가위바위보 결과
-        gameTurn = gameScissorRockPaper(user: userHand, computer: computerHand)
-        // 비긴 경우는 다시 게임
-        if gameTurn == 0 {
+        // 잘못된 입력이라면 메세지 출력 후 다시 입력 받기
+        guard let input = readLine() else {
+            print(Msg.errorInput.rawValue)
             continue
         }
         
-        // 이기거나 진 경우는 묵찌빠로 넘어감 -> step1, step2에서 숫자가 의미하는 손 상태가 다르므로 변환 필요
-        userHand = convertHand(userHand)
-        computerHand = convertHand(computerHand)
-        
-        break
+        if let number = Int(input),
+           number >= 0 && number <= 3 {
+            // 게임 종료
+            if number == 0 {
+                print(Msg.exit.rawValue)
+                exit = true
+                return
+            }
+            
+            userHand = SRPNumberToHand(number)
+            computerHand = SRPNumberToHand(Int.random(in: 1...3))
+            
+            gameTurn = game(user: userHand, computer: computerHand)
+            // 비긴 경우는 다시 게임
+            if gameTurn == .draw {
+                print("비겼습니다!")
+                continue
+            }
+            
+            // 이기거나 진 경우는 묵찌빠로 가야하므로 탈출
+            if gameTurn == .user {
+                print("이겼습니다!")
+            }
+            else {
+                print("졌습니다!")
+            }
+            return
+        }
+        else {
+            print(Msg.errorInput.rawValue)
+        }
     }
-    // 잘못된 입력
-    else {
-        print(errorInput)
+    
+}
+
+// 묵찌빠 게임
+func gameRSP() {
+    while !exit {
+        guard let order = order["RSP"] else {
+            print(Msg.errorPrint.rawValue)
+            continue
+        }
+        
+        print(turnMessage(gameTurn) + order)
+        
+        // 잘못된 입력이라면 메세지 출력 후 다시 입력 받기
+        guard let input = readLine() else {
+            print(Msg.errorInput.rawValue)
+            continue
+        }
+        
+        if let number = Int(input),
+           number >= 0 && number <= 3 {
+            if number == 0 {
+                print(Msg.exit.rawValue)
+                exit = true
+                return
+            }
+            
+            userHand = RSPNumberToHand(number)
+            computerHand = RSPNumberToHand(Int.random(in: 1...3))
+            
+            let nextTurn = game(user: userHand, computer: computerHand)
+            // 패가 동일하다면 현재 턴인 사람이 승리
+            if nextTurn == .draw {
+                print(winMessage(gameTurn))
+                exit = true
+                return
+            }
+            
+            gameTurn = nextTurn
+            print(nextTurnMessage(gameTurn))
+        }
+        else {
+            print(Msg.errorInput.rawValue)
+        }
     }
 }
 
 
-/*
- * step 2
- */
-while !exit {
-    print((gameTurn == 1 ? userString : computerString) + orderRPS)
+func gameStart() {
+    gameSRP()
     
-    guard let input = readLine() else {
-        exit = true
-        break
+    if exit {
+        return
     }
     
-    if let number = Int(input),
-       number >= 0 && number <= 3 {
-        // 게임 종료
-        if number == 0 {
-            exit = true
-            break
-        }
-        
-        userHand = number
-        computerHand = Int.random(in: 1...3)
-        
-        // 묵찌빠 결과
-        let result = gameRockPaperScissor(user: userHand, computer: computerHand)
-        // 패가 동일하다면 턴이 이김
-        if result == 0 {
-            // 사용자 턴이라면 사용자가 승리
-            gameTurn > 0 ? print(userWin) : print(computerWin)
-            exit = true
-            break
-        }
-        
-        gameTurn = result
-    }
-    // 잘못된 입력
-    else {
-        // 컴퓨터한테 턴이 넘어감
-        gameTurn = -1
-        print(errorInput)
-    }
+    gameRSP()
 }
+
+gameStart()
