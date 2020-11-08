b3d2d68df37bfdde0e6cd2139f1201b642ae91d4
odong-Tree <ss12357@naver.com>
2020-11-08 18:07:24 +0900

    [new project #1] 유저와 컴퓨터의 픽

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,102 +1,39 @@
+var userPick: String = ""
+var computerPick: String = ""
+var matchResult: result = .draw
+// 초기화는 뭘로 해둬야 적절하지?
 
-func matchResult(_ user: Int, _ computer: Int) -> String {
-    switch user {
-    case 0:
-        return "exit"
-    case computer:
-        return "draw"
-    case computer + 1, computer - 2:
-        return "userWin"
-    default:
-        return "computerWin"
-    }
-}
-
-func rockPaperScissors() {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+func userInput() {
     guard let input = readLine() else {
         print("잘못된 입력값입니다. 다시 시도해주세요.")
-        rockPaperScissors()
         return
     }
-
-    var userPick = 0
-    let computerPick = Int.random(in: 1...3)
     
     switch input {
-    case "0", "1", "2", "3" :
-        userPick = Int(input)!
+    case "0":
+        userPick = "종료"
+    case "1":
+        userPick = "묵"
+    case "2":
+        userPick = "찌"
+    case "3":
+        userPick = "빠"
     default :
         print("잘못된 입력값입니다. 다시 시도해주세요.")
-        rockPaperScissors()
-    }
-
-    switch matchResult(userPick, computerPick) {
-    case "exit":
-        break
-    case "draw":
-        print("비겼습니다!")
-        rockPaperScissors()
-    case "userWin":
-        print("이겼습니다!")
-        mukzzipa(userPick, computerPick)
-    default:
-        print("졌습니다!")
-        mukzzipa(userPick, computerPick)
     }
 }
 
-func mukzzipa(_ userPicked: Int, _ computerPicked: Int) {
-    switch matchResult(userPicked, computerPicked) {
-    case "userWin":
-        print("[사용자 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
-    default:
-        print("[컴퓨터 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
-    // 가위바위보 결과, 이기고 진 경우에만 들어갈 함수이기 때문에 2가지 case만 필요
-    }
-    
-    guard let mukzzipaInput = readLine() else {
-        print("잘못된 입력값입니다. 다시 시도해주세요.")
-        mukzzipa(computerPicked, userPicked)
-        // 승패가 바뀌는 것과 같으니 입력값을 서로 바꿔준다.
+func computerChoice() {
+    let ableToPick = ["묵", "찌", "빠"]
+    guard computerPick == ableToPick.randomElement() else {
+        print("컴퓨터가 무엇을 낼지 고르지 못하고 있습니다..")
         return
     }
-
-    var mukzzipaUserPick = 0
-    let mukzzipaComputerPick = Int.random(in: 1...3)
-    
-    switch mukzzipaInput {
-    case "0", "1", "2", "3" :
-        mukzzipaUserPick = Int(mukzzipaInput)!
-    default :
-        print("잘못된 입력값입니다. 다시 시도해주세요.")
-        if matchResult(userPicked, computerPicked) == "userWin" {
-            mukzzipa(computerPicked, userPicked)
-            // 턴이 바뀌어야하니 입력값을 서로 바꿔준다.
-        } else {
-            mukzzipa(userPicked, computerPicked)
-            // 컴퓨터 턴일 때 잘못입력할 경우, 턴이 유지 되어야 한다.
-        }
-    }
-    
-    switch matchResult(mukzzipaUserPick, mukzzipaComputerPick) {
-    case "exit":
-        break
-    case "draw":
-        if matchResult(userPicked, computerPicked) == "userWin" {
-            print("사용자의 승리!")
-        } else {
-            print("컴퓨터의 승리!")
-        }
-    case "userWin":
-        print("사용자의 턴입니다.")
-        mukzzipa(mukzzipaUserPick, mukzzipaComputerPick)
-    default:
-        print("컴퓨터의 턴입니다.")
-        mukzzipa(mukzzipaUserPick, mukzzipaComputerPick)
-    }
 }
 
-rockPaperScissors()
- 
-// pr test
+enum result {
+    case userWin
+    case computerWin
+    case draw
+    case exit
+}
