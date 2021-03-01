39173c4951ab4fd0836125e73e45b414d4550b8c
fezravien <yjaewoong@naver.com>
2021-03-01 20:10:23 +0900

    [STEP 1] 요구사항 만족하는 코드 완성
    RockScissorsPaper 클레스 인스턴스를 생성하여 presentGameMemu 초기 출력 메소드를 기준으로 입력값 유효성(isValidInput, checkedInput)을 확보했습니다.
    그리고 startGame 메소드를 통해 게임의 규칙에 맞게 judgeRockScissorsPaper 메소드로 동작하도록 했습니다.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,9 +2,94 @@
 //  RockPaperScissors - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
-
-import Foundation
-
-print("Hello, World!")
-
+//
+class RockScissorsPaperGame {
+    func presentGameMemu() {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+        startGame()
+    }
+    func startGame() {
+        let userInput = isValidInput()
+        let computerNumber = createComputerRandomNumber()
+        judgeRockScissorsPaper(userNumber: userInput, computerNumber: computerNumber)
+    }
+}
+extension RockScissorsPaperGame {
+    func createComputerRandomNumber() -> Int {
+        let computerRandomNumber = Int.random(in: 1...3)
+        return computerRandomNumber
+    }
+    func isValidInput() -> Int {
+        var inputNumbers: [Int] = []
+        if let optionalInputNumber = readLine() {
+            inputNumbers = optionalInputNumber.split(separator: " ").map { Int(String($0)) ?? -1 }
+            if checkedInput(inputArrays: inputNumbers) == false {
+                printError()
+                presentGameMemu()
+            }
+        } else {
+            printError()
+        }
+        return inputNumbers.first!
+    }
+    func checkedInput(inputArrays validArrays: [Int]) -> Bool {
+        let validedNumbers = [0, 1, 2, 3]
+        if validArrays.count != 1 {
+            return false
+        } else if validArrays.contains(-1) {
+            return false
+        } else if validedNumbers.contains(validArrays.first!) == false {
+            return false
+        } else {
+            return true
+        }
+    }
+    func judgeRockScissorsPaper(userNumber userStatus: Int, computerNumber computerStatus: Int) {
+        switch userStatus {
+        case 0:
+            print("게임 종료")
+        case 1:
+            switch computerStatus {
+            case 1:
+                print("비겼습니다!")
+                presentGameMemu()
+            case 2:
+                print("졌습니다!")
+            case 3:
+                print("이겼습니다!")
+            default:
+                return
+            }
+        case 2:
+            switch computerStatus {
+            case 1:
+                print("이겼습니다!")
+            case 2:
+                print("비겼습니다!")
+                presentGameMemu()
+            case 3:
+                print("졌습니다!")
+            default:
+                return
+            }
+        case 3:
+            switch computerStatus {
+            case 1:
+                print("졌습니다!")
+            case 2:
+                print("이겼습니다!")
+            case 3:
+                print("비겼습니다!")
+                presentGameMemu()
+            default:
+                return
+            }
+        default:
+            return
+        }
+    }
+    func printError() {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
+RockScissorsPaperGame().presentGameMemu()
