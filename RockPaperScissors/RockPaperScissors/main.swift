8db35d06ce83db5d6e7470099368c1c02536958e
Wody95 <kwu0626@gmail.com>
2021-03-01 18:38:15 +0900

    컴퓨터가 랜덤으로 가위(“1”) 바위(“2”) 보(“3”)를 내는 함수 사용자 입력을 받는 함수 1, 2 함수를 통해 게임 결과를 계산하는 함수 게임을 진행하는 함수 기능을 병합했습니다.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,20 +7,12 @@
 import Foundation
 
 class RockPaperScissors {
-    // 가위바위보 게임의 상태값(프로퍼티)
-    // 컴퓨터가 랜덤으로 생성한 가위바위보 번호를 저장하는 프로퍼티 (String 타입)
-    // 사용자가 입력한 가위바위보 번호를 저장하는 프로퍼티 (String 타입)
-    // 게임의 진행 상태를 저장하는 프로퍼티
     var comNumberValue = 0
     var userNumberValue = 0
     
-    // 가위바위보 게임의 기능(메서드)
-    // 1. 컴퓨터가 랜덤으로 1, 2, 3을 반환하는 기능
-    // 2. 사용자의 가위바위보를 입력받는 기능
-    func comNumberGenerator() -> Int {
+    func comNumberGenerator() {
         let numberGeneration = Int.random(in: 1...3)
         comNumberValue = numberGeneration
-        return numberGeneration
     }
     
     func userInputNumber() -> Int {
@@ -51,7 +43,59 @@ class RockPaperScissors {
         }
     }
     
-    // 3. 1,2 메서드를 통해 게임 결과를 반환하는 기능
-    // 4. 게임스타트 기능(잘못된 입력 처리)
+    func checkWinLoseResult(user: Int, computer: Int) -> Int {
+            let whoWinResult: Int = user - computer
+            var returnGameResult: Int = 0
+            
+            if whoWinResult == 0 {
+                returnGameResult = 3
+            } else if whoWinResult == 1 || whoWinResult == -2 {
+                returnGameResult = 1
+            } else if whoWinResult == -1 || whoWinResult == 2 {
+                returnGameResult = 2
+            }
+            
+            return returnGameResult
+        }
+    
+    func playGame() {
+        var gameState: Bool = true
+        var winLoseResult: Int = 0
+        
+        
+        while gameState {
+            print("가위(1). 바위(2). 보(3)! <종료 : 0> : ", terminator: "")
+            
+            comNumberGenerator()
+            
+            userNumberValue = userInputNumber()
+            
+            winLoseResult = checkWinLoseResult(
+                user: userNumberValue,
+                computer: comNumberValue)
+            
+            if winLoseResult == 1 {
+                print("이겼습니다!")
+                gameState = false
+            } else if winLoseResult == 2 {
+                print("졌습니다!")
+                gameState = false
+            } else if winLoseResult == 0 {
+                gameState = false
+            } else {
+                print("비겼습니다!")
+                continue
+            }
+            
+        }
+    }
+
+    
+    
 }
+
+var testGame = RockPaperScissors()
+
+testGame.playGame()
+
 
