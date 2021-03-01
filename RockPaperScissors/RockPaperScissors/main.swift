9f3cf90646892a877706f5081f3a4b3d4556b621
Neph <tony3322@naver.com>
2021-03-01 17:51:16 +0900

    Feat : Make blueprint of RockPaperScissors project

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,53 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
+enum GameError: Error {
+    case invalidInput
+    case unknownError
+}
+enum GameResult {
+    case win
+    case lose
+    case draw
+}
 
-import Foundation
+class RockScissorPaper {
+    var handOfComputer = 0
+    var handOfUser = 0
+    
+    func initialize() {
+        handOfUser = 0
+        handOfComputer = Int.random(in: 1...3)
+    }
+    func startGame() {
+        var userInput = 0
+        outer: while true{
+            initialize()
+            printMenu()
+            do {
+                userInput = try getUserInput()
+                if userInput == 0 {
+                    break outer
+                }
+            } catch {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+            }
+            let winner = compareHands(handOfUser, with: handOfComputer)
+            printResult(winner)
+        }
+    }
 
-print("Hello, World!")
+    func compareHands(_ user: Int, with computer: Int) -> GameResult {
+        return GameResult.win
+    }
+    
+    func printMenu() {
+        
+    }
+    
+    func getUserInput() throws ->Int {
+        return 0
+    }
+    
+    func printResult(_ input: GameResult) {
+        
+    }
+}
 
