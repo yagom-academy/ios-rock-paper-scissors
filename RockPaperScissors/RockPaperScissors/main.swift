fbc1f89c91183f0f687dc45bf97cb39e7814464c
ryan-son <ryan.son1002@gmail.com>
2021-03-02 01:27:30 +0900

    feat: step-1 기능 구현
    게임을 상속하여 재사용할 필요성은 없을 것으로 판단하여 구조체로 작성함.
    가위바위보 게임 이후 묵찌빠 게임에서 getUserChoice() 메서드를 안내문 출력 메서드와
    분리하여 재사용성을 높임.
    가위바위보 구현에 필요한 핵심 메서드를 protocol - extension으로 구현하여 신규
    프로그래머의 코드 가독성과 유지보수성을 높임.
    invalidSelection Error Handling은 구조체의 startGame() 메서드에서 구현.
    신규 에러 발생 시 switch문의 default로 탈출하므로 향후 신규 프로그래머의 유지보수성
    떨어짐. 다음 버전에서는 신규 에러 발생 시 컴파일 에러 유도하도록 fix 예정.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,40 +4,88 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
+// 변수, 함수명을 후속 게임이 있다는 것을 고려하지 않고 명명해서 일반적일 수 있습니다.
+// 가위바위보와 묵찌빠에서 변수, 함수명을 어떻게 정의할지 같이 생각해봐요.
+// 설명이 필요한 구문이 있으면 디스코드로 메시지 보내주세요~
+
 import Foundation
-//
-//구조체 {
-//    enum 이름 {
-//        .가위: 1
-//        .바위: 2
-//        .보: 3
-//    }
-//
-//    승패 출력 메서드 {
-//
-//    }
-//
-//    승패 판단 메서드 {
-//        switch
-//        승패 출력 메서드()
-//    }
-//
-//    무작위 숫자 생성 메서드 {
-//
-//    }
-//
-//    가위바위보 출력 메서드 {
-//
-//    }
-//
-//    사용자 입력 받기 메서드 {
-//            가위바위보 출력 메서드
-//    }
-//
-//    게임시작 메서드 {
-//        randomNumber = 무작위 숫자 생성 메서드
-//
-//        사용자 입력 받기 메서드
-//        입력 받은 값을 원하는 값으로 가공하는 전처리 메서드 (옵셔널 처리)
-//    }
-//}
+
+protocol RockPaperScissorsable {
+    func generateRandomNumber() -> Int
+    func printUserChoiceGuide()
+    func getUserChoice() throws -> Int
+    func decideWinner(userChoice: Int, computerChoice: Int)
+}
+
+extension RockPaperScissors: RockPaperScissorsable {
+    func generateRandomNumber() -> Int {
+        let randomNumber: Int = Int.random(in: 1...3)
+        return randomNumber
+    }
+    
+    func printUserChoiceGuide() {
+        print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
+    }
+    
+    func getUserChoice() throws -> Int {
+        guard let userChoice: String = readLine() else {
+            throw GameError.invalidSelection
+        }
+        guard let intUserChoice: Int = Int(userChoice) else {
+            throw GameError.invalidSelection
+        }
+        return intUserChoice
+    }
+    
+    func decideWinner(userChoice: Int, computerChoice: Int) {
+        let winningCase: Int = computerChoice % 3 + 1
+        if userChoice == computerChoice {
+            print("비겼습니다!")
+        } else if userChoice == winningCase {
+            print("아이고.. 지셨네..")
+        } else {
+            print("이겼습니다!")
+        }
+    }
+}
+
+struct RockPaperScissors {
+    enum GameError: Error {
+        case invalidSelection
+        case unknownError
+    }
+    
+    // 아래 열거형 활용처 같이 고민해봅시다~!
+    enum Choices: Int {
+        case exitGame
+        case scissor
+        case rock
+        case paper
+    }
+    
+    func startGame() {
+        startGameLoop: while true {
+            let computerChoice: Int = generateRandomNumber()
+            do {
+                printUserChoiceGuide()
+                let userChoice: Int = try getUserChoice()
+                if userChoice == Choices.exitGame.rawValue {
+                    break startGameLoop
+                }
+                decideWinner(userChoice: userChoice, computerChoice: computerChoice)
+                // 묵찌빠 게임 이어 작성
+            } catch {
+                switch error {
+                case GameError.invalidSelection:
+                    print("잘못된 입력입니다. 다시 시도해주세요.")
+                default:
+                    print("알 수 없는 에러.")
+                }
+            }
+        }
+    }
+}
+
+let rockPaperScissors = RockPaperScissors()
+rockPaperScissors.startGame()
+
