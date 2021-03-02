db59baa77e47435f55f564f43f543544ed3af338
Wody95 <kwu0626@gmail.com>
2021-03-02 17:43:49 +0900

    for sj

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,170 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
-print("Hello, World!")
+enum RockPaperScissorsCase: Int {
+    case exit = 0
+    case rock = 1
+    case paper = 2
+    case scissors = 3
+}
+
+class RockPaperScissors {
+    var computerCase = 0
+    var userCase = 0
+    
+    func createRandomComputerCase() -> Int {
+        let randomCase = Int.random(in: 1...3)
+        print("개발용 컴퓨터 확인 \(randomCase)")
+        return randomCase
+    }
+    
+    func userInputCase(mode: String) -> Int {
+        while true {
+            guard let userInputText = readLine() else {
+                continue
+            }
+            guard let userInputNumber = Int(userInputText) else {
+                continue
+            }
+            
+            let userCase = RockPaperScissorsCase(rawValue: userInputNumber)
+            
+            
+            switch userCase {
+            case .exit:
+                print("게임 종료")
+                return 0
+            case .rock:
+                return 1
+            case .paper:
+                return 2
+            case .scissors:
+                return 3
+            default:
+                if mode == "가위바위보" {
+                    print("잘못된 입력입니다. 다시 입력해주세요.")
+                } else {
+                    print("잘못된 입력입니다. 다시 시도해주세요.")
+                    return 4
+                }
+                
+            }
+        }
+    }
+    
+    func checkWinLoseResult(user: Int, computer: Int) -> Int {
+        let whoWinCheck: Int = user - computer
+        var gameResult: Int = 0
+        
+        // 가위바위보
+        if user == 0 {
+            gameResult = 0
+        } else if user == 4 {
+            gameResult = 1
+        } else if whoWinCheck == 1 || whoWinCheck == -2 {
+            gameResult = 1
+        } else if whoWinCheck == -1 || whoWinCheck == 2 {
+            gameResult = 2
+        } else if whoWinCheck == 0 {
+            gameResult = 3
+        }
+        
+        return gameResult
+    }
+    
+    func printResultChangeGameState(gameResult: Int) -> Int {
+        switch gameResult {
+        case 0:
+            return 0
+        case 1:
+            print("이겼습니다!")
+            return 1
+        case 2:
+            print("졌습니다!")
+            return 2
+        default:
+            print("비겼습니다!")
+            return 3
+        }
+    }
+    
+    func playGame() {
+        var playGameSwitch: Int = 1
+        var winLoseResult: Int = 0
+        var gameMode: String = "가위바위보"
+        
+        while playGameSwitch != 0 {
+            print("가위(1). 바위(2). 보(3)! <종료 : 0> : ", terminator: "")
+            
+            computerCase = createRandomComputerCase()
+            userCase = userInputCase(mode: gameMode)
+            
+            winLoseResult = checkWinLoseResult(user: userCase, computer: computerCase)
+            
+            playGameSwitch = printResultChangeGameState(gameResult: winLoseResult)
+            
+            while playGameSwitch != 0 && playGameSwitch != 3 {
+                gameMode = "묵찌빠"
+                
+                // 현재 턴 확인
+                let nowPlayTurn = whoNowTurn(gameResult: playGameSwitch)
+                // 묵찌빠 안내문 출력
+                print("[\(nowPlayTurn) 턴] 묵(1). 찌(2). 빠(3)! <종료: 0> : ", terminator: "")
+                
+                // 컴퓨터 케이스 선택
+                computerCase = createRandomComputerCase()
+                // 유저 케이스 선택
+                userCase = userInputCase(mode: gameMode)
+                
+                // 묵찌빠 승패 판정
+                winLoseResult = checkWinLoseResult(user: userCase, computer: computerCase)
+                
+                // 묵찌빠 승패 출력
+                playGameSwitch = printMukjjibbaResult(gameResult: winLoseResult, nowTurn: nowPlayTurn)
+            }
+        }
+    }
+
+    // 묵찌빠 현재 턴 상황 출력 메서드 - Wody
+    // 묵찌빠의 현재 턴 조건 : 가위바위보 승자 및 묵찌빠 승자, 잘못 입력된 경우 컴퓨터에게 턴 이동
+    // 기존 가위바위보 체크 메소드는 user기준, return값 이겼을 때(1), 졌을 때(2), 비겼을 때(0)
+    func whoNowTurn(gameResult: Int)  -> String {
+        var nowTurn:String
+        
+        if gameResult == 1 {
+            nowTurn = "유저"
+        } else {
+            nowTurn = "컴퓨터"
+        }
+        
+        return nowTurn
+    }
+    
+    // 묵찌빠 승패 출력 메소드
+    func printMukjjibbaResult(gameResult: Int, nowTurn: String) -> Int {
+        switch gameResult {
+        case 2:
+            print("사용자 턴입니다.")
+            return 1
+        case 1:
+            print("컴퓨터 턴입니다.")
+            return 2
+        default:
+            print("\(nowTurn)의 승리!")
+            return 0
+        }
+    }
+    
+}
+
+
+var testGame = RockPaperScissors()
+
+testGame.playGame()
+
 
