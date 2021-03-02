fbde4ccdbc3aa573ce1fb3ca3853b9991f5d97a6
summer0817 <step33@naver.com>
2021-03-02 20:18:43 +0900

    [#1]-summer-게임시작함수

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -13,5 +13,10 @@ class RockPaperScissors {
         let newRandomNumber: Int = Int.random(in: 1...3)
         computerValue = newRandomNumber
     }
-
+    func startGame() {
+        makeRandomNumber()
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+        userValue = Int(readLine()!)!
+        compareNumber()
+    } 
 }
