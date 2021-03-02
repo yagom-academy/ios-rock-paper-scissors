9fd5782c429a9d04927ef1ca0b5c397b6ca00ea2
Tak <oit5538@gamil.com>
2021-03-02 10:25:12 +0900

    feat : 배열만들기 & 배열에 따라 조건으로 결과값분류
    [randomNumber, inputNumber] 배열의 값에 따라 결괏값 나누기

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -3,47 +3,63 @@
 //  Created by Coda & Tak
 //  Copyright © yagom academy. All rights reserved.
 //
-
 import Foundation
 
+var inputNumber: Int = 0
 var randomNumber: Int = 0
+var storageOfComputerAndUserNumber: Array<Int> = []
 
 print("가위(1), 바위(2), 보(3)! <종료: 0> : ")
 
 func makeRandomNumber() {
     randomNumber = Int.random(in: 1...3)
 }
-/*
 makeRandomNumber()
-print(randomNumber)
-*/
 
-func inputPlayerNumber() {
+func inputPlayersNumber() {
     let storage: String? = readLine()
     guard let pocket: String = storage else { return }
-    guard let inputNumber = Int(pocket) else { return }
-    print(inputNumber) // 확인차
+    guard let a = Int(pocket) else { return }
+    inputNumber = a
 }
-inputPlayerNumber()
+inputPlayersNumber()
 
-func 숫자에따라다른반응() {
-//    숫자가 0일때 -
-//    숫자가 1일때 -
-//    숫자가 2일때 -
-//    숫자가 3일때 -
-//    숫자가 기타일때 -
+func makeArrayOrReturn() {
+    if inputNumber == 0 {
+        return
+    } else if inputNumber == 1 || inputNumber == 2 || inputNumber == 3 {
+        배열만들기()
+    } else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        // 최초상태로 복귀
+    }
 }
+makeArrayOrReturn()
 
 func 배열만들기() {
+    storageOfComputerAndUserNumber.append(randomNumber)
+    storageOfComputerAndUserNumber.append(inputNumber)
+    print(storageOfComputerAndUserNumber)
     //[컴퓨터가 만든 수, 사용자 입력 수]를 배열로 만드는 함수
-}
+    }
 
 func 배열에따라조건으로결과값나누기() {
+    if storageOfComputerAndUserNumber == [1,2] || storageOfComputerAndUserNumber == [2,3] || storageOfComputerAndUserNumber == [3,1] {
+        print("이겼습니다!")
+        return
+    } else if storageOfComputerAndUserNumber == [2,1] ||
+                storageOfComputerAndUserNumber == [3,2] || storageOfComputerAndUserNumber == [1,3] {
+        print("졌습니다!")
+        return
+    } else if randomNumber == inputNumber {
+        print("비겼습니다!")
+        // 최초상태로 복귀
+    }
 //    승리하는 경우 -> 이겼습니다.
 //    패배하는 경우 -> 졌습니다.
 //    비기는 경우 -> 비겼습니다. -> 재시작
 }
-
+배열에따라조건으로결과값나누기()
 
 
 
