467cb89847ce511525857a551d935b64f6d196e6
inwoodev <dlsdn1207@gmail.com>
2021-03-02 15:57:06 +0900

    Refactor : Increase code brevity

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -11,30 +11,28 @@ enum ResultOfMukChiBa {
     case userTurn
     case computerTurn
     case somebodyWin
+    case exitInput
 }
 class RockPaperScissors {
     var handOfComputer = 0
     var handOfUser = 0
-    func initialize() {
-        handOfUser = 0
+    func renewComputerHand() {
         handOfComputer = Int.random(in: 1...3)
     }
     func startGame() {
-        var userInput = 0
         outer: while true {
-            initialize()
+            renewComputerHand()
             showMenu()
             do {
-                userInput = try getUserInput()
-                if userInput == 0 {
+                handOfUser = try getUserInput()
+                if handOfUser == 0 {
                     break outer
                 }
             } catch {
                 print("잘못된 입력입니다. 다시 시도해주세요.")
                 continue outer
             }
-            handOfUser = userInput
-            let ResultOfRockPaperScissors = compareHandsOfRockPaperScissors(handOfUser, with: handOfComputer)
+            let ResultOfRockPaperScissors = compareHandsOfRockPaperScissors()
             showResult(ResultOfRockPaperScissors)
             if ResultOfRockPaperScissors == .draw{
                 continue outer
@@ -43,8 +41,8 @@ class RockPaperScissors {
             break outer
         }
     }
-    func compareHandsOfRockPaperScissors(_ user: Int, with computer: Int) -> ResultOfRockPaperScissors {
-        switch (user, computer) {
+    func compareHandsOfRockPaperScissors() -> ResultOfRockPaperScissors {
+        switch (handOfUser, handOfComputer) {
         case (1, 1), (2, 2), (3, 3):
             return .draw
         case (1, 3), (2, 1), (3, 2):
@@ -93,42 +91,47 @@ class MukChiBa : RockPaperScissors {
         currentTurn = winner
     }
     
+    override func showMenu() {
+        print("[\(currentTurn) 턴] 묵(1). 찌(2). 빠(3)! <종료 : 0>", terminator: " : ")
+    }
     func mukChiBaStart() {
         outer: while true {
-            print("[\(currentTurn)턴] 묵(1). 찌(2). 빠(3)! <종료 : 0>", terminator: " : ")
-            handOfComputer = Int.random(in: 1...3)
+            showMenu()
+            renewComputerHand()
             do {
                 handOfUser = try getUserInput()
             } catch {
                 currentTurn = "컴퓨터"
                 continue outer
             }
-            let resultOfMukChiBa = compareHandsOfMukChiBa(handOfUser, with: handOfComputer, turn: currentTurn)
+            let resultOfMukChiBa = compareHandsOfMukChiBa()
             switch resultOfMukChiBa {
             case .somebodyWin:
                 print("\(currentTurn) 승리!")
                 break outer
             case .userTurn:
                 currentTurn = "사용자"
-                print("\(currentTurn)의 턴입니다")
             case .computerTurn:
                 currentTurn = "컴퓨터"
-                print("\(currentTurn)의 턴입니다")
+            case .exitInput:
+                break outer
             }
-            
+            print("\(currentTurn)의 턴입니다")
         }
     }
-    func compareHandsOfMukChiBa(_ user: Int, with computer: Int, turn: String) -> ResultOfMukChiBa {
-        switch (user, computer, turn) {
-        case (1, 1, _), (2, 2, _), (3, 3, _):
+    func compareHandsOfMukChiBa() -> ResultOfMukChiBa {
+        switch (handOfUser, handOfComputer) {
+        case (1, 1), (2, 2), (3, 3):
             return .somebodyWin
-        case (1, 3, _), (2, 1, _), (3, 2, _):
+        case (1, 3), (2, 1), (3, 2):
             return .computerTurn
-        case (1, 2, _), (2, 3, _), (3, 1, _):
+        case (1, 2), (2, 3), (3, 1):
             return .userTurn
+        case (0,_):
+            return .exitInput
         default:
             print("알 수 없는 오류입니다.")
-            return .somebodyWin
+            return .exitInput
         }
     }
 }
