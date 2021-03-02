59e466a2abc809da56560bb0ffc68d1b604872d4
unme88 <sunny.on.a.roll@gmail.com>
2021-03-02 17:47:01 +0900

    Chore: navigator JM-2

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,11 +6,8 @@
 
 import Foundation
 
-
 var firstFlag: Bool = true
 var randomNumberOfComputer: Int = 0
-
-func checkGameResult()
 
 func startGame() {
     while true {
@@ -18,24 +15,54 @@ func startGame() {
             randomNumberOfComputer = Int.random(in: 1...3)
             firstFlag = false
         }
-        print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: "")
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
         let userInput: Int? = Int(readLine()!)
         guard let userInputIsInt = userInput else {
-            print("userInput is nil")
-            return
-        }
-        if userInput == 0 {
-            print("게임 종료")
-            return
-        } else if userInputIsInt == 1 && randomNumberOfComputer {
-            
-        }
-        else if userInputIsInt < 1 || userInputIsInt > 3 {
             print("잘못된 입력입니다. 다시 시도해주세요.")
             continue
         }
-        
+        if userInputIsInt == 0 {
+            print("게임 종료")
+            return
+        } else if userInputIsInt < 1 || userInputIsInt > 3 {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            continue
+        } else if userInputIsInt == 1 {
+            if randomNumberOfComputer == 1 {
+                print("비겼습니다!")
+                continue
+            } else if randomNumberOfComputer == 2 {
+                print("졌습니다!")
+                return
+            } else if randomNumberOfComputer == 3 {
+                print("이겼습니다!")
+                return
+            }
+        } else if userInputIsInt == 2 {
+            if randomNumberOfComputer == 2 {
+                print("비겼습니다!")
+                continue
+            } else if randomNumberOfComputer == 3 {
+                print("졌습니다!")
+                return
+            } else if randomNumberOfComputer == 1 {
+                print("이겼습니다!")
+                return
+            }
+        } else if userInputIsInt == 3 {
+            if randomNumberOfComputer == 3 {
+                print("비겼습니다!")
+                continue
+            } else if randomNumberOfComputer == 1 {
+                print("졌습니다!")
+                return
+            } else if randomNumberOfComputer == 2 {
+                print("이겼습니다!")
+                return
+            }
+        }
+
     }
 }
 
-
+startGame()
