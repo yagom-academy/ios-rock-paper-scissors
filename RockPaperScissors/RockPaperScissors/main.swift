548d990526d5ae1776ac81e38d9953b5ddbb8789
kane-young <lyw2100@naver.com>
2021-03-02 16:11:09 +0900

    Feat : implement makeRandomHand()

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -18,7 +18,20 @@ class RockPaperScissorsGame {
 
     func gameStart() {}
     
-    func makeRandomHand() -> Hand { return .none }
+    func makeRandomHand() -> Hand {
+        let random = Int.random(in: 1...3)
+        
+        switch random {
+        case 1:
+            return Hand.scissors
+        case 2:
+            return Hand.rock
+        case 3:
+            return Hand.paper
+        default:
+            return Hand.none
+        }
+    }
     
     func getGameResult(_ usersHand: Hand, vs computersHand: Hand) -> GameResult { return .win }
         
