37f0140023454ffcd3b0308044cbb3f38440011f
eunseo5355 <eunseo5355@naver.com>
2021-03-02 21:04:26 +0900

    [Step-1] 게임 실행하는 함수, 게임 결과 출력하는 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,43 @@
 
 import Foundation
 
-print("Hello, World!")
+func playRockPaperScissors() {
+    let computerNumber = Int.random(in: 1...3)
+    
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator:"")
+    
+    if let inputUserNumber = readLine() {
+        switch Int(inputUserNumber) {
+            case 0:
+                print("게임 종료")
+                return
+            case 1, 2, 3:
+                gameResult(userNumber: Int(inputUserNumber)!, computerNumber: computerNumber)
+            default:
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                playRockPaperScissors()
+            }
+    }
+}
+
+func gameResult(userNumber: Int, computerNumber: Int) {
+    if(userNumber == computerNumber) {
+        print("비겼습니다!")
+        playRockPaperScissors()
+    } else {
+        switch computerNumber {
+        case 1:
+            if(userNumber == 2) { print("이겼습니다!") }
+            else if(userNumber == 3) { print("졌습니다!") }
+        case 2:
+            if(userNumber == 3) { print("이겼습니다!") }
+            else if(userNumber == 1) { print("졌습니다!") }
+        default:
+            if(userNumber == 1) { print("이겼습니다!") }
+            else if(userNumber == 2) { print("졌습니다!") }
+        }
+    }
+}
+
+playRockPaperScissors()
 
