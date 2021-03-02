158cf1493ae4e8b6155dec507aa37fe330b35544
DragonTigerXYZ <yb1229@kpu.ac.kr>
2021-03-02 12:03:25 +0900

    [#1] Add properties for MukChiBaGame.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,27 @@
 
 import Foundation
 
-print("Hello, World!")
+struct MukChiBaGame {
+    var computerAnswer: Int = 0
+    var userAnswer: Int = 0
+    enum GameResult {
+        case draw, win, lose
+    }
+    enum Hand: Int {
+        case scissors = 1
+        case rock = 2
+        case paper = 3
+    }
+    enum GameMessage: String {
+        case menu = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
+        case exit = "게임 종료"
+        case error = "잘못된 입력입니다. 다시 시도해주세요."
+        case draw = "비겼습니다!"
+        case win = "이겼습니다!"
+        case lose = "졌습니다!"
+    }
+    func startGame() {
+    }
+}
 
+MukChiBaGame().startGame()
