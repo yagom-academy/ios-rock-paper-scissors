353bfeef1db87cf195ad5d9e787206815de9a473
unme88 <sunny.on.a.roll@gmail.com>
2021-03-02 17:01:27 +0900

    Comment: navigatorSunny-1

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,25 @@
 
 import Foundation
 
-print("Hello, World!")
+var firstFlag: Bool = true
+var randomNumberOfComputer: Int = 0
+
+func startGame() {
+    while true {
+        if firstFlag {
+            randomNumberOfComputer = Int.random(in: 1...3)
+            firstFlag = false
+        }
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: "")
+        let userInput: Int? = Int(readLine()!)
+        guard let userInputIsInt = userInput else {
+            print("userInput is nil")
+            return
+        }
+        if userInput == 0 {
+            return
+        }
+        
+    }
+}
 
