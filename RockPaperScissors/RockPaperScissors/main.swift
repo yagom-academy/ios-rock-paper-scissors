b24a97a46a19655a0f1a9e723d7897b52d87ca01
Neph <tony3322@naver.com>
2021-03-02 11:23:50 +0900

    Style : Change some spacing based on API Guideline

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,18 +7,16 @@ enum GameResult {
     case lose
     case draw
 }
-
 class RockScissorPaper {
     var handOfComputer = 0
     var handOfUser = 0
-    
     func initialize() {
         handOfUser = 0
         handOfComputer = Int.random(in: 1...3)
     }
     func startGame() {
         var userInput = 0
-        outer: while true{
+        outer: while true {
             initialize()
             showMenu()
             do {
@@ -35,7 +33,6 @@ class RockScissorPaper {
             showResult(winner)
         }
     }
-
     func compareHands(_ user: Int, with computer: Int) -> GameResult {
         switch (user, computer) {
         case (1, 1), (2, 2), (3, 3):
@@ -46,21 +43,21 @@ class RockScissorPaper {
             return .lose
         default:
             print("알 수 없는 오류입니다.")
-            return GameResult.lose
+            return .lose
         }
     }
-    
     func showMenu() {
         print("가위(1). 바위(2). 보(3)! <종료 : 0>", terminator: " : ")
     }
-    
     func getUserInput() throws -> Int {
         guard let unsafeUserInput = readLine() else {
             throw GameError.invalidInput
         }
+        
         guard let integerUserInput = Int(unsafeUserInput) else {
             throw GameError.invalidInput
         }
+        
         switch integerUserInput {
         case 0, 1, 2, 3:
             return integerUserInput
@@ -68,7 +65,6 @@ class RockScissorPaper {
             throw GameError.invalidInput
         }
     }
-    
     func showResult(_ input: GameResult) {
         switch input {
         case .win:
