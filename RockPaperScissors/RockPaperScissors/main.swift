153b478647c95be96d0e6327e18adc381b04355e
ryan-son <ryan.son1002@gmail.com>
2021-03-02 20:23:53 +0900

    refactor: 프로토콜, 익스텐션으로 기능 분할, 승리 판단 로직 변경
    프로토콜, 익스텐션으로 무작위 숫자 생성, 사용자 입력 메서드가 재사용 가능하도록 구현
    승리 판단 메서드를 하나로 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,92 +1,124 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by Steven, Ryan.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-// 변수, 함수명을 후속 게임이 있다는 것을 고려하지 않고 명명해서 일반적일 수 있습니다.
-// 가위바위보와 묵찌빠에서 변수, 함수명을 어떻게 정의할지 같이 생각해봐요.
-// 설명이 필요한 구문이 있으면 디스코드로 메시지 보내주세요~
-
 import Foundation
 
-protocol RockPaperScissorsable {
-    func generateRandomNumber() -> Int
-    func printUserChoiceGuide()
-    func getUserChoice() throws -> Int
-    func decideWinner(userChoice: Int, computerChoice: Int)
-    func startGame()
+enum GameError: Int, Error {
+    case nilInput = -1
+    case typeCastingFailed = -2
 }
 
-extension RockPaperScissors: RockPaperScissorsable {
-    func generateRandomNumber() -> Int {
-        let randomNumber: Int = Int.random(in: 1...3)
-        return randomNumber
-    }
-    
-    func printUserChoiceGuide() {
-        print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
-    }
-    
-    func getUserChoice() throws -> Int {
-        guard let userChoice: String = readLine() else {
-            throw GameError.invalidSelection
+enum RockPaperScissors: Int, RandomNumberGenerator {
+    case none = 0
+    case scissor = 1
+    case rock = 2
+    case paper = 3
+
+    func getInputHandSign(_ inputValue: Int) -> RockPaperScissors {
+        guard let inputHandSign = RockPaperScissors(rawValue: inputValue) else {
+            return .none
         }
-        guard let intUserChoice: Int = Int(userChoice) else {
-            throw GameError.invalidSelection
-        }
-        return intUserChoice
+        return inputHandSign
     }
     
-    func decideWinner(userChoice: Int, computerChoice: Int) {
-        let winningCase: Int = computerChoice % 3 + 1
-        if userChoice == computerChoice {
+}
+
+protocol RandomNumberGenerator {
+    func getRandomHandSign() -> RockPaperScissors
+}
+
+extension RandomNumberGenerator {
+    func getRandomHandSign() -> RockPaperScissors {
+        guard let randomHandSign = RockPaperScissors(rawValue: Int.random(in: 1...3)) else {
+            return .none
+        }
+        return randomHandSign
+    }
+}
+
+protocol GetUserInput {
+    func getUserInput() -> Int
+    func castStringtoInt(inputString: String) -> Int
+}
+
+extension GetUserInput {
+    func getUserInput() -> Int {
+        guard let userInput: String = readLine() else {
+            return GameError.nilInput.rawValue
+        }
+        return castStringtoInt(inputString: userInput)
+    }
+    func castStringtoInt(inputString: String) -> Int {
+        guard let resultInteger: Int = Int(inputString) else {
+            return GameError.typeCastingFailed.rawValue
+        }
+        return resultInteger
+    }
+}
+
+class Player {
+    var handSign: RockPaperScissors = .none
+
+    func setRandomHandSign() {
+        handSign = handSign.getRandomHandSign()
+    }
+    func setInputHandSign(_ userInput: Int) {
+        handSign = handSign.getInputHandSign(userInput)
+    }
+}
+
+class RockPaperScissorsGame: GetUserInput {
+    let player = Player()
+    let computer = Player()
+    var userInput = 0
+
+    private func printMenu() {
+        print("가위(1), 바위(2), 보(3)! <종료:0> : ", terminator:"")
+    }
+    private func exitGame() {
+        print("게임 종료")
+        exit(0)
+    }
+    private func checkInvaildInput(of userInput: Int) -> Bool {
+        if userInput == 0 {
+            exitGame()
+        }
+        if (userInput >= 1 && userInput <= 3) {
+            return true
+        } else {
+            return false
+        }
+    }
+    private func choicePlayerHands(_ userInput: Int) {
+        computer.setRandomHandSign()
+        player.setInputHandSign(userInput)
+    }
+    private func comparePlayersHands() {
+        let winningHandSignOfUser: Int = computer.handSign.rawValue % 3 + 1
+        if player.handSign == computer.handSign {
             print("비겼습니다!")
-        } else if userChoice == winningCase {
+        } else if player.handSign.rawValue == winningHandSignOfUser {
             print("이겼습니다!")
         } else {
             print("아이고.. 지셨네.. 다시 해봐요~")
         }
     }
-}
-
-struct RockPaperScissors {
-    enum GameError: Error {
-        case invalidSelection
-        case unknownError
-    }
-    
-    // 아래 열거형 활용처 같이 고민해봅시다~!
-    enum Choices: Int {
-        case exitGame
-        case scissor
-        case rock
-        case paper
-    }
-    
-    func startGame() {
-        startGameLoop: while true {
-            let computerChoice: Int = generateRandomNumber()
-            do {
-                printUserChoiceGuide()
-                let userChoice: Int = try getUserChoice()
-                if userChoice == Choices.exitGame.rawValue {
-                    break startGameLoop
-                }
-                decideWinner(userChoice: userChoice, computerChoice: computerChoice)
-                // 묵찌빠 게임 이어 작성
-            } catch {
-                switch error {
-                case GameError.invalidSelection:
-                    print("잘못된 입력입니다. 다시 시도해주세요.")
-                default:
-                    print("알 수 없는 에러.")
-                }
+    func play() {
+        repeat {
+            printMenu()
+            userInput = getUserInput()
+            if checkInvaildInput(of: userInput) {
+                choicePlayerHands(userInput)
+                comparePlayersHands()
+            } else {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
             }
-        }
+        } while true
     }
 }
 
-let rockPaperScissors = RockPaperScissors()
-rockPaperScissors.startGame()
-
+let game = RockPaperScissorsGame()
+game.play()
