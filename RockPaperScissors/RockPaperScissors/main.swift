5d045de458b1b77bd1e9616e4a1d2489b57bed47
fezravien <yjaewoong@naver.com>
2021-03-03 11:28:24 +0900

    Refactoring : 재귀 방식에서 반복문 방식으로 로직 변경
    재귀 방식을 반복문 방식으로 바꾸면서 guard를 통한 오류처리 방식을 사용
    열거형과 구조체를 추가하면서 구조화 시킴

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -3,16 +3,55 @@
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
 //
-enum ErrorStatus:Error {
+enum ErrorStatus: Error {
     case inputError
+    case beginAgain
 }
+enum Result: String {
+    case win = "이겼습니다!"
+    case lose = "졌습니다!"
+    case draw = "비겼습니다!"
+}
+enum RockScissorsPaper: Int {
+    case initStatus = -1
+    case end, rock, scissors, paper
+}
+
+struct GameStatus {
+    let rock = RockScissorsPaper.rock.rawValue
+    let scissors = RockScissorsPaper.scissors.rawValue
+    let paper = RockScissorsPaper.paper.rawValue
+    let end = RockScissorsPaper.end.rawValue
+    let initValue = RockScissorsPaper.initStatus.rawValue
+}
+struct GameResult {
+    let win = Result.win.rawValue
+    let lose = Result.lose.rawValue
+    let draw = Result.draw.rawValue
+}
+
 class RockScissorsPaperGame {
-    let initValue = -1
+    var gameStatus = GameStatus()
+    var gameResult = GameResult()
+    var userInput = RockScissorsPaper.initStatus.rawValue
     func startGame() {
-        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-        let userInput = userTyping()
-        let computerNumber = createRandomNumber()
-        judgeRockScissorsPaper(userNumber: userInput, computerNumber: computerNumber)
+        for _ in 0...Int.max {
+            print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+            let computerNumber = createRandomNumber()
+            do {
+                userInput = try userTyping()
+            } catch {
+                printError()
+                continue
+            }
+            do {
+                try judgeRockScissorsPaper(userNumber: userInput, computerNumber: computerNumber)
+            } catch {
+                print(gameResult.draw)
+                continue
+            }
+            break
+        }
     }
 }
 
@@ -22,28 +61,33 @@ extension RockScissorsPaperGame {
     // MARK: - Computer Number
     
     func createRandomNumber() -> Int {
-        let computerRandomNumber = Int.random(in: 1...3)
+        let rock1Scissors2Paper3 = 1...3
+        let computerRandomNumber = Int.random(in: rock1Scissors2Paper3)
         return computerRandomNumber
     }
     
     // MARK: - User input valid check
-    func userTyping() -> Int {
+    func userTyping() throws -> Int {
+        var validedInputNumber = gameStatus.initValue
         let optionalInput = readLine()
-        let validedInputNumber = isValidInput(userInput: optionalInput)
+        do {
+            validedInputNumber = try isValidInput(userInput: optionalInput)
+        } catch {
+            throw ErrorStatus.inputError
+        }
         return validedInputNumber
     }
-    func isValidInput(userInput validInput: String?) -> Int {
-        var valiedNumber = initValue
+    func isValidInput(userInput validInput: String?) throws -> Int {
+        var valiedNumber = RockScissorsPaper.initStatus.rawValue
         do {
             valiedNumber = try checkedInput(inputOptionalString: validInput)
         } catch {
-            printError()
-            startGame()
+            throw ErrorStatus.inputError
         }
         return valiedNumber
     }
     func checkedInput(inputOptionalString OptionalString: String?) throws -> Int {
-        let validedNumbers = [0, 1, 2, 3]
+        let validedNumbers = [gameStatus.end, gameStatus.rock, gameStatus.scissors, gameStatus.paper]
         guard let validString = OptionalString, let validNumber = Int(validString), validedNumbers.contains(validNumber) else {
             throw ErrorStatus.inputError
         }
@@ -52,21 +96,20 @@ extension RockScissorsPaperGame {
     
     // MARK: - Game judge
     
-    func judgeRockScissorsPaper(userNumber userState: Int, computerNumber computerState: Int) {
+    func judgeRockScissorsPaper(userNumber userState: Int, computerNumber computerState: Int) throws {
         let decisionStatus = userState - computerState
         let userWinStatus = [1, -2]
         let computerWinStatus = [-1, 2]
-        dump(userState)
-        if userState == 0 {
+        let userComputerDraw = 0
+        if userState == userComputerDraw {
             finishGame()
-        } else if decisionStatus == 0 {
-            print("비겼습니다!")
-            startGame()
+        } else if decisionStatus == userComputerDraw {
+            throw ErrorStatus.beginAgain
         } else {
             if userWinStatus.contains(decisionStatus) {
-                print("이겼습니다!")
+                print(gameResult.win)
             } else if computerWinStatus.contains(decisionStatus) {
-                print("졌습니다!")
+                print(gameResult.lose)
             }
         }
     }
