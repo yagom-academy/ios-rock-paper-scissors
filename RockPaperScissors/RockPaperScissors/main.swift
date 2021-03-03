0e86201858eed6261b66af95ca599c5bdab2c981
babywalnut <anfuza@naver.com>
2021-03-03 10:25:44 +0900

    [#7] FeedBack

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -8,7 +8,7 @@ import Foundation
 
 
 struct RockScissorsPaper {
-  
+    
     private func playRockScissorsPaper(with userChoice: Int) {
         let randomRockScissorsPaperChoice: Int = Int.random(in: 1...3)
         let criterion: Int = (userChoice - randomRockScissorsPaperChoice + 3) % 3
@@ -21,13 +21,13 @@ struct RockScissorsPaper {
             startGame()
         }
     }
-
+    
     private func decideGameOption(with convertedUserSelection: Int) {
-        if [1, 2, 3].contains(convertedUserSelection) {
+        if (1...3).contains(convertedUserSelection) {
             playRockScissorsPaper(with: convertedUserSelection)
         }
     }
-
+    
     private func checkValidation(_ userSelection: String) -> Bool {
         if let selection = Int(userSelection) {
             if (0...3).contains(selection) {
@@ -36,31 +36,37 @@ struct RockScissorsPaper {
         }
         return false
     }
-
+    
     private func convert(_ userSelection: String) -> Int {
         if let convertedUserSelection: Int = Int(userSelection) {
             return convertedUserSelection
+        } else {
+            let notIntError: Int = -1
+            return notIntError
         }
-        return -1
+    }
+    
+    private func restartGame() {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        startGame()
     }
     
     private func getUserSelection() {
-        print("가위(1), 바위(2), 보(3)! <종료:0> : ", terminator: "")
         if let userSelection = readLine() {
             if checkValidation(userSelection) {
                 let convertedUserSelection: Int = convert(userSelection)
                 decideGameOption(with: convertedUserSelection)
             } else {
-                print("잘못된 입력입니다. 다시 시도해주세요.")
-                startGame()
+                restartGame()
             }
         }
-    }
-
+        }
+    
     func startGame() {
+        print("가위(1), 바위(2), 보(3)! <종료:0> : ", terminator: "")
         getUserSelection()
     }
 }
 
-let rockScissorsPaper = RockScissorsPaper()
-rockScissorsPaper.startGame()
+let newGame = RockScissorsPaper()
+newGame.startGame()
