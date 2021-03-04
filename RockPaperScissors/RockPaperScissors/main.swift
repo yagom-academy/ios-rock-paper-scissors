a13fba57d02127d5e7775da55a838f8767efb64a
DragonTigerXYZ <yb1229@kpu.ac.kr>
2021-03-04 17:33:22 +0900

    [#4] Refactor MukChiBaGame by correcting parameters to optional.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,8 +7,8 @@
 import Foundation
 
 class MukChiBaGame {
-    private var computerAnswer: Hand = .rock
-    private var userAnswer: Hand = .rock
+    private var computerAnswer: Hand?
+    private var userAnswer: Hand?
     private enum GameResult {
         case draw, win, lose
     }
@@ -19,19 +19,40 @@ class MukChiBaGame {
     }
     private enum GameMessage: String {
         case menu = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
-        case exit = "게임 종료"
-        case error = "잘못된 입력입니다. 다시 시도해주세요."
+        case finish = "게임 종료"
+        case invalidHandError = "잘못된 입력입니다. 다시 시도해주세요."
         case draw = "비겼습니다!"
         case win = "이겼습니다!"
         case lose = "졌습니다!"
+    }
+    private enum GameError: Error {
+        case invalidHand
     }
     
     private func generatedRandomAnswer() -> Int {
         return Int.random(in: 1...3)
     }
     
-    private func castedHand(from number: Int) -> Hand {
-         return Hand(rawValue: number) ?? .rock
+    private func castedHand(from possibleText: String?) throws -> Hand {
+        guard let text: String = possibleText else {
+            throw GameError.invalidHand
+        }
+        guard let possibleHand: Int = Int(text) else {
+            throw GameError.invalidHand
+        }
+        guard let hand = Hand(rawValue: possibleHand) else {
+            throw GameError.invalidHand
+        }
+        
+        return hand
+    }
+    
+    private func castedHand(from possibleHand: Int) throws -> Hand {
+        guard let hand = Hand(rawValue: possibleHand) else {
+            throw GameError.invalidHand
+        }
+        
+        return hand
     }
     
     private func print(gameMessage: GameMessage) {
@@ -46,29 +67,6 @@ class MukChiBaGame {
     private func finishGame(withMessage message: GameMessage) {
         print(gameMessage: message)
         exit(0)
-    }
-    
-    private func castedInteger(from input: String?) -> Int {
-        guard let _input: String = input else { return -1 }
-        guard let result: Int = Int(_input) else { return -1 }
-        
-        return result
-    }
-    
-    private func isValidUserAnswer(_ userInput: String?) -> Bool {
-        let userAnswer: Int = castedInteger(from: userInput)
-        
-        if userAnswer == -1 { return false }
-        
-        switch userAnswer {
-        case 0:
-            finishGame(withMessage: .exit)
-        case 1...3:
-            return true
-        default: break
-        }
-        
-        return false
     }
     
     private func getGameResult() -> GameResult {
@@ -108,16 +106,25 @@ class MukChiBaGame {
     }
     
     func startGame() {
-        computerAnswer = castedHand(from: generatedRandomAnswer())
+        computerAnswer = try! castedHand(from: generatedRandomAnswer())
+        
         while true {
             print(gameMessage: .menu)
             let userInput: String? = readLine()
             
-            if false == isValidUserAnswer(userInput) {
-                print(gameMessage: .error)
+            if userInput == "0" {
+                finishGame(withMessage: .finish)
+            }
+            do {
+                userAnswer = try castedHand(from: userInput)
+            } catch GameError.invalidHand {
+                print(gameMessage: .invalidHandError)
+                continue
+            } catch {
+                Swift.print("그 외 오류 발생 : ", error)
                 continue
             }
-            userAnswer = castedHand(from: castedInteger(from: userInput))
+            
             gameResultHandling(getGameResult())
         }
     }
