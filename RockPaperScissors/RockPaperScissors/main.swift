e218809b0ffc47fed5a40ffdf4686e53dc052a72
Wody95 <kwu0626@gmail.com>
2021-03-04 17:03:23 +0900

    메소드 순서 변경, userinputCase의 Int형변환 guard let 삭제

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,91 +6,16 @@
 
 import Foundation
 
-enum RockPaperScissorsCase: Int {
-    case exit = 0
-    case rock = 1
-    case paper = 2
-    case scissors = 3
+enum RockPaperScissorsCase: String {
+    case exit = "0"
+    case rock = "1"
+    case paper = "2"
+    case scissors = "3"
 }
 
 class RockPaperScissors {
     var computerCase = 0
     var userCase = 0
-    
-    func createRandomComputerCase() -> Int {
-        let randomCase = Int.random(in: 1...3)
-        print("개발용 컴퓨터 확인 \(randomCase)")
-        return randomCase
-    }
-    
-    func userInputCase(mode: String) -> Int {
-        while true {
-            guard let userInputText = readLine() else {
-                continue
-            }
-            guard let userInputNumber = Int(userInputText) else {
-                continue
-            }
-            
-            let userCase = RockPaperScissorsCase(rawValue: userInputNumber)
-            
-            switch userCase {
-            case .exit:
-                print("게임 종료")
-                return 0
-            case .rock:
-                return 1
-            case .paper:
-                return 2
-            case .scissors:
-                return 3
-            default:
-                if mode == "가위바위보" {
-                    print("잘못된 입력입니다. 다시 입력해주세요.")
-                } else {
-                    print("잘못된 입력입니다. 다시 시도해주세요.")
-                    return 4
-                }
-                
-            }
-        }
-    }
-    
-    func checkWinLoseResult(user: Int, computer: Int) -> Int {
-        let whoWinCheck: Int = user - computer
-        var gameResult: Int = 0
-        
-        // 가위바위보
-        if user == 0 {
-            gameResult = 0
-        } else if user == 4 {
-            gameResult = 1
-        } else if whoWinCheck == 1 || whoWinCheck == -2 {
-            gameResult = 1
-        } else if whoWinCheck == -1 || whoWinCheck == 2 {
-            gameResult = 2
-        } else if whoWinCheck == 0 {
-            gameResult = 3
-        }
-        
-        return gameResult
-    }
-    
-    func printResultChangeGameState(gameResult: Int) -> Int {
-        switch gameResult {
-        case 0:
-            return 0
-        case 1:
-            print("이겼습니다!")
-            return 1
-        case 2:
-            print("졌습니다!")
-            return 2
-        default:
-            print("비겼습니다!")
-            return 3
-        }
-    }
     
     func playGame() {
         var playGameSwitch: Int = 1
@@ -120,6 +45,79 @@ class RockPaperScissors {
                 playGameSwitch = printMukjjibbaResult(
                     gameResult: winLoseResult, nowTurn: nowPlayTurn)
             }
+        }
+    }
+    
+    func createRandomComputerCase() -> Int {
+        let randomCase = Int.random(in: 1...3)
+        print("개발용 컴퓨터 확인 \(randomCase)")
+        return randomCase
+    }
+    
+    func userInputCase(mode: String) -> Int {
+        while true {
+            guard let userInputText = readLine() else {
+                continue
+            }
+            
+            let userCase = RockPaperScissorsCase(rawValue: userInputText)
+            
+            switch userCase {
+            case .exit:
+                print("게임 종료")
+                return 0
+            case .rock:
+                return 1
+            case .paper:
+                return 2
+            case .scissors:
+                return 3
+            default:
+                if mode == "가위바위보" {
+                    print("잘못된 입력입니다. 다시 입력해주세요.")
+                } else {
+                    print("잘못된 입력입니다. 다시 시도해주세요.")
+                    return 4
+                }
+                
+            }
+        }
+    }
+    
+    func checkWinLoseResult(user: Int, computer: Int) -> Int {
+        let whoWinCheck: Int = user - computer
+        var gameResult: Int = 0
+        
+        // 가위바위보 gameResult => 0. 종료, 1. 승리, 2. 패배, 3. 비김
+        // 묵찌빠    gameResult => 0. 종료, 1. 턴넘김, 2. 턴가져옴, 3. 승리
+        if user == 0 {
+            gameResult = 0
+        } else if user == 4 {
+            gameResult = 1
+        } else if whoWinCheck == 1 || whoWinCheck == -2 {
+            gameResult = 1
+        } else if whoWinCheck == -1 || whoWinCheck == 2 {
+            gameResult = 2
+        } else if whoWinCheck == 0 {
+            gameResult = 3
+        }
+        
+        return gameResult
+    }
+    
+    func printResultChangeGameState(gameResult: Int) -> Int {
+        switch gameResult {
+        case 0:
+            return 0
+        case 1:
+            print("이겼습니다!")
+            return 1
+        case 2:
+            print("졌습니다!")
+            return 2
+        default:
+            print("비겼습니다!")
+            return 3
         }
     }
 
