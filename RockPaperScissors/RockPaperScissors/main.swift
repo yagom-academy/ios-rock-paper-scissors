a79c0ee34a74369f21c827861fe812e5d0abc4e4
Tak <oit5538@gamil.com>
2021-03-05 15:36:12 +0900

    refactor : PR 피드백 내용 수정 (미완성)
    의미없는 주석 제거
    안쓰는 프로퍼티 제거
    Coda 이어서 완성할 예정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -12,7 +12,6 @@ enum GameDecision {
     case exit
 }
 
-///
 enum GameResult {
     case win
     case draw
@@ -27,19 +26,18 @@ enum GameResult {
 - 승리할 시 (-1,2) / 패배할 시 (-2,1) / 비겼을 시 (0) */
 class RockPaperScissorsGame {
     var inputNumber: Int = 0
-    var inputString: String = ""
     var randomNumber: Int = 0
     var gameMenu: GameDecision = GameDecision.restart
     var gameCommand: GameResult = GameResult.draw
     
     ///컴퓨터에서 가위바위보 중 무엇을 낼지를 결정하는 단계
-    func makeRandomNumber() {
+    func setRandomNumber() {
         randomNumber = Int.random(in: 1...3)
     }
 
     /// 입력값을 받아 Bool 타입으로 반환.
     /// 입력값은 class 내부의 변수로 저장.
-    func checkInputNumber() -> Bool {
+    func isValidInputNumber() -> Bool {
         guard let temporaryValue: String = readLine(),
               let deliveryNumber: Int = Int(temporaryValue)
         else { return false }
@@ -50,30 +48,34 @@ class RockPaperScissorsGame {
     /// checkInputNumber의 Bool값을 통해 예외 사항(nil 혹은 문자열을 입력했을 시)을 처리하고
     /// inputNumber에 저장된 숫자를 바탕으로 게임진행여부 결정
     func decideGamePlaying() {
-        switch checkInputNumber() {
-        case false:
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            gameMenu = GameDecision.restart
-        case true:
+        if isValidInputNumber() == true {
             switch inputNumber {
             case 0:
                 gameMenu = GameDecision.exit
-                gameCommand = GameResult.error
             case 1, 2, 3:
                 gameMenu = GameDecision.start
             default:
                 print("잘못된 입력입니다. 다시 시도해주세요.")
                 gameMenu = GameDecision.restart
             }
+        } else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            gameMenu = GameDecision.restart
         }
     }
+    
+    func bbb() {                                        // 네이밍
+        if gameMenu == GameDecision.exit {
+            gameCommand = GameResult.error
+        }
+    }
+
     /**
     ### 컴퓨터의 수에서 사용자 입력값을 뺀 값에 따라 각각의 case 분류
     - ex) 컴퓨터의 수: 1(가위) - 사용자입력값: 2(바위) = -1로 사용자승리.
     ------
     - case에 따라 win, lose, draw 값을 gameCommand에 저장.*/
-    func sortResult(a: Int, b: Int, c: GameDecision) { // #1 이 부분을 물어보고 싶습니다!!!!
-        if c == GameDecision.start {
+    func sortResult(a: Int, b: Int) {                   // sortResult, a, b 파라미터 네이밍
             switch (a - b) {
             case -1, 2:
                 gameCommand = GameResult.win
@@ -83,10 +85,15 @@ class RockPaperScissorsGame {
                 gameCommand = GameResult.draw
             default:
                 return
-            }
         }
     }
     
+    func aaa() {                                            // 메서드 네이밍
+        if gameMenu == GameDecision.start {
+            sortResult(a: randomNumber, b: inputNumber)
+        }
+    }
+        
     ///결과값 출력
     func printResult() {
         switch gameCommand {
@@ -116,9 +123,10 @@ class RockPaperScissorsGame {
     func start() {
         while gameCommand == GameResult.draw || gameMenu == GameDecision.restart {
             print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-            makeRandomNumber()
+            setRandomNumber()
             decideGamePlaying()
-            sortResult(a: randomNumber, b: inputNumber, c: gameMenu)
+            bbb()
+            aaa()
             printResult()
             initializeNumbers()
         }
