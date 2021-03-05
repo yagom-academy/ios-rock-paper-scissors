a3a9286e1545ef904771ae7df288b596a3972f4c
sjsj <boromir21@naver.com>
2021-03-05 13:29:10 +0900

    코드내용 직관적으로 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,145 +6,131 @@
 
 import Foundation
 
-enum Hand: String {
-    case exit = "0"
-    case rock = "1"
-    case paper = "2"
-    case scissors = "3"
+enum RockPaperScissorsHand: Int {
+    case exit = 0, scissors, rock, paper
+}
+
+enum MukChiPaHand: Int {
+    case exit = 0, muk, chi, pa
+}
+
+enum GameResult: String {
+    case win = "이겼습니다!"
+    case lose = "졌습니다!"
+    case draw = "비겼습니다!"
+    case exit = "게임 종료"
+    case error = "다시 입력해주세요"
+}
+
+enum TurnStatus: String {
+    case userTurn = "사용자"
+    case computerturn = "컴퓨터"
 }
 
 class RockPaperScissors {
-    var computerCase = 0
-    var userCase = 0
-    
-    func playGame() {
-        var playGameSwitch: Int = 1
-        var winLoseResult: Int = 0
-        var gameMode: String = "가위바위보"
-        
-        while playGameSwitch != 0 {
-            print("가위(1). 바위(2). 보(3)! <종료 : 0> : ", terminator: "")
-            
-            computerCase = choiceRandomComputerHand()
-            userCase = choiceUserHand(mode: gameMode)
-            
-            winLoseResult = checkWinLoseResult(user: userCase, computer: computerCase)
-            
-            playGameSwitch = printRockPaperScissorsResult(gameResult: winLoseResult)
-            
-            while playGameSwitch != 0 && playGameSwitch != 3 {
-                gameMode = "묵찌빠"
-                
-                let nowPlayTurn = whoNowTurn(gameResult: playGameSwitch)
-                print("[\(nowPlayTurn) 턴] 묵(1). 찌(2). 빠(3)! <종료: 0> : ", terminator: "")
-                
-                computerCase = choiceRandomComputerHand()
-                userCase = choiceUserHand(mode: gameMode)
-                
-                winLoseResult = checkWinLoseResult(user: userCase, computer: computerCase)
-                playGameSwitch = printMukjjibbaResult(
-                    gameResult: winLoseResult, nowTurn: nowPlayTurn)
-            }
-        }
-    }
+    var userHand = 0
+    var computerHand = 0
+    var turnSwitch = TurnStatus.userTurn
     
     func choiceRandomComputerHand() -> Int {
-        return Int.random(in: 1...3)
+        let createNumber = Int.random(in: 1...3)
+        return createNumber
     }
     
-    func choiceUserHand(mode: String) -> Int {
-        while true {
-            guard let userInputText = readLine() else {
-                continue
-            }
-            
-            let userCase = Hand(rawValue: userInputText)
-            
-            switch userCase {
-            case .exit:
-                print("게임 종료")
-                return 0
-            case .rock:
-                return 1
-            case .paper:
-                return 2
-            case .scissors:
-                return 3
+    func choiceUserHand() -> Int {
+        guard let userText = readLine(), let userNumber = Int(userText) else {
+            return 4 // 옵셔널 해제 실패시 반환할 값
+        }
+        return userNumber
+    }
+    
+    func checkRockPaperScissorsResult(userNumber: Int, computerNumber: Int) -> GameResult {
+        var result = GameResult.draw
+        let userHand = RockPaperScissorsHand(rawValue: userNumber)
+        let computerHand = RockPaperScissorsHand(rawValue: computerNumber)
+        
+        if userHand == .exit {
+            result = .exit
+        } else if userHand == computerHand {
+            result = .draw
+        } else {
+            switch (userHand, computerHand) {
+            case (.scissors, .paper), (.rock, .scissors), (.paper, .rock):
+                result = .win
+                turnSwitch = .userTurn
+            case (.scissors, .rock), (.rock, .paper), (.paper, .scissors):
+                result = .lose
+                turnSwitch = .computerturn
             default:
-                if mode == "가위바위보" {
-                    print("잘못된 입력입니다. 다시 입력해주세요.")
-                } else {
-                    print("잘못된 입력입니다. 다시 시도해주세요.")
-                    return 4
+                result = .error
+            }
+        }
+        return result
+    }
+    
+    func checkMukChiPaResult(userNumber: Int, computerNumber: Int) -> GameResult {
+        var result = GameResult.draw
+        let userHand = MukChiPaHand(rawValue: userNumber)
+        let computerHand = MukChiPaHand(rawValue: computerNumber)
+        
+        if userHand == .exit {
+            result = .exit
+        } else if userHand == computerHand {
+            switch turnSwitch {
+            case .userTurn:
+                result = .win
+            case .computerturn:
+                result = .lose
+            }
+        } else {
+            switch (userHand, computerHand) {
+            case (.muk, .chi), (.chi, .pa), (.pa, .muk):
+                turnSwitch = .userTurn
+            case (.muk, .pa), (.chi, .muk), (.pa, .chi):
+                turnSwitch = .computerturn
+            default:
+                turnSwitch = .computerturn
+            }
+        }
+    return result
+}
+    
+    // 승패결과 출력 메서드 분리
+    func playGame() {
+        outer: while true {
+            computerHand = choiceRandomComputerHand()
+            print("가위(1), 바위(2), 보(3)! <종료 : 0> : \(computerHand)", terminator: " ")
+            userHand = choiceUserHand()
+            
+            let result = checkRockPaperScissorsResult(userNumber: userHand, computerNumber: computerHand)
+            print(result.rawValue)
+            
+            switch result {
+            case .draw, .error:
+                continue
+            case .exit:
+                return
+            default:
+                inner: while true {
+                    computerHand = choiceRandomComputerHand()
+                    print("[\(turnSwitch.rawValue) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : \(computerHand)", terminator: " ")
+                    userHand = choiceUserHand()
+                    
+                    let result = checkMukChiPaResult(userNumber: userHand, computerNumber: computerHand)
+                    
+                    switch result {
+                    case .win, .lose, .exit:
+                        print(result.rawValue)
+                        return
+                    default:
+                        print("[\(turnSwitch.rawValue)의 턴입니다.]")
+                        continue inner
+                    }
                 }
             }
         }
     }
-    
-    func checkWinLoseResult(user: Int, computer: Int) -> Int {
-        let whoWinCheck: Int = (user - computer + 3) % 3
-        var gameResult: Int = 0
-        
-        // 가위바위보 gameResult => 0. 종료, 1. 승리, 2. 패배, 3. 비김
-        // 묵찌빠    gameResult => 0. 종료, 1. 턴넘김, 2. 턴가져옴, 3. 승리
-        if user == 0 {
-            gameResult = 0
-        } else if user == 4 {
-            gameResult = 1
-        } else if whoWinCheck == 1 {
-            gameResult = 1
-        } else if whoWinCheck == 2 {
-            gameResult = 2
-        } else if whoWinCheck == 0 {
-            gameResult = 3
-        }
-        
-        return gameResult
-    }
-    
-    func printRockPaperScissorsResult(gameResult: Int) -> Int {
-        switch gameResult {
-        case 0:
-            return 0
-        case 1:
-            print("이겼습니다!")
-            return 1
-        case 2:
-            print("졌습니다!")
-            return 2
-        default:
-            print("비겼습니다!")
-            return 3
-        }
-    }
-
-    func whoNowTurn(gameResult: Int)  -> String {
-        var nowTurn:String
-        
-        if gameResult == 1 {
-            nowTurn = "유저"
-        } else {
-            nowTurn = "컴퓨터"
-        }
-        
-        return nowTurn
-    }
-    
-    func printMukjjibbaResult(gameResult: Int, nowTurn: String) -> Int {
-        switch gameResult {
-        case 2:
-            print("사용자 턴입니다.")
-            return 1
-        case 1:
-            print("컴퓨터 턴입니다.")
-            return 2
-        default:
-            print("\(nowTurn)의 승리!")
-            return 0
-        }
-    }
-    
 }
 
-var testGame = RockPaperScissors()
+let testGame = RockPaperScissors()
 testGame.playGame()
