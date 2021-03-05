de6cde91fbc78b6c1e8a137209a2ab8233cc46a3
unme88 <sunny.on.a.roll@gmail.com>
2021-03-05 11:57:48 +0900

    feat: 묵찌빠 기능 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -18,6 +18,69 @@ func rockPaperScissorResult(userNumber:Int, computerNumber: Int) -> Int {
     }
     return result
 }
+/// 배열의 첫번째 값은 패가 같은 경우에는 true, 패가 다른 경우에는 false
+/// 배열의 두번째 값은 사용자 턴인 경우엔 true, 컴퓨터 턴인 경우에는 false
+func step2_1(turnState: Int, userNumber: Int, computerNumber: Int) -> [Bool] {
+    if userNumber == computerNumber {
+        if turnState == 1 {
+            print("사용자의 승리!")
+        } else {
+            print("컴퓨터의 승리!")
+        }
+        return [true]
+    } else {
+        if turnState == 1 {
+            return [false, true]
+        } else {
+            return [false, false]
+        }
+    }
+}
+
+/// 반환값이 true인 경우 게임 종료
+/// 반환값이 false인 경우 반복
+func step2(state: Int) -> Bool {
+    resetFlag = true
+    var turn: String = ""
+    var turnState: Int = state
+    var isContinue: Bool = true
+    while isContinue {
+        if turnState == 1 {
+            turn = "사용자 턴"
+        } else {
+            turn = "컴퓨터 턴"
+        }
+        if resetFlag {
+            computerNumber = Int.random(in: 1...3)
+            resetFlag = false
+        }
+        print("[\(turn)] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
+        let userInput: Int? = Int(readLine()!)
+        guard let userNumber = userInput else {
+            if turnState == 1 {
+                turnState = 0
+            } else {
+                turnState = 1
+            }
+            continue
+        }
+        guard userNumber >= 1 && userNumber <= 3 else {
+            if turnState == 1 {
+                turnState = 0
+            } else {
+                turnState = 1
+            }
+            continue
+        }
+        let gameResult: [Bool] = step2_1(turnState: turnState, userNumber: userNumber, computerNumber: computerNumber)
+        /// 패가 같은 경우
+        if gameResult[0] {
+            return true
+        } else {
+            
+        }
+    }
+}
 
 func startGame() {
     var isContinue: Bool = true
@@ -45,10 +108,10 @@ func startGame() {
         gameResult = rockPaperScissorResult(userNumber: userNumber, computerNumber: computerNumber)
         if gameResult == win {
             print("이겼습니다!")
-            return
+            
         } else if gameResult == lose {
             print("졌습니다!")
-            return
+            
         } else {
             print("비겼습니다!")
             resetFlag = true
