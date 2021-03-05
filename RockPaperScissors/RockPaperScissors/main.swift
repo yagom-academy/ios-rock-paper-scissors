d74f2932ceba6d3f0aebb9af0d9e74d3d4a4e820
ryan-son <ryan.son1002@gmail.com>
2021-03-05 11:52:01 +0900

    refactor: 묵찌빠 게임 대응을 위한 메서드 분리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -13,9 +13,9 @@ enum GameError: Error {
     case outOfRange
 }
 
-//indirect enum GameMode {
-//    case rockPaperScissors, mukChiPa
-//}
+ enum GameMode {
+    case rockPaperScissors, mukChiPa
+}
 
 enum RockPaperScissors: Int, CaseIterable {
     case none, scissor, rock, paper
@@ -52,25 +52,25 @@ class Player {
 class RockPaperScissorsGame {
     private let player = Player()
     private let computer = Player()
-//    private var userInput = 0
-    // private var phase, mode = 1 // 1번이면 가위바위보, 2번이면 묵찌바 --> enum
-    // var isMyTurn: Bool = true
-
+    private var mode = GameMode.rockPaperScissors // 1번이면 가위바위보, 2번이면 묵찌바 --> enum
+    private var isUserTurn: Bool = true
+    
     private func printMenu() {
-//        if 게임 모드 == 게임모드 타입.가위바위보 {
-        print("가위(1), 바위(2), 보(3)! <종료:0> : ", terminator:"")
-//        } else {
-//        if isMyTurn == true {
-//            print("[사용자 턴]")
-//        } else {
-//            print("[컴퓨터 턴]")
-//        }
+        if mode == GameMode.rockPaperScissors {
+            print("가위(1), 바위(2), 보(3)! <종료:0> : ", terminator:"")
+        } else {
+            if isUserTurn == true {
+                print("[사용자 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator:"")
+            } else {
+                print("[컴퓨터 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator:"")
+            }
+        }
     }
     private func exitGame() {
-            print("게임 종료")
-            exit(0)
+        print("게임 종료")
+        exit(0)
     }
-    func getUserInput() throws -> Int {
+    private func getUserInput() throws -> Int {
         guard let userInput: String = readLine(), let resultInteger: Int = Int(userInput) else {
             throw GameError.invalidInput
         }
@@ -79,44 +79,55 @@ class RockPaperScissorsGame {
         }
         return resultInteger
     }
-//    private func checkOutOfRange(of userInput: Int) -> Bool {
-//        if (userInput >= 1 && userInput <= 3) {
-//            return true
-//        } else {
-//            return false
-//        }
-//    }
     private func choicePlayerHands(_ userInput: Int) {
         computer.setRandomHandSign()
         player.setInputHandSign(userInput)
     }
-    private func decideWinner() { // decideOrder로 변경 고려
+    private func decideOrder()  { // decideOrder로 변경 고려
         let winningHandSignOfUser: Int = computer.handSign.index() % 3 + 1
         if player.handSign == computer.handSign {
-            print("비겼습니다!") // 가위바위보 계속 진행
+            print("비겼습니다!")
         } else if player.handSign.index() == winningHandSignOfUser {
             print("이겼습니다!") // 묵찌빠 타입으로 이동, 사용자가 선
+            mode = GameMode.mukChiPa
+            isUserTurn = true
         } else {
-            print("아이고.. 지셨네.. 다시 해봐요~") // 묵찌빠 타입으로 이동,
+            print("아이고.. 지셨네..") // 묵찌빠 타입으로 이동,
+            mode = GameMode.mukChiPa
+            isUserTurn = false
         }
     }
     
 //    func play가위바위보
+    func playRockScissorPaper() {
+        var userInput: Int = 0
+        printMenu()
+        do {
+            userInput = try getUserInput()
+            if userInput == 0 {
+                exitGame()
+            }
+            choicePlayerHands(userInput)
+            decideOrder()
+        } catch {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            mode = GameMode.mukChiPa
+            isUserTurn = false
+        }
+    }
 //    func play묵찌빠
+    func playMukChiPa() {
+        var userInput: Int = 0
+        printMenu()
+    }
     
     func play() {
-        var userInput: Int = 0
+        
         repeat {
-            printMenu()
-            do {
-                userInput = try getUserInput()
-                if userInput == 0 {
-                    exitGame()
-                }
-                choicePlayerHands(userInput)
-                decideWinner()
-            } catch {
-                print("잘못된 입력입니다. 다시 시도해주세요.")
+            if mode == GameMode.rockPaperScissors {
+                playRockScissorPaper()
+            } else {
+                // 묵찌빠게임 플레이
             }
         } while true
     }
