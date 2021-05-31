9a85e8d568b171b001e2af1fc4f3581be88fd415
홍정아 <isjoeyful@gmail.com>
2021-05-31 20:57:49 +0900

    feat: 프로그램 기본 플로우 및 플로우에 필요한 함수 프로토타입, 열거형 타입 작성
    - RPS 열거형: 가위, 바위, 보를 대표하는 타입으로 계산의 편의를 위해 Int로 선언
    - GameResult 열거형: 게임의 승리, 패배, 비김을 대표하는 타입
    - showInfoMessage(): 메뉴를 출력하고 사용자가 입력한 메뉴값을 리턴하는 함수
    - generateComputerRPSValue(): 컴퓨터의 패에 해당하는 가위바위보를 리턴하는 함수
    - playRockScissorsPaperWith(): 두 플레이어의 승패를 리턴하는 함수
    - showResultMessage(): 게임의 승패에 따라 결과를 출력해주는 함수
    - showGameEndMessage(): 게임 종료를 알려주는 함수

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,53 @@
 
 import Foundation
 
-print("Hello, World!")
 
+enum RPS: Int {
+    case scissors = 1
+    case rock = 2
+    case paper = 3
+}
+
+enum GameResult: String {
+    case win = "win"
+    case lose = "lose"
+    case tie = "tie"
+}
+
+func showInfoMessage() -> Int {
+    return 1
+}
+
+func generateComputerRPSValue() -> RPS {
+    return RPS.rock
+}
+
+func playRockScissorsPaperWith(player1: RPS, player2: RPS) -> GameResult {
+    return GameResult.win
+}
+
+func showResultMessage(gameResult: GameResult) {
+    
+}
+
+func showGameEndMessage() {
+    
+}
+
+func main() {
+    let EXIT_NUMBER = 0
+    while true {
+        let userInput: Int = showInfoMessage()
+        if userInput == EXIT_NUMBER {
+            showGameEndMessage()
+            break
+        }
+        if let userRPS: RPS = RPS(rawValue: userInput) {
+            let computerRPS: RPS = generateComputerRPSValue()
+            let resultRPS: GameResult = playRockScissorsPaperWith(player1: computerRPS, player2: userRPS)
+            showResultMessage(gameResult: resultRPS)
+        }
+    }
+}
+
+main()
