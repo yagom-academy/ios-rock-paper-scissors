036d9bd506b2801559bf9d50ae6e9fd59cf749f2
hosinging <pdhou23@gmail.com>
2021-05-31 17:55:01 +0900

    feat: 사용자 입력받기

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,19 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Game {
+    var isPlaying = true
+    func inputFromUser() -> Int {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+        guard let input = readLine(), let number = Int(input), (0...3).contains(number) else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            return inputFromUser()
+        }
+        return number
+    }
+    
+}
+
+let game = Game()
+print(game.inputFromUser())
 
