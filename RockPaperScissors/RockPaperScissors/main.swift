6aba47fc2ea29b47fcf70b82f81bc97242263ea6
KimWanKi <seoulkwk95@gmail.com>
2021-05-31 22:27:50 +0900

    feat : requestUserInput 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -12,4 +12,13 @@ enum RockPaperScissors: Int {
     case rock
     case paper
 }
-print(RockPaperScissors.paper.rawValue)
+
+func requestUserInput() -> Int{
+    guard let inputString = readLine(), let inputNumber = Int(inputString), 0 <= inputNumber && inputNumber <= 3 else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return requestUserInput()
+    }
+    return inputNumber
+}
+
+let userNumber = requestUserInput()
