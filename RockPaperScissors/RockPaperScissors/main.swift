4dbc90b7ddc06bea1a5631bb7875c7c926f397af
jsim27 <simjh27@gmail.com>
2021-06-01 14:22:50 +0900

    feat: 입력값 유효성 검사 기능 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,42 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
+var userChoice: String = ""
+var computerChoice: Int = 0
 
-import Foundation
+func startGame() -> Void {
+    print(showStartMessage(), terminator: "")
+    recieveInput()
+    if userChoice == "" {
+        return
+    }
+}
 
-print("Hello, World!")
+func showStartMessage() -> String {
+    return "가위(1), 바위(2), 보(3)! <종료 : 0> : "
+}
 
+func recieveInput() -> Void {
+    let input = readLine()
+    testValid(of: input)
+}
+
+func testValid(of input: String?) {
+    switch input {
+    case "0":
+        print("게임 종료")
+        return
+    case "1":
+        userChoice = "1"
+    case "2":
+        userChoice = "2"
+    case "3":
+        userChoice = "3"
+    default:
+        print(showErrorMessage())
+        startGame()
+    }
+}
+
+func showErrorMessage() -> String {
+    return "잘못된 입력입니다. 다시 시도해주세요."
+}
+
+startGame()
