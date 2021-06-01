4fbf6d03b7fb8947f1328591fb90c217876e4784
jaemuyeo <jemking94@gmail.com>
2021-06-01 11:37:27 +0900

    Feat. 전역변수 생성, 메뉴프린트 함수 생성, 사용자 숫자 입력결과 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,34 @@
 
 import Foundation
 
-print("Hello, World!")
+enum RockPaperScissors: Int {
+    case scissors = 1
+    case rock = 2
+    case paper = 3
+    case etc
+}
+var userValue: RockPaperScissors = .etc
+var computerValue: RockPaperScissors = .etc
+typealias WhosWin = (computer: Bool, user: Bool, draw: Bool)
+var whosWin: WhosWin = (false, false, false)
 
+func printMenu() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+}
+
+func inputUserNumber() {
+    if let userNumber = readLine() {
+        switch userNumber {
+        case "1":
+            userValue = .scissors
+        case "2":
+            userValue = .rock
+        case "3":
+            userValue = .paper
+        case "0":
+            return
+        default:
+            printMenu()
+        }
+    }
+}
