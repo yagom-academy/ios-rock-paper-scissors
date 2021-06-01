a138cec74fb1474ca9818e2532903956f89807d6
soll4u <dasollpark@gmail.com>
2021-06-01 11:52:27 +0900

    feat: 게임을 시작하고 사용자의 입력을 받는 함수 playGame() 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,12 @@
 
 import Foundation
 
-print("Hello, World!")
+func playGame() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    guard let input = readLine(), let inputNumber = Int(input) else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return playGame()
+    }
+}
 
+playGame()
