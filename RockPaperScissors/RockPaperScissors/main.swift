793fc1a745f881db2f68a33af72b41dfc9d06908
hrjy6278 <hrjy6278@naver.com>
2021-06-01 12:17:20 +0900

    feat: 컴퓨터와 사용자의 값을 비교하여 출력하는 기능 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -12,6 +12,21 @@ func playGame() {
         print("잘못된 입력입니다. 다시 시도해주세요.")
         return playGame()
     }
+    let computerNumber = Int.random(in: 1...3)
+    switch (inputNumber, computerNumber) {
+    case (0, _):
+        print("게임 종료")
+    case (1,3), (3,2), (2,1):
+        print("이겼습니다!")
+    case (1,2), (2,3), (3,1):
+        print("졌습니다.")
+    case let (user,computer) where user == computer:
+        print("비겼습니다.")
+        playGame()
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        playGame()
+    }
 }
 
 playGame()
