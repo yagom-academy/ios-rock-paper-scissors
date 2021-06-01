dd991884e19bf0bb9f42e1ba125a0fb8448c2b7c
blanche37 <wkddbsghk16@gmail.com>
2021-06-01 17:21:26 +0900

    feat: 변수 computerResult, userResult 추가 및 구조체 RockScissorsPaper 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -5,7 +5,6 @@
 // 
 
 import Foundation
-
 enum Hand : Int, CaseIterable {
     case scissors = 1
     case rock = 2
@@ -18,24 +17,29 @@ enum Result {
     case lose
 }
 
-func choiceUserHand() -> Hand? {
-       let userInputArray: Array<Int> = [0, 1, 2, 3]
-       print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
-       guard let userInput = readLine(), let convertedUserInput = Int(userInput) else {
-           return choiceUserHand()
-       }
-
-       guard userInputArray.contains(convertedUserInput) else {
-           return choiceUserHand()
-       }
-
-       return Hand(rawValue: convertedUserInput)
-}
-
-func computerHands() -> Hand {
-       if let randomHand = Hand.allCases.randomElement() {
-           return randomHand
-       } else {
-           return computerHands()
-       }
+struct RockScissorsPaper {
+    private var computerResult: Hand?
+    private var userResult: Hand?
+    
+    private func choiceUserHand() -> Hand? {
+        let userInputArray: Array<Int> = [0, 1, 2, 3]
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+        guard let userInput = readLine(), let convertedUserInput = Int(userInput) else {
+            return choiceUserHand()
+        }
+        
+        guard userInputArray.contains(convertedUserInput) else {
+            return choiceUserHand()
+        }
+        
+        return Hand(rawValue: convertedUserInput)
+    }
+    
+    private func computerHands() -> Hand {
+        if let randomHand = Hand.allCases.randomElement() {
+            return randomHand
+        } else {
+            return computerHands()
+        }
+    }
 }
