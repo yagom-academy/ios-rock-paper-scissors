f6bf05dc4130ea8b9cf2508380afcefebb5202c7
ictechgy <ictechgy@gmail.com>
2021-06-01 18:42:48 +0900

    feat: 사용자의 입력값을 검증하는 함수 작성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -13,3 +13,12 @@ func generateComputerCard() -> Int {
 func receiveInputFromUser() -> String? {
     readLine()
 }
+
+func judgeUserInput() -> Int {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0>: ", terminator: "")
+    guard let userInput = receiveInputFromUser(), let userCard = Int(userInput), userCard > 3 && userCard < 0 else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return judgeUserInput()
+    }
+    return userCard
+}
