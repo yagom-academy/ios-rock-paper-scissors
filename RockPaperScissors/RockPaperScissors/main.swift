7efb9b79a49156da4b495ed4e72fea8f14a32b76
soll4u <dasollpark@gmail.com>
2021-06-01 12:51:13 +0900

    refactor: 열거형을 통해 텍스트 그룹화 및 input 값 유효성 검사 코드 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,25 +6,33 @@
 
 import Foundation
 
+enum Text: String {
+    case menu = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
+    case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
+    case gameOver = "게임 종료"
+    case userWin = "이겼습니다!"
+    case userLose = "졌습니다."
+    case draw = "비겼습니다."
+}
+
 func playGame() {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-    guard let input = readLine(), let inputNumber = Int(input) else {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
+    print(Text.menu.rawValue, terminator: "")
+    guard let input = readLine(), let inputNumber = Int(input), inputNumber >= 0 && inputNumber <= 3 else {
+        print(Text.wrongInput.rawValue)
         return playGame()
     }
+    
     let computerNumber = Int.random(in: 1...3)
+    print(computerNumber)
     switch (inputNumber, computerNumber) {
     case (0, _):
-        print("게임 종료")
+        print(Text.gameOver.rawValue)
     case (1,3), (3,2), (2,1):
-        print("이겼습니다!")
+        print(Text.userWin.rawValue)
     case (1,2), (2,3), (3,1):
-        print("졌습니다.")
-    case let (user,computer) where user == computer:
-        print("비겼습니다.")
-        playGame()
+        print(Text.userLose.rawValue)
     default:
-        print("잘못된 입력입니다. 다시 시도해주세요.")
+        print(Text.draw.rawValue)
         playGame()
     }
 }
