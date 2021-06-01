4072d18eb3cdf449353d09355e3bdf5516ab9504
Jiwon Yoon <jiwonyoon@Jiwonui-MacBookPro.local>
2021-06-01 11:56:05 +0900

    Feat: 출력함수들 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,4 +10,22 @@ enum RockPaperScissors: Int {
 	case Scissors = 1, Rock, Paper
 }
 
+func printMenu() {
+  print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+}
 
+func printWrongInput() {
+  print("잘못된 입력입니다. 다시 시도해주세요.")
+}
+
+func printWinMessage() {
+  print("이겼습니다!")
+}
+
+func printLoseMessage() {
+  print("졌습니다!")
+}
+
+func printTieMessage() {
+  print("비겼습니다!")
+}
