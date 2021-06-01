50aa4f01064231667c72a4f4d56ec6e478b70faf
Ellen <80380535+jcrescent61@users.noreply.github.com>
2021-06-01 14:03:20 +0900

    feat : 사용자메뉴 출력 및 인풋 바인딩

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,18 @@
 
 import Foundation
 
-print("Hello, World!")
+var userScissorsRockPaper: Int = 0
 
+let cissors = 1, rock = 2, paper = 3
+let cissorsRockPaper: Array<Int> = [cissors,rock,paper]
+
+// 입력 후 바인딩
+func bindingUserInput() -> Int {
+    while true {
+        print("가위(1),바위(2),보(3)!<종료 : 0>", terminator: " : ")
+        let userInput: String? = readLine()
+        if let unWrappedUserInput: String = userInput, let userInputNum: Int = Int(unWrappedUserInput) {
+            return userInputNum
+        }
+    }
+}
