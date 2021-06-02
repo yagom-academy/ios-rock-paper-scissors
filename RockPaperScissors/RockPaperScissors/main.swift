728ac7750c2b1fa3a4c0509a9d5c79655257cfd1
hrjy6278 <hrjy6278@naver.com>
2021-06-02 12:14:24 +0900

    refactor: CustomStringConvertible protocol을 열겨형 Message에 채택하여 코드 간소화

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,13 +6,17 @@
 
 import Foundation
 
-enum Message: String {
+enum Message: String, CustomStringConvertible {
     case menu = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
     case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
     case gameOver = "게임 종료"
     case userWin = "이겼습니다!"
     case userLose = "졌습니다."
     case draw = "비겼습니다."
+    
+    var description: String {
+        return self.rawValue
+    }
 }
 
 enum RockPaperScissors: Int {
@@ -25,9 +29,9 @@ func playGame() {
     var isGameOn: Bool = true
     
     while isGameOn {
-        print(Message.menu.rawValue, terminator: "")
+        print(Message.menu, terminator: "")
         guard let input = readLine(), let inputNumber = Int(input), (0...3).contains(inputNumber) else {
-            print(Message.wrongInput.rawValue)
+            print(Message.wrongInput)
             continue
         }
         
@@ -36,19 +40,19 @@ func playGame() {
         }
         
         guard let userHand: RockPaperScissors = RockPaperScissors(rawValue: inputNumber) else {
-            print(Message.gameOver.rawValue)
+            print(Message.gameOver)
             return
         }
         
         switch (userHand, computerHand) {
         case (.scissors, .paper), (.paper, .rock), (.rock, .scissors):
-            print(Message.userWin.rawValue)
+            print(Message.userWin)
             isGameOn = false
         case (.scissors, .rock), (.rock, .paper), (.paper, .scissors):
-            print(Message.userLose.rawValue)
+            print(Message.userLose)
             isGameOn = false
         default:
-            print(Message.draw.rawValue)
+            print(Message.draw)
             continue
         }
     }
