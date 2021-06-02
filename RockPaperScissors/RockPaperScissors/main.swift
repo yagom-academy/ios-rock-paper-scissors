4e7d6a0339768a6e61db40f35ab5ef60e751fef8
jsim27 <simjh27@gmail.com>
2021-06-02 20:36:04 +0900

    refactor: 전역변수 제거 및 함수의 매개변수 추가, 플로우차트 수정
    * 코드의 가독성을 위해 1, 2, 3으로 표현되던 손패를 Hand라는 enum을 생성해 값을 전달
    * show로 시작하는 메세지 출력 함수들을 이름에 맞게 직접 출력하는 기능으로 변경
    * 전역변수에 값을 저장하여 함수간 값을 주고받던 구조에서 전달인자를 통해 주고받는 구조로 변경
    * recieveInput() 함수에서 readLine()으로 입력받은 값을 옵셔널바인딩하는 코드 추가
    * 코딩 컨벤션에 어긋나는 일관적이지 않은 표현 수정
    * setComputerChoice() 함수를 기능에 맞게 generateComputerChoice()로 네이밍 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,74 +1,83 @@
-var userChoice: String = ""
-var computerChoice: Int = 0
-
-func startGame() -> Void {
-    print(showStartMessage(), terminator: "")
-    recieveInput()
-    if userChoice == "" {
-        return
-    }
-    judgeWinner()
-
+enum Hand: String {
+    case exitGame = "0"
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
 }
 
-func showStartMessage() -> String {
-    return "가위(1), 바위(2), 보(3)! <종료 : 0> : "
+func startGame() -> Void {
+    showStartMessage()
+}
+
+func showStartMessage() -> Void {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    recieveInput()
 }
 
 func recieveInput() -> Void {
-    let input = readLine()
-    testValid(of: input)
+    if let input = readLine() {
+        testValid(of: input)
+    } else {
+        showErrorMessage()
+    }
 }
 
-func testValid(of input: String?) {
+func testValid(of input: String) -> Void {
     switch input {
     case "0":
         print("게임 종료")
         return
     case "1":
-        userChoice = "1"
+        judgeWinner(userChoice: Hand.scissors)
     case "2":
-        userChoice = "2"
+        judgeWinner(userChoice: Hand.rock)
     case "3":
-        userChoice = "3"
+        judgeWinner(userChoice: Hand.paper)
     default:
-        print(showErrorMessage())
-        startGame()
+        showErrorMessage()
     }
 }
 
-func showErrorMessage() -> String {
-    return "잘못된 입력입니다. 다시 시도해주세요."
+func showErrorMessage() -> Void {
+    print("잘못된 입력입니다. 다시 시도해주세요.")
+    showStartMessage()
 }
 
-func judgeWinner() -> Void {
-    setComputerChoice()
+func judgeWinner(userChoice: Hand) -> Void {
+    let computerChoice = generateComputerChoice()
 
-    if userChoice == String(computerChoice) {
+    if userChoice == computerChoice {
         print("비겼습니다!")
-        startGame()
+        showStartMessage()
     } else {
-        pickWinner()
-        startGame()
+        pickWinner(userChoice: userChoice, computerChoice: computerChoice)
     }
 }
 
-func setComputerChoice() {
-    computerChoice = Int.random(in: 1...3)
+func generateComputerChoice() -> Hand {
+    let randomNumber = Int.random(in: 1...3)
+    
+    if randomNumber == 1 {
+       return Hand.scissors
+    } else if randomNumber == 2 {
+       return Hand.rock
+    } else {
+       return Hand.paper
+    }
 }
 
-func pickWinner() -> Void{
+func pickWinner(userChoice: Hand, computerChoice: Hand) -> Void {
     switch userChoice {
-    case "1" where (computerChoice == 3):
+    case .scissors where (computerChoice == .paper):
         print("이겼습니다!")
-    case "2" where (computerChoice == 1):
+    case .rock where (computerChoice == .scissors):
         print("이겼습니다!")
-    case "3" where (computerChoice == 2):
+    case .paper where (computerChoice == .rock):
         print("이겼습니다!")
     default:
         print("졌습니다!")
     }
-
+    showStartMessage()
 }
 
 startGame()
