2796f7fd17cace2b76146df3483f4b49f8ed4532
filmcanister <filmcanister97@gmail.com>
2021-06-02 11:00:44 +0900

    feat RPS's functions

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -14,9 +14,15 @@ class RPS {
         case paper = 3
     }
     
+    enum GameState: Int {
+        case continueGame = 1
+        case endGame = 0
+        case errorGame = -1
+    }
+    
     var usersHand = Hand.rock
     
-    func userInput() -> Hand {
+    func userInput() -> Int {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
         
         guard let input = Int(readLine() ?? "") , input >= 0 && input <= 3 else {
@@ -24,9 +30,43 @@ class RPS {
             return userInput()
         }
         
-        switch input {
-        case 0 : return
-        
+        return input
+    }
+    
+    func convertInput(hand userHand: Int) -> Hand? {
+        switch userHand {
+        case 1, 2, 3:
+            return Hand(rawValue: userHand)
+        default:
+            return nil
         }
     }
+    
+    func matchOutcome(hand userHand: Hand?) -> GameState {
+        guard let hand: Hand = userHand else {
+            return GameState.errorGame
+        }
+        let computerHand = Hand(rawValue: Int.random(in: 1...3))
+        if  (hand == Hand.scissors && computerHand == Hand.rock) ||
+            (hand == Hand.rock && computerHand == Hand.paper) ||
+            (hand == Hand.paper && computerHand == Hand.scissors) {
+            print("졌습니다!")
+        } else {
+            print("이겼습니다!")
+        }
+        
+        return GameState.continueGame
+    }
+    
+    func startGame() {
+        var gameContinue = true
+        while gameContinue {
+            var userInput = userInput()
+            var userHand = convertInput(hand: userInput)
+            gameContinue = userHand == nil
+        }
+    }
+    
+    
+    
 }
