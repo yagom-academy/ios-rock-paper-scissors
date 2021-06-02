c76a1f801c75d173c9fd0d2e759082a981fc8e41
YesCoach <sbsii1020@gmail.com>
2021-06-02 23:30:03 +0900

    refactor: 열거형 타입 두가지 모두 구조체 내부로 위치 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -5,32 +5,33 @@
 //
 
 import Foundation
-enum Hand : Int, CaseIterable {
-    case scissors = 1
-    case rock = 2
-    case paper = 3
-}
-
-enum Result: String {
-    case win = "이겼습니다!"
-    case draw = "비겼습니다!"
-    case lose = "졌습니다!"
-    
-    static func compareHand(_ user: Hand, with computer: Hand) -> Result {
-        switch (user, computer) {
-        case let (x, y) where x == y:
-            return .draw
-        case (.scissors, let x):
-            return x == .rock ? .lose : .win
-        case (.rock, let x):
-            return x == .paper ? .lose : .win
-        case (.paper, let x):
-            return x == .scissors ? .lose : .win
-        }
-    }
-}
 
 struct RockScissorsPaper {
+    private enum Hand : Int, CaseIterable {
+        case scissors = 1
+        case rock = 2
+        case paper = 3
+    }
+    
+    private enum Result: String {
+        case win
+        case draw
+        case lose
+        
+        static func compareHand(_ user: Hand, with computer: Hand) -> Result {
+            switch (user, computer) {
+            case let (x, y) where x == y:
+                return .draw
+            case (.scissors, let x):
+                return x == .rock ? .lose : .win
+            case (.rock, let x):
+                return x == .paper ? .lose : .win
+            case (.paper, let x):
+                return x == .scissors ? .lose : .win
+            }
+        }
+    }
+    
     private func choiceUserHand() -> Hand? {
         let userInputArray: Array<Int> = [0, 1, 2, 3]
         print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
@@ -43,19 +44,24 @@ struct RockScissorsPaper {
         return Hand(rawValue: userInput)
     }
 
-    private func computerHands() -> Hand {
+    private func computerHand() -> Hand {
         if let randomHand = Hand.allCases.randomElement() {
             return randomHand
         } else {
-            return computerHands()
+            return computerHand()
         }
     }
 
     private func compare(userHand: Hand, computerHand: Hand) {
         let result = Result.compareHand(userHand, with: computerHand)
-        print(result.rawValue)
-        if result == .draw {
-            startGame()
+        print("사용자: \(userHand), 컴퓨터: \(computerHand), 결과: \(result)")
+        switch result {
+        case .draw:
+            print("비겼습니다")
+        case .win:
+            print("이겼습니다")
+        case .lose:
+            print("졌습니다")
         }
     }
 
@@ -64,8 +70,7 @@ struct RockScissorsPaper {
             print("게임 종료")
             return
         }
-
-        compare(userHand: userHand, computerHand: computerHands())
+        compare(userHand: userHand, computerHand: computerHand())
     }
 }
 
