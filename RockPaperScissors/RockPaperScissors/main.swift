838b0e2c7f7129690ee70cadf8a634fc7c819973
Ellen <80380535+jcrescent61@users.noreply.github.com>
2021-06-02 13:32:44 +0900

    feat: 함수 수정 및 변수명 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,9 +1,9 @@
+
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
-
+//
 import Foundation
 
 let youWin = "이겼습니다!", youLose = "졌습니다!", draw = "비겼습니다!"
@@ -11,23 +11,24 @@ let youWin = "이겼습니다!", youLose = "졌습니다!", draw = "비겼습니
 let stop = 0, scissors = 1, rock = 2, paper = 3
 let scissorsRockPaper: Array<Int> = [stop,scissors,rock,paper]
 
-func bindingUserInput() -> Int {
-    while true {
-        print("가위(1),바위(2),보(3)!<종료 : 0>", terminator: " : ")
-        let userInput: String? = readLine()
-        if let unWrappedUserInput: String = userInput, let userInputNum: Int = Int(unWrappedUserInput) {
-            return userInputNum
-        }
-        print("잘못된 입력입니다.")
+func unWrappedUserInput(_ userInput: String?) -> Int {
+    if let unWrappedUserInput: String = userInput, let userInputNum: Int = Int(unWrappedUserInput) {
+        return userInputNum
     }
+    return -1
+}
+
+func printMenu() -> String?{
+    print("가위(1),바위(2),보(3)!<종료 : 0>", terminator: " : ")
+    let userInput: String? = readLine()
+    return userInput
 }
 
 func makeRandomComputerOutput() -> Int {
-    let output = scissorsRockPaper[Int.random(in: 1...3)]
-    return output
+    return scissorsRockPaper[Int.random(in: 1...3)]
 }
 
-func userVsComputer(user: Int, computer: Int) {
+func judgeGame(user: Int, computer: Int) {
     switch (user, computer) {
     case (scissors, scissors), (rock, rock), (paper, paper):
         print(draw)
@@ -38,18 +39,26 @@ func userVsComputer(user: Int, computer: Int) {
     }
 }
 
-func gameStart() {
-    while true {
-        let user = bindingUserInput()
-        if 1 <= user && user <= 3 {
-            userVsComputer(user: user, computer: makeRandomComputerOutput())
-        } else if user == 0 {
-            print("게임종료")
-            return
-        } else {
-            print("잘못된 입력입니다.")
-        }
+func checkUserInput(_ bindingUserInput: Int) -> Bool{
+    if 1 <= bindingUserInput && bindingUserInput <= 3 {
+        judgeGame(user: bindingUserInput, computer: makeRandomComputerOutput())
+    } else if bindingUserInput == 0 {
+        print("게임종료")
+        return false
+    } else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
     }
+    return true
+}
+
+
+func gameStart() {
+    var check = true
+    repeat {
+        let menu = printMenu()
+        let user = unWrappedUserInput(menu)
+        check = checkUserInput(user)
+    } while check
 }
 
 gameStart()
