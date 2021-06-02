ce182580704a18970611252b09a3c522ded80dad
hosinging <pdhou23@gmail.com>
2021-06-02 18:09:01 +0900

    refactor: sanitize함수를 inputFromUser함수의 guard문안에서 해결하는 방식으로 통합

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -32,23 +32,13 @@ enum Winner {
 }
 
 struct Game {
-    func sanitizeNumber(from userInput: Int) -> Int {
-        switch userInput {
-        case 0...3:
-            return userInput
-        default:
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            return inputFromUser()
-        }
-    }
-    
     func inputFromUser() -> Int {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-        guard let input = readLine(), let number = Int(input) else {
+        guard let input = readLine(), let number = Int(input), (0...3).contains(number) else {
             print("잘못된 입력입니다. 다시 시도해주세요.")
             return inputFromUser()
         }
-        return sanitizeNumber(from: number)
+        return number
     }
     
     func whoIsWinner(userChoice: RockScissorsPaper , computerChoice: RockScissorsPaper) -> Winner {
