a056676133fc446b420a6c5e580a029599773478
soo941226 <rhrnakrnakr@naver.com>
2021-06-02 10:31:51 +0900

    feat rewrite RPS

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,80 +6,27 @@
 
 import Foundation
 
-class IOMachine{
-    func readInt()->Int{
-        guard let input:String = readLine() else {
-            return -1
-        }
-        
-        guard let int:Int = Int(input) else {
-            return -1
-        }
-        
-        return int
-    }
-}
-
-class RPSMachine{
-    private let ioMachine = IOMachine()
+class RPS {
     
-    private enum RPSState : Int{
-        case continueGame = 1
-        case endGame = 0
-        case error = -1
+    enum Hand : Int {
+        case scissors = 1
+        case rock = 2
+        case paper = 3
     }
     
-    private func vaildateWin(RPSType type:Int){
-        
-        let computerRPSType = Int.random(in: 1...3)
-        
-        guard type != computerRPSType else {
-            print("비겼습니다")
-            return
-        }
-        
-        if  (type == 1 && computerRPSType == 2)
-        ||  (type == 2 && computerRPSType == 3)
-        ||  (type == 3 && computerRPSType == 1) {
-            print("졌습니다")
-        } else {
-            print("이겼습니다")
-        }
-        
-    }
+    var usersHand = Hand.rock
     
-    private func checkUserInput() -> RPSState {
-        let watingMessage = "가위(1), 바위(2), 보(3)!<종료 : 0>"
-        let errorMessage = "잘못된 입력입니다"
+    func userInput() -> Hand {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
         
-        print(watingMessage, terminator: " ")
-        
-        let userInput = ioMachine.readInt()
-        
-        guard 0...3 ~= userInput else {
-            print(errorMessage)
-            
-            return RPSState.error
+        guard let input = Int(readLine() ?? "") , input >= 0 && input <= 3 else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            return userInput()
         }
         
-        if userInput == 0  {
-            return RPSState.endGame
-        } else {
-            self.vaildateWin(RPSType: userInput)
-            
-            return RPSState.continueGame
-        }
-
-    }
-    
-    func gameStart(){
-        var isEnd = false
+        switch input {
+        case 0 : return
         
-        while isEnd == false {
-            isEnd = self.checkUserInput() == RPSState.endGame
         }
     }
 }
-
-RPSMachine().gameStart()
-
