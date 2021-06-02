03a1c541262fde3c575efdc315a8c169587a970e
brian2931 <sem789456@gmail.com>
2021-06-02 14:38:55 +0900

    feat: 사용자 입력값 받는 기능 구현
    * 사용자 입력값 readline()을 guard let으로 optional binding 후 전역변수에 저장
    * 입력받은 사용자 값이 0이면 true를 반환하는 함수 구현
    * 전체 게임의 흐름을 진행하는 startGame() 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,34 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-print("Hello, World!")
+var userNumber: Int = 0
+var computerNumber: Int = 0
 
+func startGame() {
+    userNumber = userInput()
+    
+    if isEnd() {
+        return
+    } else {
+        generateComputerNumber()
+        compareNumber()
+    }
+}
+
+func userInput() -> Int {
+    print("가위(1), 바위(2), 보(3), 종료(0)", terminator: "")
+    
+    guard let stringInput = readLine(), let intInput = Int(stringInput) else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return userInput()
+    }
+    
+    return Int(intInput)
+}
+
+func isEnd() -> Bool {
+    if userNumber == 0 {
+        print("게임 종료")
+        return true
+    }
+    return false
+}
