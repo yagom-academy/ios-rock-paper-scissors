e700f3bb40d29697e2d874f90aa8861b1ed02dae
YesCoach <sbsii1020@gmail.com>
2021-06-03 10:44:17 +0900

    feat: 열거형 타입 Turn 추가 및 Compare 함수에 묵찌빠 기능 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -12,12 +12,12 @@ struct RockScissorsPaper {
         case rock = 2
         case paper = 3
     }
-    
+
     private enum Result: CustomStringConvertible {
         case win
         case draw
         case lose
-        
+
         var description: String {
             switch self {
             case .win:
@@ -28,7 +28,7 @@ struct RockScissorsPaper {
                 return "졌습니다!"
             }
         }
-        
+ 
         static func compareHand(_ user: Hand, with computer: Hand) -> Result {
             switch (user, computer) {
             case let (x, y) where x == y:
@@ -43,6 +43,22 @@ struct RockScissorsPaper {
         }
     }
     
+    private enum Turn: CustomStringConvertible {
+        case userTurn
+        case computerTurn
+        
+        var description: String {
+            switch self {
+            case .userTurn:
+                return "사용자"
+            case .computerTurn:
+                return "컴퓨터"
+            }
+        }
+    }
+    
+    private var gameTurn: Turn?
+
     private func choiceUserHand() -> Hand? {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
         let userInputArray: Array<Int> = [0, 1, 2, 3]
@@ -62,19 +78,60 @@ struct RockScissorsPaper {
         }
     }
 
-    private func compare(userHand: Hand, computerHand: Hand) {
+    private mutating func compare(userHand: Hand, computerHand: Hand) {
         let result = Result.compareHand(userHand, with: computerHand)
         switch result {
         case .draw:
             print(Result.draw)
+            if let turn = gameTurn {
+                print(turn,"의 승리!", separator: "")
+                return
+            } else {
+                startGame()
+            }
+            
         case .win:
             print(Result.win)
+            if var turn = gameTurn {
+                switch turn {
+                case .userTurn:
+                    turn = .computerTurn
+                    
+                case .computerTurn:
+                    turn = .userTurn
+                }
+                print("[\(turn)턴]",terminator: "")
+                gameTurn = turn
+                startGame()
+            } else {
+                gameTurn = .userTurn
+                print("[\(Turn.userTurn)턴]",terminator: "")
+                startGame()
+            }
+            
         case .lose:
             print(Result.lose)
+            if var turn = gameTurn {
+                switch turn {
+                case .userTurn:
+                    turn = .computerTurn
+                case .computerTurn:
+                    turn = .userTurn
+                }
+                print("[\(turn)턴]",terminator: "")
+                gameTurn = turn
+                startGame()
+            } else {
+                gameTurn = .computerTurn
+                print("[\(Turn.computerTurn)턴]",terminator: "")
+                startGame()
+            }
         }
     }
 
-    func startGame() {
+    //묵찌빠 비교 함수 -> 해당 분기로 이동 1. 비기면 승리 출력하고 게임 종료, 2. 이기거나 질 경우 턴 넘기고 양쪽 손모양 생성 -> 묵찌빠 비교
+
+    mutating func startGame() {
         guard let userHand = choiceUserHand() else {
             print("게임 종료")
             return
@@ -85,4 +142,3 @@ struct RockScissorsPaper {
 
 var rockScissorsPaper = RockScissorsPaper()
 rockScissorsPaper.startGame()
-
