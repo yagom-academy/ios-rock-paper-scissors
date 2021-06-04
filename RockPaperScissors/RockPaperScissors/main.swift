bdacfaf773abd7dff10578dfd6b14eb865d0a794
hosinging <pdhou23@gmail.com>
2021-06-04 11:28:39 +0900

    feat: 가위바위보 게임과 묵찌빠 게임의 결과 메세지 출력 구현
    - RockScissorsPaper를 프로젝트 명과 동일하게RockPaperScissors로 통일
    - Winner Enum삭제
    - Turn Enum의 Computed Property를 rawValue로 대체
    - returnRockPaperScissorsWinner함수와 returnMukjjibaWinner함수를 playRockPaperScissors함수와 playMukjjibba함수로 대체
    - 기존의 playRound함수의 일부 내용을 start함수로 옮기고 함수명을 play로 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,52 +6,16 @@
 
 import Foundation
 
-enum RockScissorsPaper: Int {
+enum RockPaperScissors: Int {
     case scissors = 1
     case rock
     case paper
 }
 
-enum Winner {
-    case user
-    case computer
-    case tie
-    
-    var resultMessage: String {
-        switch self {
-        case .user:
-            return "이겼습니다!"
-        case .computer:
-            return "졌습니다!"
-        case .tie:
-            return "비겼습니다!"
-        }
-    }
-}
-
-enum Turn {
-    case none
-    case user
-    case computer
-    
-    var turnString: String {
-        switch self {
-        case .user:
-            return "사용자"
-        case .computer:
-            return "컴퓨터"
-        default:
-            return ""
-        }
-    }
-    
-    func convertToWinner() -> Winner {
-        if self == .user {
-            return Winner.user
-        } else {
-            return Winner.computer
-        }
-    }
+enum Turn: String {
+    case notDecided = ""
+    case user = "사용자"
+    case computer = "컴퓨터"
 }
 
 struct Game {
@@ -61,19 +25,18 @@ struct Game {
         case lose
     }
     
-    var turn = Turn.none
+    var turn = Turn.notDecided
     
     func isValid(number: Int) -> Bool {
         let exitNumber = 0
-        print("현재", number)
-        return RockScissorsPaper(rawValue: number) != nil || number == exitNumber
+        return RockPaperScissors(rawValue: number) != nil || number == exitNumber
     }
 
     func printInputMessage() {
-        if turn == .none {
+        if turn == .notDecided {
             print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
         } else {
-            print("[\(turn.turnString) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
+            print("[\(turn.rawValue) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
         }
     }
 
@@ -85,71 +48,68 @@ struct Game {
         }
         return number
     }
-
-    mutating func returnRockPaperScissorsWinner(userChoice: RockScissorsPaper ,computerChoice: RockScissorsPaper) -> Winner {
+    
+    mutating func playRockPaperScissors(userChoice: RockPaperScissors ,computerChoice: RockPaperScissors) -> String {
         let valueDifference = (userChoice.rawValue - computerChoice.rawValue + 3) % 3
+        var resultMessage: String
         switch valueDifference {
         case ValueDifference.win.rawValue:
             turn = .user
-            return Winner.user
+            resultMessage = "이겼습니다!"
         case ValueDifference.lose.rawValue:
             turn = .computer
-            return Winner.computer
+            resultMessage = "졌습니다!"
         default:
-            turn = .none
-            return Winner.tie
+            turn = .notDecided
+            resultMessage = "비겼습니다!"
         }
+        return resultMessage
     }
     
-    mutating func returnMukjjibbaWinner(userChoice: RockScissorsPaper, computerChoice: RockScissorsPaper) -> Winner {
+    mutating func playMukjjibba(userChoice: RockPaperScissors, computerChoice: RockPaperScissors) -> String {
         let valueDifference = (userChoice.rawValue - computerChoice.rawValue + 3) % 3
+        var resultMessage: String
         switch valueDifference {
         case ValueDifference.win.rawValue:
             turn = .user
-            return Winner.tie
+            resultMessage = "\(turn.rawValue)의 턴입니다!"
         case ValueDifference.lose.rawValue:
             turn = .computer
-            return Winner.tie
+            resultMessage = "\(turn.rawValue)의 턴입니다!"
         default:
-            let winner = turn.convertToWinner()
-            turn = .none
-            return winner
+            resultMessage = "\(turn.rawValue)의 승리!"
+            turn = .notDecided
         }
+        return resultMessage
     }
     
-    func printGameResult(winner result: Winner) {
-        print(result.resultMessage)
-    }
-    
-    mutating func playRound(userInput: Int) {
-        guard let userChoice = RockScissorsPaper(rawValue: userInput),
-              let computerChoice = RockScissorsPaper(rawValue: Int.random(in: 1...3))
-        else {
-            return
-        }
-        let result: Winner
-        if turn == .none {
-            result = returnRockPaperScissorsWinner(userChoice: userChoice, computerChoice: computerChoice)
+    mutating func play(userChoice: RockPaperScissors, computerChoice: RockPaperScissors) {
+        let resultMessage: String
+        if turn == .notDecided {
+            resultMessage = playRockPaperScissors(userChoice: userChoice, computerChoice: computerChoice)
         } else {
-            result = returnMukjjibbaWinner(userChoice: userChoice, computerChoice: computerChoice)
+            resultMessage = playMukjjibba(userChoice: userChoice, computerChoice: computerChoice)
         }
-        
-        printGameResult(winner: result)
-        start()
+        print(resultMessage)
     }
-    
+
     func isGameEnd(userInput: Int) -> Bool {
         return userInput == 0
     }
     
     mutating func start() {
-        print(turn)
         let userInput = inputFromUser()
         if isGameEnd(userInput: userInput) {
             print("게임 종료")
             return
         }
-        playRound(userInput: userInput)
+        guard let userChoice = RockPaperScissors(rawValue: userInput),
+              let computerChoice = RockPaperScissors(rawValue: Int.random(in: 1...3))
+        else {
+            return
+        }
+        play(userChoice: userChoice, computerChoice: computerChoice)
+        start()
     }
 }
 
