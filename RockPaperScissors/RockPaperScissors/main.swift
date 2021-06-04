8831437d5c8e2e22fd2172483d44b4ac2136f2a5
blanche37 <wkddbsghk16@gmail.com>
2021-06-04 17:13:29 +0900

    feat: 열거형 GameError 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -11,3 +11,13 @@ fileprivate enum Message {
     static let invaild = "잘못된 입력입니다. 다시 시도해주세요."
 }
 
+fileprivate enum GameError: Error, CustomStringConvertible {
+    case emptyCollection
+    
+    var description: String {
+        switch self {
+        case .emptyCollection:
+            return "열거형이 비어있어요"
+        }
+    }
+}
