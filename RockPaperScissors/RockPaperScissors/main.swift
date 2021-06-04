428f908483cd49f3bd7462f25498c30b9ab3d56d
blanche37 <wkddbsghk16@gmail.com>
2021-06-04 17:16:22 +0900

    feat: 열거형 Turn 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -24,38 +24,60 @@ fileprivate enum GameError: Error, CustomStringConvertible {
 
 class RockScissorsPaper {
     fileprivate enum Hand : Int, CaseIterable {
-            case scissors = 1
-            case rock = 2
-            case paper = 3
+        case scissors = 1
+        case rock = 2
+        case paper = 3
     }
     
     fileprivate enum Result: CustomStringConvertible {
-           case win
-           case draw
-           case lose
-           
-           var description: String {
-               switch self {
-               case .win:
-                   return "이겼습니다!"
-               case .draw:
-                   return "비겼습니다!"
-               case .lose:
-                   return "졌습니다!"
-               }
-           }
-           
-           static func compareHand(_ user: Hand, with computer: Hand) -> Result {
-               switch (user, computer) {
-               case let (x, y) where x == y:
-                   return .draw
-               case (.scissors, let x):
-                   return x == .rock ? .lose : .win
-               case (.rock, let x):
-                   return x == .paper ? .lose : .win
-               case (.paper, let x):
-                   return x == .scissors ? .lose : .win
-               }
-           }
-       }
+        case win
+        case draw
+        case lose
+        
+        var description: String {
+            switch self {
+            case .win:
+                return "이겼습니다!"
+            case .draw:
+                return "비겼습니다!"
+            case .lose:
+                return "졌습니다!"
+            }
+        }
+        
+        static func compareHand(_ user: Hand, with computer: Hand) -> Result {
+            switch (user, computer) {
+            case let (x, y) where x == y:
+                return .draw
+            case (.scissors, let x):
+                return x == .rock ? .lose : .win
+            case (.rock, let x):
+                return x == .paper ? .lose : .win
+            case (.paper, let x):
+                return x == .scissors ? .lose : .win
+            }
+        }
+    }
+    
+    fileprivate enum Turn: CustomStringConvertible {
+        case userTurn
+        case computerTurn
+        
+        var description: String {
+            switch self {
+            case .userTurn:
+                return "사용자"
+            case .computerTurn:
+                return "컴퓨터"
+            }
+        }
+        
+        static func convert(_ turn: Turn) -> Turn {
+            if turn == .userTurn {
+                return .computerTurn
+            } else {
+                return .userTurn
+            }
+        }
+    }
 }
