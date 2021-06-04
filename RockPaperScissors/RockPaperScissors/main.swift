8b01d5892ddf2be150f5db79d5aaca41ab1e750f
Ellen <80380535+jcrescent61@users.noreply.github.com>
2021-06-04 14:47:57 +0900

    fix: 0 입력 받아도 끝나지 않는 문제 수정.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,160 +7,103 @@
 import Foundation
 
 let youWin = "이겼습니다!", youLose = "졌습니다!", draw = "비겼습니다!"
-let muk = 4, chi = 5, pa = 6
+let muk = 1, chi = 2, pa = 3
 let stop = 0, scissors = 1, rock = 2, paper = 3
 let scissorsRockPaper: Array<Int> = [stop,scissors,rock,paper]
 let mukChiPa: Array<Int> = [stop,muk,chi,pa]
 var attacker: String = ""
 let userTurn = "사용자", computerTurn = "컴퓨터"
 
-func printMenu() -> String?{
+func inputScissorsRockPaper()->Int{
     print("가위(1),바위(2),보(3)!<종료 : 0>", terminator: " : ")
-    let userInput: String? = readLine()
-    return userInput
-}
-
-func unWrappedUserInput(_ userInput: String?) -> Int {
-    if let unWrappedUserInput: String = userInput, let userInputNum: Int = Int(unWrappedUserInput) {
-        return userInputNum
+    guard let someInput = userInput(),let num = Int(someInput), num>=0 && num<=3 else {
+        print("잘못된 입력입니다. 다시 시도해주세요")
+        return inputScissorsRockPaper()
     }
-    return -1
-}
-
-func checkUserInput(_ checkUserInput: Int) -> Bool{
-    if 1 <= checkUserInput && checkUserInput <= 3 {
-        judgeGame(user: checkUserInput, computer: makeRandomComputerOutput())
-    } else if checkUserInput == 0 {
-        print("게임종료")
-        return false
-    } else {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-    }
-    return true
-}
-
-func gameStart() {
-    var check = true
-    repeat {
-        let menu = printMenu()
-        let user = unWrappedUserInput(menu)
-        check = checkUserInput(user)
-    } while check
+    return num
 }
 
 func makeRandomComputerOutput() -> Int {
     return scissorsRockPaper[Int.random(in: 1...3)]
 }
- 
-func judgeGame(user: Int, computer: Int) {
+
+func judgeGame(user: Int, computer: Int) -> String {
     switch (user, computer) {
-    case (scissors, scissors), (rock, rock), (paper, paper):
-<<<<<<< HEAD
-        print(draw)
-    case (scissors, rock), (rock, paper), (paper, scissors):
-        attacker = computerTurn
-    case (scissors, paper), (rock, scissors), (paper, rock):
-        attacker = userTurn
-    case (muk, muk), (chi, chi), (pa, pa):
-        print("\(attacker)의 승리!")
-    default :
-        attacker = userTurn
-=======
-        print(draw) // 여기서 함수로 보내야할것같은뎅
     case (scissors, rock), (rock, paper), (paper, scissors):
         print(youLose)
         attacker = computerTurn
+        return attacker
     case (scissors,paper), (rock,scissors), (paper,rock):
         print(youWin)
         attacker = userTurn
+        return attacker
+    default :
+        print(draw)
+        return draw
+
+    }
+}
+
+func judgeMukChiPa(user: Int, computer: Int, attack: String) -> String {
+    var attacker: String = attack
+    switch (user, computer) {
     case (chi,muk), (muk,pa), (pa,chi):
         attacker = computerTurn
+        return attacker
     case (chi,pa), (muk,chi), (pa,muk):
         attacker = userTurn
-    default :
-        print("\(attacker)의 승리!")
->>>>>>> ac3e8123f2c47a5b017f6355b4f11099adbfc83d
-    }
-}
-
-gameStart()
-
-// ------------------------------------------
-<<<<<<< HEAD
-var attacker: String = ""
-let userTurn = "사용자", computerTurn = "컴퓨터"
-=======
->>>>>>> ac3e8123f2c47a5b017f6355b4f11099adbfc83d
-
-func convertMukChiPa(_ someInt: Int) -> Int {
-    var convertMukChiPa = someInt
-    switch convertMukChiPa {
-    case scissors:
-        convertMukChiPa = chi
-    case rock:
-        convertMukChiPa = muk
+        return attacker
     default:
-        convertMukChiPa = pa
+        return draw
     }
-    return convertMukChiPa
 }
 
-<<<<<<< HEAD
-=======
-func convertScissorsRockPaper(_ someInt: Int) -> Int {
-    var convertMukChiPa = someInt
-    switch convertMukChiPa {
-    case scissors:
-        convertMukChiPa = chi
-    case rock:
-        convertMukChiPa = muk
-    default:
-        convertMukChiPa = pa
-    }
-    return convertMukChiPa
+func userInput() -> String?{
+    return readLine()
 }
 
-func changeAttaker(_ someTurn: String) -> String {
-    var changeAttaker = someTurn
-    if someTurn == userTurn {
-        changeAttaker = userTurn
-    } else {
-        changeAttaker = computerTurn
-    }
-    return changeAttaker
-}
-
->>>>>>> ac3e8123f2c47a5b017f6355b4f11099adbfc83d
-func mukChiPaOutput() -> String? {
+func inputMukChiPa()->Int{
     print("[\(attacker) 턴] 묵(1),찌(2),빠(3)!<종료 : 0>", terminator: " : ")
-    let userInput: String? = readLine()
-    return userInput
-} //unWrappedUserInput 재사용가능?
+    guard let someInput = userInput(),let num = Int(someInput), num>=0 && num<=3 else {
+        print("잘못된 입력입니다. 다시 시도해주세요")
+        attacker = computerTurn
+        return inputMukChiPa()
+    }
+    return num
+}
+
 func makeRandomMukChiPa() -> Int {
     return mukChiPa[Int.random(in: 1...3)]
 }
 
-<<<<<<< HEAD
-func judgeMukChiPa(user: Int, computer: Int) {
-    if user == computer {
-        return print("\(attacker)승리!")
+func gameStart() {
+    while true {
+        var user = inputScissorsRockPaper()
+        if user == 0 {
+            print("게임종료")
+            user = 0
+            break
+        }
+        let computer = makeRandomComputerOutput()
+        let result = judgeGame(user: user, computer: computer)
+        if result != draw {
+            break
+        }
+    }
+
+    while true {
+        let user = inputMukChiPa()
+        let computer = makeRandomMukChiPa()
+        if user == 0 {
+            print("게임종료")
+            break
+        }
+        let result = judgeMukChiPa(user: user, computer: computer, attack: attacker)
+        if result == draw {
+            print("\(attacker)의 승리!")
+            break
+        }
     }
 }
 
-func startMukChiPa() {
-    
-}
-=======
-func inputMukChiPa()->Int{
-    guard let inputinput = input(),let num = Int(inputinput), num>=0 && num<=3 else {
-        print("잘못된 입력입니다. 다시 시도해주세요")
-        return inputMukChiPa()
-    }
-    return num + 3
-}
-
-func input() -> String?{
-    return readLine()
-}
-
->>>>>>> ac3e8123f2c47a5b017f6355b4f11099adbfc83d
+gameStart()
