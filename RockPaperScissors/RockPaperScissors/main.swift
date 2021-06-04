cbefc9d9b471e609dec30dc4f77f65d5635d3595
soo941226 <rhrnakrnakr@naver.com>
2021-06-04 11:04:50 +0900

    feat to add types of GameState

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -15,21 +15,28 @@ class RockPaperScissors {
     }
     
     enum GameState {
-        case continueGame
+        case userWin
+        case computerWin
+        case drawGame
         case endGame
     }
     
     func setUsersHand() -> Hand {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-        
+
         guard let input = readLine(),
               let value = Int(input),
               let usersHand = Hand(rawValue: value) else {
             print("잘못된 입력입니다. 다시 시도해주세요.")
             return setUsersHand()
         }
-        
+
         return usersHand
+    }
+    
+    func step2SetUserHand() -> Hand {
+        print("[*** 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :")
+        
     }
     
     func matchOutcome(of userHand: Hand) -> GameState {
@@ -41,24 +48,29 @@ class RockPaperScissors {
         
         switch Int.random(in: 1...3) {
         case 1:
-            computerHand = .rock
-        case 2:
-            computerHand = .paper
-        default:
             computerHand = .scissors
+        case 2:
+            computerHand = .rock
+        default:
+            computerHand = .paper
         }
+        
+        var outcome: GameState
         
         if (userHand == .scissors && computerHand == .rock) ||
             (userHand == .rock && computerHand == .paper) ||
             (userHand == .paper && computerHand == .scissors) {
             print("졌습니다!")
+            outcome = .computerWin
         } else if userHand == computerHand {
             print("비겼습니다!")
+            outcome = .drawGame
         } else {
             print("이겼습니다!")
+            outcome = .userWin
         }
         
-        return .continueGame
+        return outcome
     }
     
     func startGame() {
