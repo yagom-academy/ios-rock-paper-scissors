d7e4d731ba7e37a8a69709e12d5c97471383bdb2
JT <dd3557@naver.com>
2021-10-11 20:15:56 +0900

    feat: Sign 타입 추가
    - scissors, rock, paper 값 추가
    - 각 값을 이기는 counter 추가
    - 랜덤한 Sign 값을 생성하는 타입 메소드generateRandom()  추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,26 @@
 
 import Foundation
 
-print("Hello, World!")
-
+enum Sign: Int {
+    case scissors = 1
+    case rock = 2
+    case paper = 3
+    
+    var counter: Self {
+        switch self {
+        case .scissors:
+            return .rock
+        case .paper:
+            return .scissors
+        case .rock:
+            return .paper
+        }
+    }
+    
+    static func generateRandom() -> Self {
+        guard let randomSign = Self(rawValue: Int.random(in: 1...3)) else {
+            fatalError("\(#function) 함수에서 랜덤값을 생성하는 데에 오류가 발생했습니다")
+        }
+        return randomSign
+    }
+}
