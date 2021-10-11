492efd0fa0be7250eb5c67d0f0cd7e8904ee2c4a
just1103 <hyojuson1103@gmail.com>
2021-10-11 18:57:23 +0900

    feat: 사용자 입력 받기 및 입력값 검증 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,28 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
+var playerInput: String = ""
 
-import Foundation
+func varifyInputValue(input: String) {
+    guard let inputNumber = Int(input), inputNumber > 0, inputNumber < 4 else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return
+    }
+}
 
-print("Hello, World!")
+func startReceiveInput() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    
+    guard let input: String = readLine(), input != "0" else {
+        print("게임 종료")
+        return
+    }
+    
+    playerInput = input
+}
 
+
+func startProgram() {
+    startReceiveInput()
+    varifyInputValue(input: playerInput)
+    
+}
+
+startProgram()
