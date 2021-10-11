aa7060b5eb7ebb3cd9880953bdca07eb1cf9c3b1
horeng2 <huski.a.a@gmail.com>
2021-10-11 18:16:35 +0900

    Feat : selectOption함수 및 enum Message 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,18 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-print("Hello, World!")
+
+enum Message: String {
+    case win = "이겼습니다!"
+    case draw = "비겼습니다!"
+    case lose = "졌습니다!"
+    case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
+}
+
+func selectOption() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
+    guard let option = readLine() else {
+        print(Message.wrongInput.rawValue)
+        return
+    }
+}
 
