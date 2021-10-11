0d04430e3a89ae336ee3f0b43706e5435b830936
junbangg <wnstjr4620@naver.com>
2021-10-11 21:12:36 +0900

    feat: 사용자의 입력/검증 기능 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,9 +2,44 @@
 //  RockPaperScissors - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
-print("Hello, World!")
+struct RockPaperScissorsGame {
+    
+    func validate(playerNumber: String) -> Bool {
+        let validInputs = ["0", "1", "2", "3"]
+        return validInputs.contains(playerNumber)
+    }
+    
+    func main() {
+        var playerNumber: String? = nil
+        var computerNumber = ""
+        repeat {
+            computerNumber = String(Int.random(in: 1...3))
+            playerNumber = getUserInput()
+        } while isDraw(computerNumber, playerNumber) || isWrong(playerNumber: playerNumber)
+    }
+    
+    private func isDraw(_ computerNumber: String, _ playerNumber: String?) -> Bool {
+        return computerNumber == playerNumber
+    }
+    
+    private func isWrong(playerNumber: String?) -> Bool {
+        return playerNumber == nil
+    }
+    
+    private func getUserInput() -> String? {
+        print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
+        if let userInput = readLine(), validate(playerNumber: userInput) {
+            return userInput
+        }
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return nil
+    }
+}
 
+let rockPaperScissors = RockPaperScissorsGame()
+
+rockPaperScissors.main()
