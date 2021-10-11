4e21f6b6c26438f3ffb8e04b19933e2f10ea6c0c
aCafela-coffee <seym7894@gmail.com>
2021-10-11 18:33:26 +0900

    feat: 사용자 입력을 받는 함수 추가
    readUserInput() 함수 추가 HandShape 열거형의 이름을 ExpectedUserInput로 변경
    ExpectedUserInput의 열거형의 원시값 타입을 String으로 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,11 +2,32 @@
 //  RockPaperScissors - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
-enum HandShape: Int {
-    case scissors = 1
-    case rock = 2
-    case paper = 3
+enum ExpectedUserInput: String {
+    case exit = "0"
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
 }
+
+func readUserInput() -> ExpectedUserInput? {
+    let input = readLine()
+    
+    switch input {
+    case nil:
+        return nil
+    case ExpectedUserInput.exit.rawValue:
+        return .exit
+    case ExpectedUserInput.scissors.rawValue:
+        return .scissors
+    case ExpectedUserInput.rock.rawValue:
+        return .rock
+    case ExpectedUserInput.paper.rawValue:
+        return .paper
+    default:
+        return nil
+    }
+}
+
 
