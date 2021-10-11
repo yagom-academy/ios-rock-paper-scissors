ab5a846230e7f46d7255f9010ab5e2de7dd5ad62
aCafela-coffee <seym7894@gmail.com>
2021-10-11 21:24:39 +0900

    refactor: readUserInput() 함수가 에러를 반환하도록 수정
    GameError 열거형 추가
    ExpectedUserInput 열거형의 이름을 ExpectedHand로 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,32 +4,35 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-var computerHand: ExpectedUserInput {
-    var hands = [ExpectedUserInput.scissors, ExpectedUserInput.rock, ExpectedUserInput.paper].shuffled()
+var computerHand: ExpectedHand {
+    var hands = [ExpectedHand.scissors, ExpectedHand.rock, ExpectedHand.paper].shuffled()
     return hands.removeFirst()
 }
 
-enum ExpectedUserInput: String, CaseIterable {
-    case exit = "0"
+enum GameError: Error {
+    case exit
+    case invalidInput
+}
+
+enum ExpectedHand: String, CaseIterable {
     case scissors = "1"
     case rock = "2"
     case paper = "3"
 }
 
-func readUserInput() -> ExpectedUserInput? {
+func readUserInput() throws -> ExpectedHand {
     let input = readLine()
     
     switch input {
-    case ExpectedUserInput.exit.rawValue:
-        return .exit
-    case ExpectedUserInput.scissors.rawValue:
+    case "0":
+        throw GameError.exit
+    case ExpectedHand.scissors.rawValue:
         return .scissors
-    case ExpectedUserInput.rock.rawValue:
+    case ExpectedHand.rock.rawValue:
         return .rock
-    case ExpectedUserInput.paper.rawValue:
+    case ExpectedHand.paper.rawValue:
         return .paper
     default:
-        return nil
+        throw GameError.invalidInput
     }
 }
-
