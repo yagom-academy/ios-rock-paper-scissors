4a172a5435026ed0b2dcfdd66135024dd6de4cc9
zoe <kny0651@naver.com>
2021-10-11 23:04:04 +0900

    feat: Sign 열거형 타입 추가, generateComputerSign 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,7 +6,18 @@
 
 import Foundation
 
+enum Sign: String {
+    case rock = "1"
+    case scissors = "2"
+    case paper = "3"
+}
+
 func printStartMessage() {
     print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
 }
 
+func generateComputerSign() -> Sign? {
+    let indexOfSign: String = String(Int.random(in: 1...3))
+    let computerSign = Sign(rawValue: indexOfSign)
+    return computerSign
+}
