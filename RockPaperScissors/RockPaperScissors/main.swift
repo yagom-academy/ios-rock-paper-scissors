bb25349fec973dbfef9293d7cc7cc0bd4f07d172
yim2627 <yim2627@gmail.com>
2021-10-11 18:30:56 +0900

    Feat : checkValidInput 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -5,6 +5,7 @@ enum Message: String {
     case win = "이겼습니다!"
     case draw = "비겼습니다!"
     case lose = "졌습니다!"
+    case exit = "게임 종료"
     case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
 }
 
@@ -14,5 +15,22 @@ func selectOption() {
         print(Message.wrongInput.rawValue)
         return
     }
+    checkValidInput(option: option)
 }
 
+func checkValidInput(option: String) {
+    if option == "1" || option == "2" || option == "3" {
+        startGame(option: option)
+    } else if option == "0" {
+        print(Message.exit.rawValue)
+    } else {
+        print(Message.wrongInput.rawValue)
+        selectOption()
+    }
+}
+
+func startGame(option: String) {
+    
+}
+
+selectOption()
