42df5b285b0fdd51dfa0cd7a5b584257aa1e034d
namu <simjh27@gmail.com>
2021-10-11 21:40:57 +0900

    feat: 사용자 입력을 받는 함수 구현
    * 가위바위보 매뉴얼을 출력하는
    	printRockPaperSiccorsManual() 함수 구현
    * 사용자 입력을 받아 분기하는 recieveUserInput() 함수 구현
    * recieveUserInput() 내부에서 printRockPaperSiccorsManual() 호출

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,15 +1,35 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
 //STEP 1. 가위바위보 게임
 
 func startRockPaperSiccorsGame() {
+    recieveUserInput()
+}
+
+func printRockPaperSiccorsManual() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
+}
+
+func recieveUserInput() {
+    printRockPaperSiccorsManual()
+    let userInput = readLine()?.replacingOccurrences(of: " ", with: "")
     
+    switch userInput {
+    case "1":
+        print("generateRandomHands()로 이동")
+    case "2":
+        print("generateRandomHands()로 이동")
+    case "3":
+        print("generateRandomHands()로 이동")
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
 }
 
 startRockPaperSiccorsGame()
