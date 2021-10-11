3ffeb9f9718a2cf49e3d527a6127baaea8d581c5
horeng2 <huski.a.a@gmail.com>
2021-10-11 20:43:33 +0900

    Feat : generateChoiceOfComputer함수, compareChoices함수, startGame함수 추가
    gernerateChoiceOfComputer함수: 컴퓨터 임의의 패 생성 기능
    compareChoices함수: 승패 판정 및 결과 출력
    startGame: 게임 시작을 위한 함수(테스트)

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,30 +7,57 @@ enum Message: String {
     case lose = "졌습니다!"
     case exit = "게임 종료"
     case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
+    case systemError = "[SystemError: nil]"
 }
 
-func selectOption() {
+func startGame() {
+    selectUserChoice()
+}
+
+func selectUserChoice() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
-    guard let option = readLine() else {
-        print(Message.wrongInput.rawValue)
+    
+    guard let inputUserChoice = readLine() else {
+        print("\n"+Message.systemError.rawValue)
         return
     }
-    checkValidInput(option: option)
+    
+    if let userChoice = Int(inputUserChoice) {
+        checkValidInput(userChoice: userChoice)
+    } else {
+        print(Message.wrongInput.rawValue)
+        selectUserChoice()
+    }
 }
 
-func checkValidInput(option: String) {
-    if option == "1" || option == "2" || option == "3" {
-        startGame(option: option)
-    } else if option == "0" {
+func checkValidInput(userChoice: Int) {
+    if userChoice == 1 || userChoice == 2 || userChoice == 3 {
+        compareChoices(userChoice: userChoice)
+    } else if userChoice == 0 {
         print(Message.exit.rawValue)
     } else {
         print(Message.wrongInput.rawValue)
-        selectOption()
+        selectUserChoice()
     }
 }
 
-func startGame(option: String) {
-    
+func generateChoiceOfComputer() -> Int {
+    return Int.random(in: 1...3)
 }
 
-selectOption()
+func compareChoices(userChoice: Int) {
+    let choiceOfComputer = generateChoiceOfComputer()
+    if choiceOfComputer == userChoice {
+        print(Message.draw.rawValue)
+        selectUserChoice()
+    } else if userChoice == choiceOfComputer + 1 || userChoice == choiceOfComputer - 2 {
+        print(Message.win.rawValue)
+        print(Message.exit.rawValue)
+    } else {
+        print(Message.lose.rawValue)
+        print(Message.exit.rawValue)
+    }
+}
+
+
+startGame()
