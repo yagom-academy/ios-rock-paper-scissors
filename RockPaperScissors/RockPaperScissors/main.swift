0701f370c94e623b75a33f4487498003c59a7a0f
oksunwoo <oksunwo@naver.com>
2021-10-12 20:02:12 +0900

    refactor: 예외처리 로직 삭제, checkInputNumber 함수명, return 타입 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -3,29 +3,27 @@
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
 //
-enum GameError: Error {
-    case wrongInput
-}
-
 var computerNumber: Int = 0
 
-func inputUserData() throws -> Int {
+func inputUserData() -> Int {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ",terminator: "")
     guard let userInput = readLine(), let userInputNumber = Int(userInput) else {
-        throw GameError.wrongInput
+        return -1
     }
     return userInputNumber
 }
 
 func generateRandomNumber() {
     var computerNumbers: [Int] = [1,2,3]
+    let randomIndex = 0
+    
     computerNumbers.shuffle()
-    computerNumber = computerNumbers[0]
+    computerNumber = computerNumbers[randomIndex]
 }
 
 func playGame(inputNumber: Int) {
     let matchedWinNumber: [Int:Int] = [1: 3, 2: 1, 3: 2]
-    print("컴퓨터 번호 \(computerNumber) , 내 번호 \(inputNumber)")
+    
     if matchedWinNumber[inputNumber] == computerNumber {
         print("이겼습니다!")
     } else if inputNumber != computerNumber {
@@ -35,35 +33,34 @@ func playGame(inputNumber: Int) {
 
 func isDraw(inputNumber: Int) -> Bool {
     if inputNumber == computerNumber {
+        print("비겼습니다!")
         return true
     }
     return false
 }
 
-func checkInputNumber(inputNumber: Int) throws {
+func isRestartGame(inputNumber: Int) -> Bool {
+    var isRestart: Bool = false
+    
     switch inputNumber {
-    case 0 : print("게임 종료")
-    case 1...3 : isDraw(inputNumber: inputNumber) ? print("비겼습니다!") : playGame(inputNumber: inputNumber)
-    default : throw GameError.wrongInput
+    case 0 :
+        print("게임 종료")
+        isRestart = false
+    case 1...3 : isDraw(inputNumber: inputNumber) ? isRestart = true : playGame(inputNumber: inputNumber)
+    default :
+        isRestart = true
     }
+    return isRestart
 }
 
 func startGame() {
-    var isReturn: Bool = false
+    var isRestart: Bool = false
+    let userInputNumber =  inputUserData()
+
     generateRandomNumber()
-    do {
-        let userInputNumber =  try inputUserData()
-        try checkInputNumber(inputNumber: userInputNumber)
-        
-    } catch GameError.wrongInput {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        isReturn = true
-    } catch {
-        print(error)
-        isReturn = true
-    }
+    isRestart = isRestartGame(inputNumber: userInputNumber)
     
-    if isReturn {
+    if isRestart {
         startGame()
     }
 }
