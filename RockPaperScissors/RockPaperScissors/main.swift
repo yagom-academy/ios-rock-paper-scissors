7e37c0ef41aaf7136afc57eb0bc3b53f566cb785
llghdud921 <llghdud921@naver.com>
2021-10-12 15:03:47 +0900

    feat: 컴퓨터 임의의 숫자를 생성하는 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,11 +2,13 @@
 //  RockPaperScissors - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
+var computerNumber: Int = 0
 
 func printGameStart() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
     guard let userInput = readLine(), let userInputNumber = Int(userInput) else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
         return
     }
     if userInputNumber == 0 {
@@ -14,3 +16,11 @@ func printGameStart() {
     }
 }
 
+func generateRandomNumber() {
+    var computerNumbers: [Int] = [1,2,3]
+    computerNumbers.shuffle()
+    
+    computerNumber = computerNumbers[0]
+}
+
+generateRandomNumber()
