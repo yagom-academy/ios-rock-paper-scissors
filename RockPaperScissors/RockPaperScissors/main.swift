f1ed77be384742d163eec65cc153238c33e5ed80
llghdud921 <llghdud921@naver.com>
2021-10-12 16:12:23 +0900

    feat: 사용자에게 입력받은 숫자를 검증하는 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,7 +10,7 @@ enum GameError: Error {
 var computerNumber: Int = 0
 
 func inputUserData() throws -> Int {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ",terminator: "")
     guard let userInput = readLine(), let userInputNumber = Int(userInput) else {
         throw GameError.wrongInput
     }
@@ -23,9 +23,18 @@ func generateRandomNumber() {
     computerNumber = computerNumbers[0]
 }
 
+func checkInputNumber(inputNumber: Int) throws {
+    switch inputNumber {
+    case 0 : print("게임 종료")
+    case 1...3 : print("게임 실행")
+    default : throw GameError.wrongInput
+    }
+}
+
 func startGame() {
     do {
         let userInputNumber =  try inputUserData()
+        print(try checkInputNumber(inputNumber: userInputNumber))
     } catch GameError.wrongInput {
         print("잘못된 입력입니다. 다시 시도해주세요.")
     } catch {
@@ -35,4 +44,4 @@ func startGame() {
 
 
 
-generateRandomNumber()
+startGame()
