281d08bce7b6f50c5dce717c787fe41bbdad02e7
hwangjeha <hyhpwang@gmail.com>
2021-10-12 09:58:53 +0900

    feat: 사용자 입력 받는 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,15 @@
 
 import Foundation
 
-print("Hello, World!")
+func printMenu() {
+    print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
+}
 
+func receiveUserInput() {
+    printMenu()
+    
+    guard let input = readLine(), input != "0" else {
+        print("게임 종료")
+        return
+    }
+}
