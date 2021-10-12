4c414326c62092c985205b8b3f93036087250e76
wooyani77 <wooyani77@gmail.com>
2021-10-12 12:01:51 +0900

    feat: 컴퓨터 랜덤 숫자 생성 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,5 +10,7 @@ func printGameMenu() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 }
 
-
-
+func generateRandomNumber() -> Int {
+    let randomNumber = Int.random(in: 1...3)
+    return randomNumber
+}
