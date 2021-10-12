363995be2d4639f8fc4453225b64246bd5f24d6b
namu <simjh27@gmail.com>
2021-10-12 21:09:49 +0900

    feat: 게임 결과를 판정하는 기능 추가
    * 게임결과를 판정하는 letCheckResultGame(by:) 함수 추가
    * 게임결과를 출력하는 printMessageAboutResultGame(of:) 함수
    	추가
    * 게임의 결과 케이스를 정의한 GameHand 열거형 정의

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -8,12 +8,11 @@ import Foundation
 
 //STEP 1. 가위바위보 게임 [차분히 천천히하세요]
 enum HandGameMessage: String {
-    case win = "이겼습니다!"
-    case lose = "졌습니다!"
-    case tie = "비겼습니다!"
     case rockPaperSiccorsManual = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
     case endGame = "게임 종료"
     case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
+    case userTurn = "[사용자 턴]"
+    case computerTurn = "[컴퓨터 턴]"
 }
 
 enum GameHand: CaseIterable {
@@ -22,14 +21,14 @@ enum GameHand: CaseIterable {
     case siccors
 }
 
-func generateRandomHand() -> GameHand {
-    let randomIndex = Int.random(in: 0..<GameHand.allCases.count)
-    return GameHand.allCases[randomIndex]
+enum GameResult: String {
+    case user  = "이겼습니다!"
+    case computer = "졌습니다!"
+    case tie = "비겼습니다!"
 }
 
 func startRockPaperSiccorsGame() {
     let wrappedUserInput = receiveUserManualInput()
-    
     switch wrappedUserInput.errorMessage {
     case .wrongInput:
         printErrorMessage(of: .wrongInput)
@@ -41,12 +40,44 @@ func startRockPaperSiccorsGame() {
     default:
         break
     }
-    
     guard let userHand = wrappedUserInput.userHand else {
         return
     }
+    letCheckResultGame(by: userHand)
+    
     // 1,2,3 중에 선택된 값을 판별해주는 기능 구현
-    // 결과 출력 함수? 구현
+}
+
+func letCheckResultGame(by userHand: GameHand) -> GameResult {
+    var resultGame: GameResult = .user
+    let computerHand = generateRandomHand()
+    print(computerHand)
+    if userHand == computerHand {
+        printMessageAboutResultGame(of: .tie)
+        startRockPaperSiccorsGame()
+    } else if userHand == .paper, computerHand == .rock {
+        printMessageAboutResultGame(of: .user)
+        return resultGame
+    } else if userHand == .rock, computerHand == .siccors {
+        printMessageAboutResultGame(of: .user)
+        return resultGame
+    } else if userHand == .siccors, computerHand == .paper {
+        printMessageAboutResultGame(of: .user)
+        return resultGame
+    } else {
+        printMessageAboutResultGame(of: .computer)
+        resultGame = .computer
+    }
+    return resultGame
+}
+// 결과 출력 함수? 구현
+
+func generateRandomHand() -> GameHand {
+    let randomIndex = Int.random(in: 0..<GameHand.allCases.count)
+    return GameHand.allCases[randomIndex]
+}
+func printMessageAboutResultGame(of gameResult: GameResult) {
+    print(gameResult.rawValue)
 }
 
 func printErrorMessage(of message: HandGameMessage) {
