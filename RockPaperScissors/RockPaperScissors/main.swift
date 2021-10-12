0b127e723b102a28334b1934737d7f7f6f3518b6
nicholas <graysilver18@gmail.com>
2021-10-12 09:17:43 +0900

    feat: makeRandomNumeber() 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -60,6 +60,10 @@ func printRockPaperScissors(){
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 }
 
+func makeRandomNumber() -> Int {
+    return Int.random(in: 1...3)
+}
+
 playRockPaperScissors()
 
 
