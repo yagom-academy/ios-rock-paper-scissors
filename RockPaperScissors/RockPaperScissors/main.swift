86f09ec234b6da72655081c9590b1442565d5466
smart8612 <smart8612@gmail.com>
2021-10-12 16:25:00 +0900

    feat: 사용자의 입력을 받고 검증하는 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,25 @@
 
 import Foundation
 
-print("Hello, World!")
+let possibleInputRange = 1...3
 
+func inputingUserNumber(range: ClosedRange<Int> = possibleInputRange) -> Int {
+    guard let input = readLine(), input != "" else {
+        fatalError("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+    
+    let convertedInput = input.compactMap { Int(String($0)) }
+    guard convertedInput.count == input.count else {
+        fatalError("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+    
+    guard let firstItem = convertedInput.first else {
+        fatalError("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+    
+    guard convertedInput.count == 1 && range ~= firstItem else {
+        fatalError("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+    
+    return firstItem
+}
