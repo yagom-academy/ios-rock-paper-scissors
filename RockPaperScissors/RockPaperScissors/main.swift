e922a9ba759dc3cdca086519bfb42076835b037b
forestjae <forestsj30@gmail.com>
2021-10-12 09:44:03 +0900

    feat: enumeration 선언

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,6 +6,23 @@
 
 import Foundation
 
+enum Hand: Int {
+    case scissors = 1
+    case rock = 2
+    case paper = 3
+}
+
+enum PlayerType {
+    case user
+    case computer
+}
+
+enum GameResult: String {
+    case win = "이겼습니다!"
+    case lose = "졌습니다!"
+    case draw = "비겼습니다!"
+}
+
 var inputedChoice = ""
 
 /*
