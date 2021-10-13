839214d2dc3f2b74a6b5165d388d7a60d3b5a046
Quokkaaa <rktrkdml@gmail.com>
2021-10-13 16:12:27 +0900

    refactor: 열거형 타입 네이밍 수정 및 유사기능 함수 병합
    * 용도에 따라 열거형 타입 case분리
    * 제네릭을 사용하여 동일한 기능을 하는 함수병합

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -9,10 +9,13 @@ import Foundation
 //STEP 1. 가위바위보 게임 [차분히 천천히하세요]
 enum HandGameMessage: String {
     case rockPaperSiccorsManual = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
-    case endGame = "게임 종료"
-    case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
     case userTurn = "[사용자 턴]"
     case computerTurn = "[컴퓨터 턴]"
+}
+
+enum HandGameExceptionMessage: String {
+    case endGame = "게임 종료"
+    case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
 }
 
 enum HandGameHand: CaseIterable {
@@ -31,11 +34,11 @@ func startRockPaperSiccorsGame() {
     let wrappedUserInput = receiveUserManualInput()
     switch wrappedUserInput.errorMessage {
     case .wrongInput:
-        printMessage(of: .wrongInput)
+        printMessage(of: HandGameExceptionMessage.wrongInput)
         startRockPaperSiccorsGame()
         return
     case .endGame:
-        printMessage(of: .endGame)
+        printMessage(of: HandGameExceptionMessage.endGame)
         return
     default:
         break
@@ -43,6 +46,7 @@ func startRockPaperSiccorsGame() {
     guard let userHand = wrappedUserInput.userHand else {
         return
     }
+    
     let computerHand = generateRandomHand()
     let gameResult = checkGameResult(by: userHand, computerHand: computerHand)
     printMessage(of: gameResult)
@@ -73,16 +77,13 @@ func generateRandomHand() -> HandGameHand {
     let randomIndex = Int.random(in: 0..<HandGameHand.allCases.count)
     return HandGameHand.allCases[randomIndex]
 }
-func printMessage(of gameResult: HandGameResult) {
-    print(gameResult.rawValue)
+
+func printMessage<T: RawRepresentable>(of message: T) {
+    print(message.rawValue)
 }
 
-func printMessage(of errorMessage: HandGameMessage) {
-    print(errorMessage.rawValue)
-}
-
-func receiveUserManualInput() -> (userHand: HandGameHand?, errorMessage: HandGameMessage?) {
-    var statusMessage: HandGameMessage?
+func receiveUserManualInput() -> (userHand: HandGameHand?, errorMessage: HandGameExceptionMessage?) {
+    var statusMessage: HandGameExceptionMessage?
     var userHandResult: HandGameHand?
     print(HandGameMessage.rockPaperSiccorsManual.rawValue, terminator: "")
     let userInput = readLine()?.replacingOccurrences(of: " ", with: "")
