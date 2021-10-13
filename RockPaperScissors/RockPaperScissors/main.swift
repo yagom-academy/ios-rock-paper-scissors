4fa2da4a685e326dbd3aef62025ce51226a0060c
just1103 <hyojuson1103@gmail.com>
2021-10-13 16:59:29 +0900

    refactor: 함수명 수정, createRandomCard 함수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -28,22 +28,24 @@ enum Card: CaseIterable {
     }
 }
 
-
 /// 에러를 방지하기 위해 특정 값으로 초기화
 var playerCard: Card = Card.scissors
 
 func receiveValidPlayerInput() {
     let programTerminator = "0"
     print("가위(1), 바위(2), 보(3)! <종료 : \(programTerminator)> : ", terminator: "")
+    
     guard let playerInputString: String = readLine(), playerInputString != programTerminator else {
         print("게임 종료")
         return
     }
-    verifyValue(of: playerInputString)
+    
+    assignPlayerCard(with: playerInputString)
 }
 
-func verifyValue(of playerInputString: String) {
-    guard let playerInputNumber = Int(playerInputString), let cardFromInputNumber = Card.changeNumberToCard(number: playerInputNumber) else {
+func assignPlayerCard(with playerInputString: String) {
+    guard let playerInputNumber = Int(playerInputString),
+          let cardFromInputNumber = Card.changeNumberToCard(number: playerInputNumber) else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
         receiveValidPlayerInput()
         return
@@ -53,17 +55,21 @@ func verifyValue(of playerInputString: String) {
     startGame()
 }
 
-
-
 func startGame() {
-    var computerCard: Card = Card.scissors
-    
-    if let randomChoice = Card.allCases.randomElement() {
-        computerCard = randomChoice
-        print(computerCard)
+    guard let computerCard: Card = createRandomCard() else {
+        print("오류 - 컴퓨터 패 생성 실패")
+        return
     }
-    
+
+    print(computerCard)
     compareCards(with: computerCard)
+}
+
+func createRandomCard() -> Card? {
+    if let randomChoice = Card.allCases.randomElement() {
+        return randomChoice
+    }
+    return nil
 }
 
 func compareCards(with computerCard: Card) {
