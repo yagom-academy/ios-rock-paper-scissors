901f72d6d42c1f07565e29ef8e17d0949723f4a6
yim2627 <yim2627@gmail.com>
2021-10-13 14:47:47 +0900

    Refactor : RockPaperScissors 캡슐화 및 검증 함수, 에러 처리 방법 수정
    검증 함수 조건문 if else -> guard else 수정
    do-catch, throw를 이용해 에러 처
    generateChoiceOfComputer함수를 연산변수로 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,63 +1,79 @@
 import Foundation
 
-
-enum Message: String {
+enum Message: String, CustomStringConvertible {
+    var description: String {
+        return rawValue
+    }
+    
     case win = "이겼습니다!"
     case draw = "비겼습니다!"
     case lose = "졌습니다!"
     case exit = "게임 종료"
-    case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
     case systemError = "[SystemError: nil]"
 }
 
-func startGame() {
-    selectUserChoice()
+enum errormessage: Error {
+    case wrongInput
+    case systemError
 }
 
-func selectUserChoice() {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
-    
-    guard let inputUserChoice = readLine() else {
-        print("\n"+Message.systemError.rawValue)
-        return
+struct RockPaperScissors {
+    private var generatedChoiceOfComputer: Int {
+        return Int.random(in: 1...3)
     }
     
-    if let userChoice = Int(inputUserChoice) {
-        checkValidInput(userChoice: userChoice)
-    } else {
-        print(Message.wrongInput.rawValue)
+    func startGame() {
         selectUserChoice()
+    }
+    
+    private func selectUserChoice() {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
+        
+        guard let inputUserChoice = readLine() else {
+            print("\n"+Message.systemError.rawValue)
+            return
+        }
+        
+        do {
+            try checkValidInput(userChoice: inputUserChoice)
+        } catch errormessage.wrongInput {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            selectUserChoice()
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func checkValidInput(userChoice: String) throws {
+        guard let userChoice = Int(userChoice) else {
+            throw errormessage.wrongInput
+        }
+        
+        guard userChoice == 1 || userChoice == 2 || userChoice == 3 || userChoice == 0 else {
+            throw errormessage.wrongInput
+        }
+        
+        guard userChoice == 0 else {
+            compareChoices(userChoice: userChoice)
+            return
+        }
+        print(Message.exit)
+    }
+    
+    private func compareChoices(userChoice: Int) {
+        let choiceOfComputer = generatedChoiceOfComputer
+        if choiceOfComputer == userChoice {
+            print(Message.draw)
+            selectUserChoice()
+        } else if userChoice == choiceOfComputer + 1 || userChoice == choiceOfComputer - 2 {
+            print(Message.win)
+            print(Message.exit)
+        } else {
+            print(Message.lose)
+            print(Message.exit)
+        }
     }
 }
 
-func checkValidInput(userChoice: Int) {
-    if userChoice == 1 || userChoice == 2 || userChoice == 3 {
-        compareChoices(userChoice: userChoice)
-    } else if userChoice == 0 {
-        print(Message.exit.rawValue)
-    } else {
-        print(Message.wrongInput.rawValue)
-        selectUserChoice()
-    }
-}
-
-func generateChoiceOfComputer() -> Int {
-    return Int.random(in: 1...3)
-}
-
-func compareChoices(userChoice: Int) {
-    let choiceOfComputer = generateChoiceOfComputer()
-    if choiceOfComputer == userChoice {
-        print(Message.draw.rawValue)
-        selectUserChoice()
-    } else if userChoice == choiceOfComputer + 1 || userChoice == choiceOfComputer - 2 {
-        print(Message.win.rawValue)
-        print(Message.exit.rawValue)
-    } else {
-        print(Message.lose.rawValue)
-        print(Message.exit.rawValue)
-    }
-}
-
-
-startGame()
+let rockPaperScissors = RockPaperScissors()
+rockPaperScissors.startGame()
