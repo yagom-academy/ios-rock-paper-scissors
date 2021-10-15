f29d31e3296d1fce9be6ffa4c5dfdbba7816c441
yanghojoon <yanghojoon@gmail.com>
2021-10-15 21:04:39 +0900

    refactor: 컴퓨터 랜덤 값 생성하는 함수 병합

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,12 +4,6 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-enum RockScissorsPaper: Int {
-    case scissors = 1
-    case rock = 2
-    case paper = 3
-}
-
 enum MukJjiBba: Int {
     case muk = 1
     case jji = 2
@@ -35,18 +29,18 @@ enum GameMenu: String {
 
 func runRockScissorsPaper() {
     print(GameMenu.rockScissorsPaper.rawValue, terminator: "")
-    
+
     let userInput = readLine()
-   
+
     do {
         switch userInput {
         case "0":
             print(GameMessage.end.rawValue)
         case "1", "2", "3":
-            try compareRockScissorsPaper(generateRockScissorsPaper(), to: convertUserInputType(input: userInput))
+            try compareRockScissorsPaper(generateComputerHand(rock: 2, scissors: 1, paper: 3), to: convertUserInputType(input: userInput))
         default:
             print(GameMessage.error.rawValue)
-            
+
             runRockScissorsPaper()
         }
     } catch {
@@ -54,28 +48,44 @@ func runRockScissorsPaper() {
     }
 }
 
-func generateRockScissorsPaper() -> Int {
+func compareRockScissorsPaper(_ computerValue: Int, to userInputValue: Int) {
+    let subtractionValue = userInputValue - computerValue
+
+    if subtractionValue == 0 {
+        print(GameMessage.draw.rawValue)
+
+        runRockScissorsPaper()
+    }
+
+    switch subtractionValue {
+    case 1, -2:
+        print(GameMessage.win.rawValue)
+
+        let player = "사용자"
+
+        runMukJjiBba(turn: player)
+    case -1, 2:
+        print(GameMessage.lose.rawValue)
+
+        let player = "컴퓨터"
+
+        runMukJjiBba(turn: player)
+    default:
+        break
+    }
+}
+
+func generateComputerHand(rock: Int, scissors: Int, paper: Int) -> Int {
     var rockScissorsPaper = [Int]()
-    rockScissorsPaper.append(RockScissorsPaper.scissors.rawValue)
-    rockScissorsPaper.append(RockScissorsPaper.rock.rawValue)
-    rockScissorsPaper.append(RockScissorsPaper.paper.rawValue)
     
+    rockScissorsPaper.append(rock)
+    rockScissorsPaper.append(scissors)
+    rockScissorsPaper.append(paper)
+
     rockScissorsPaper.shuffle()
-    
+
     return rockScissorsPaper[0]
 }
-
-func generateMukJjiBba() -> Int {
-    var mukJjiBba = [Int]()
-    mukJjiBba.append(MukJjiBba.muk.rawValue)
-    mukJjiBba.append(MukJjiBba.jji.rawValue)
-    mukJjiBba.append(MukJjiBba.bba.rawValue)
-        
-    mukJjiBba.shuffle()
-        
-    return mukJjiBba[0]
-}
-
 
 func convertUserInputType(input: String?) throws -> Int {
     var stringTypeUserInput = ""
@@ -83,7 +93,7 @@ func convertUserInputType(input: String?) throws -> Int {
     if let userInput = input {
         stringTypeUserInput = userInput
     }
-    
+
     guard let integer = Int(stringTypeUserInput) else {
         throw Errorcase.invalidInput
     }
@@ -91,86 +101,60 @@ func convertUserInputType(input: String?) throws -> Int {
     return integer
 }
 
-func compareRockScissorsPaper(_ computerValue: Int, to userInputValue: Int) {
-    let subtractionValue = userInputValue - computerValue
-    
-    if subtractionValue == 0 {
-        print(GameMessage.draw.rawValue)
-        
-        runRockScissorsPaper()
-    }
-    
-    switch subtractionValue {
-    case 1, -2:
-        print(GameMessage.win.rawValue)
-        
-        let player = "사용자"
-        
-        runMukJjiBba(turn: player)
-    case -1, 2:
-        print(GameMessage.lose.rawValue)
-        
-        let player = "컴퓨터"
-        
-        runMukJjiBba(turn: player)
-    default:
-        break
-    }
-}
-
 func runMukJjiBba(turn: String) {
     print("[\(turn) 턴]", GameMenu.mukJjiBba.rawValue, terminator: "")
-    
+
     let userInput = readLine()
-    
+
     do {
         switch userInput {
         case "0":
             print(GameMessage.end.rawValue)
         case "1", "2", "3":
-            try compareMukJjiBba(generateMukJjiBba(), to: convertUserInputType(input: userInput), at: turn)
+            try compareMukJjiBba(generateComputerHand(rock: 1, scissors: 2, paper: 3), to: convertUserInputType(input: userInput), at: turn)
         default:
             print(GameMessage.error.rawValue)
-            
+
             let player = "컴퓨터"
-            
+
             runMukJjiBba(turn: player)
         }
     } catch {
         print(GameMessage.error.rawValue)
     }
-    
+
 }
 
 func compareMukJjiBba(_ computerValue: Int, to userInputValue: Int, at turn: String) {
     guard let computerData = MukJjiBba(rawValue: computerValue) else {
         return
     }
-    
+
     guard let userData = MukJjiBba(rawValue: userInputValue) else {
         return
     }
-    
+
     switch (computerData, userData) {
     case (.muk, .jji), (.jji, .bba), (.bba, .muk):
         let player = "컴퓨터"
-        
+
         print("\(player)의 턴입니다.")
-        
+
         runMukJjiBba(turn: player)
     case (.muk, .bba), (.jji, .muk), (.bba, .jji):
         let player = "사용자"
-        
+
         print("\(player)의 턴입니다.")
-        
+
         runMukJjiBba(turn: player)
     case (.muk, .muk), (.jji, .jji), (.bba, .bba):
         print("\(turn)의 승리!")
-        
+
         print(GameMessage.end.rawValue)
-        
+
         return
     }
 }
 
 runRockScissorsPaper()
+
