de646f49d4960835723cbb2c8986e7ba1b700cf2
Taeangel <wngks2050@gmail.com>
2022-02-15 15:40:38 +0900

    feat: playRockPaperScissors함수

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,18 @@
 
 import Foundation
 
-print("Hello, World!")
+func playRockPaperScissors() {
+    print("가위(1),바위(2),보(3)!<종료 :0>:", terminator: "")
+    let userInput = readLine()
+    guard let userHand = userInput else {
+        print("잘못된 입력입니다. 다시 시도해 주세요.")
+        return playRockPaperScissors()
+    }
+    let computerHand = String(Int.random(in: 1...3))
+    
+    match(userHand: userHand, computerHand: computerHand)
+}
 
+func match(userHand: String, computerHand: String) {
+    
+}
