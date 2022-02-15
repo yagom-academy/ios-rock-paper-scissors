92f69827d649638149bf60780e4ea7f3d9fadfc7
Eddy <kimkyunghun3@naver.com>
2022-02-15 20:47:21 +0900

    Auto stash before revert of "refactor: enum 구현에 따른 함수 수정, 네이밍 수정"

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -13,37 +13,14 @@ func showMenu() {
     selectMenu()
 }
 
-func receiveNumber() -> Int {
-    guard let inputNumber = readLine() else { return 5 }
     let userInput = convertNumber(inputNumber: inputNumber)
-    return userInput
 }
 
-func convertNumber(inputNumber: String) -> Int {
-    let trimmedNumber = inputNumber.trimmingCharacters(in: .whitespaces)
     guard let numberToInt = Int(trimmedNumber) else { return 5 }
-    return numberToInt
 }
 
-func selectMenu() {
     let number = receiveNumber()
-    switch number {
-    case 0:
-        print("게임 종료")
-        break
-    case 1:
-        print("1")
-    case 2:
-        print("2")
-    case 3:
-        print("3")
-    default:
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        showMenu()
-    }
 }
 
-func makeRandomNumber() -> Int {
     let computerRandomNumber = Int.random(in: 1...3)
-    return computerRandomNumber
 }
