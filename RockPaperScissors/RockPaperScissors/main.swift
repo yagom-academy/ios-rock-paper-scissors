f31a8d0d97ee45f5364c25413d52257642c09f14
malrang <ruehd7924@gmail.com>
2022-02-15 15:50:08 +0900

    refactor: class인스턴스 메소드를 enum의 인스턴스 메소드로 변경
    checkNumber(computer: RockPaperScissors, user: RockPaperScissors) -> Result 를
    playGame(with opponentPlayer: RockPaperScissors) -> Result 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,13 +10,34 @@ enum RockPaperScissors: Int, CaseIterable {
     case scissor = 1
     case rock = 2
     case paper = 3
+    
+    func playGame(with opponentPlayer: RockPaperScissors) -> Result {
+        
+        if self == opponentPlayer {
+            return .draw
+        }
+        
+        if self == .scissor {
+            return opponentPlayer == .paper ? .win : .lose
+        }
+        
+        if self == .paper {
+            return opponentPlayer == .rock ? .win : .lose
+        }
+        
+        if self == .rock {
+            return opponentPlayer == .scissor ? .win : .lose
+        }
+        
+        return .draw
+    }
 }
 
 enum Result {
     case win
     case lose
     case draw
-    case unknown
+//    case unknown
 }
 
 enum ErrorCase: Error {
@@ -33,7 +54,7 @@ class RockPaperScissorsGame {
         return randomData
     }
     
-    func inputUserNumber() -> RockPaperScissors? {
+    func inputUserNumber(flag: inout Bool) -> RockPaperScissors? {
         guard let inputNumber = readLine() else {
             return nil
         }
@@ -42,7 +63,13 @@ class RockPaperScissorsGame {
             return nil
         }
         
+        guard convertedInt != Int.zero else {
+            flag = false
+            return nil
+        }
+        
         guard let RockPaperScissorsData = RockPaperScissors(rawValue: convertedInt) else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
             return nil
         }
         
@@ -59,13 +86,15 @@ class RockPaperScissorsGame {
                 continue
             }
             
-            guard let userNumber = inputUserNumber() else {
+            guard let userNumber = inputUserNumber(flag: &flag) else {
                 continue
             }
             
-            let checkResult = checkNumber(computer: computerNumber, user: userNumber)
+//            let checkResult = checkNumber(computer: computerNumber, user: userNumber)
+            let checkResult = userNumber.playGame(with: computerNumber)
             flag = printResult(checkResult: checkResult)
         }
+        print("게임 종료")
     }
     
     func printResult(checkResult: Result) -> Bool {
@@ -79,44 +108,44 @@ class RockPaperScissorsGame {
         case .draw:
             print("비겼습니다")
             return true
-        case .unknown:
-            print("모릅니다")
-            return true
+//        case .unknown:
+//            print("모릅니다")
+//            return true
         }
     }
     
-    func checkNumber(computer: RockPaperScissors, user: RockPaperScissors) -> Result {
-
-        if computer == user {
-            return Result.draw
-        }
-        
-        if computer == .scissor && user == .paper {
-            return Result.lose
-        }
-        
-        if computer == .scissor && user == .rock {
-            return Result.win
-        }
-        
-        if computer == .rock && user == .paper {
-            return Result.win
-        }
-        
-        if computer == .rock && user == .scissor {
-            return Result.lose
-        }
-        
-        if computer == .paper && user == .scissor {
-            return Result.win
-        }
-        
-        if computer == .paper && user == .rock {
-            return Result.lose
-        }
-        
-        return Result.unknown
-    }
+//    func checkNumber(computer: RockPaperScissors, user: RockPaperScissors) -> Result {
+//
+//        if computer == user {
+//            return Result.draw
+//        }
+//
+//        if computer == .scissor && user == .paper {
+//            return Result.lose
+//        }
+//
+//        if computer == .scissor && user == .rock {
+//            return Result.win
+//        }
+//
+//        if computer == .rock && user == .paper {
+//            return Result.win
+//        }
+//
+//        if computer == .rock && user == .scissor {
+//            return Result.lose
+//        }
+//
+//        if computer == .paper && user == .scissor {
+//            return Result.win
+//        }
+//
+//        if computer == .paper && user == .rock {
+//            return Result.lose
+//        }
+//
+//        return Result.unknown
+//    }
 }
 
 let c = RockPaperScissorsGame()
