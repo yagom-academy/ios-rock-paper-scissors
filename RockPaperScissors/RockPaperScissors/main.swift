5954a0d46646511b149a43f3a4d3733a5a291776
Donnie <seo_17@naver.com>
2022-02-15 14:42:10 +0900

    feat: rockPaperScissors타입구현, 승자판별함수 구현, 타입변환함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,18 +4,35 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
+enum rockPaperScissors: String {
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
+    case error = "Error"
+}
 
 func rockPaperScissorsGameStart() {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
-    let oneDigitComputerNumber = makeOneDigitComputerNumber()
-    let userSelectedNumber = selectedUserGameMenuNumber()
     
 //    switch userSelectedNumber {
 //    case userSelectedNumber == oneDigitComputerNumber:
 //        print("비겼습니다!")
 //        case
 //    }
+    var ifWinnerIsFound: Bool = false
+    
+    repeat {
+        printMenu()
+        let oneDigitComputerNumber: Int = makeOneDigitComputerNumber()
+        let userSelectedNumber: String = selectedUserGameMenuNumber()
+        guard let computerHand: rockPaperScissors = convert(number: String(oneDigitComputerNumber)) else { return }
+        guard let userHand: rockPaperScissors = convert(number: userSelectedNumber) else { return }
+        ifWinnerIsFound = findIfUserWin(computerNumber: computerHand, userNumber: userHand)
+    } while !ifWinnerIsFound
+}
+
+func printMenu() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+
 }
 
 func makeOneDigitComputerNumber() -> Int {
@@ -25,8 +42,6 @@ func makeOneDigitComputerNumber() -> Int {
 
 func selectedUserGameMenuNumber() -> String {
     guard let userSelectedNumber = readLine() else { return "" }
-    print(userSelectedNumber)
-    print(type(of: userSelectedNumber))
     return userSelectedNumber
 }
 
@@ -35,4 +50,32 @@ func selectedUserGameMenuNumber() -> String {
 //    return convertedIntTypeElement
 //}
 
-selectedUserGameMenuNumber()
+
+
+func findIfUserWin(computerNumber: rockPaperScissors, userNumber: rockPaperScissors) -> Bool {
+    switch (computerNumber, userNumber) {
+    case (.rock, .paper):
+        return true
+    case (.rock, .scissors):
+        return false
+    case (.paper, .scissors):
+        return true
+    default:
+        return false
+    }
+}
+
+func convert(number: String) -> rockPaperScissors? {
+    switch number {
+    case "1":
+        return rockPaperScissors.scissors
+    case "2":
+        return rockPaperScissors.rock
+    case "3":
+        return rockPaperScissors.paper
+    default:
+        return nil
+    }
+}
+
+rockPaperScissorsGameStart()
