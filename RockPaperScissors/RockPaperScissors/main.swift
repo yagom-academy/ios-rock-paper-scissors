4b76e76efc19266c1abd4b8bf5a637dabf7bb52c
Tiana <97kth01@naver.com>
2022-02-15 13:01:38 +0900

    feat: 게임 메뉴 함수, 사용자 입력 함수, 랜덤 수 생성 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,33 @@
 
 import Foundation
 
-print("Hello, World!")
+func rockPaperScissorsGameStart() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
+    let oneDigitComputerNumber = makeOneDigitComputerNumber()
+    let userSelectedNumber = selectedUserGameMenuNumber()
+    
+//    switch userSelectedNumber {
+//    case userSelectedNumber == oneDigitComputerNumber:
+//        print("비겼습니다!")
+//        case
+//    }
+}
 
+func makeOneDigitComputerNumber() -> Int {
+    let oneDigitComputerNumber = Int.random(in: 1...3)
+    return oneDigitComputerNumber
+}
+
+func selectedUserGameMenuNumber() -> String {
+    guard let userSelectedNumber = readLine() else { return "" }
+    print(userSelectedNumber)
+    print(type(of: userSelectedNumber))
+    return userSelectedNumber
+}
+
+//func convertStringToInt(_ stringTypeComputerNumber: String) -> Int {
+//    guard let convertedIntTypeElement = stringTypeComputerNumber.compactMap { Int($0) } else { return }
+//    return convertedIntTypeElement
+//}
+
+selectedUserGameMenuNumber()
