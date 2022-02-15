4a7114efc84159dcec3d3360737355120a3f56e6
minseong <msb0987@naver.com>
2022-02-15 15:56:19 +0900

    feat: generatedRandomHandShape()함수 구현 및 HandShapes타입 구현
    * rock, paper, scissors중 랜덤으로 하나의 케이스를 반환한다.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+enum HandShapes: CaseIterable {
+    case rock, paper, scissors
+}
+
 func printInputGuidanceMessage() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 }
@@ -8,4 +12,12 @@ func receiveUserInputHandShape() -> String? {
     let inputtedHandShape = readLine()?.replacingOccurrences(of: " ", with: "")
     return inputtedHandShape
 }
+
+func generatedRandomHandShape() -> HandShapes {
+    if let handShape = HandShapes.allCases.randomElement() {
+        return handShape
+    }
+    return generatedRandomHandShape()
+}
+
 
