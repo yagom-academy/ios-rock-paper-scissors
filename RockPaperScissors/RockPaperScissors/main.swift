bbd209db8bee70706d2ee500f9c61369a6420bfc
saafaaari <111dltldnjs@naver.com>
2022-02-15 11:22:10 +0900

    feat : 메뉴 출력 기능과 사용자 입력받는 기능 추가
    startGame()함수에서 printMenu()호출 해준 뒤 readLine()함수를 통해 사용자 메뉴를 입력 받는다.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,20 @@
 
 import Foundation
 
-print("Hello, World!")
+func printMenu() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
+}
 
+func startGame() {
+    printMenu()
+    let userInput: String? = readLine()
+    switch userInput {
+    case "1": print("가위")
+    case "2": print("바위")
+    case "3": print("보")
+    case "0": return
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        startGame()
+    }
+}
