ca4bdeff70324c709706b6411ce2e824e3d8bf89
dnwhd0112 <dnwhd0112@naver.com>
2022-02-15 14:25:42 +0900

    feat: 게임시작 함수 구현
    게임의 진입점이 되는 메인 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,12 +6,36 @@
 
 import Foundation
 
+enum GameInput: String {
+    case menu = "가위(1), 바위(2), 보(3)! <종료 : 0> :"
+    case gameEnd = "게임 종료"
+}
+
+enum GameSetting: Int {
+    case gameEnd = 0
+}
+
 func getUserInput() -> Int? {
-    guard let userInput = readLine(), let userIntInput = Int(userInput) else { return nil }
-    
+    guard let userInput = readLine(), let userIntInput = Int(userInput)
+    else {
+        return nil
+    }
     if userIntInput >= 0 && userIntInput <= 3 {
         return userIntInput
     }
     return nil
 }
 
+func startGame() {
+    print(GameInput.menu.rawValue, terminator: " ")
+    guard let userSelection = getUserInput()
+    else {
+        startGame()
+        return
+    }
+    if userSelection == GameSetting.gameEnd.rawValue {
+        print(GameInput.gameEnd.rawValue)
+        return
+    }
+    let computerNumber = Int.random(in: 1...3)
+}
