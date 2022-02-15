701c490ee0a3cf7884bbad19535a7a982e8ba86c
papriOS <bluecrab30@naver.com>
2022-02-15 16:08:02 +0900

    feat: inputNumberCheck 함수

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,23 +1,40 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
 func playRockPaperScissors() {
     print("가위(1),바위(2),보(3)!<종료 :0>:", terminator: "")
-    let userInput = readLine()
-    guard let userHand = userInput else {
-        print("잘못된 입력입니다. 다시 시도해 주세요.")
-        return playRockPaperScissors()
-    }
-    let computerHand = String(Int.random(in: 1...3))
-    
+    let userHand = inputValue()
+    let computerHand = makeComputerHand()
     match(userHand: userHand, computerHand: computerHand)
 }
 
 func match(userHand: String, computerHand: String) {
     
+}
+
+func inputValue() -> String {
+    guard let inputValue = readLine() else {return ""}
+    return inputValue
+}
+
+func inputNumberCheck() -> String {
+    let checkNumber = inputValue()
+    switch checkNumber {
+    case "0" :
+        print("함수를 종료합니다")
+    default :
+        print("잘못입력하셨습니다")
+        playRockPaperScissors()
+    }
+    return checkNumber
+}
+
+func makeComputerHand() -> String {
+    let computerHand = String(Int.random(in: 1...3))
+    return computerHand
 }
