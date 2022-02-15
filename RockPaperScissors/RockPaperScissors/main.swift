e4bfd93c7598db4ed4b9984cff1bfb50025ce286
papriOS <bluecrab30@naver.com>
2022-02-15 17:48:38 +0900

    feat: match함수

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,34 +10,22 @@ func playRockPaperScissors() {
     print("가위(1),바위(2),보(3)!<종료 :0>: ", terminator: "")
     let userHand = checkInputNumber()
     let computerHand = makeComputerHand()
-    match(userHand: userHand, computerHand: computerHand)
+    if userHand != 0 {
+        match(userHand: userHand, computerHand: computerHand)
+    }
 }
 
-func match(userHand: String, computerHand: String) {
-    let matchScore = (userHand, computerHand)
-    switch (matchScore) {
-    case ("1","1"):
-        print("비김")
-    case ("2","2"):
-        print("비김")
-    case ("3","3"):
-        print("비김")
-    case ("1","3"):
-        print("유저승리")
-    case ("2","1"):
-        print("유저승리")
-    case ("3","2"):
-        print("유저승리")
-    case ("3","1"):
-        print("컴퓨터승리")
-    case ("1","2"):
-        print("컴퓨터승리")
-    case ("2","3"):
-        print("컴퓨터승리")
-    default:
-        return
+func match(userHand: Int, computerHand: Int) {
+    if userHand == 0 {
+        
+    } else if userHand == computerHand {
+        print("비겼습니다")
+        playRockPaperScissors()
+    } else if userHand-1 == computerHand || userHand+2 == computerHand {
+        print("유저가 이겼습니다")
+    } else {
+        print("컴퓨터가 이겼습니다")
     }
-    
 }
 
 func inputValue() -> String {
@@ -45,11 +33,16 @@ func inputValue() -> String {
     return inputValue
 }
 
-func checkInputNumber() -> String {
-    let checkNumber = inputValue()
-    if checkNumber == "1" || checkNumber == "2" || checkNumber == "3" {
+func convertStringToInt() -> Int {
+    guard let intValue = Int(inputValue()) else { return 0 }
+    return intValue
+}
+
+func checkInputNumber() -> Int {
+    let checkNumber = convertStringToInt()
+    if checkNumber == 1 || checkNumber == 2 || checkNumber == 3 {
         return checkNumber
-    } else if checkNumber == "0" {
+    } else if checkNumber == 0 {
         print("종료합니다.")
     } else {
         print("잘못된입력입니다. 다시 시도해 주세요.")
@@ -58,8 +51,8 @@ func checkInputNumber() -> String {
     return checkNumber
 }
 
-func makeComputerHand() -> String {
-    let computerHand = String(Int.random(in: 1...3))
+func makeComputerHand() -> Int {
+    let computerHand = Int.random(in: 1...3)
     return computerHand
 }
 
