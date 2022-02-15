28c03a40810ffa296cf56866f6446492752e2695
dudu <whqtkf12@naver.com>
2022-02-15 14:15:57 +0900

    feat: add StartGame()

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -48,4 +48,20 @@ class RockPaperScissorsGame {
         return RockPaperScissorsData
     }
     
+    func startGame() {
+        var flag = true
+        
+        while flag {
+            print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: "")
+            
+            guard let computerNumber = generateComputerNumber() else {
+                continue
+            }
+            
+            guard let userNumber = inputUserNumber() else {
+                continue
+            }
+        }
+    }
+    
 }
