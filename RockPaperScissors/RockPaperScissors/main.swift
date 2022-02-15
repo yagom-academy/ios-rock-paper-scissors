eef5c3d4aa8fba728db2ca7ba7cc38263495ea03
GoMyungju171 <5211616@likelion.org>
2022-02-15 17:28:25 +0900

    refactor: 인덱스 설정하는 함수 분리, 전역 변수를 지역 변수로 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,40 +6,43 @@
 
 import Foundation
 
-var userNumber: String = ""
-let computerNumber = String(Int.random(in: 1...3))
-
 func selectGameMenu() -> String {
     print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
     guard let userInput = readLine() else { return "" }
     return userInput
 }
 
-func checkValidity() -> String {
-    userNumber = selectGameMenu()
+func checkValidity(userInput: String) -> String {
     let arrayOfValidNumbers = ["0", "1", "2", "3"]
-    if arrayOfValidNumbers.contains(userNumber) {
-        return userNumber
+    if arrayOfValidNumbers.contains(userInput) {
+        return userInput
     } else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        return checkValidity()
+        return checkValidity(userInput: selectGameMenu())
     }
 }
 
-func checkStartGame() {
-    let result = checkValidity()
-    if result == "0" {
+func checkStartGame(userInput: String) {
+    if userInput == "0" {
         print("게임 종료")
         return
     } else {
-        decideWinner()
+        decideWinner(indexArray: setIndexArray(userNumber: userInput))
     }
 }
 
-func decideWinner() {
+func setIndexArray(userNumber: String) -> [Int] {
+    let validNumber = checkValidity(userInput: userNumber)
+    let computerNumber = String(Int.random(in: 1...3))
     let indexArray = ["1","2","3","1"]
-    guard let computerIndex = indexArray.firstIndex(of: computerNumber).map({Int($0)}) else { return }
-    guard let userIndex = indexArray.firstIndex(of: userNumber).map({Int($0)}) else { return }
+    guard let computerIndex = indexArray.firstIndex(of: computerNumber).map({Int($0)}) else { return [] }
+    guard let userIndex = indexArray.firstIndex(of: validNumber).map({Int($0)}) else { return [] }
+    return [computerIndex, userIndex]
+}
+
+func decideWinner(indexArray: [Int]) {
+    let userIndex = indexArray[1]
+    let computerIndex = indexArray[0]
     if userIndex == computerIndex + 1 {
         print("이겼습니다!\n게임 종료")
     } else if userIndex == computerIndex {
@@ -49,4 +52,4 @@ func decideWinner() {
     }
 }
 
-print(checkStartGame())
+print(checkStartGame(userInput: selectGameMenu()))
