06065dae5799fe7797ece6f453b755b70f96b182
Eddy <kimkyunghun3@naver.com>
2022-02-15 14:15:37 +0900

    feat: 메뉴 출력 함수, 입력 함수, 유효성 검사 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,32 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by Doogie, Eddy
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import Foundation
 
-print("Hello, World!")
+func showMenu() {
+    print("""
+가위(1), 바위(2), 보(3)! <종료 : 0> :
+""", terminator: " ")
+}
+
+func receiveNumber() -> Int {
+    guard let inputNumber = readLine() else { return 5 }
+    if checkInputNumber(userNumber: inputNumber).bool == true {
+        return checkInputNumber(userNumber: inputNumber).userNumber ?? 5
+    } else {
+        return 5
+    }
+}
+
+func checkInputNumber(userNumber: String) -> (bool: Bool, userNumber: Int?) {
+    let trimmedNumber = userNumber.trimmingCharacters(in: .whitespaces)
+    guard let numberToInt = Int(trimmedNumber) else { return (false, nil) }
+    if numberToInt < 0 || numberToInt > 3 {
+        return (false, nil)
+    }
+    return (true, numberToInt)
+}
 
