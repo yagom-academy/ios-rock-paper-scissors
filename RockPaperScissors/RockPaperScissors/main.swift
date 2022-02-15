036d4b375a3a348fb5b70900c6dbbdba6f6df6a6
LIMGAUI <dangmon@LIMGAUIs-MacBook-Air.local>
2022-02-15 15:21:36 +0900

    feat: printInputGuidanceMessage() & receiveUserInputHandShape() 함수 구현
    * 입력시 가이드 안내사항 출력함수 구현
    * 사용자 패를 입력받는 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,11 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-print("Hello, World!")
+func printInputGuidanceMessage() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+}
+
+func receiveUserInputHandShape() -> String? {
+    let inputtedHandShape = readLine()?.replacingOccurrences(of: " ", with: "")
+    return inputtedHandShape
+}
 
