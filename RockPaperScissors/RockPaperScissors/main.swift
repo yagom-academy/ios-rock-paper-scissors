921821189fe19d3209f90c3081ae88452fae382b
Lingo <llingo@kakao.com>
2022-02-15 15:04:54 +0900

    feat: 가위, 바위, 보 게임 결과 체크 함수 구현
    checkGameResult 함수 구현 및 열거형 타입 GameDisplaySetting 항목 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,9 +6,13 @@
 
 import Foundation
 
-enum GameInput: String {
+enum GameDisplaySetting: String {
     case menu = "가위(1), 바위(2), 보(3)! <종료 : 0> :"
     case gameEnd = "게임 종료"
+    case gameWin = "이겼습니다!"
+    case gameLose = "졌습니다!"
+    case gameDraw = "비겼습니다!"
+    case gameWrongInput = "잘못된 입력입니다. 다시 시도해주세요."
 }
 
 enum GameSetting: Int {
@@ -26,6 +30,22 @@ func getUserInput() -> Int? {
     return nil
 }
 
+func checkGameResult(player playerInput: Int, computer computerInput: Int) {
+    if playerInput == computerInput {
+        print(GameDisplaySetting.gameDraw.rawValue)
+        startGame()
+    } else if (playerInput == 1 && computerInput == 2) ||
+              (playerInput == 2 && computerInput == 3) ||
+              (playerInput == 3 && computerInput == 1)
+    {
+        print(GameDisplaySetting.gameLose.rawValue)
+        print(GameDisplaySetting.gameEnd.rawValue)
+    } else {
+        print(GameDisplaySetting.gameWin.rawValue)
+        print(GameDisplaySetting.gameEnd.rawValue)
+    }
+}
+
 func startGame() {
     print(GameInput.menu.rawValue, terminator: " ")
     guard let userSelection = getUserInput()
@@ -37,5 +57,6 @@ func startGame() {
         print(GameInput.gameEnd.rawValue)
         return
     }
-    let computerNumber = Int.random(in: 1...3)
+    let computerInput = Int.random(in: 1...3)
+    checkGameResult(player: playerInput, computer: computerInput)
 }
