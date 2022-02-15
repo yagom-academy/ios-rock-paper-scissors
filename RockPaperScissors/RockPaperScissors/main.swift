94c63a386cde1ace34855bcb6b176843a852093e
marisol <marisol.develop@gmail.com>
2022-02-15 16:18:40 +0900

    refactor: 이긴사람을 결정하는 함수 리팩토링

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,60 +6,47 @@
 
 import Foundation
 
-func selectGameMenu() -> String {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-    guard let userInput = readLine() else { return "" }
+var userNumber: String = ""
+let computerNumber = String(Int.random(in: 1...3))
 
+func selectGameMenu() -> String {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
+    guard let userInput = readLine() else { return "" }
     return userInput
 }
 
 func checkValidity() -> String {
-    let userNumber = selectGameMenu()
+    userNumber = selectGameMenu()
     let arrayOfValidNumbers = ["0", "1", "2", "3"]
     if arrayOfValidNumbers.contains(userNumber) {
         return userNumber
     } else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        return selectGameMenu()
+        return checkValidity()
     }
 }
 
 func checkStartGame() {
-    if checkValidity() == "0" {
+    let result = checkValidity()
+    if result == "0" {
         print("게임 종료")
         return
     } else {
-        
+        decideWinner()
     }
 }
 
-func decideWinner1() {
-    let computerNumber = String(Int.random(in: 1...3))
-    let userNumber = checkValidity()
-    let tuple: (String, String) = (computerNumber, userNumber)
-    switch tuple {
-    case ("1","2"),("2","3"), ("3","1"):
-        print("이겼습니다!")
-    case ("1","1"), ("2","2"), ("3","3"):
-        print("비겼습니다!")
-    default:
-        print("졌습니다!")
-    }
-}
-
-func decideWinner2() {
-    let computerNumber = String(Int.random(in: 1...3))
-    let userNumber = checkValidity()
+func decideWinner() {
     let indexArray = ["1","2","3","1"]
     guard let computerIndex = indexArray.firstIndex(of: computerNumber).map({Int($0)}) else { return }
     guard let userIndex = indexArray.firstIndex(of: userNumber).map({Int($0)}) else { return }
     if userIndex == computerIndex + 1 {
-        print("이겼습니다.")
+        print("이겼습니다!\n게임 종료")
     } else if userIndex == computerIndex {
-        print("비겼습니다.")
+        print("비겼습니다!")
     } else {
-        print("졌습니다.")
-}
+        print("졌습니다!\n게임 종료")
+    }
 }
 
-print(decideWinner2())
+print(checkStartGame())
