b2b0913f1cfa081e7c5a73fc8655c21ad619a734
Lingo <llingo@kakao.com>
2022-02-15 13:55:00 +0900

    feat: 사용자로부터 입력 받는 함수 구현
    getUserInput 에서 0에서 3까지 반환 혹은 nil 반환하도록 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,19 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by Lingo, 우롱차.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import Foundation
 
-print("Hello, World!")
+func getUserInput() -> Int? {
+    let userInput = readLine()
+    
+    guard let checkInput = userInput else { return nil }
+    guard let userIntInput = Int(checkInput) else { return nil }
+    if userIntInput >= 0 && userIntInput <= 3 {
+        return userIntInput
+    }
+    return nil
+}
 
