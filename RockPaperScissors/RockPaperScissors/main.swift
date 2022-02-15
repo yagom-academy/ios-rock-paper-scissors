b2f2c141f8ff8301809f5217220fe993d8a4b734
Tiana <97kth01@naver.com>
2022-02-15 18:08:53 +0900

    refactor: 함수와 변수 네이밍 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,83 +4,59 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-enum rockPaperScissors: String {
+enum RockPaperScissors: String {
     case scissors = "1"
     case rock = "2"
     case paper = "3"
     case error
 }
 
-enum gameResult {
+enum GameResult {
     case win
     case loss
     case tie
     case error
 }
 
-func rockPaperScissorsGameStart() {
-    var ifWinnerIsFound: Bool = false
+func startRockPaperScissorsGame() {
+    var isWinnerFound: Bool = false
+    let gameoverNumber: String = "0"
     
     repeat {
-        printMenu()
-        let oneDigitComputerNumber: Int = makeOneDigitComputerNumber()
-        let userSelectedNumber: String = selectedUserGameMenuNumber()
-        if userSelectedNumber == "0" { break }
+        printGameMenu()
+        let oneDigitComputerNumber: Int = makeRandomComputerNumber()
+        let selectedUserNumber: String = selectGameMenuUserNumber()
         
-        let computerHand: rockPaperScissors = convert(number: String(oneDigitComputerNumber))
-        let userHand: rockPaperScissors = convert(number: userSelectedNumber)
-        let result: gameResult = findIfUserWin(computerNumber: computerHand, userNumber: userHand)
+        if selectedUserNumber == gameoverNumber { break }
         
-        if result == .win || result == .loss {
-            ifWinnerIsFound = true
+        let computerValue: RockPaperScissors = convertType(of: String(oneDigitComputerNumber))
+        let userValue: RockPaperScissors = convertType(of: selectedUserNumber)
+        let gameResult: GameResult = findGameResult(computerValue: computerValue, userValue: userValue)
+        
+        if gameResult == .win || gameResult == .loss {
+            isWinnerFound = true
         }
-        printResult(result: result)
-    } while !ifWinnerIsFound
+        printGameResult(gameResult)
+    } while !isWinnerFound
     print("게임 종료")
 }
 
-func printResult(result: gameResult) {
-    switch result {
-    case .win:
-        print("이겼습니다!")
-    case .loss:
-        print("졌습니다!")
-    case .tie:
-        print("비겼습니다!")
-    case .error:
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-    }
-}
-
-func printMenu() {
+func printGameMenu() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 }
 
-func makeOneDigitComputerNumber() -> Int {
+func makeRandomComputerNumber() -> Int {
     let oneDigitComputerNumber = Int.random(in: 1...3)
     return oneDigitComputerNumber
 }
 
-func selectedUserGameMenuNumber() -> String {
-    guard let userSelectedNumber = readLine() else { return "" }
-    return userSelectedNumber
+func selectGameMenuUserNumber() -> String {
+    guard let selectedUserNumber = readLine() else { return "" }
+    return selectedUserNumber
 }
 
-func findIfUserWin(computerNumber: rockPaperScissors, userNumber: rockPaperScissors) -> gameResult {
-    switch (computerNumber, userNumber) {
-    case (.rock, .paper), (.scissors, .rock), (.paper, .scissors):
-        return .win
-    case (.scissors, .paper), (.rock, .scissors), (.paper, .rock):
-        return .loss
-    case (.rock, .rock), (.scissors, .scissors), (.paper, .paper):
-        return .tie
-    default:
-        return .error
-    }
-}
-
-func convert(number: String) -> rockPaperScissors {
-    switch number {
+func convertType(of selectedNumber: String) -> RockPaperScissors {
+    switch selectedNumber {
     case "1":
         return .scissors
     case "2":
@@ -92,4 +68,30 @@ func convert(number: String) -> rockPaperScissors {
     }
 }
 
-rockPaperScissorsGameStart()
+func findGameResult(computerValue: RockPaperScissors, userValue: RockPaperScissors) -> GameResult {
+    switch (computerValue, userValue) {
+    case (.rock, .paper), (.scissors, .rock), (.paper, .scissors):
+        return .win
+    case (.scissors, .paper), (.rock, .scissors), (.paper, .rock):
+        return .loss
+    case (.rock, .rock), (.scissors, .scissors), (.paper, .paper):
+        return .tie
+    default:
+        return .error
+    }
+}
+
+func printGameResult(_ gameResult: GameResult) {
+    switch gameResult {
+    case .win:
+        print("이겼습니다!")
+    case .loss:
+        print("졌습니다!")
+    case .tie:
+        print("비겼습니다!")
+    case .error:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
+
+startRockPaperScissorsGame()
