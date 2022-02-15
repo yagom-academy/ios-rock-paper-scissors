f72f39e52b4dc6059cdc385990b5ba57c75ca7d6
doogie <chris_97@naver.com>
2022-02-15 20:27:59 +0900

    Revert "refactor: enum 구현에 따른 함수 수정, 네이밍 수정"
    This reverts commit 9760ecc0f028e723e28eebb4092c365024aab0d1.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,100 +6,44 @@
 
 import Foundation
 
-enum GameValue: Int {
-    case exit = 0
-    case scissor = 1
-    case rock = 2
-    case paper = 3
-    case error
-}
-
-enum GameResult {
-    case exit
-    case win
-    case draw
-    case lose
-    case error
-}
-
 func showMenu() {
     print("""
 가위(1), 바위(2), 보(3)! <종료 : 0> :
 """, terminator: " ")
-    showResult(gameResult: compareValues(userValue: makeUserValue(), computerValue: makeComputerValue()))
+    selectMenu()
 }
 
-func showResult(gameResult: GameResult) {
-    switch gameResult {
-    case .win:
-        print("이겼습니다!")
+func receiveNumber() -> Int {
+    guard let inputNumber = readLine() else { return 5 }
+    let userInput = convertNumber(inputNumber: inputNumber)
+    return userInput
+}
+
+func convertNumber(inputNumber: String) -> Int {
+    let trimmedNumber = inputNumber.trimmingCharacters(in: .whitespaces)
+    guard let numberToInt = Int(trimmedNumber) else { return 5 }
+    return numberToInt
+}
+
+func selectMenu() {
+    let number = receiveNumber()
+    switch number {
+    case 0:
         print("게임 종료")
-    case .draw:
-        print("비겼습니다!")
-        showMenu()
-    case .lose:
-        print("졌습니다!")
-        print("게임 종료")
-    case .exit:
-        print("게임 종료")
-    case .error:
+        break
+    case 1:
+        print("1")
+    case 2:
+        print("2")
+    case 3:
+        print("3")
+    default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
         showMenu()
     }
 }
 
-func compareValues(userValue: GameValue, computerValue: GameValue) -> GameResult {
-    switch (userValue, computerValue) {
-    case (.scissor, .paper), (.rock, .scissor), (.paper, .rock):
-        return .win
-    case (.scissor, .scissor), (.rock, .rock), (.paper, .paper):
-        return .draw
-    case (.paper, .scissor), (.scissor, .rock), (.rock, .paper):
-        return .lose
-    case (.exit, _):
-        return .exit
-    default:
-        return .error
-    }
+func makeRandomNumber() -> Int {
+    let computerRandomNumber = Int.random(in: 1...3)
+    return computerRandomNumber
 }
-
-func makeUserValue() -> GameValue {
-    let userNumber = receiveNumber()
-    switch userNumber {
-    case "0":
-        return .exit
-    case "1":
-        return .scissor
-    case "2":
-        return .rock
-    case "3":
-        return .paper
-    default:
-        return .error
-    }
-}
-
-func makeComputerValue() -> GameValue {
-    let computerNumber = makeRandomNumber()
-    switch computerNumber {
-    case "1":
-        return .scissor
-    case "2":
-        return .rock
-    case "3":
-        return .paper
-    default:
-        return .error
-    }
-}
-
-func receiveNumber() -> String {
-    guard let inputNumber = readLine() else { return " " }
-    return inputNumber
-}
-
-func makeRandomNumber() -> String {
-    return String(Int.random(in: 1...3))
-}
-
-showMenu()
