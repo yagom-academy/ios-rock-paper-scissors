eddc6f9724781c40e7b5bbe6471adee2cc5c2c87
Quokkaa <rktrkdml@gmail.com>
2022-02-16 15:25:05 +0900

    refactor: 연관된 타입 묶기 및 비교 타입 수정
    * verifiedUserHandShape()함수 부터 반환값을 HandGame.Shapes로 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,69 +1,76 @@
 import Foundation
-
-enum Hands: CaseIterable {
-    case rock, paper, scissors
+struct HandGame {
+    enum Shapes: CaseIterable, Equatable {
+        case rock, paper, scissors
+        // zero를 넣었더니 generated임의의수 생성할떄도 같이 섞여서 오류남
+        
+        var description: String {
+            switch self {
+            case .scissors:
+                return "가위"
+            case .rock:
+                return "바위"
+            case .paper:
+                return "보"
+            }
+        }
+    }
     
-    var description: String {
-        switch self {
-        case .scissors:
-            return "가위"
-        case .rock:
-            return "바위"
-        case .paper:
-            return "보"
+    enum Guide: Equatable {
+        case message, one, two, three, zero
+        
+        var description: String {
+            switch self {
+            case .message:
+                return "가위(1), 바위(2), 보(3)! <종료 : 0> : "
+            case .one:
+                return "1"
+            case .two:
+                return "2"
+            case .three:
+                return "3"
+            case .zero:
+                return "0"
+            }
+        }
+    }
+    
+    enum Result {
+        case win, lose, draw, endGame
+        
+        var description: String {
+            switch self {
+            case .win:
+                return "이겼습니다!"
+            case .lose:
+                return "졌습니다!"
+            case .draw:
+                return "비겼습니다!"
+            case .endGame:
+                return "게임종료"
+            }
+        }
+    }
+    
+    enum Error {
+        case wrongInput
+        
+        var description: String {
+            switch self {
+            case .wrongInput:
+                return "잘못된 입력입니다. 다시 시도해주세요."
+            }
         }
     }
 }
-
-enum GameGuide {
-    case message
-    
-    var description: String {
-        switch self {
-        case .message:
-            return "가위(1), 바위(2), 보(3)! <종료 : 0> : "
-        }
-    }
-}
-
-enum GameResult {
-    case win, lose, draw, endGame
-    
-    var description: String {
-        switch self {
-        case .win:
-            return "이겼습니다!"
-        case .lose:
-            return "졌습니다!"
-        case .draw:
-            return "비겼습니다!"
-        case .endGame:
-            return "게임종료"
-        }
-    }
-}
-
-enum GameError {
-    case wrongInput
-    
-    var description: String {
-        switch self {
-        case .wrongInput:
-            return "잘못된 입력입니다. 다시 시도해주세요."
-        }
-    }
-}
-
 func startGame() {
     printInputGuidanceMessage()
-    guard let userHandShape = verifiedUserHandShape(receiveUserInputHandShape()) else {
-        return
-    }
+    let userHandShape = verifiedUserHandShape(receiveUserInputHandShape())
     let computerHand = generatedRandomHandShape()
     if isNumberZero(input: userHandShape) {
-        print(GameResult.endGame.description)
+        print(HandGame.Result.endGame.description)
         return
-    } else if userHandShape == GameError.wrongInput.description {
+    } else if userHandShape.description == HandGame.Guide.zero.description {
         startGame()
     } else {
         compareHandShape(userHand: userHandShape, computerHand: computerHand)
@@ -71,7 +78,7 @@ func startGame() {
 }
 
 func printInputGuidanceMessage() {
-    print(GameGuide.message.description, terminator: "")
+    print(HandGame.Guide.message.description, terminator: "")
 }
 
 func receiveUserInputHandShape() -> String? {
@@ -79,47 +86,53 @@ func receiveUserInputHandShape() -> String? {
     return inputtedHandShape
 }
 
-func isNumberZero(input: String) -> Bool {
-    if input == "0" {
+func isNumberZero(input: HandGame.Shapes) -> Bool {
+    if input.description == HandGame.Guide.zero.description {
         return true
     }
     return false
 }
 
-func verifiedUserHandShape(_ input: String?) -> String? {
+func verifiedUserHandShape(_ input: String?) -> HandGame.Shapes {
     switch input {
     case "1":
-        return Hands.scissors.description
+        return HandGame.Shapes.scissors
     case "2":
-        return Hands.rock.description
+        return HandGame.Shapes.rock
     case "3":
-        return Hands.paper.description
+        return HandGame.Shapes.paper
     case "0" :
-        return input
+        return HandGame.Guide.zero // 매개변수 안받고, 재귀함수로 진행하면 가능할수도?
     default:
-        print(GameError.wrongInput.description)
-        return GameError.wrongInput.description
+        print(HandGame.Error.wrongInput.description)
+        return HandGame.Shapes.zero // 매개변수 안받고, 재귀함수로 진행하면 가능할수도?
     }
 }
 
-func generatedRandomHandShape() -> String {
-    if let handShape = Hands.allCases.randomElement()?.description {
+func generatedRandomHandShape() -> HandGame.Shapes {
+    if let handShape = HandGame.Shapes.allCases.randomElement() {
         return handShape
     }
     return generatedRandomHandShape()
 }
 
-func compareHandShape(userHand: String, computerHand: String) {
+func compareHandShape(userHand: HandGame.Shapes, computerHand: HandGame.Shapes) {
     if userHand == computerHand {
-        print(GameResult.draw.description)
+        print(HandGame.Result.draw.description)
+        startGame()
     } else if (userHand, computerHand) ==
-                (Hands.scissors.description, Hands.paper.description) ||
+                (HandGame.Shapes.scissors, HandGame.Shapes.paper) ||
                 (userHand, computerHand) ==
-                (Hands.rock.description, Hands.scissors.description) ||
+                (HandGame.Shapes.rock, HandGame.Shapes.scissors) ||
                 (userHand, computerHand) ==
-                (Hands.paper.description, Hands.rock.description) {
-        print(GameResult.win.description)
+                (HandGame.Shapes.paper, HandGame.Shapes.rock) {
+        print(HandGame.Result.win.description)
+        print(HandGame.Result.endGame.description)
+        return
     } else {
-        print(GameResult.lose.description)
+        print(HandGame.Result.lose.description)
+        print(HandGame.Result.endGame.description)
+        return
     }
 }
+startGame()
