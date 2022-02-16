6b4f56d21f34478780fea06fb70b2a6d464cc791
saafaaari <111dltldnjs@naver.com>
2022-02-16 17:32:49 +0900

    refactor :  Result타입 내부 값 수정 및 승패비교 방법 수정
    Result타입 내부 값 네임스페이스 사용
    승리 케이스를 배열에 담은후 contains()함수를 이용하여 판단
    makeRandomCard()함수 반환 타입 String에서 Card로 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,10 +6,10 @@
 
 import Foundation
 
-enum Result: Int {
-    case win = 1
-    case draw = 0
-    case lose = -1
+enum Result {
+    static let win = "이겼습니다."
+    static let draw = "비겼습니다."
+    static let lose = "졌습니다."
 }
 
 enum Card: String {
@@ -27,8 +27,12 @@ func inputSelectionCard() {
     printMenu()
     let userInput: String? = readLine()
     switch userInput {
-    case Card.scissors.rawValue, Card.rock.rawValue, Card.paper.rawValue:
-        playGame(selectedCard: userInput)
+    case Card.scissors.rawValue:
+        playGame(selectedCard: Card.scissors)
+    case Card.rock.rawValue:
+        playGame(selectedCard: Card.rock)
+    case Card.paper.rawValue:
+        playGame(selectedCard: Card.paper)
     case Command.terminator.rawValue:
         print("게임 종료")
     case .none:
@@ -43,35 +47,39 @@ func printMenu() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
 }
 
-func playGame(selectedCard: String?) {
-    guard let computerCard: Int = (makeRandomCard().flatMap{ Int($0) }) else { return }
-    guard let userCard: Int = (selectedCard.flatMap{ Int($0) }) else { return }
+func playGame(selectedCard: Card?) {
+    guard let computerCard: Card = makeRandomCard() else { return }
+    guard let userCard: Card = selectedCard else { return }
     printResult(gameResult: compareEachCard(userCard: userCard, computerCard: computerCard))
 }
 
-func makeRandomCard() -> String? {
+func makeRandomCard() -> Card? {
     let randomCard: Card = Card.cases[Int.random(in: 0..<Card.cases.count)]
-    return randomCard.rawValue
+    return randomCard
 }
 
-func printResult(gameResult: Result) {
+func printResult(gameResult: String) {
     switch gameResult {
     case Result.draw:
-        print("비겼습니다.")
+        print(Result.draw)
         inputSelectionCard()
     case Result.win:
-        print("이겼습니다.")
+        print(Result.win)
         print("게임 종료")
     case Result.lose:
-        print("졌습니다.")
+        print(Result.lose)
         print("게임 종료")
+    default: return
     }
+    
 }
 
-func compareEachCard(userCard: Int, computerCard: Int) -> Result {
+func compareEachCard(userCard: Card, computerCard: Card) -> String {
+    let winCase = [[Card.scissors, Card.paper],[Card.rock, Card.scissors],[Card.paper, Card.rock]]
+    let cardPair = [userCard, computerCard]
     if userCard == computerCard {
         return Result.draw
-    } else if (computerCard - userCard) == -1 || (computerCard - userCard) == 2 {
+    } else if winCase.contains(cardPair) {
         return Result.win
     } else {
         return Result.lose
