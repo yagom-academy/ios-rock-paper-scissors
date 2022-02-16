0d5be249d7fe9d511e5a0d7d1a66dfdb69afe87f
xogns8696@gmail.com <xogns8696@gmail.com>
2022-02-16 17:23:59 +0900

    ♻️[REFACTOR] : all refactoring
    compare func 추가 및 userInput func 추가, enum RockScissorsPaper 추가, isProgramExit를 bool 타입으로 선언하여 판단하도록 변경

--- RockPaperScissors/RockPaperScissors/StartMenu.swift
+++ RockPaperScissors/RockPaperScissors/StartMenu.swift
@@ -8,50 +8,96 @@
 import Foundation
 
 var computerRockScissorsPaper: ClosedRange<Int> = 1...3
+var isProgramExit = false
 
+enum RockScissorsPaperGameView: String {
+    case gameMenu = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
+}
 
-func compare(userInput: String?) {
-    let computerCard = Int.random(in: computerRockScissorsPaper)
+enum RockScissorsPaper: CaseIterable {
+    case scissors
+    case rock
+    case paper
     
-    guard let userInput = userInput else {
-        return
+    func judgeWinAndLose() -> RockScissorsPaper {
+        switch self {
+        case .scissors:
+            return .rock
+        case .rock:
+            return .paper
+        case .paper:
+            return .scissors
+        }
     }
-
-    if computerCard == Int(userInput) {
-        print("비겼습니다!")
+    
+    static func convertUserCard(userInput: String?) -> RockScissorsPaper? {
+        switch userInput {
+        case "1":
+            return .scissors
+        case "2":
+            return .rock
+        case "3":
+            return .paper
+        default:
+            return nil
+        }
     }
-    if computerCard == 1 && Int(userInput) == 2 {
-        print("이겼습니다!")
-    }
-    if computerCard == 1 && Int(userInput) == 3 {
-        print("졌습니다!")
-    }
-    if computerCard == 2 && Int(userInput) == 3 {
-        print("이겼습니다!")
-    }
-    if computerCard == 3 && Int(userInput) == 1 {
-        print("이겼습니다!")
-    }
-    if computerCard == 2 && Int(userInput) == 1 {
-        print("졌습니다!")
-    }
-    if computerCard == 3 && Int(userInput) == 2 {
-        print("졌습니다!")
+    
+    static func createRandomCard() -> RockScissorsPaper? {
+        guard let randomCard = RockScissorsPaper.allCases.randomElement() else {
+            print("생성에 실패했습니다.")
+            return nil
+        }
+        return randomCard
     }
 }
 
-func printMenu() {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-    
-    let userInput = readLine()
-    
-    switch userInput {
-    case "1","2","3":
-        compare(userInput: userInput)
-    case "0":
+func startRockScissorsPaperGame(userInputValues: String?) {
+    guard let computerCard = creatComputerCard() else {
+        print("생성에 실패했습니다.")
+        return
+    }
+    guard let userCard: RockScissorsPaper = .convertUserCard(userInput: userInputValues) else {
+        print("변환에 실패했습니다.")
+        return
+    }
+    compare(userCard: userCard, computerCard: computerCard)
+}
+
+func compare(userCard: RockScissorsPaper, computerCard: RockScissorsPaper) {
+    if userCard == computerCard{
+        print("비겼습니다.")
+    } else if userCard == computerCard.judgeWinAndLose() {
+        print("이겼습니다!")
         print("종료되었습니다.")
-    default:
-        print("입력이 잘못되었습니다.")
+        isProgramExit = true
+    } else{
+        print("졌습니다!")
+        print("종료되었습니다.")
+        isProgramExit = true
+    }
+}
+    
+func creatComputerCard() -> RockScissorsPaper? {
+    guard let computerCard: RockScissorsPaper = .createRandomCard() else {
+        return nil
+    }
+    return computerCard
+}
+
+func printMenu() {
+    while isProgramExit == false {
+        print(RockScissorsPaperGameView.gameMenu.rawValue, terminator: "")
+        let userInput = readLine()
+        switch userInput {
+        case "1","2","3":
+            startRockScissorsPaperGame(userInputValues: userInput)
+        case "0":
+            print("종료되었습니다.")
+            isProgramExit = true
+        default:
+            print("입력이 잘못되었습니다.")
+        }
     }
 }
 
