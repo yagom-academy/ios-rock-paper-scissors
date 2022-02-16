eddc89b5814e4388806ec41a76882d0ccfb440f1
Roy-wonji <shuwj81@daum.net>
2022-02-16 16:39:47 +0900

    feat: Player() 함수 구현 및  파일 분리

---
+++ RockPaperScissors/RockPaperScissors/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  RockPaperScissors
+//
+//  Created by 서원지 on 2022/02/16.
+//
+
+import Foundation
+
+enum Player: String {
+    case user = "사용자"
+    case computer = "컴퓨터"
+    case none = "플레이어 없음"
+}
+
