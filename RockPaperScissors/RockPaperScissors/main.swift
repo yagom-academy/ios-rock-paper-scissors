e578cb897363b9cc5e879e33d63eb2c0ad4730e6
Tiana <97kth01@naver.com>
2022-02-16 15:33:16 +0900

    feat: print묵찌빠gameMenu 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -95,6 +95,10 @@ func printGameResult(_ gameResult: GameResult) {
     }
 }
 
+func print묵찌빠gameMenu(turnValue: String) {
+    print("[\(turnValue) 턴] 묵(1), 찌(2), 빠(3)! <종료: 0>: ", terminator: "")
+}
+
 startRockPaperScissorsGame()
 
 
