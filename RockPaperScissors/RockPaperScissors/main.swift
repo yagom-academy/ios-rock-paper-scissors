40fb4120a2679f088b7c01ce809c6e2970e6538a
marisol <marisol.develop@gmail.com>
2022-02-16 14:46:43 +0900

    refactor: 함수 / 상수 네이밍 수정, Message 열거형 분리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,41 +6,47 @@
 
 import Foundation
 
-enum Message: String {
-    case rockScissorsPaper = "가위(1), 바위(2), 보(3)! <종료 : 0>"
-    case wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
-    case endOfGame = "게임 종료"
-    case win = "이겼습니다!\n게임 종료"
-    case defeat = "졌습니다!\n게임 종료"
-    case draw = "비겼습니다!"
+enum StartAndEndMessage {
+    static let startOfGame = "가위(1), 바위(2), 보(3)! <종료 : 0>"
+    static let endOfGame = "게임 종료"
 }
 
-enum PunctuationMarks: String {
-    case colon = " : "
-    case emptyString = ""
+enum ErrorMessage {
+    static let wrongInput = "잘못된 입력입니다. 다시 시도해주세요."
+}
+
+enum GameResultMessage {
+    static let win = "이겼습니다!\n게임 종료"
+    static let defeat = "졌습니다!\n게임 종료"
+    static let draw = "비겼습니다!"
+}
+
+enum PunctuationMarks {
+    static let colon = " : "
+    static let emptyString = ""
 }
 
 func selectGameMenu() -> String {
-    print(Message.rockScissorsPaper.rawValue, terminator: PunctuationMarks.colon.rawValue)
-    guard let userInput = readLine() else { return PunctuationMarks.emptyString.rawValue }
+    print(StartAndEndMessage.startOfGame, terminator: PunctuationMarks.colon)
+    guard let userInput = readLine() else { return PunctuationMarks.emptyString }
     return userInput
 }
 
-func checkValidity(userInput: String) -> Int? {
-    let arrayOfValidNumbers = ["0", "1", "2", "3"]
-    if arrayOfValidNumbers.contains(userInput) {
+func checkValidity(_ userInput: String) -> Int? {
+    let validNumbersForGame = ["0", "1", "2", "3"]
+    if validNumbersForGame.contains(userInput) {
         guard let intUserInput = Int(userInput) else { return nil }
         return intUserInput
     } else {
-        print(Message.wrongInput.rawValue)
-        return checkValidity(userInput: selectGameMenu())
+        print(ErrorMessage.wrongInput)
+        return checkValidity(selectGameMenu())
     }
 }
 
 func startGame(userInput: String) {
     let endingNumber = "0"
     if userInput == endingNumber {
-        print(Message.endOfGame.rawValue)
+        print(StartAndEndMessage.endOfGame)
         return
     } else {
         decideWinner(userNumber: userInput)
@@ -48,17 +54,17 @@ func startGame(userInput: String) {
 }
 
 func decideWinner(userNumber: String) {
-    guard let validNumber = checkValidity(userInput: userNumber) else { return }
-    let numberRange: ClosedRange<Int> = 1...3
-    let computerNumber = Int.random(in: numberRange)
-    let number: Int = validNumber - computerNumber
-    switch number {
+    guard let validNumber = checkValidity(userNumber) else { return }
+    let allowedNumberRange: ClosedRange<Int> = 1...3
+    let computerNumber = Int.random(in: allowedNumberRange)
+    let numberToDecideWinner: Int = validNumber - computerNumber
+    switch numberToDecideWinner {
     case 1, -2:
-        print(Message.win.rawValue)
+        print(GameResultMessage.win)
     case 2, -1:
-        print(Message.defeat.rawValue)
+        print(GameResultMessage.defeat)
     default:
-        print(Message.draw.rawValue)
+        print(GameResultMessage.draw)
     }
 }
 
