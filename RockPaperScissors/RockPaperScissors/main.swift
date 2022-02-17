02c1e602943e8af7a92d29cd0594999510c8b5b5
saafaaari <111dltldnjs@naver.com>
2022-02-17 19:33:50 +0900

    feat : MukChiBaGame타입 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,6 +6,8 @@
 
 import Foundation
 
+
+
 enum Result: String {
     case win = "이겼습니다."
     case draw = "비겼습니다."
@@ -54,29 +56,24 @@ struct Player {
     func getName() -> String {
         return name
     }
-    
-    func makeRandomCard() -> Card {
-        let randomCard: Card = Card.cases[Int.random(in: 0..<Card.cases.count)]
-        return randomCard
-    }
-    
-    mutating func setRandomCard() {
-        let randomCard: Card = makeRandomCard()
-        card = randomCard
-    }
-    
-    mutating func setCardFromInput(selectedCard: Card) {
+        
+    mutating func setCard(selectedCard: Card) {
         //guard let userCard: Card = selectedCard else { return }
         card = selectedCard
     }
 }
 
-struct Game {
+class Game {
+    var winner: String = ""
     var user: Player
     var computer: Player
-    var turn: String
     
-    mutating func inputSelectionCard() {
+    init(user: Player, computer: Player){
+        self.user = user
+        self.computer = computer
+    }
+    
+    func inputSelectionCard() {
         printMenu()
         guard let userInput: String = readLine() else { return }
         if userInput == Command.terminator.rawValue {
@@ -90,82 +87,122 @@ struct Game {
         }
         playGame(selectedCard: userCard)
     }
+    
+    func makeRandomCard() -> Card {
+        let randomCard: Card = Card.cases[Int.random(in: 0..<Card.cases.count)]
+        return randomCard
+    }
 
     func printMenu() {
-        if turn.isEmpty {
-            print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
-        } else {
-            print("[\(turn) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0>", terminator: " : ")
-        }
+        print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
     }
 
-    mutating func playGame(selectedCard: Card) {
-        computer.setRandomCard()
-        user.setCardFromInput(selectedCard: selectedCard)
+    func playGame(selectedCard: Card) {
+        computer.setCard(selectedCard: makeRandomCard())
+        user.setCard(selectedCard: selectedCard)
         
         guard let userCard: Card = user.getCard() else { return }
         guard let computerCard: Card = computer.getCard() else { return }
         
         let resultOfCompare = userCard.compareEachCard(computerCard: computerCard)
         printResult(gameResult: resultOfCompare)
+        
+        
     }
     
-    mutating func playMukChiBa(selectedCard: Card) {
-        computer.setRandomCard()
-        user.setCardFromInput(selectedCard: selectedCard)
-        
-        guard let userCard: Card = user.getCard() else { return }
-        guard let computerCard: Card = computer.getCard() else { return }
-        
-        let resultOfCompare = userCard.compareEachCard(computerCard: computerCard)
-        printResult(gameResult: resultOfCompare)
-    }
+//    func playMukChiBa(selectedCard: Card) {
+//        computer.setCard(selectedCard: makeRandomCard())
+//        user.setCard(selectedCard: selectedCard)
+//
+//        guard let userCard: Card = user.getCard() else { return }
+//        guard let computerCard: Card = computer.getCard() else { return }
+//
+//        let resultOfCompare = userCard.compareEachCard(computerCard: computerCard)
+//        printResult(gameResult: resultOfCompare)
+//    }
 
-    mutating func printResult(gameResult: Result) {
+    func printResult(gameResult: Result) {
         switch gameResult {
         case Result.draw:
-            print("비겼습니다.")
+            gameResult.printMessage()
             inputSelectionCard()
         case Result.win:
-            print("이겼습니다.")
-            turn = user.getName()
-            startMukChiBa()
+            winner = user.getName()
+            gameResult.printMessage()
         case Result.lose:
-            print("졌습니다.")
-            turn = computer.getName()
-            startMukChiBa()
+            winner = computer.getName()
+            gameResult.printMessage()
+            
         }
     }
     
-    func convertValue(input: String) -> String {
-        switch input {
-        case "1": return Card.rock.rawValue
-        case "2": return Card.scissors.rawValue
-        case "3": return Card.paper.rawValue
-        default: return input
+//    func convertValue(input: String) -> String {
+//        switch input {
+//        case "1": return Card.rock.rawValue
+//        case "2": return Card.scissors.rawValue
+//        case "3": return Card.paper.rawValue
+//        default: return input
+//        }
+//    }
+    
+//    func startMukChiBa() {
+//        printMenu()
+//
+//        guard let userInput: String = readLine() else { return }
+//        if userInput == Command.terminator.rawValue {
+//            print("게임 종료")
+//            return
+//        }
+//        guard let userCard: Card = Card(rawValue: convertValue(input: userInput)) else {
+//            print("잘못된 입력입니다. 다시 시도해주세요.")
+//            startMukChiBa()
+//            return
+//        }
+//        //playGame(selectedCard: userCard)
+//        print(userCard.rawValue)
+//    }
+}
+
+class MukChiBaGame: Game {
+    var turn: String = ""
+    
+    func setTurn(name: String) {
+        turn = name
+    }
+    
+    override func printMenu() {
+        print("[\(turn) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0>", terminator: " : ")
+    }
+    
+    override func printResult(gameResult: Result) {
+        switch gameResult {
+        case Result.draw:
+            print(turn + "승리")
+        case Result.win:
+            setTurn(name: user.getName())
+            print(turn + "의 턴입니다.")
+            inputSelectionCard()
+        case Result.lose:
+            setTurn(name: computer.getName())
+            print(turn + "의 턴입니다.")
+            inputSelectionCard()
         }
     }
     
-    mutating func startMukChiBa() {
-        printMenu()
-        
-        guard let userInput: String = readLine() else { return }
-        if userInput == Command.terminator.rawValue {
-            print("게임 종료")
-            return
-        }
-        guard let userCard: Card = Card(rawValue: convertValue(input: userInput)) else {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            startMukChiBa()
-            return
-        }
-        //playGame(selectedCard: userCard)
-        print(userCard.rawValue)
-    }
+    
+    
+    
+    //게임결과 판단 기능
+    //입력값 변환 묵찌바 -> 가위바위보
 }
 
 var user: Player = Player(name: "사용자")
 var computer: Player = Player(name: "컴퓨터")
 
-var newGame: Game = Game(user: user, computer: computer, turn: "")
+var newGame: Game = Game(user: user, computer: computer)
+
+var sGame: MukChiBaGame = MukChiBaGame(user: user, computer: computer)
 newGame.inputSelectionCard()
+sGame.setTurn(name: newGame.winner)
+sGame.inputSelectionCard()
+
