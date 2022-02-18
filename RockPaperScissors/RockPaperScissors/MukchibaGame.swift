f04d1fc693ff440304ef33786f28cf11989a4c5a
dudu <whqtkf12@naver.com>
2022-02-18 23:00:06 +0900

    refactor: Game Class 삭제

--- RockPaperScissors/RockPaperScissors/MukchibaGame.swift
+++ RockPaperScissors/RockPaperScissors/MukchibaGame.swift
@@ -5,7 +5,7 @@
 //  Created by 김동욱 on 2022/02/16.
 //
 
-final class MukchibaGame: Game {
+final class MukchibaGame: RockPaperScissorsGame {
     
     var turn: Turn
     
@@ -13,12 +13,46 @@ final class MukchibaGame: Game {
         self.turn = turn
     }
     
+    override func start() {
+        printGameGuide()
+        let userInput = inputUserNumber()
+        
+        switch userInput {
+        case 0:
+            print("게임 종료")
+        case 1, 2, 3:
+            compareSigns(userInput: userInput)
+        default:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            changeTurn(to: .computer)
+            start()
+        }
+    }
+    
     override func printGameGuide() {
         print("[\(turn.message) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :", terminator: "")
     }
     
-    override func changeTurn(to player: Turn) {
+    func changeTurn(to player: Turn) {
         turn = player
+    }
+    
+    override func makeUserSign(userInput: Int?) -> RockPaperScissors? {
+        guard let userNumber = userInput else {
+            return nil
+        }
+        
+        guard var userSign = RockPaperScissors(rawValue: userNumber) else {
+            return nil
+        }
+        
+        if userSign == .rock {
+            userSign = .scissors
+        } else if userSign == .scissors {
+            userSign = .rock
+        }
+    
+        return userSign
     }
     
     override func printGameResult(gameResult: GameResult) {
--- RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.swift
@@ -5,13 +5,93 @@
 //  Created by 김동욱 on 2022/02/16.
 //
 
-final class RockPaperScissorsGame: Game {
+class RockPaperScissorsGame {
     
-    override func printGameGuide() {
+    func start() {
+        printGameGuide()
+        let userInput = inputUserNumber()
+        
+        switch userInput {
+        case 0:
+            print("게임 종료")
+        case 1, 2, 3:
+            compareSigns(userInput: userInput)
+        default:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            start()
+        }
+    }
+    
+    func printGameGuide() {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: "")
     }
     
-    override func printGameResult(gameResult: GameResult) {
+    func inputUserNumber() -> Int? {
+        guard let userInput = readLine() else {
+            return nil
+        }
+        
+        guard let userNumber = Int(userInput) else {
+            return nil
+        }
+        
+        return userNumber
+    }
+    
+    func compareSigns(userInput: Int?) {
+        guard let computerSign = makeComputerSign() else {
+            return
+        }
+        
+        guard let userSign = makeUserSign(userInput: userInput) else {
+            return
+        }
+        
+        let gameResult = decideResult(computerSign: computerSign, userSign: userSign)
+        printGameResult(gameResult: gameResult)
+    }
+    
+    func makeComputerSign() -> RockPaperScissors? {
+        guard let randomSign = RockPaperScissors.allCases.randomElement() else {
+            return nil
+        }
+        
+        return randomSign
+    }
+    
+    func makeUserSign(userInput: Int?) -> RockPaperScissors? {
+        guard let userNumber = userInput else {
+            return nil
+        }
+        
+        guard let userSign = RockPaperScissors(rawValue: userNumber) else {
+            return nil
+        }
+        
+        return userSign
+    }
+    
+    func decideResult(computerSign: RockPaperScissors, userSign: RockPaperScissors) -> GameResult {
+        if userSign == computerSign {
+            return .draw
+        }
+        
+        if userSign == .scissors {
+            return computerSign == .paper ? .win : .lose
+        }
+        
+        if userSign == .paper {
+            return computerSign == .rock ? .win : .lose
+        }
+        
+        if userSign == .rock {
+            return computerSign == .scissors ? .win : .lose
+        }
+        
+        return .draw
+    }
+    
+    func printGameResult(gameResult: GameResult) {
         
         print(gameResult.message)
         
@@ -26,4 +106,5 @@ final class RockPaperScissorsGame: Game {
             start()
         }
     }
+    
 }
