f0c24b93dc712d1a26fb3fe219c0c15ef6163b0c
웡빙 <wb277@naver.com>
2022-04-18 16:57:53 +0900

    feat: 게임 클래스와 가위바위보 열거형 생성

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.swift
@@ -0,0 +1,22 @@
+//
+//  RockPaperScissorsGame.swift
+//  RockPaperScissors
+//
+//  Created by 이원빈 on 2022/04/18.
+//
+
+import Foundation
+
+class RockPaperScissorsGame {
+    var userNumber: String
+    var computerNumber: String
+    
+    init(userNumber: String, computerNumber: String) {
+        self.userNumber = userNumber
+        self.computerNumber = computerNumber
+    }
+}
+
+extension RockPaperScissorsGame {
+    
+}
--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,5 @@
 
 import Foundation
 
-print("Hello, World!")
+
 
