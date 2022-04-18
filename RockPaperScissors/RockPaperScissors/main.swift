9b7366bbb30ec757a1a8a706378472338203e4d6
bard <schy2503@naver.com>
2022-04-18 15:01:20 +0900

    feat: 최초 출력문 함수 및, 유저 입력값을 받는 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -5,3 +5,17 @@
 // 
 
 import Foundation
+
+func printMenu() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+}
+
+func inputMenu() -> String {
+    let userInput = readLine()
+    print(userInput)
+    guard let menuNumber = userInput  else { return "asdfsf" }  // 질문: 아무것도 입력을 안했을 때else 안의 return값이 안나오고 optional("")이 나오는 이유
+    return menuNumber
+}
+
+printMenu()
+inputMenu()
