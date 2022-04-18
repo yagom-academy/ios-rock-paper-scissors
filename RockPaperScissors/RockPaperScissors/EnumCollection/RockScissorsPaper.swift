f2bc2691c108afe0f225365389f7d2d903a6c0da
starting1231 <starting1231@naver.com>
2022-04-18 16:41:31 +0900

    feat: RockScissorsPaper 파일 생성 및 RockScissorsPaper 열거형 구현

---
+++ RockPaperScissors/RockPaperScissors/EnumCollection/RockScissorsPaper.swift
@@ -0,0 +1,14 @@
+//
+//  Option.swift
+//  RockPaperScissors
+//
+//  Created by NAMU on 2022/04/18.
+//
+
+import Foundation
+
+enum RockScissorsPaper: Int {
+    case scissors = 1
+    case rock = 2
+    case paper = 3
+}
