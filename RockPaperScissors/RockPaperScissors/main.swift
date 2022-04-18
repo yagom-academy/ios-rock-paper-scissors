3aaa733ae142f7664abf985716de2825d592cfd5
finnn1 <tgss3535@gmail.com>
2022-04-18 16:41:28 +0900

    feat: 게임시작,선택출력,컴퓨터숫자생성,입력값검증,승패판단,승패출력,종료출력 함수추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,76 @@
 
 import Foundation
 
-print("Hello, World!")
+let randomNumberRange = 1...3
+var continueGameCheck = true
+let selectMenu = "0123"
 
+func startGame() {
+    
+    while continueGameCheck {
+        
+        printUserInterface()
+        let userInputValue = readLine()?.trimmingCharacters(in: .whitespaces) ?? ""
+        
+        if userInputValue == "0" {
+            printGameOver()
+            continueGameCheck = false
+            break
+        } else {
+            if verifyUserInput(userInput: userInputValue) {
+                
+            } else {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+            }
+        }
+    }
+    
+}
+
+func printUserInterface() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+}
+
+func generateComputerNumber() -> Int {
+    var randomNumber = Int.random(in: randomNumberRange)
+    return randomNumber
+}
+
+func verifyUserInput(userInput: String) -> Bool {
+    if userInput.contains(selectMenu) {
+        return true
+    } else {
+        return false
+    }
+}
+
+func printGameOver() {
+    print("게임 종료")
+}
+
+func judgeMatchResult(inputUserNumber: String) {
+    let computerRandomNumber = generateComputerNumber()
+    
+    if inputUserNumber == "1" {
+        if computerRandomNumber == 1 {
+            printMatchResult(matchResult: "tie")
+        }
+    } else {
+        
+    }
+}
+
+func printMatchResult(matchResult: String) {
+    switch matchResult {
+    case "win":
+        print("이겼습니다!")
+        break
+    case "lose":
+        print("졌습니다!")
+        break
+    default:
+        print("비겼습니다!")
+    }
+}
+
+startGame()
