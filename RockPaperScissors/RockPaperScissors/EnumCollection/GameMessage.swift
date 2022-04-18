9f8a88f28a65a7ade5b764afb2f5ebf474c694e8
starting1231 <starting1231@naver.com>
2022-04-18 20:22:21 +0900

    refactor: GameMessage case draw 오타 수정

--- RockPaperScissors/RockPaperScissors/EnumCollection/GameMessage.swift
+++ RockPaperScissors/RockPaperScissors/EnumCollection/GameMessage.swift
@@ -11,7 +11,7 @@ enum GameMessage: String {
     case menu = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
     case winning = "이겼습니다!"
     case defeat = "졌습니다!"
-    case drow = "비겼습니다!"
+    case draw = "비겼습니다!"
     case ending = "게임 종료"
     case error = "잘못된 입력입니다. 다시 시도해주세요."
 }
