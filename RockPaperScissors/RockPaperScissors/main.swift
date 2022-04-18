343c65e9105ce3e92924c20f61f3b7771ec46219
bard <schy2503@naver.com>
2022-04-18 19:57:43 +0900

    feat: 결과 출력 함수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -22,3 +22,22 @@ func getComputerNumber() -> String {
     let computerRandomNumber = String(Int.random(in: 1...3))
     return computerRandomNumber
 }
+
+func getResult(userNumber: String) {
+    if computerNumber == userNumber {
+        print("비겼습니다!")
+        computerNumber = getComputerNumber()
+    } else if computerNumber == "1" && userNumber == "3" {
+        print("졌습니다!")
+        print("게임 종료")
+    } else if computerNumber == "2" && userNumber == "1" {
+        print("졌습니다!")
+        print("게임 종료")
+    } else if computerNumber == "3" && userNumber == "2" {
+        print("졌습니다!")
+        print("게임 종료")
+    } else {
+        print("이겼습니다!")
+        print("게임 종료")
+    }
+}
