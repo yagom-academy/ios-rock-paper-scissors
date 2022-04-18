70658f6b3068af7d70ea9a3d40662ea374043dab
seohyeon2 <seo_hyeon418@naver.com>
2022-04-18 18:44:24 +0900

    feat: 가위바위보와 게임결과를 enum 타입으로 처리하는 기능 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,15 +4,53 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-var computerInput: Int = 0
-var winningNumberCases = [(1,3),(2,1),(3,2)]
+enum RockPaperScissorsGame: Int {
+    case inputError
+    case terminate
+    case scissor
+    case rock
+    case paper
+    
+    var number: Int {
+        switch self {
+        case .inputError:
+            return -1
+        case .terminate:
+            return 0
+        case .scissor:
+            return 1
+        case .rock:
+            return 2
+        case .paper:
+            return 3
+        }
+    }
+}
+
+enum GameResult: Int {
+    case draw
+    case win
+    case lose
+    
+    var result: Int {
+        switch self {
+        case .draw:
+            return 1
+        case .win:
+            return 2
+        case .lose:
+            return 3
+        }
+    }
+}
 
 func printUserOption() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 }
 
-func makeComputerRandomNumber() {
-    computerInput = Int.random(in: 1...3)
+func makeComputerRandomNumber() -> Int {
+    let computerInput = Int.random(in: 1...3)
+    return computerInput
 }
 
 func inputUserNumber() -> Int {
@@ -24,13 +62,16 @@ func inputUserNumber() -> Int {
 }
 
 func checkUserInputNumber(userInput: String) -> Int {
-    var selectedNumber = -1
+    var selectedNumber = RockPaperScissorsGame.inputError.number
     if let verifiedUserInput = Int(userInput) {
         switch verifiedUserInput {
-        case 0,1,2,3:
+        case RockPaperScissorsGame.terminate.number,
+             RockPaperScissorsGame.scissor.number,
+             RockPaperScissorsGame.rock.number,
+             RockPaperScissorsGame.paper.number:
             selectedNumber = verifiedUserInput
         default:
-            selectedNumber = -1
+            selectedNumber = RockPaperScissorsGame.inputError.number
         }
     }
     return selectedNumber
@@ -40,50 +81,52 @@ func printErrorMessage() {
     print("잘못된 입력입니다. 다시 시도해주세요.")
 }
 
-func startGame() {
-    makeComputerRandomNumber()
-    while true {
-        let userInput = inputUserNumber()
-        if userInput == -1 {
-            printErrorMessage()
-            continue
-        }
-        if userInput == 0 {
-            print("게임 종료")
-            break
-        }
-        let extractedNumber = compareTwoNumbers(userInput: userInput, computerInput: computerInput)
-        printResult(matchResult: extractedNumber)
-        if extractedNumber == 2 || extractedNumber == 3 {
-            print("게임 종료")
-            break
-        }
-    }
-}
-
 func compareTwoNumbers (userInput: Int, computerInput: Int) -> Int {
-    var matchResult = 1
+    let winningNumberCases = [(RockPaperScissorsGame.scissor.number, RockPaperScissorsGame.paper.number),
+                              (RockPaperScissorsGame.rock.number, RockPaperScissorsGame.scissor.number),
+                              (RockPaperScissorsGame.paper.number, RockPaperScissorsGame.rock.number)]
+    var matchResult = GameResult.draw.result
     let comparisonGroup = (userInput, computerInput)
     if userInput == computerInput {
-        matchResult = 1
+        matchResult = GameResult.draw.result
     } else if winningNumberCases.contains(where: { $0 == comparisonGroup }) {
-        matchResult = 2
+        matchResult = GameResult.win.result
     } else {
-        matchResult = 3
+        matchResult = GameResult.lose.result
     }
     return matchResult
 }
 
 func printResult(matchResult: Int) {
     switch matchResult {
-    case 2 :
+    case GameResult.win.result :
         print("이겼습니다!")
-    case 3 :
+    case GameResult.lose.result :
         print("졌습니다!")
     default:
         print("비겼습니다!")
     }
 }
 
-startGame()
+func startGame() {
+    let computerInput = makeComputerRandomNumber()
+    while true {
+        let userInput = inputUserNumber()
+        if userInput == RockPaperScissorsGame.inputError.number {
+            printErrorMessage()
+            continue
+        }
+        if userInput == RockPaperScissorsGame.terminate.number {
+            print("게임 종료")
+            break
+        }
+        let extractedNumber = compareTwoNumbers(userInput: userInput, computerInput: computerInput)
+        printResult(matchResult: extractedNumber)
+        if extractedNumber == GameResult.win.result || extractedNumber == GameResult.lose.result {
+            print("게임 종료")
+            break
+        }
+    }
+}
 
+startGame()
