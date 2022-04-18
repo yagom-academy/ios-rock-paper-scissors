4ec1fcc56bedf496024a6a54d59de07322e6c797
OldStar <tyr2293@naver.com>
2022-04-18 17:32:31 +0900

    feat: 메뉴 출력 및 사용자 입력 받는 함수 작성

--- RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.swift
@@ -18,5 +18,32 @@ class RockPaperScissorsGame {
 }
 
 extension RockPaperScissorsGame {
+    func printMenu() {
+        print("가위(1), 바위(2), 보(3)! <종료: 0>:", terminator: " ")
+        getUserInput()
+        return
+    }
     
+    private func getUserInput() {
+        let userInput = readLine()
+        
+        switch userInput {
+        case "1", "2", "3":
+            judgeVictory(selectedMenu: userInput)
+        case "0":
+            endGame()
+        default:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            printMenu()
+            return
+        }
+    }
+    
+    private func judgeVictory(selectedMenu: String?) {
+        
+    }
+    
+    private func endGame() {
+        print("게임 종료")
+    }
 }
