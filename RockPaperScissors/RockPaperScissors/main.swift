56ad0d29799ca7e565d6a3b1152fc1514adefaab
yusw10 <yusw10@naver.com>
2022-04-18 15:47:21 +0900

    feat: make startMenu function

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,6 +6,13 @@ enum RPS: Int {
     case scissors
 }
 
+func startMenu() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    if let choicedMenu = readLine() {
+        choiceMenu(number: choicedMenu)
+    }
+}
+
 func choiceMenu(number: String) {
     if number == "1" {
         RPS.scissors
@@ -18,3 +25,4 @@ func choiceMenu(number: String) {
     }
 }
 
+startMenu()
