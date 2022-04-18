e28866e6c07d600bd7104527871486b4ada763a5
yusw10 <yusw10@naver.com>
2022-04-18 15:46:38 +0900

    feat: make choiceMenu function

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,5 +1,20 @@
+import Foundation
+
 enum RPS: Int {
     case rock
     case paper
     case scissors
 }
+
+func choiceMenu(number: String) {
+    if number == "1" {
+        RPS.scissors
+    } else if number == "2" {
+        RPS.rock
+    } else if number == "3" {
+        RPS.paper
+    } else {
+        print("게임 종료")
+    }
+}
+
