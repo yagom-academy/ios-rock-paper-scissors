18928228e343a818ceddc7ded2dbb985aba8172c
ic_6_user_28 <toskw@naver.com>
2022-04-18 19:55:30 +0900

    feat: 컴퓨터 랜덤 변수 생성 함수 및 유저 입력 함수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,6 +6,19 @@
 
 import Foundation
 
+var computerNumber = getComputerNumber()
+
 func printMenu() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+}
+
+func inputNumber() -> String {
+    let userNumber = readLine()
+    guard let inputNumber = userNumber else { return "" }
+    return inputNumber
+}
+
+func getComputerNumber() -> String {
+    let computerRandomNumber = String(Int.random(in: 1...3))
+    return computerRandomNumber
 }
