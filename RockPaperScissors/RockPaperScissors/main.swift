67650cc12c70a24896f248934b4e8866166ebafa
apwierk2451 <apwierk2451@gmail.com>
2022-04-19 12:16:34 +0900

    feat: getUserInput함수 구현 기능별로 함수 구현하기 위하여 함수를 나눴습니다.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,20 +10,23 @@ func showGameMenu() {
     var isStart = true
     while isStart {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-        let inputMenuNumber = readLine() ?? ""
+        isStart = getUserInput()
+    }
+}
+
+func getUserInput() -> Bool {
+    let inputMenuNumber = readLine() ?? ""
         switch inputMenuNumber {
         case "1", "2", "3" :
             let rockPaperScissors = makeRandomNumber()
-            isStart = comparePlayerInputAndComputerNumber(userInput: inputMenuNumber, computerInput: rockPaperScissors)
+            return comparePlayerInputAndComputerNumber(userInput: inputMenuNumber, computerInput: rockPaperScissors)
         case "0" :
             print("게임 종료")
-            isStart = false
-            break
+            return false
         default :
             print("잘못된 입력입니다. 다시 시도해주세요.")
-            break
+            return true
         }
-    }
 }
 
 func makeRandomNumber() -> String {
@@ -31,7 +34,7 @@ func makeRandomNumber() -> String {
 }
 
 func comparePlayerInputAndComputerNumber(userInput: String, computerInput: String) -> Bool {
-    if computerInput == "1" && userInput == "2" || computerInput == "2" && userInput == "3" || computerInput == "3" && userInput == "0" {
+    if computerInput == "1" && userInput == "2" || computerInput == "2" && userInput == "3" || computerInput == "3" && userInput == "1" {
         printResult(result: 1)
         return false
     } else if computerInput == userInput {
