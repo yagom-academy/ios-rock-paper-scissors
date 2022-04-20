4e513756c389ee3c3092d4ba0a9c9dd19e80ced1
iilim <42.4.ilim@gmail.com>
2022-04-20 16:29:24 +0900

    피드백 반영 후 수정 1. 열거형 이용하여 가위바위보, 결과 값 저장하여 결과값 출력 및 승패 여부 결정에 사용함 2. 네이밍 수정 3. 사용자의 입력값, 컴퓨터의 난수값을 이용하여 패를 꺼내오는 기능 구현 4. 기능별로 함수 분류

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,26 +4,73 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-private func showGameMenu() {
-    var isStart = true
-    while isStart {
-        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-        isStart = getUserInput()
+import Foundation
+
+enum GameResult: String {
+    case win = "WIN"
+    case draw = "DRAW"
+    case lose = "LOSE"     
+}
+
+enum Hand: Int {
+    case scissors = 1
+    case rock = 2
+    case paper = 3
+
+    func returnRockPaperScissors() -> String {
+        switch self {
+        case .scissors:
+            return "scissors"
+        case .rock:
+            return "rock"      
+        case .paper:
+            return "paper"
+        }
     }
 }
 
-private func exitGame() {
+private func convertUserInputToHand(input: String) -> String {
+    guard let convertedInput = Int(input) else { return "" }
+    if let userHandCase = Hand(rawValue: convertedInput) {
+        return userHandCase.returnRockPaperScissors()
+    }
+    return ""
+}
+
+private func convertComputerNumberToHand() -> String {
+    if let computerHandCase = Hand(rawValue: Int.random(in: 1...3)) {
+        return computerHandCase.returnRockPaperScissors()
+    }
+    return ""
+}
+
+private func showGameMenu() {
+    var isStart = true
+    while isStart {
+        isStart = printGameMenu()
+    }
+}
+
+private func printExitMessage() {
     print("게임 종료")
 }
 
-private func getUserInput() -> Bool {
-    let inputMenuNumber = readLine() ?? ""
-        switch inputMenuNumber {
+private func printGameMenu() -> Bool {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    return playGame()
+}
+
+private func inputUserHand() -> String {
+    return readLine() ?? ""
+}
+
+private func playGame() -> Bool {
+    let userHand = inputUserHand()
+        switch userHand {
         case "1", "2", "3" :
-            let rockPaperScissors = makeRandomNumber()
-            return comparePlayerInputAndComputerNumber(userInput: inputMenuNumber, computerNumber: rockPaperScissors)
+            return comparePlayerAndComputerHand(userHand: convertUserInputToHand(input: userHand), computerHand: convertComputerNumberToHand())
         case "0" :
-            exitGame()
+            printExitMessage()
             return false
         default :
             print("잘못된 입력입니다. 다시 시도해주세요.")
@@ -31,35 +78,29 @@ private func getUserInput() -> Bool {
         }
 }
 
-private func makeRandomNumber() -> String {
-    return String(Int.random(in: 1...3))
-}
-
-private func comparePlayerInputAndComputerNumber(userInput: String, computerNumber: String) -> Bool {
-    if computerNumber == "1" && userInput == "2" || computerNumber == "2" && userInput == "3" || computerNumber == "3" && userInput == "1" {
-        printResult(result: 1)
-        return false
-    } else if computerNumber == userInput {
-        printResult(result: 0)
+private func comparePlayerAndComputerHand(userHand: String, computerHand: String) -> Bool {
+    if computerHand == "scissors" && userHand == "rock" || computerHand == "rock" && userHand == "paper" || computerHand == "paper" && userHand == "scissors" {
+        printResult(gameResult: GameResult.win)
+    } else if computerHand == userHand {
+        printResult(gameResult: GameResult.draw)
+        return true
     } else {
-        printResult(result: 2)
-        return false
+        printResult(gameResult: GameResult.lose)
     }
-    return true
+    return false
 }
 
-private func printResult(result: Int) {
+private func printResult(gameResult: GameResult) {
+    let result: GameResult = gameResult
     switch result {
-    case 0:
+    case .draw:
         print("비겼습니다!")
-    case 1:
+    case .win:
         print("이겼습니다!")
-        exitGame()
-    case 2:
+        printExitMessage()
+    case .lose:
         print("졌습니다!")
-        exitGame()
-    default:
-        break
+        printExitMessage()
     }
 }
 
