7ca8a854e12dacffcbf749705d6928bcfe6ce37b
yeeton37 <lyeeun37@naver.com>
2022-04-20 12:20:44 +0900

    feat: enum 이름 변경 후 enum안에 메서드(printMessage) 구현

--- RockPaperScissors/RockPaperScissors/EnumCollection/GameMessage.swift
+++ RockPaperScissors/RockPaperScissors/EnumCollection/GameMessage.swift
@@ -5,13 +5,28 @@
 //  Created by 그루트, 예톤 on 2022/04/18.
 //
 
-import Foundation
-
-enum GameMessage: String {
-    case menu = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
-    case win = "이겼습니다!"
-    case lose = "졌습니다!"
-    case draw = "비겼습니다!"
-    case end = "게임 종료"
-    case error = "잘못된 입력입니다. 다시 시도해주세요."
+enum GameStatus {
+    case menu
+    case win
+    case lose
+    case draw
+    case end
+    case error
+    
+    func printMessage() {
+        switch self {
+        case .menu:
+            print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+        case .win:
+            print("이겼습니다!")
+        case .lose:
+            print("졌습니다!")
+        case .draw:
+            print("비겼습니다!")
+        case .end:
+            print("게임 종료")
+        case .error:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+        }
+    }
 }
