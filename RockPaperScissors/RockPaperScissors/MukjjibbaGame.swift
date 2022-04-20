ca2b6fd37a02791a3f541cfb144831e5e5fb1e54
gjrjs31@naver.com <gjrjs31@naver.com>
2022-04-20 22:05:20 +0900

    feat: 가위 바위 보 게임 누구 차례 인지 확인하는 함수, 묵찌빠 게임 턴 확인하는 함수, 묵찌빠 게임 룰 함수 추가

--- RockPaperScissors/RockPaperScissors/MukjjibbaGame.swift
+++ RockPaperScissors/RockPaperScissors/MukjjibbaGame.swift
@@ -1,3 +1,29 @@
 class MukjjibbaGame: RockPaperScissorsGame {
+    var currentTurn: String = ""
     
+    func checkTurn() {
+        if gameResult == "이겼습니다." {
+            self.currentTurn = "사용자"
+        } else {
+            self.currentTurn = "컴퓨터"
+        }
+    }
+    
+    func toggleTurn() {
+        if currentTurn == "사용자" {
+            currentTurn = "컴퓨터"
+        } else {
+            currentTurn = "사용자"
+        }
+    }
+    
+    func setMukjjibbaRule(userInput: String, randomNumber: String) {
+        print("[\(currentTurn) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0>")
+        print(userInput, randomNumber)
+        if userInput == randomNumber {
+           print("\(currentTurn)의 승리!")
+        } else {
+            toggleTurn()
+        }
+    }
 }
