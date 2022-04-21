79ac0167e22901ea4b8a362864844b72dee8731e
yeeton37 <lyeeun37@naver.com>
2022-04-21 16:40:51 +0900

    feat: enum(GameMukChiBaChoice) 파일 생성 및 타입 추가

---
+++ RockPaperScissors/RockPaperScissors/GameCollection/GameMukChiBaChoice.swift
@@ -0,0 +1,20 @@
+//
+//  GameMukchibaChoice.swift
+//  RockPaperScissors
+//
+//  Created by 그루트, 예톤 on 2022/04/21.
+//
+
+enum GameMukChiBaChoice: Int {
+    case end = 0
+    case muk = 1
+    case chi = 2
+    case ba = 3
+    case error
+    
+    var number: Int {
+        get {
+            return self.rawValue
+        }
+    }
+}
