7d0722b9e4257de1c76da6f4d95e96650e6a9c8c
starting1231 <starting1231@naver.com>
2022-04-21 16:01:54 +0900

    refactor: GameChoice 타입명 변경 - > GameRockPaperScissorsChoice:

--- RockPaperScissors/RockPaperScissors/GameCollection/GameRockPaperScissorsChoice.swift
+++ RockPaperScissors/RockPaperScissors/GameCollection/GameRockPaperScissorsChoice.swift
@@ -5,11 +5,12 @@
 //  Created by 그루트, 예톤 on 2022/04/18.
 //
 
-enum GameChoice: Int {
+enum GameRockPaperScissorsChoice: Int {
     case end = 0
     case scissors = 1
     case rock = 2
     case paper = 3
+    case error
     
     var number: Int {
         get {
