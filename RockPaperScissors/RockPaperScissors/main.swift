dab84381db0ad08c8790e9173e90923e79d25c7c
unchain123 <unchain123@kakao.com>
2022-04-21 15:11:15 +0900

    chore: addwhitespace

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -15,7 +15,7 @@ enum GameGuideMessage {
     static let endGame = "게임 종료"
     static let inputError = "잘못된 입력입니다. 다시 시도해주세요."
     static let computerHandDesignError = "컴퓨터의 값을 얻어오지 못했습니다."
-    static let startMukChiBaGame = "묵(1), 찌(2), 빠(3)! <종료 : 0>:"
+    static let startMukChiBaGame = "묵(1), 찌(2), 빠(3)! <종료 : 0> : "
 }
 
 enum Winner {
