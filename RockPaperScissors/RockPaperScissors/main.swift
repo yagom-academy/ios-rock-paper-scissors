fc4b6abeaf9624da6c022deb5cc149bdac82e9e5
jeongmin <lj845854@gmail.com>
2022-08-22 19:54:20 +0900

    feat: fetchUserInput 함수 선언

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,5 +1,22 @@
 import Foundation
 
+enum RPS {
+    case rock
+    case paper
+    case scissors
+}
+
 func printMenu() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+}
+
+func fetchUserInput() -> String? {
+    let userInput = readLine()
+
+    guard let userInput = userInput else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return nil
+    }
+    
+    return userInput
 }
