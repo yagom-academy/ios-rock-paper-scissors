7edd471b1550ff4fe484c10e431470d3a1f48599
미니 <cow970814@naver.com>
2022-08-22 11:25:14 +0900

    feat : 임의의 컴퓨터 패 생성 함수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,33 +1,36 @@
 //
 //  RockPaperScissors - main.swift
 //  Created by Wonbi, mini.
-// 
+//
 
-func fetchUserInput() -> Int {
+enum InputError: Error {
+	case invalidNumber
+}
+
+func generateComputerCard() -> Int {
+	let randomCard = Int.random(in: 1...3)
+	return randomCard
+}
+
+func fetchUserInput() -> Result<Int, InputError> {
 	print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 	guard let inputedValue = readLine(),
 		  let inputedNumber = Int(inputedValue),
-          0...3 ~= inputedNumber else { return 4 }
-	return inputedNumber
+		  0...3 ~= inputedNumber else { return .failure(.invalidNumber) }
+	return .success(inputedNumber)
 }
 
-func vaildate(at userInputed: Int) {
-    switch userInputed {
-    case 0:
-        print("종료")
-    case 1:
-        print("가위")
-    case 2:
-        print("바위")
-    case 3:
-        print("보")
-    default:
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-    }
+func validate(at userInputedResult: Result<Int,InputError>) {
+	switch userInputedResult {
+	case .success(let userInput):
+		print("userInput \(userInput)")
+	case .failure:
+		print("잘못된 입력입니다. 다시 시도해주세요.")
+	}
 }
 
 while true {
     let a = fetchUserInput()
-    vaildate(at: a)
+	validate(at: a)
 }
 
