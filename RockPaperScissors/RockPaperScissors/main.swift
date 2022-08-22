e390f2524394543fe135169b1cf15a4e9dd85915
lws2269 <lws2269@kakao.com>
2022-08-22 18:45:03 +0900

    refactor: 기능분리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,31 +1,43 @@
-import Foundation
-
-enum RPS: Int {
-    case Rock = 1, Paper, Scissors
-}
 
 func gameStart() {
     while true {
         let computerNumber = Int.random(in: 1...3)
-        print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: "")
-        guard let userInput = readLine(), let userNumber = Int(userInput), 0...3 ~= userNumber else {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
+        var result = 0
+        
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+        guard let userNumber = fetchUserInput() else {
+            print("잘못된 입력입니다. 다시 시도해주세요")
             continue
         }
-        if userNumber == 0 {
-            break
-        }
         
-        let result = computerNumber - userNumber
+        identifyUserInput(userNumber: userNumber)
         
-        if result == -2 || result == 1 {
-            print("이겼습니다!")
-        } else if result == -1 || result == 2 {
-            print("졌습니다")
-        } else {
-            print("비겼습니다!")
-        }
     }
 }
 
+func fetchUserInput() -> Int? {
+    guard let userInput = readLine(), let userNumber = Int(userInput) else { return nil }
+    return userNumber
+}
+
+func identifyUserInput(userNumber: Int) {
+    switch userNumber {
+    case 1...3:
+        showResult()
+    case 0:
+        print("게임 종료")
+        return
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
+func showResult(computerNumber: Int) {
+    if result == -2 || result == 1 {
+        print("이겼습니다!")
+    } else if result == -1 || result == 2 {
+        print("졌습니다")
+    } else {
+        print("비겼습니다!")
+    }
+}
 gameStart()
