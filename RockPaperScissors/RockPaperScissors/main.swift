e8a8800e733a1133d518d38bc4d154886afabb77
Mangdi <lkw4670@naver.com>
2022-08-22 12:47:35 +0900

    feat: 유저와 컴퓨터간의 값을 비교하는 judgeWhoWins메서드를 생성 드라이버:Mangdi 네비게이터:애종

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,34 +7,39 @@
 import Foundation
 
 func startRockScissorsPaperGame() {
+    let computerAnswer = Int.random(in: 1...3)
+    
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: "")
     guard let input = readLine() else {
         return
     }
-    let userAnswer = Int(input.replacingOccurrences(of: " ", with: ""))
-    let computerAnswer = Int.random(in: 1...3)
+    guard let userAnswer = Int(input.replacingOccurrences(of: " ", with: "")) else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return
+    }
     
     switch userAnswer {
     case 0:
         print("게임 종료")
         break
     case 1:
-        if computerAnswer == 1 {
-            print("비겼습니다!")
-            break
-        }
-        if computerAnswer == 2 {
-            print("졌습니다!")
-            break
-        }
-        if computerAnswer == 3 {
-            print("이겼습니다!")
-            break
-        }
+        
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
         break
     }
-    
 }
 
+startRockScissorsPaperGame()
+func judgeWhoWins(_ computerAnswer: Int, a useran) {
+    // 가위, 바위, 보
+    if computerAnswer == 1 {
+        print("비겼습니다!")
+    }
+    if computerAnswer == 2 {
+        print("졌습니다!")
+    }
+    if computerAnswer == 3 {
+        print("이겼습니다!")
+    }
+}
