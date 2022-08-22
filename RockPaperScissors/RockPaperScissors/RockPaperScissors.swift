6a171de361ee0836db222aa1a41163db2829d03a
oneStar92 <onestar7984@gmail.com>
2022-08-22 11:57:29 +0900

    feat: RockPaperScissors enum타입 정의.

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -0,0 +1,9 @@
+//
+//  RockPaperScissors - RockPaperScissors.swift
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+enum RockPaperScissors: Int {
+    case rock = 1, paper, scissors
+}
