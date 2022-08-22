10faaac3d14859e9e13bf5132b7be75437b87a94
jonghancha <jonghancha@gmail.com>
2022-08-22 12:00:26 +0900

    feat: 가위바위보 게임 시작함수 추가, 컴퓨터와 사용자의 입력을 비교하는 switch문 작성. 드라이버: 애종, 네비게이터: Mangdi

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,40 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by 애종, Mangdi.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import Foundation
 
-print("Hello, World!")
+func startRockScissorsPaperGame() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: "")
+    guard let input = readLine() else {
+        return
+    }
+    let userAnswer = Int(input.replacingOccurrences(of: " ", with: ""))
+    let computerAnswer = Int.random(in: 1...3)
+    
+    switch userAnswer {
+    case 0:
+        print("게임 종료")
+        break
+    case 1:
+        if computerAnswer == 1 {
+            print("비겼습니다!")
+            break
+        }
+        if computerAnswer == 2 {
+            print("졌습니다!")
+            break
+        }
+        if computerAnswer == 3 {
+            print("이겼습니다!")
+            break
+        }
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        break
+    }
+    
+}
 
