002144b25d118ad9cb260b1a52497882868e611d
lxodud <xodud1998223@naver.com>
2022-08-22 11:07:32 +0900

    :sparkles: printRockScissorPaper(), getUserNumber() 추가
    가위바위보출력 함수와 사용자의 입력을 받아오는 함수를 작성하였습니다.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,4 +6,13 @@
 
 import Foundation
 
+func printRockScissorPaper() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :")
+}
+
+func getUserNumber() -> Int {
+    guard let input = readLine() else { return 0 }
+    guard let userNumber = Int(input) else { return 0 }
+    return userNumber
+}
 
