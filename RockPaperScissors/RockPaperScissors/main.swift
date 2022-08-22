5b485b3b86c2318acc5e208fca920de739eef12d
jin <pearlisrich@naver.com>
2022-08-22 11:27:04 +0900

    :art: getUserNumber() 수정 및 playRockScissorPaper() 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,13 +6,21 @@
 
 import Foundation
 
+var userHand: Int?
+let computerHand: Int = Int.random(in: 1...3)
+
 func printRockScissorPaper() {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> :")
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
 }
 
-func getUserNumber() -> Int {
-    guard let input = readLine() else { return 0 }
-    guard let userNumber = Int(input) else { return 0 }
-    return userNumber
+func getUserNumber() {
+    guard let input = readLine() else { return }
+    guard let userNumber = Int(input) else { return }
+    userHand = userNumber
 }
 
+func playRockScissorPaper() {
+    printRockScissorPaper()
+}
+
+playRockScissorPaper()
