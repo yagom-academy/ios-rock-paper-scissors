c5dfdf9e057a752e5f037b3c08e19bf6d145118d
유연수 <yeon@yuyeonsuui-MacBookPro.local>
2022-08-22 20:21:27 +0900

    feat: startGame() 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,22 @@
 
 import Foundation
 
-print("Hello, World!")
+func startGame() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
+    guard let input = readLine(), input.count == 1, let userNumber = Int(input) else { return }
+    let computerNumber = Int.random(in: 1...3)
+    var compareUserToComputer: Int = userNumber - computerNumber
+    
+    switch userNumber {
+    case 1, 2, 3:
+        return
+    case 0:
+        print("게임 종료")
+        return
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        startGame()
+    }
+}
 
+startGame()
