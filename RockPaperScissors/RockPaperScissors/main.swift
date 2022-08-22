e5b6f33b4ebec959c530e164d8016884206529fb
lxodud <xodud1998223@naver.com>
2022-08-22 11:44:18 +0900

    :art: getUserNumber() 함수 수정
    함수에서 user입력값의 validation을 모두 처리하였습니다.
    
    :art: getUserNumber() 함수 수정 및 playRockScissorPater() 함수 수정
    
    playRockScissorPater()에서 user 입력값을 validation처리 후 받아올 수 있도록 하였습니다.
    
    :art: getUserNumberWithValidation() 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,21 +6,26 @@
 
 import Foundation
 
-var userHand: Int?
-let computerHand: Int = Int.random(in: 1...3)
-
 func printRockScissorPaper() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
 }
 
-func getUserNumber() {
-    guard let input = readLine() else { return }
-    guard let userNumber = Int(input) else { return }
-    userHand = userNumber
+func getUserNumberWithValidation() -> Int? {
+    guard let input = readLine(),
+          let userNumber = Int(input),
+          userNumber > 3,
+          userNumber < 0
+    else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return nil
+    }
+    return userNumber
 }
 
 func playRockScissorPaper() {
+    let computerHand: Int = Int.random(in: 1...3)
     printRockScissorPaper()
+    guard let userHand = getUserNumberWithValidation() else { return }
 }
 
 playRockScissorPaper()
