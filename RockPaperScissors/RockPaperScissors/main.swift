66fa7e30b75a3888622b10310bd888b629840ac0
jin <pearlisrich@naver.com>
2022-08-22 18:10:19 +0900

    :sparkles: HandType Enum 생성 및 playRockScissorPaper() 기능 일부 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,6 +6,12 @@
 
 import Foundation
 
+enum HandType: Int {
+    case scissor = 1
+    case rock = 2
+    case paper = 3
+}
+
 func printRockScissorPaper() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
 }
@@ -13,8 +19,8 @@ func printRockScissorPaper() {
 func getUserNumberWithValidation() -> Int? {
     guard let input = readLine(),
           let userNumber = Int(input),
-          userNumber > 3,
-          userNumber < 0
+          userNumber > 0,
+          userNumber < 3
     else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
         return nil
@@ -23,9 +29,18 @@ func getUserNumberWithValidation() -> Int? {
 }
 
 func playRockScissorPaper() {
-    let computerHand: Int = Int.random(in: 1...3)
     printRockScissorPaper()
-    guard let userHand = getUserNumberWithValidation() else { return }
+    guard let userNumber = getUserNumberWithValidation() else {
+        playRockScissorPaper()
+        return
+    }
+    guard let userHand = HandType(rawValue: userNumber) else {
+        print("게임 종료")
+        return
+    }
+    guard let computerHand = HandType(rawValue: Int.random(in: 1...3)) else { return }
+
+
 }
 
 playRockScissorPaper()
