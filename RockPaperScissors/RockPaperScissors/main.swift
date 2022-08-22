76ac4afe1c2e14bc4679dba0f19ddb4bcc3453a1
미니 <cow970814@naver.com>
2022-08-22 10:35:19 +0900

    feat : 사용자 입력 함수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,18 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
+//  Created by Wonbi, mini.
 // 
 
-import Foundation
+func fetchUserInput() -> Int? {
+	print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+	guard let inputedValue = readLine(),
+		  let inputedNumber = Int(inputedValue),
+		  0...3 ~= inputedNumber else {
+		print("잘못된 입력입니다. 다시 시도해주세요.")
+		return nil
+	}
+	return inputedNumber
+}
 
-print("Hello, World!")
-
+let value = fetchUserInput()
+print(value)
