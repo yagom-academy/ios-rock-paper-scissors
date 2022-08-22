f1e0c3f3e8faec4142d0140e41d3c54f80e4e3ff
ttokin <jyerin0812@gmail.com>
2022-08-22 18:17:44 +0900

    feat: 유저 입력 받기 구현 및 승패 체크 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,31 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-print("Hello, World!")
+enum RPS: Int {
+    case Rock = 1, Paper, Scissors
+}
 
+func gameStart() {
+    while true {
+        let computerNumber = Int.random(in: 1...3)
+        print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: "")
+        guard let userInput = readLine(), let userNumber = Int(userInput), 0...3 ~= userNumber else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            continue
+        }
+        if userNumber == 0 {
+            break
+        }
+        
+        let result = computerNumber - userNumber
+        
+        if result == -2 || result == 1 {
+            print("이겼습니다!")
+        } else if result == -1 || result == 2 {
+            print("졌습니다")
+        } else {
+            print("비겼습니다!")
+        }
+    }
+}
+
+gameStart()
