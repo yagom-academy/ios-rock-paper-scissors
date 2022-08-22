e780a1be258d5021a563c5db5f2af678666d48f5
wonbi <wonbi@Wonbiui-MacBookAir.local>
2022-08-22 11:06:25 +0900

    feat: 사용자 입력의 유효성을 검사하는 함수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -3,16 +3,31 @@
 //  Created by Wonbi, mini.
 // 
 
-func fetchUserInput() -> Int? {
+func fetchUserInput() -> Int {
 	print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 	guard let inputedValue = readLine(),
 		  let inputedNumber = Int(inputedValue),
-		  0...3 ~= inputedNumber else {
-		print("잘못된 입력입니다. 다시 시도해주세요.")
-		return nil
-	}
+          0...3 ~= inputedNumber else { return 4 }
 	return inputedNumber
 }
 
-let value = fetchUserInput()
-print(value)
+func vaildate(at userInputed: Int) {
+    switch userInputed {
+    case 0:
+        print("종료")
+    case 1:
+        print("가위")
+    case 2:
+        print("바위")
+    case 3:
+        print("보")
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
+
+while true {
+    let a = fetchUserInput()
+    vaildate(at: a)
+}
+
