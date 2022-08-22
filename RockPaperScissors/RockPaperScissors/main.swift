61c7855e88035393cb22955c21adcbb07093cf4d
Jung Su <wjdwjdtn92@naver.com>
2022-08-22 18:08:52 +0900

    feat: setGame 함수 및 변수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,30 @@
 
 import Foundation
 
-print("Hello, World!")
+var userNumber: Int = 0
+var computerNumber: Int
 
+func setGame(_ computerNumber: Int) -> String {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0>:", terminator: "")
+    
+    guard let inputUserNumber = readLine(), inputUserNumber.isEmpty == false else {
+        return "입력된 값이 없습니다."
+    }
+    userNumber = Int(inputUserNumber) ?? 4
+    
+    switch userNumber - computerNumber{
+    case -1, 2:
+        return "졌습니다!"
+    case -2, 1:
+        return "이겼습니다!"
+    case 0:
+        return "비겼습니다!"
+    default:
+        return "잘못된 입력입니다. 다시 시도해주세요."
+    }
+    return inputUserNumber
+}
+
+
+
+print(setGame(3))
