7af49833cc892485919a114047158f324b89761e
hashswim <ss991117@naver.com>
2022-08-22 18:29:45 +0900

    feat: 유저입력 및 컴퓨터입력 함수 구현, 승패 결정 함수 구현 네비게이터: Kyo, 드라이버: Aaron

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,40 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-print("Hello, World!")
+enum RSP: String {
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
+}
 
+
+func bringUserInput() -> Int {
+    guard let userInput = readLine() else { return 404 }
+    guard let userInput = Int(userInput) else { return 404 }
+    return userInput
+}
+
+func generateComputerInput() -> Int {
+    let computerInput = Int.random(in: 1...3)
+    print(computerInput)
+    return computerInput
+}
+
+func rockScissorsPaper() {
+    print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
+    let userInput = bringUserInput()
+    let computerInput = generateComputerInput()
+    print(decideWinLose(userInput, computerInput))
+}
+
+func decideWinLose(_ userInput: Int, _ computerInput: Int) -> String{
+    let checkNumber = userInput - computerInput
+    if checkNumber == 0 {
+        return "비겼습니다!"
+    }else if checkNumber == 1 || checkNumber == -2 {
+        return "이겼습니다!"
+    }else{
+        return "졌습니다!"
+    }
+}
+
+rockScissorsPaper()
