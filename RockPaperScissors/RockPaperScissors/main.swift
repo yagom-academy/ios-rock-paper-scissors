2948c4e3a525cc007a6847050b1fdc144efafc3b
yuvinrho <yvrho@naver.com>
2022-08-22 18:25:59 +0900

    feat: judgeWin(), startGame() 함수생성, fix: judgeValidInput() 함수수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,10 +6,39 @@ func inputUserSelction() -> String? {
     return readLine()
 }
 
-func judgeValidInput(_ userInput: String?) {
-    
+func judgeValidInput(userInput: String?) -> Int? {
+    switch userInput {
+    case "0":
+        return 0
+    case "1":
+        return 1
+    case "2":
+        return 2
+    case "3":
+        return 3
+    default:
+        return nil
+    }
 }
 
 func generateRandomComputerHand() -> Int {
     return (1...3).shuffled()[0]
 }
+
+func judgeWin(userInput: Int?, computerHand: Int) {
+    if userInput != nil {
+        print("judge")
+    } else {
+        print("fail")
+    }
+}
+
+func startGame() {
+    displayGameMessege()
+    judgeWin(
+        userInput: judgeValidInput(userInput: inputUserSelction()),
+        computerHand: generateRandomComputerHand()
+    )
+}
+
+startGame()
