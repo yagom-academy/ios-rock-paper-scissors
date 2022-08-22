c846a79d6eb3c508474f0be6aa4cb8d6d0007d43
lnhk98 <hozzang666@gmai.com>
2022-08-22 12:28:54 +0900

    feat: GameManager 타입 정의.

---
+++ RockPaperScissors/RockPaperScissors/GameManager.swift
@@ -0,0 +1,13 @@
+//
+//  RockPaperScissors - GameManager.swift
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+struct GameManager {
+    var players: [Player]
+    
+    init(_ players: Player...) {
+        self.players = players
+    }
+}
