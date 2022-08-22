80aa81d3e66345f97290c4b76fa7fac58e1f07d3
ttokin <jyerin0812@gmail.com>
2022-08-22 18:58:23 +0900

    refactor: 승리 조건 분기처리 switch-case문으로 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,16 +1,30 @@
-
 func gameStart() {
-    while true {
+    var isGame = true
+    while isGame {
         let computerNumber = Int.random(in: 1...3)
-        var result = 0
         
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-        guard let userNumber = fetchUserInput() else {
+        guard let userNumber = fetchUserInput(), 0...3 ~= userNumber else {
             print("잘못된 입력입니다. 다시 시도해주세요")
             continue
         }
         
-        identifyUserInput(userNumber: userNumber)
+        if userNumber == 0 {
+            print("게임 종료")
+            break
+        }
+        
+        switch computerNumber - userNumber {
+        case -2, 1:
+            print("이겼습니다!")
+            isGame = false
+        case -1, 2:
+            print("졌습니다!")
+            isGame = false
+        default:
+            print("비겼습니다!")
+        }
+//        compare(computerNumber: computerNumber, userNumber: userNumber)
         
     }
 }
@@ -20,24 +34,27 @@ func fetchUserInput() -> Int? {
     return userNumber
 }
 
-func identifyUserInput(userNumber: Int) {
-    switch userNumber {
-    case 1...3:
-        showResult()
-    case 0:
-        print("게임 종료")
-        return
-    default:
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-    }
-}
-func showResult(computerNumber: Int) {
-    if result == -2 || result == 1 {
-        print("이겼습니다!")
-    } else if result == -1 || result == 2 {
-        print("졌습니다")
-    } else {
-        print("비겼습니다!")
-    }
-}
+
+
+//func compare(computerNumber: Int, userNumber: Int) {
+//    switch computerNumber - userNumber {
+//    case 1...3:
+//        showResult()
+//    case 0:
+//        print("게임 종료")
+//        return
+//    default:
+//        print("잘못된 입력입니다. 다시 시도해주세요.")
+//    }
+//}
+//
+//func showResult(computerNumber: Int) {
+//    if result == -2 || result == 1 {
+//        print("이겼습니다!")
+//    } else if result == -1 || result == 2 {
+//        print("졌습니다")
+//    } else {
+//        print("비겼습니다!")
+//    }
+//}
 gameStart()
