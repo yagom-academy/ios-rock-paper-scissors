8d83f98782efd46ca7a314605b083d1d13646c54
HyosungP <hyosing92@gmail.com>
2022-08-22 20:40:02 +0900

    feat : 유효하지 않은 Input 필터링 구현 및 게임 실행 함수 구현 네비게이터: Aaron 드라이버: Kyo

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,39 +1,55 @@
 import Foundation
 
-enum RSP: String {
-    case scissors = "1"
-    case rock = "2"
-    case paper = "3"
+func bringUserInput() -> Int {
+    guard let input = readLine() else { return 404 }
+    guard let userInput = Int(input) else { return 404 }
+    return userInput
 }
 
-
-func bringUserInput() -> Int {
-    guard let userInput = readLine() else { return 404 }
-    guard let userInput = Int(userInput) else { return 404 }
-    return userInput
+func isValidInput(input: Int) -> Bool {
+    if 1...3 ~= input {
+        return true
+    } else {
+        return false
+    }
 }
 
 func generateComputerInput() -> Int {
     let computerInput = Int.random(in: 1...3)
-    print(computerInput)
     return computerInput
 }
 
 func rockScissorsPaper() {
-    print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
-    let userInput = bringUserInput()
-    let computerInput = generateComputerInput()
-    print(decideWinLose(userInput, computerInput))
+    var gameStatus = true
+    
+    while (gameStatus) {
+        print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
+        let userInput = bringUserInput()
+        
+        if isValidInput(input: userInput) == false && userInput == 0 {
+            print("게임 종료")
+            return
+        } else if isValidInput(input: userInput) == false && userInput != 0 {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            continue
+        }
+        
+        let computerInput = generateComputerInput()
+        gameStatus = decideWinLose(userInput, computerInput)
+    }
 }
 
-func decideWinLose(_ userInput: Int, _ computerInput: Int) -> String{
+func decideWinLose(_ userInput: Int, _ computerInput: Int) -> Bool {
     let checkNumber = userInput - computerInput
     if checkNumber == 0 {
-        return "비겼습니다!"
-    }else if checkNumber == 1 || checkNumber == -2 {
-        return "이겼습니다!"
-    }else{
-        return "졌습니다!"
+        print("비겼습니다!")
+        return true
+    } else if checkNumber == 1 || checkNumber == -2 {
+        print("이겼습니다!")
+        return false
+    } else {
+        print("졌습니다!")
+        return false
     }
 }
 
