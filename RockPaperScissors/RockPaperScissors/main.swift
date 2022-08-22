116097e8111001e93fc0ef1d87378642cbf27941
sunnycookie <sunnyday2007@naver.com>
2022-08-22 19:14:06 +0900

    feat: printMenu함수선언

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,5 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-print("Hello, World!")
-
+func printMenu() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+}
