29ef4ca19cd7f8f2b629769b4d8b0d136976c277
jonghancha <jonghancha@gmail.com>
2022-08-22 13:21:17 +0900

    feat: 게임종료를 위한 변수 exitGame추가, while반복문을 통해 특정한 상황일 때 반복하도록 구현 (잘못 입력했을 경우, 비겼을 경우). 드라이버: 애종, 네비게이터: Mangdi

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,23 +7,62 @@
 import Foundation
 
 func startRockScissorsPaperGame() {
-    let computerAnswer = Int.random(in: 1...3)
+    var computerAnswer: Int
+    var exitGame: Bool = false
     
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: "")
-    guard let input = readLine() else {
-        return
+    while exitGame == false {
+        computerAnswer = Int.random(in: 1...3)
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+        guard let input = readLine() else {
+            continue
+        }
+        guard let userAnswer = Int(input.replacingOccurrences(of: " ", with: "")) else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            continue
+        }
+        judgeWhoWins(computerAnswer: computerAnswer, userAnswer: userAnswer, exitGame: &exitGame)
     }
-    guard let userAnswer = Int(input.replacingOccurrences(of: " ", with: "")) else {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        return
-    }
-    
+}
+
+func judgeWhoWins(computerAnswer: Int, userAnswer: Int, exitGame: inout Bool) {
     switch userAnswer {
     case 0:
         print("게임 종료")
+        exitGame = true
         break
     case 1:
-        
+        if computerAnswer == 1 {
+            print("비겼습니다!")
+        } else if computerAnswer == 2 {
+            print("졌습니다!")
+            exitGame = true
+        } else if computerAnswer == 3 {
+            print("이겼습니다!")
+            exitGame = true
+        }
+        break
+    case 2:
+        if computerAnswer == 1 {
+            print("이겼습니다!")
+            exitGame = true
+        } else if computerAnswer == 2 {
+            print("비겼습니다!")
+        } else if computerAnswer == 3 {
+            print("졌습니다!")
+            exitGame = true
+        }
+        break
+    case 3:
+        if computerAnswer == 1 {
+            print("졌습니다!")
+            exitGame = true
+        } else if computerAnswer == 2 {
+            print("이겼습니다!")
+            exitGame = true
+        } else if computerAnswer == 3 {
+            print("비겼습니다!")
+        }
+        break
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
         break
@@ -31,15 +70,3 @@ func startRockScissorsPaperGame() {
 }
 
 startRockScissorsPaperGame()
-func judgeWhoWins(_ computerAnswer: Int, a useran) {
-    // 가위, 바위, 보
-    if computerAnswer == 1 {
-        print("비겼습니다!")
-    }
-    if computerAnswer == 2 {
-        print("졌습니다!")
-    }
-    if computerAnswer == 3 {
-        print("이겼습니다!")
-    }
-}
