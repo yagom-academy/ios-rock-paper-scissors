0f6e106519a2c11b64f4b31c10ffe352d190380c
oneStar92 <onestar7984@gmail.com>
2022-08-22 13:22:30 +0900

    feat: recieveHandShapeFromUser메서드 구현.

--- RockPaperScissors/RockPaperScissors/GameManager.swift
+++ RockPaperScissors/RockPaperScissors/GameManager.swift
@@ -10,6 +10,29 @@ struct GameManager {
     init(_ players: Player...) {
         self.players = players
     }
+        
+    func recieveHandShapeFromUser() -> HandShape? {
+        while true {
+            printRockPaperScissorsManual()
+            let userInput = recieveInputFromUser()
+            guard let convertedInput = Int(userInput) else {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                continue
+            }
+            switch convertedInput {
+            case 1:
+                return HandShape.rock
+            case 2:
+                return HandShape.paper
+            case 3:
+                return HandShape.scissors
+            case 0:
+                return nil
+            default :
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+            }
+        }
+    }
     
     func printRockPaperScissorsManual() {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
