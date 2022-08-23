c6d9f911b737f011d07cb7fc86a77729f80dd3fb
Mangdi <lkw4670@naver.com>
2022-08-23 13:38:54 +0900

    refactor : if -> switch문으로 변경 및 변수네이밍수정

--- RockPaperScissors/RockPaperScissors/step1.swift
+++ RockPaperScissors/RockPaperScissors/step1.swift
@@ -10,84 +10,63 @@ import Foundation
 let rockScissorsPaper: [Int:String] = [1:"가위", 2:"바위", 3:"보"]
 
 func startRockScissorsPaperGame() {
-    var computerPick: Int
+    var computerNumber: Int
     var exitGame: Bool = false
 
     while exitGame == false {
-        computerPick = Int.random(in: 1...3)
+        computerNumber = Int.random(in: 1...3)
 
         print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
 
         guard let input = readLine() else {
             continue
         }
-        guard let userPick = Int(input.replacingOccurrences(of: " ", with: "")) else {
+        guard let userNumber = Int(input.replacingOccurrences(of: " ", with: "")) else {
             print("잘못된 입력입니다. 다시 시도해주세요.")
             continue
         }
 
-        exitGame = showGameResult(computerPick: computerPick, userPick: userPick)
+        exitGame = showGameResult(computerNumber: computerNumber, userNumber: userNumber)
     }
 }
 
-func showGameResult(computerPick: Int, userPick: Int) -> Bool {
+func showGameResult(computerNumber: Int, userNumber: Int) -> Bool {
     var exit: Bool = true
-    let computerasdf = rockScissorsPaper[computerPick]
-    let userasdf = rockScissorsPaper[userPick]
+    let selectMenu = userNumber
+    let computerPick = rockScissorsPaper[computerNumber]
+    let userPick = rockScissorsPaper[userNumber]
     
-    if userPick == 0 {
+    if selectMenu == 0 {
         print("게임 종료")
+        return exit
     } else if computerPick == userPick {
         print("비겼습니다!")
         exit = false
-    } else if computerasdf == "가위" && userasdf == "바위" {
-        print("이겼습니다!")
-    } else if computerasdf == "가위" && userasdf == "보" {
-        print("졌습니다!")
-    } else if computerasdf == "바위" && userasdf == "가위" {
-        print("졌습니다!")
-    } else if computerasdf == "바위" && userasdf == "보" {
-        print("이겼습니다!")
-    } else if computerasdf == "보" && userasdf == "가위" {
-        print("이겼습니다!")
-    } else if computerasdf == "보" && userasdf == "바위" {
-        print("졌습니다!")
-    } else {
+    }
+    
+    switch userPick {
+    case "가위":
+        if computerPick == "바위" {
+            print("졌습니다!")
+        } else if computerPick == "보" {
+            print("이겼습니다!")
+        }
+    case "바위":
+        if computerPick == "가위" {
+            print("이겼습니다!")
+        } else if computerPick == "보" {
+            print("졌습니다!")
+        }
+    case "보":
+        if computerPick == "가위" {
+            print("졌습니다!")
+        } else if computerPick == "바위" {
+            print("이겼습니다!")
+        }
+    default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
         exit = false
     }
-    
-//    switch userPick {
-//    case 0:
-//        print("게임 종료")
-//        exit = true
-//    case 1:
-//        if computerPick == 2 {
-//            print("졌습니다!")
-//            exit = true
-//        } else if computerPick == 3 {
-//            print("이겼습니다!")
-//            exit = true
-//        }
-//    case 2:
-//        if computerPick == 1 {
-//            print("이겼습니다!")
-//            exit = true
-//        } else if computerPick == 3 {
-//            print("졌습니다!")
-//            exit = true
-//        }
-//    case 3:
-//        if computerPick == 1 {
-//            print("졌습니다!")
-//            exit = true
-//        } else if computerPick == 2 {
-//            print("이겼습니다!")
-//            exit = true
-//        }
-//    default:
-//        print("잘못된 입력입니다. 다시 시도해주세요.")
-//    }
 
     return exit
 }
