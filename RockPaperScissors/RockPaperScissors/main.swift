ff382f17dd43795cc456e8da4963ef7db6a085c2
dylan-yoon <d.dylany21@gmail.com>
2022-08-23 11:32:55 +0900

    feat: gameStart함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,14 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by Baem, Dragon
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
-
-print("Hello, World!")
+func gameStart() -> Int? {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    let userInput = readLine()
+    guard let userInput = userInput else { return nil }
+    
+    return Int(userInput)
+}
 
