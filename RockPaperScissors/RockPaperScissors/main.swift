134846a2a2580a5261fa62dd62da425c77635865
hashswim <ss991117@naver.com>
2022-08-23 15:42:32 +0900

    refactor: 피드백 반영 및 에러 처리 구현 네비게이터: Kyo, 드라이버: Aaron

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,17 +1,49 @@
 import Foundation
 
-func bringUserInput() -> Int {
-    guard let input = readLine() else { return 404 }
-    guard let userInput = Int(input) else { return 404 }
+enum InputError: Error {
+    case nilError(message: String)
+    case numberError(message: String)
+    case rangeError(message: String)
+}
+
+func bringUserInput() -> String? {
+    let input = readLine()
+    return input
+}
+
+func checkUserInput(input: String?) throws -> Int {
+    let errorSentence: String = "잘못된 입력입니다. 다시 시도해주세요."
+
+    guard let input = input else { throw InputError.nilError(message: errorSentence) }
+    guard let userInput = Int(input) else { throw InputError.numberError(message: errorSentence) }
+
+    if userInput == 0 {
+        return 0
+    }
+
+    if !(1...3 ~= userInput) {
+        throw InputError.rangeError(message: errorSentence)
+    }
     return userInput
 }
 
-func isValidInput(input: Int) -> Bool {
-    if 1...3 ~= input {
-        return true
-    } else {
-        return false
+func handlingInputError(input: String?) -> (Bool, Int) {
+    var checkNumber = -1
+    do {
+        checkNumber = try checkUserInput(input: input)
+    } catch InputError.nilError(let message) {
+        print(message)
+        return (false, checkNumber)
+    } catch InputError.numberError(let message) {
+        print(message)
+        return (false, checkNumber)
+    } catch InputError.rangeError(let message) {
+        print(message)
+        return (false, checkNumber)
+    } catch {
+        return (false, checkNumber)
     }
+    return (true, checkNumber)
 }
 
 func generateComputerInput() -> Int {
@@ -19,32 +51,36 @@ func generateComputerInput() -> Int {
     return computerInput
 }
 
-func rockScissorsPaper() {
-    var gameStatus = true
-    
-    while (gameStatus) {
+func playRPS() {
+    var isGaming = true
+
+    while (isGaming) {
         print("가위(1), 바위(2), 보(3)! <종료: 0> : ", terminator: "")
-        let userInput = bringUserInput()
-        
-        if isValidInput(input: userInput) == false && userInput == 0 {
+
+        let userInput: String? = bringUserInput()
+        let isValidNumber: (isValid: Bool, userNumber: Int) = handlingInputError(input: userInput)
+
+        if isValidNumber.isValid == false && isValidNumber.userNumber == 0 {
             print("게임 종료")
             return
-        } else if isValidInput(input: userInput) == false && userInput != 0 {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
+        } else if isValidNumber.isValid == false && isValidNumber.userNumber != 0 {
             continue
         }
-        
+
         let computerInput = generateComputerInput()
-        gameStatus = decideWinLose(userInput, computerInput)
+        isGaming = decideWinLose(isValidNumber.userNumber, computerInput)
     }
 }
 
 func decideWinLose(_ userInput: Int, _ computerInput: Int) -> Bool {
+    let winNumbers = [1, -2]
+    let drawNumber = 0
     let checkNumber = userInput - computerInput
-    if checkNumber == 0 {
+
+    if checkNumber == drawNumber {
         print("비겼습니다!")
         return true
-    } else if checkNumber == 1 || checkNumber == -2 {
+    } else if winNumbers.contains(checkNumber) {
         print("이겼습니다!")
         return false
     } else {
@@ -53,4 +89,4 @@ func decideWinLose(_ userInput: Int, _ computerInput: Int) -> Bool {
     }
 }
 
-rockScissorsPaper()
+playRPS()
