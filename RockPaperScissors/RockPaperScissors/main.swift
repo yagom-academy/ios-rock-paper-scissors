b575362ac9ac63578658ba170703564f0ce12c03
junho <junhoL1046@gmail.com>
2022-08-23 10:48:31 +0900

    feat: 가위바위보 메뉴 출력 함수, 사용자 패 입력 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,29 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by LJ, 준호.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
+struct RockPaperScissors {
+    func printMenu() {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    }
+    
+    func getUserHand() -> String? {
+        guard let userHand: String = readLine() else {
+            return nil
+        }
+        return userHand
+    }
+    
+    func startGame() {
+        var userHand: String?
+        repeat {
+            printMenu()
+            userHand = getUserHand()
+        } while userHand != "0"
+    }
+}
 
-print("Hello, World!")
-
+var game = RockPaperScissors()
+game.startGame()
