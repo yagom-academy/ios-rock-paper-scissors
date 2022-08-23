94d96b9498e1f6d7c437fdfacf001cb5df107fcf
chacha <hyhy0429_@nvaer.com>
2022-08-23 13:15:07 +0900

    style: 클래스타입으로 결정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,14 +6,6 @@
 
 import Foundation
 
-//startGame()
-
-// 클래스 인스턴스
 let rockPaperScissorsGame: RockPaperScissors = .init()
 
 rockPaperScissorsGame.startGame()
-
-// 구조체 인스턴스
-let rockPaperGame: RockPaper = .init()
-
-rockPaperGame.startGame()
