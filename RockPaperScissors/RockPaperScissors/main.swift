97da04f119b0cce19128a5a55dd9b8ae7f524ab0
Mangdi <lkw4670@naver.com>
2022-08-23 09:43:07 +0900

    refactor : 변수 compuerAnswer와 userAnswer의 네이밍 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,59 +7,59 @@
 import Foundation
 
 func startRockScissorsPaperGame() {
-    var computerAnswer: Int
+    var computerPick: Int
     var exitGame: Bool = false
     
     while exitGame == false {
-        computerAnswer = Int.random(in: 1...3)
+        computerPick = Int.random(in: 1...3)
         print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
         guard let input = readLine() else {
             continue
         }
-        guard let userAnswer = Int(input.replacingOccurrences(of: " ", with: "")) else {
+        guard let userPick = Int(input.replacingOccurrences(of: " ", with: "")) else {
             print("잘못된 입력입니다. 다시 시도해주세요.")
             continue
         }
-        judgeWhoWins(computerAnswer: computerAnswer, userAnswer: userAnswer, exitGame: &exitGame)
+        judgeWhoWins(computerPick: computerPick, userPick: userPick, exitGame: &exitGame)
     }
 }
 
-func judgeWhoWins(computerAnswer: Int, userAnswer: Int, exitGame: inout Bool) {
-    switch userAnswer {
+func judgeWhoWins(computerPick: Int, userPick: Int, exitGame: inout Bool) {
+    switch userPick {
     case 0:
         print("게임 종료")
         exitGame = true
         break
     case 1:
-        if computerAnswer == 1 {
+        if computerPick == 1 {
             print("비겼습니다!")
-        } else if computerAnswer == 2 {
+        } else if computerPick == 2 {
             print("졌습니다!")
             exitGame = true
-        } else if computerAnswer == 3 {
+        } else if computerPick == 3 {
             print("이겼습니다!")
             exitGame = true
         }
         break
     case 2:
-        if computerAnswer == 1 {
+        if computerPick == 1 {
             print("이겼습니다!")
             exitGame = true
-        } else if computerAnswer == 2 {
+        } else if computerPick == 2 {
             print("비겼습니다!")
-        } else if computerAnswer == 3 {
+        } else if computerPick == 3 {
             print("졌습니다!")
             exitGame = true
         }
         break
     case 3:
-        if computerAnswer == 1 {
+        if computerPick == 1 {
             print("졌습니다!")
             exitGame = true
-        } else if computerAnswer == 2 {
+        } else if computerPick == 2 {
             print("이겼습니다!")
             exitGame = true
-        } else if computerAnswer == 3 {
+        } else if computerPick == 3 {
             print("비겼습니다!")
         }
         break
