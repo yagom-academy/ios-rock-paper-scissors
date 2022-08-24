8e2200db1b8522bbe6bf968a71e13f9db3d7c4e1
junho <junhoL1046@gmail.com>
2022-08-24 13:32:39 +0900

    feat: 묵찌빠 열거형 추가

---
+++ RockPaperScissors/RockPaperScissors/MukChiBa.swift
@@ -0,0 +1,14 @@
+//
+//  MukChiBa.swift
+//  RockPaperScissors
+//
+//  Created by LJ, 준호.
+//
+
+import Foundation
+
+enum MukChiBa: Int, CaseIterable {
+    case muk = 1
+    case chi = 2
+    case ba = 3
+}
