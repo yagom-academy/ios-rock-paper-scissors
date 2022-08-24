66a28efd6f6a53f6b6fc1b6229afe1c867341c7e
Jae Hyung Kim <smfc.mene@gmail.com>
2022-08-24 12:18:10 +0800

    feat: MukJjiPpa Type enum으로 구현

---
+++ RockPaperScissors/RockPaperScissors/MukJjiPpa.swift
@@ -0,0 +1,15 @@
+//
+//  MukJjiPpa.swift
+//  RockPaperScissors
+//
+//  Created by woong, mene on 24/8/2022.
+//
+
+import Foundation
+
+enum MukJjiPpa: Int {
+    case none = 0
+    case muk = 1
+    case jji = 2
+    case ppa = 3
+}
