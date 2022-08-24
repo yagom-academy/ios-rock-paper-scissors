e0c32bce6a6fa9ede9ceffcccc886b9aa7490e54
jonghancha <jonghancha@gmail.com>
2022-08-24 11:28:43 +0900

    feat: startMukChiBbaGame함수 추가. 묵찌빠 승리조건 추가

--- RockPaperScissors/RockPaperScissors/RockScissorsPaperGame.swift
+++ RockPaperScissors/RockPaperScissors/RockScissorsPaperGame.swift
@@ -35,7 +35,8 @@ func startRockScissorsPaperGame() {
 }
 
 func showGameResult(by computerChoice: Int, and userChoice: Int) -> Bool {
-    var exit: Bool = true
+    var exit: Bool = false
+    var userWin: Bool
     let selectMenu = userChoice
     let computerPick = RockScissorsPaper(rawValue: computerChoice)
     let userPick = RockScissorsPaper(rawValue: userChoice)
@@ -43,10 +44,10 @@ func showGameResult(by computerChoice: Int, and userChoice: Int) -> Bool {
     
     if selectMenu == 0 {
         print("게임 종료")
+        exit = true
         return exit
     } else if computerPick == userPick {
         print("비겼습니다!")
-        exit = false
         return exit
     }
     
@@ -55,14 +56,58 @@ func showGameResult(by computerChoice: Int, and userChoice: Int) -> Bool {
         (컴퓨터가낸것: .바위, 유저가낸것: .보),
         (컴퓨터가낸것: .보, 유저가낸것: .가위):
         print("이겼습니다!")
+        userWin = true
+        startMukChiBbaGame(takeUserWin: userWin)
     case (컴퓨터가낸것: .가위, 유저가낸것: .보),
         (컴퓨터가낸것: .바위, 유저가낸것: .가위),
         (컴퓨터가낸것: .보, 유저가낸것: .바위):
         print("졌습니다!")
+        userWin = false
+        startMukChiBbaGame(takeUserWin: userWin)
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        exit = false
     }
 
     return exit
+}
+
+func startMukChiBbaGame(takeUserWin: Bool) -> Bool {
+    var exit: Bool = false
+    var computerNumber: Int
+    var roundOneWinner: String {
+        get {
+            if takeUserWin == true { return "사용자" } else { return "컴퓨터" }
+        }
+    }
+    
+    computerNumber = Int.random(in: 1...3)
+    
+    while exit == false {
+        print("[\(roundOneWinner) 턴] (묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
+        // ---
+        guard let input = readLine() else {
+            continue
+        }
+        guard let userNumber = Int(input.replacingOccurrences(of: " ", with: "")) else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            continue
+        }
+        let computerPick = RockScissorsPaper(rawValue: computerNumber)
+        let userPick = RockScissorsPaper(rawValue: userNumber)
+        let compareTwoThings = (컴퓨터가낸것: computerPick, 유저가낸것: userPick)
+        
+        if userNumber == 0 {
+            print("게임 종료")
+            exit = true
+            return exit
+        } else if computerPick == userPick && takeUserWin == true {
+            print("사용자의 승리!")
+            exit = true
+            return exit
+        } else if computerPick == userPick && takeUserWin == false {
+            print("컴퓨터의 승리!")
+            exit = true
+            return exit
+        }
+    }
 }
