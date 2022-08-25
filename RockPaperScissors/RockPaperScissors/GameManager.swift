238dccda68c5be2bada065b90e55fa6d17133af0
oneStar92 <onestar7984@gmail.com>
2022-08-25 10:17:23 +0900

    refactor: GameMessage NameSpace파일 분리.

--- RockPaperScissors/RockPaperScissors/GameManager.swift
+++ RockPaperScissors/RockPaperScissors/GameManager.swift
@@ -5,31 +5,23 @@
 //
 
 struct GameManager {
-    enum GameMessage {
-        static let endingMessage = "게임 종료"
-        static let winningMessage = "이겼습니다!"
-        static let losingMessage = "졌습니다!"
-        static let drawMessage = "비겼습니다!"
-        static let invalidHandShapeRawValue = "잘못된 입력입니다. 다시 시도해주세요."
-        static let userInputManual = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
-    }
     
     func startRockPaperScissorsGame() {
         guard let userHandShape = receiveHandShapeFromUser() else {
-            print(GameMessage.endingMessage)
+            print(ManualMessage.ending)
             return
         }
         let gameResult = fetchGameResult(of: userHandShape)
         
         switch gameResult {
         case .win:
-            print(GameMessage.winningMessage)
+            print(GameResultMessage.winning)
             return
         case .lose:
-            print(GameMessage.losingMessage)
+            print(GameResultMessage.losing)
             return
         case .draw:
-            print(GameMessage.drawMessage)
+            print(GameResultMessage.draw)
             startRockPaperScissorsGame()
         }
     }
@@ -44,12 +36,12 @@ struct GameManager {
     }
     
     private func receiveHandShapeRawValueFromUser() -> Int {
-        print(GameMessage.userInputManual, terminator: "")
+        print(ManualMessage.userInputManual, terminator: "")
         guard let input = readLine(),
               let HandShapeRawValue = Int(input),
               HandShapeRawValue >= 0,
               HandShapeRawValue <= 3 else {
-            print(GameMessage.invalidHandShapeRawValue)
+            print(ErrorMessage.invalidHandShapeRawValue)
             return receiveHandShapeRawValueFromUser()
         }
         
---
+++ RockPaperScissors/RockPaperScissors/GameMessage.swift
@@ -0,0 +1,20 @@
+//
+//  RockPaperScissors - GameMessage.swift
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+enum GameResultMessage {
+    static let winning = "이겼습니다!"
+    static let losing = "졌습니다!"
+    static let draw = "비겼습니다!"
+}
+
+enum ErrorMessage {
+    static let invalidHandShapeRawValue = "잘못된 입력입니다. 다시 시도해주세요."
+}
+
+enum ManualMessage {
+    static let userInputManual = "가위(1), 바위(2), 보(3)! <종료 : 0> : "
+    static let ending = "게임 종료"
+}
