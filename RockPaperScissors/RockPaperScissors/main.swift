4f28d99641086a9349f602ddae77166acfb70947
DragonYG <kioswe92@gmail.com>
2022-08-25 18:22:23 +0900

    style: 함수 위치 정리 (코드 직관성 위함)

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,60 +4,31 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-func createUserNumber() -> Int? {
-    let userInput = readLine()
-    
-    if let userNumber = checkUserInput(userInput) {
-        return userNumber
-    } else {
-        return nil
-    }
-}
+startGame()
 
-func checkUserInput(_ userInput: String?) -> Int? {
-    guard let userInput = userInput else {
-        return nil
-    }
-    let trimedUserInput = userInput.split(separator: " ").joined()
-    guard let userNumber = Int(trimedUserInput) else {
-        return nil
+func startGame() {
+    var winner = excuteScissorsRockPaper()
+    
+    if winner == "종료" {
+        return
     }
     
-    if userNumber < 0 || userNumber >= 4 {
-        return nil
-    }
+    var gameResult = excuteMukChiBa(from: winner)
     
-    return userNumber
-}
-
-func createRandomNumber() -> Int {
-    let computerInput = Int.random(in: 1...3)
-    
-    return computerInput
-}
-
-func compareTwoInput(_ userNumber: Int, with computerNumber: Int) -> String {
-    let user = RockScissorsPaper(rawValue: userNumber)
-    let computer = RockScissorsPaper(rawValue: computerNumber)
-    
-    switch (user, computer) {
-    case (.scissors, .rock), (.rock, .paper), (.paper, .scissors) :
-        return "컴퓨터"
-    case (.rock, .scissors), (.paper, .rock), (.scissors, .paper) :
-        return "사용자"
-    default :
-        return "무승부"
-    }
-}
-
-func checkResult(to userNumber: Int) -> String {
-    let computerNumber = createRandomNumber()
-
-    switch userNumber {
-    case 1, 2, 3 :
-        return compareTwoInput(userNumber, with: computerNumber)
-    default :
-        return "종료"
+    while true {
+        switch gameResult {
+        case "무승부" :
+            print("\(winner)의 승리")
+            return
+        case "사용자" :
+            winner = "사용자"
+            gameResult = excuteMukChiBa(from: winner)
+        case "컴퓨터" :
+            winner = "컴퓨터"
+            gameResult = excuteMukChiBa(from: winner)
+        default :
+            return
+        }
     }
 }
 
@@ -88,30 +59,59 @@ func excuteMukChiBa(from scissorsRockPaperWinner: String) -> String {
     }
 }
 
-func startGame() {
-    var winner = excuteScissorsRockPaper()
+func createUserNumber() -> Int? {
+    let userInput = readLine()
     
-    if winner == "종료" {
-        return
-    }
-    
-    var gameResult = excuteMukChiBa(from: winner)
-    
-    while true {
-        switch gameResult {
-        case "무승부" :
-            print("\(winner)의 승리")
-            return
-        case "사용자" :
-            winner = "사용자"
-            gameResult = excuteMukChiBa(from: winner)
-        case "컴퓨터" :
-            winner = "컴퓨터"
-            gameResult = excuteMukChiBa(from: winner)
-        default :
-            return
-        }
+    if let userNumber = checkUserInput(userInput) {
+        return userNumber
+    } else {
+        return nil
     }
 }
 
-startGame()
+func checkUserInput(_ userInput: String?) -> Int? {
+    guard let userInput = userInput else {
+        return nil
+    }
+    let trimedUserInput = userInput.split(separator: " ").joined()
+    guard let userNumber = Int(trimedUserInput) else {
+        return nil
+    }
+    
+    if userNumber < 0 || userNumber >= 4 {
+        return nil
+    }
+    
+    return userNumber
+}
+
+func checkResult(to userNumber: Int) -> String {
+    let computerNumber = createRandomNumber()
+
+    switch userNumber {
+    case 1, 2, 3 :
+        return compareTwoInput(userNumber, with: computerNumber)
+    default :
+        return "종료"
+    }
+}
+
+func createRandomNumber() -> Int {
+    let computerInput = Int.random(in: 1...3)
+    
+    return computerInput
+}
+
+func compareTwoInput(_ userNumber: Int, with computerNumber: Int) -> String {
+    let user = RockScissorsPaper(rawValue: userNumber)
+    let computer = RockScissorsPaper(rawValue: computerNumber)
+    
+    switch (user, computer) {
+    case (.scissors, .rock), (.rock, .paper), (.paper, .scissors) :
+        return "컴퓨터"
+    case (.rock, .scissors), (.paper, .rock), (.scissors, .paper) :
+        return "사용자"
+    default :
+        return "무승부"
+    }
+}
