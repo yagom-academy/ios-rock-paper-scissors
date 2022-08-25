4abb6d10b62afe986b36a0f49bda38bd0896fdaa
hashswim <ss991117@naver.com>
2022-08-25 10:46:32 +0900

    feat: 코드 리팩토링 및 턴 타입 추가 구현 네비게이터: Kyo, 드라이버: Aaron (파일 누락)

---
+++ RockPaperScissors/RockPaperScissors/Model/Turn.swift
@@ -0,0 +1,14 @@
+//
+//  MukZiBa.swift
+//  RockPaperScissors
+//
+//  Created by Kyo,Aaron on 2022/08/24.
+//
+
+import Foundation
+
+enum Turn: String {
+    case userTurn = "사용자"
+    case computerTurn = "컴퓨터"
+    case undecidedTurn = "정해지지 않은 턴"
+}
