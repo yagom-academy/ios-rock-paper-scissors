b407bfcedafe85862f846394b95b4f4fac6582bf
LJ <swtw72@gmail.com>
2022-08-25 10:15:15 +0900

    feat: 사용자입력값 관련 메서드 추가
    - getSelectedUserMenu 입력값검증 후 잘못입력 시 에러메세지 출력, 정상입력 시 입력값 반환
    - userInput 입력값 받는 메서드
    - isCorrectUserMenu 입력값이 0~3범위인지 여부확인 메서드

--- RockPaperScissors/RockPaperScissors/MukChiBaGame.swift
+++ RockPaperScissors/RockPaperScissors/MukChiBaGame.swift
@@ -13,7 +13,38 @@ struct MukChiBaGame {
         }
     }
     
+    private func getSelectedUserMenu(turnOwner: Participant) -> Int {
+        printUserMenu(turnOwner: turnOwner)
+        guard let selectedUserMenu: Int = userInput() else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            return getSelectedUserMenu(turnOwner: turnOwner)
+        }
+        
+        if isCorrectUserMenu(selectedUserMenu) {
+            return selectedUserMenu
+        } else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            return getSelectedUserMenu(turnOwner: turnOwner)
+        }
+    }
+    
     func printUserMenu(turnOwner: Participant) {
         print("[\(turnOwner.name) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
     }
+    
+    private func userInput() -> Int? {
+        let userInput: Int? = Int(readLine() ?? "")
+        return userInput
+    }
+    
+    private func isCorrectUserMenu(_ userMenu: Int) -> Bool {
+        switch userMenu {
+        case 0...3:
+            return true
+        default:
+            return false
+        }
+    }
+    
+
 }
