52dd24abd0cec8cd86990ca7c38c8336cd638e2b
parkmuri <smxhmu@naver.com>
2022-12-26 14:06:32 +0900

    feat: 가위바위보/종료 선택함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,9 @@
 
 import Foundation
 
-print("Hello, World!")
-
+func choiceRockPaperScissors() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    guard let rockPaperScissors = readLine(), rockPaperScissors != "" else {
+        return
+    }
+}
