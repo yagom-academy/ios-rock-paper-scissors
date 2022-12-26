9f795f603412697c703f6bf47da970aa8e34b8ee
kimseongj <ksj8277@naver.com>
2022-12-26 14:43:18 +0900

    feat: makeRaondComputerHand함수 선언

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,8 +1,12 @@
-//
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
+//  Created by vetto, songjun on 2022.12.26
 
 import Foundation
+
+
+func makeRandomComputerHand() -> Int {
+    return Int.random(in: 1...3)
+}
+
+
 
