00c53f60469b65159984b955d08e0dfd297ef88e
minsu3007 <minsu3007@gmail.com>
2022-12-26 15:34:11 +0900

    feat: displayMenu 함수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,6 +6,15 @@
 
 import Foundation
 
+func displayMenu() {
+    let menuMessage = "가위(1), 바위(2), 보(3)! <종료: 0> :"
+    print(menuMessage, terminator: "")
+}
+
 func generateRockScissorsPaper() -> Int {
     return Int.random(in: 1...3)
+}
+
+func decideWinner() -> String {
+    
 }
