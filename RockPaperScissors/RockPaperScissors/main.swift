1feccb767896ad936d2d02271b26c410aceeef0b
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2022-12-26 14:38:53 +0900

    feat: ShowMenuMessage함수 구현, startgame함수 구현중

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,26 @@
-//
-//  RockPaperScissors - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
-print("Hello, World!")
+func startGame() {
+    showMenuMessage()
+    
+    if let userInput = readLine() {
+        switch userInput {
+        case "0":
+            print("게임종료")
+        case "1": ""
+        case "2": ""
+        case "3": ""
+        default:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            startGame()
+        }
+    }
+}
 
+func showMenuMessage() {
+    let menuMessage = "가위(1), 바위(2), 보(3)! <종료: 0> :"
+        
+    print(menuMessage, terminator: " ")
+}
+
+startGame()
