cb92cae0fac7d997bb31796df451f4d01ccbf4b9
kokkilE <kkuldak1@naver.com>
2022-12-26 15:39:12 +0900

    feat: compareNumber 함수 구현, getRandomNumber 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,15 +1,25 @@
 import Foundation
 
+var computerNumber: Int = 0
+
+enum GameResult {
+    case computerWin, draw, userWin
+}
+
+func getRandomNumber() {
+    computerNumber = Int.random(in: 1...3)
+}
+
 func startGame() {
+
     showMenuMessage()
     
-    if let userInput = readLine() {
+    if let userInput = Int(readLine()!) {
         switch userInput {
-        case "0":
+        case 0:
             print("게임종료")
-        case "1": ""
-        case "2": ""
-        case "3": ""
+        case 1...3:
+            compareNumber(number: userInput)
         default:
             print("잘못된 입력입니다. 다시 시도해주세요.")
             startGame()
@@ -21,6 +31,29 @@ func showMenuMessage() {
     let menuMessage = "가위(1), 바위(2), 보(3)! <종료: 0> :"
         
     print(menuMessage, terminator: " ")
+}
+
+func compareNumber(number: Int) {
+    getRandomNumber()
+    
+    if number == computerNumber {
+        print("비겼습니다!")
+        startGame()
+    }
+    
+    if number == 1 && computerNumber == 2 {
+        print("졌습니다!")
+    } else if number == 1 && computerNumber == 3 {
+        print("이겼습니다!")
+    } else if number == 2 && computerNumber == 3 {
+        print("졌습니다!")
+    } else if number == 2 && computerNumber == 1 {
+        print("이겼습니다!")
+    } else if number == 3 && computerNumber == 1 {
+        print("졌습니다!")
+    } else if number == 3 && computerNumber == 2 {
+        print("이겼습니다!")
+    }
 }
 
 startGame()
