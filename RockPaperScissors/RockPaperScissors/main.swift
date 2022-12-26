f54fcc0ca44341f8a404db47e9f5528d5325d0c4
yijiye <jiyee4013@naver.com>
2022-12-26 15:31:12 +0900

    refactor: printGameMenu() 함수 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,7 +6,23 @@
 
 import Foundation
 
-func printGameMenu() {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ")
+func startGame() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: " ")
+    
+    let input: String? = readLine()
+    guard let inputValue = input else {
+        return
+    }
+    
+    switch inputValue{
+    case "1","2","3" :
+        print("")
+    case "0" :
+        print("게임을 종료합니다.")
+        return
+    default :
+        print("잘못입력하셨습니다.")
+    }
 }
 
+startGame()
