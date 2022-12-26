a6592356abb072a5dabe01c5802c21c4a33c0c51
parkmuri <smxhmu@naver.com>
2022-12-26 14:59:40 +0900

    feat: 비교하여 결과 출력하는 함수 추가, 기존 함수 반환값 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,16 +6,42 @@
 
 import Foundation
 
-func choiceRockPaperScissors() {
+func choiceRockPaperScissors() -> Int {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+    
     guard let rockPaperScissors = readLine(), rockPaperScissors != "" else {
-        return
+        return choiceRockPaperScissors()
+    }
+
+    guard let rockPaperScissorsNumber = Int(rockPaperScissors) else {
+        return choiceRockPaperScissors()
+    }
+    
+    return rockPaperScissorsNumber
+}
+
+func makeRandomRockPaperScissors() -> Int {
+    let computerRandomNumber: Int
+    computerRandomNumber = Int.random(in: 1...3)
+    
+    return computerRandomNumber
+}
+
+func playRockPaperScissors() {
+    var computerNumber: Int = makeRandomRockPaperScissors()
+    var userNumber: Int = choiceRockPaperScissors()
+    var computerUserNumber: (Int, Int) = (computerNumber, userNumber)
+    
+    switch computerUserNumber {
+    case (1, 1), (2, 2), (3, 3):
+        print("비겼습니다!")
+    case (1, 2), (2, 3), (3, 1):
+        print("이겼습니다!")
+    case (2, 1), (3, 2), (1, 3):
+        print("졌습니다!")
+    default:
+       print("아무것도 아닙니다..")
     }
 }
 
-func makeRandomRockPaperScissors() {
-    let computerRandomNumber: Int
-    
-    computerRandomNumber = Int.random(in: 1...3)
-    print(computerRandomNumber)
-}
+playRockPaperScissors()
