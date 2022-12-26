0cacdc4b8f47d35cdfd95b13b187ee700d6ef939
kimseongj <ksj8277@naver.com>
2022-12-26 15:24:04 +0900

    feat: 컴퓨터 패와 사용자 패 비교하는  compareHnad함수 선언

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,16 +4,32 @@
 import Foundation
 
 
-func makeRandomComputerHand() -> Int {
-    return Int.random(in: 1...3)
+func makeRandomComputerHand() -> String {
+    return String(Int.random(in: 1...3))
 }
 
 func inputUserHand() -> String {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
     
     guard let userHand = readLine() else {
-        return
+        return ""
     }
     
     return userHand
+}
+
+// 승부를 결정 짓다
+func compareHand(computerHand: Int, userHand: Int) -> String {
+    let result: Int = userHand - computerHand
+    let status: String
+    
+    if result == 0 {
+        status = "Draw"
+    } else if result == -2 || result == 1 {
+        status = "Win"
+    } else {
+        status = "Lose"
+    }
+    
+    return status
 }
