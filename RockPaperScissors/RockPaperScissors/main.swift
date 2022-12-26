c9843ffe09816d28559e1fa3d2ebfba29413611a
kaki <ycg02109@naver.com>
2022-12-26 14:12:50 +0900

    feat: 입출력 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,32 @@
 
 import Foundation
 
-print("Hello, World!")
+enum RSP: Int {
+    case exit
+    case scissors
+    case rock
+    case paper
+}
 
+enum GameResult: String {
+    case draw = "비겼습니다!"
+    case win = "이겼습니다!"
+    case lose = "졌습니다!"
+}
+
+func printMenu() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+}
+
+func receiveUserInput() -> String {
+    guard let input = readLine() else { return "" }
+    return input
+}
+
+func printGameResult(gameResult: GameResult) {
+    print(gameResult.rawValue)
+}
+
+func play() {
+    printMenu()
+}
