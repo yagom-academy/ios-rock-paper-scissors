d30006ebd67965b5b1c926623b92e4cb60f49a6f
seunghyunCheon <csh7215@naver.com>
2022-12-27 14:47:55 +0900

    feat: 사용자 가위바위보 선택받는 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,6 +6,24 @@
 
 import Foundation
 
+enum RockScissorsPaperError: Error {
+    case invalidRockScissorsPaper
+    case invalidMukjippa
+}
+
 func printRockScissorsPaper() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: " ")
 }
+
+func choiceRockSissorsPaper() -> Result<Int, RockScissorsPaperError> {
+    let input: String? = readLine()
+    guard let choice = input else {
+        return .failure(RockScissorsPaperError.invalidRockScissorsPaper)
+    }
+    
+    guard let userChoice = Int(choice), (0...3) ~= userChoice else {
+        return .failure(RockScissorsPaperError.invalidRockScissorsPaper)
+    }
+    return .success(userChoice)
+}
+
