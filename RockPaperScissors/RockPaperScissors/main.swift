8ca4f2c6384b124cea3ab662a0c6f1df7daedf1c
kokkilE <kkuldak1@naver.com>
2022-12-27 16:50:02 +0900

    feat: 묵찌빠 기능 추가에 따른 showMenuMessage, inputUserNumber 함수 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,78 +1,91 @@
 import Foundation
 
-var computerNumber: Int = 0
+let end = 0, scissors = 1, rock = 2, paper = 3
+var winner: String = ""
+var currentGame: CurrentGame = .rockPaperScissors
 
 enum GameResult {
-    case computerWin, draw, userWin
+    case computerWin
+    case draw
+    case userWin
+}
+enum CurrentGame {
+    case rockPaperScissors
+    case mukChiba
 }
 
-func getRandomNumber() {
-    computerNumber = Int.random(in: 1...3)
-}
-
-func userMakeNumber() -> Int {
-    showMenuMessage()
-    
-    if let userInput = readLine() {
-        if let intUserInput = Int(userInput) {
-            return intUserInput
-        } else {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            return userMakeNumber()
-        }
-    } else {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        return userMakeNumber()
-    }
-}
-
-func startGame() {
-    let userInput = userMakeNumber()
-    
-    switch userInput {
-    case 0:
-        print("게임종료")
-    case 1...3:
-        let gameResult = compareNumber(number: userInput)
-        
-        if gameResult == .computerWin {
-            print("졌습니다")
-        } else if gameResult == .userWin {
-            print("이겼습니다")
-        } else {
-            print("비겼습니다")
-            startGame()
-        }
-    default:
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        startGame()
-    }
+func getRandomNumber() -> Int {
+    return Int.random(in: scissors...paper)
 }
 
 func showMenuMessage() {
-    let menuMessage = "가위(1), 바위(2), 보(3)! <종료: 0> :"
+    var menuMessage: String = ""
+    
+    if currentGame == .rockPaperScissors {
+        menuMessage = "가위(1), 바위(2), 보(3)! <종료: 0> :"
+    } else if currentGame == .mukChiba {
+        menuMessage = "[\(winner) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :"
+    }
     
     print(menuMessage, terminator: " ")
 }
 
-func compareNumber(number: Int) -> GameResult {
-    getRandomNumber()
+func inputUserNumber() -> Int {
+    showMenuMessage()
+        
+    guard let userInput = readLine(),
+          let intUserInput = Int(userInput),
+          intUserInput >= end && intUserInput <= paper else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        winner = "컴퓨터"
+        return inputUserNumber()
+    }
 
-    if number == 1 && computerNumber == 2 {
+    return intUserInput
+}
+
+func compareNumber(number: Int) -> GameResult {
+    let computerNumber = getRandomNumber()
+    let compareNumbers = (user: number, computer: computerNumber)
+    
+    switch compareNumbers {
+    case (scissors, rock), (rock, paper), (paper, scissors):
         return GameResult.computerWin
-    } else if number == 1 && computerNumber == 3 {
+    case (rock, scissors), (paper, rock), (scissors, paper):
         return GameResult.userWin
-    } else if number == 2 && computerNumber == 3 {
-        return GameResult.computerWin
-    } else if number == 2 && computerNumber == 1 {
-        return GameResult.userWin
-    } else if number == 3 && computerNumber == 1 {
-        return GameResult.computerWin
-    } else if number == 3 && computerNumber == 2 {
-        return GameResult.userWin
-    } else {
+    default:
         return GameResult.draw
     }
 }
 
-startGame()
+func startRockPaperScissors() {
+    let userInput = inputUserNumber()
+    
+    if userInput == 0 {
+        print("게임종료")
+    } else {
+        let gameResult = compareNumber(number: userInput)
+        
+        switch gameResult {
+        case .computerWin:
+            print("졌습니다")
+            winner = "컴퓨터"
+            mukChiBa()
+        case .draw:
+            print("비겼습니다")
+            startRockPaperScissors()
+        case .userWin:
+            print("이겼습니다")
+            winner = "사용자"
+            mukChiBa()
+        }
+    }
+}
+
+func mukChiBa() {
+    currentGame = .mukChiba
+    
+    let userInput = inputUserNumber()
+}
+
+startRockPaperScissors()
