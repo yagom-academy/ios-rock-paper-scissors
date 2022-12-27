37ec1c5d736a650c27506a5daf15dd6fd6cdc088
kimseongj <ksj8277@naver.com>
2022-12-27 17:35:53 +0900

    feat: convertNumberToHand함수 선언

--- RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.swift
@@ -1,23 +1,42 @@
 //  RockPaperScissors - RockPaperScissorsGame.swift
 //  Created by vetto, songjun on 2022.12.27
 
-func makeRandomComputerHand() -> Int {
+enum ScissorsRockPaper: Int { //MARK
+    case scissors = 1
+    case rock = 2
+    case paper = 3
+    case wrongHand
+}
+
+
+func makeRandomComputerNumber() -> Int {
     return Int.random(in: 1...3)
 }
 
-func inputUserHand() -> Int {
+func inputUserNumber() -> Int {
     guard let userInput = readLine() else {
-        return inputUserHand()
+        return inputUserNumber()
     }
-    guard let userHand = Int(userInput) else {
+    guard let userNumber = Int(userInput) else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        return inputUserHand()
+        return inputUserNumber()
     }
-    return userHand
+    return userNumber
 }
 
 func printMenu() {
+    print("")
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+}
+
+func convertNumberToHand(userNumber: Int, computerNumber: Int) -> (ScissorsRockPaper, ScissorsRockPaper) {
+    guard let userHand = ScissorsRockPaper(rawValue: userNumber) else {
+        return (ScissorsRockPaper.wrongHand, ScissorsRockPaper.wrongHand)
+    }
+    guard let computerHand = ScissorsRockPaper(rawValue: computerNumber) else {
+        return (ScissorsRockPaper.wrongHand, ScissorsRockPaper.wrongHand)
+    }
+    return (userHand, computerHand)
 }
 
 func compareHand(computerHand: Int, userHand: Int) -> String {
@@ -37,16 +56,31 @@ func compareHand(computerHand: Int, userHand: Int) -> String {
 
 
 func startGame() {
+    
     while true {
-        let userInput = inputUserHand()
+        printMenu()
+        let userInput = inputUserNumber()
         switch userInput {
         case 0:
             print("게임 종료")
             return
         case 1,2,3:
-            print(compareHand(computerHand: makeRandomComputerHand(), userHand: userInput))
+            
+            print(compareHand(computerHand: makeRandomComputerNumber(), userHand: userInput))
         default:
             print("잘못된 입력입니다. 다시 시도해주세요.")
         }
     }
 }
+
+
+
+//switch userNumber {
+//case 1:
+//    userHand = a.scissors
+//case 2:
+//    userHand = a.rock
+//case 3:
+//    userHand = a.paper
+//}
+
--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,3 +2,14 @@
 //  Created by vetto, songjun on 2022.12.26
 
 startGame()
+
+//enum abc: Int {
+//    case scissors = 1
+//    case rock = 2
+//    case paper = 3
+//}
+//let abb = abc(rawValue: 1)
+//let b = abc.rock
+//
+//print("\(abb)")
+//print("\(b.rawValue)")
