c33078627f3aaec237d1d601f0ad3a5cab9ef015
HarryHyeon <hharry0728@gmail.com>
2022-12-27 11:19:45 +0900

    feat: 가위바위보 열거형 파일 작성

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -0,0 +1,14 @@
+//
+//  RockPaperScissors.swift
+//  RockPaperScissors
+//
+//  Created by Harry, Andrew on 2022/12/27.
+//
+
+enum RockPaperScissors: Int {
+    case scissors = 1
+    case rock = 2
+    case paper = 3    
+}
+
+
