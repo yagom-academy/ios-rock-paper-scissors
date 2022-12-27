27e52a2d09638061971e042b908ccd9112336b57
hyemory <cjstksla73@gmail.com>
2022-12-27 11:36:06 +0900

    feat: RockPaperScissors 타입 추가 및 함수 분리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -3,7 +3,13 @@
 //  Created by 혜모리, 무리 on 2022.12.26
 //
 
-func choiceRockPaperScissors() -> Int {
+enum RockPaperScissors: Int {
+    case scissor = 1
+    case rock = 2
+    case paper = 3
+}
+
+func choiceRockPaperScissors() -> Int? {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
     
     guard let userInput = readLine(), userInput != "" else {
@@ -11,12 +17,19 @@ func choiceRockPaperScissors() -> Int {
         return choiceRockPaperScissors()
     }
 
-    guard let userInputNumber = Int(userInput), userInputNumber >= 0, userInputNumber <= 3 else {
+    return Int(userInput)
+}
+
+func checkRockPaperScissors(number: Int?) -> Int {
+    guard let rockPaperScissorsNumber = number,
+            rockPaperScissorsNumber >= 0,
+            rockPaperScissorsNumber <= 3
+    else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        return choiceRockPaperScissors()
+        return checkRockPaperScissors(number: choiceRockPaperScissors())
     }
     
-    return userInputNumber
+    return rockPaperScissorsNumber
 }
 
 func createComputerRockPaperScissors() -> Int {
@@ -25,24 +38,30 @@ func createComputerRockPaperScissors() -> Int {
     return computerNumber
 }
 
-func playRockPaperScissors() {
-    let computerRockPaperScissors = createComputerRockPaperScissors()
-    let userRockPaperScissors = choiceRockPaperScissors()
-    
-    let computerUserRockPaperScissors: (Int, Int) = (computerRockPaperScissors,
-                                                     userRockPaperScissors)
+func compareRockPaperScissors(computers: Int, users: Int) {
+    let scissor: Int = RockPaperScissors.scissor.rawValue
+    let rock: Int = RockPaperScissors.rock.rawValue
+    let paper: Int = RockPaperScissors.paper.rawValue
+    let computerUserRockPaperScissors: (Int, Int) = (computers, users)
     
     switch computerUserRockPaperScissors {
-    case (1, 1), (2, 2), (3, 3):
+    case (scissor, scissor), (rock, rock), (paper, paper):
         print("비겼습니다!")
         playRockPaperScissors()
-    case (1, 2), (2, 3), (3, 1):
+    case (scissor, rock), (rock, paper), (paper, scissor):
         print("이겼습니다!")
-    case (2, 1), (3, 2), (1, 3):
+    case (rock, scissor), (paper, rock), (scissor, paper):
         print("졌습니다!")
     default:
         print("게임 종료")
     }
+}
+
+func playRockPaperScissors() {
+    let computerRockPaperScissors = createComputerRockPaperScissors()
+    let userRockPaperScissors = checkRockPaperScissors(number: choiceRockPaperScissors())
+    
+    compareRockPaperScissors(computers: computerRockPaperScissors, users: userRockPaperScissors)
 }
 
 playRockPaperScissors()
