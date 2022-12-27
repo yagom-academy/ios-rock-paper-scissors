331bd605ff80555abac8c4eb0f306349da49426b
whk1140 <whk1140@gmail.com>
2022-12-27 10:25:17 +0900

    feat: 사용자 입력을 받는 함수 작성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,3 +10,11 @@ func printMenu() {
     print("가위(1), 바위(2), 보(3)! <종료: 0>", terminator: ": ")
 }
 
+func readUserInput() -> Result<String, InputError> {
+    guard let userInput = readLine() else {
+        return .failure(.invalidInput)
+    }
+    
+    return .success(userInput)
+}
+
