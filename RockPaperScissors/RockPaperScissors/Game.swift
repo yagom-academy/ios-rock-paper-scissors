4506e3c33f13405b69050c5030248fe66a2b1179
HarryHyeon <hharry0728@gmail.com>
2022-12-28 18:47:31 +0900

    feat: Game 파일추가 및 Game 파일에 GameType 열거형 추가;  MatchResult, GameFlow 열거형 Game 파일로 이동

---
+++ RockPaperScissors/RockPaperScissors/Game.swift
@@ -0,0 +1,22 @@
+//
+//  GameType.swift
+//  RockPaperScissors
+//
+//  Created by Harry, Andrew on 2022/12/28.
+//
+
+enum GameFlow {
+    case keepPlaying
+    case gameOver
+}
+
+enum MatchResult {
+    case win
+    case draw
+    case lose
+}
+
+enum GameType {
+    case rockScissorsPaper
+    case mukJiPPa
+}
