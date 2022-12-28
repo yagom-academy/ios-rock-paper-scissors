bec723b78ebd39feec62b396719cf7dddb62acb8
ROWAN <kimkj93@naver.com>
2022-12-28 13:27:20 +0900

    init: step01 피드백 적용

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -5,134 +5,85 @@
 //  Created by 레옹아범, Rhode, Rowan on 2022/12/26.
 //
 
-import Foundation
-
 class RockPaperScissors {
-    let end = 0
-    let scissors = 1
-    let rock = 2
-    let paper = 3
-    
-    var winner = ""
-    var isFirst = true
-    
-    func verifyUserChoice(userChoice: Result<Int, InputError>, computerChoice: Int) {
-        switch userChoice {
-        case .success(let selectedUserNumber):
-            self.winner = decideWinner(user: selectedUserNumber, computer: computerChoice)
-        case .failure(let error):
-            print(error.rawValue)
-            startGame()
-        }
+    private enum Menu: Int, CaseIterable {
+        case end = 0
+        case scissors = 1
+        case rock = 2
+        case paper = 3
     }
     
     func startGame() {
-        displayRockPaperScissors()
+        displayMenu()
         
         let userChoice = readInput()
         let computerChoice = generateRockPaperScissors()
-        
-        switch userChoice {
-        case .success(let selectedUserNumber):
-            self.winner = decideWinner(user: selectedUserNumber, computer: computerChoice)
-        case .failure(let error):
-            print(error.rawValue)
-            startGame()
+        guard let winner = compare(userChoice, and: computerChoice) else {
+            return startGame()
         }
-        
-        if winner == "사용자" {
-            print("이겼습니다!")
-            startMookZziPpa()
-        } else if winner == "컴퓨터" {
-            print("졌습니다!")
-            startMookZziPpa()
-        } else if winner == "무승부"{
-            print("비겼습니다!")
-            startGame()
-        }
+    
+        printResult(winner: winner)
     }
     
-    func startMookZziPpa() {
-        calculateTurn(winner: winner)
-        
-        let userChoice = readInput()
-        let computerChoice = generateRockPaperScissors()
-        var mookZziPpaWinner = ""
-        
-        switch userChoice {
-        case .success(let selectedUserNumber):
-            mookZziPpaWinner = decideWinner(user: selectedUserNumber, computer: computerChoice)
-        case .failure(let error):
-            print(error.rawValue)
-            startMookZziPpa()
-        }
-        
-        if mookZziPpaWinner == "사용자" {
-            self.winner = mookZziPpaWinner
-            startMookZziPpa()
-        } else if mookZziPpaWinner == "컴퓨터" {
-            self.winner = mookZziPpaWinner
-            startMookZziPpa()
-        } else if mookZziPpaWinner == "무승부"{
-            print("\(winner)의 승리!")
-        }
-    }
-    
-    private func displayRockPaperScissors() {
+    private func displayMenu() {
         let menuMessage = "가위(1), 바위(2), 보(3)! <종료: 0> :"
         print(menuMessage, terminator: " ")
     }
     
-    private func displayMookZziPpa(winner: String) {
-        let menuMessage = "[\(winner)턴] 묵(1), 찌(2), 빠(3)! <종료: 0> :"
-        print(menuMessage, terminator: " ")
-    }
-    
-    private func readInput() -> Result<Int, InputError> {
-        guard let input = readLine() else {
-            return .failure(.invalidInput)
-        }
-        guard let number = Int(input) else {
+    private func readInput() -> Result<Menu, InputError> {
+        guard let input = readLine(),
+              let number = Int(input),
+              let menu = Menu(rawValue: number) else {
             return .failure(.invalidInput)
         }
         
-        switch number {
-        case end...paper:
-            return .success(number)
+        switch menu {
+        case .end, .scissors, .rock, .paper:
+            return .success(menu)
+        }
+    }
+    
+    private func generateRockPaperScissors() -> Menu {
+        let result = Menu.allCases[Int.random(in: 1...3)]
+        
+        return result
+    }
+    
+    private func compare(_ userChoice: Result<Menu, InputError>, and computerChoice: Menu) -> String? {
+        switch userChoice {
+        case .success(let userMenu):
+            let winner = decideWinner(user: userMenu, computer: computerChoice)
+            return winner
+        case .failure(let error):
+            print(error.rawValue)
+            return nil
+        }
+    }
+    
+    private func decideWinner(user: Menu, computer: Menu) -> String {
+        switch (user, computer) {
+        case (.rock, .scissors), (.paper, .rock), (.scissors, .paper):
+            return WinnerResult.user
+        case (.scissors, .rock), (.rock, .paper), (.paper, .scissors):
+            return WinnerResult.computer
+        case (.rock, .rock), (.scissors, .scissors), (.paper, .paper):
+            return WinnerResult.noWinner
         default:
-            return .failure(.invalidInput)
+            return ""
         }
     }
     
-    private func generateRockPaperScissors() -> Int {
-        return Int.random(in: scissors...paper)
-    }
-    
-    private func decideWinner(user: Int, computer: Int) -> String {
-        if user > computer {
-            if user == paper && computer == scissors {
-                return "컴퓨터"
-            } else {
-                return "사용자"
-            }
-        } else if user < computer {
-            if user == scissors && computer == paper {
-                return "사용자"
-            } else {
-                return "컴퓨터"
-            }
+    private func printResult(winner: String) {
+        switch winner {
+        case WinnerResult.user:
+            print("이겼습니다!")
+        case WinnerResult.computer:
+            print("졌습니다!")
+        case WinnerResult.noWinner:
+            print("비겼습니다!")
+            startGame()
+        default:
+            print("게임종료")
         }
-        
-        return "무승부"
     }
-    
-    func calculateTurn(winner: String) {
-        if isFirst {
-            isFirst = false
-        } else {
-            print("\(winner)의 턴입니다.")
-        }
-        displayMookZziPpa(winner: winner)
-    }
-    
 }
