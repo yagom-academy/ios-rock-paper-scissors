709d7612faaf5064281d142bb24b7652625b8a03
parkmuri <smxhmu@naver.com>
2022-12-29 17:19:50 +0900

    feat: GameManager 파일 분리

---
+++ RockPaperScissors/RockPaperScissors/GameManager.swift
@@ -0,0 +1,15 @@
+//
+//  RockPaperScissors - GameManager.swift
+//  Created by 혜모리, 무리 on 2022.12.28
+//
+
+class GameManager {
+    let rockPaperScissorsGame: RockPaperScissors = RockPaperScissors()
+    let mookJjiBbaGame: MookJjiBba = MookJjiBba()
+
+    func startGame() {
+        rockPaperScissorsGame.playRockPaperScissors(mookJjiBba: mookJjiBbaGame)
+        
+        mookJjiBbaGame.playMookJjiBba()
+    }
+}
