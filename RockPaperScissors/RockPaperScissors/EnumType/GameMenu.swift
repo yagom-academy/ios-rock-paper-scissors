66a56a6026e892743f29996e6e4a6cd64cee61e0
Jun <sa9343@naver.com>
2022-12-30 10:57:20 +0900

    refactor : 파일 분리, 그룹 나누기

---
+++ RockPaperScissors/RockPaperScissors/EnumType/GameMenu.swift
@@ -0,0 +1,13 @@
+//
+//  GameMenu.swift
+//  RockPaperScissors
+//
+//  Created by riji, rilla on 2022/12/30.
+//
+
+enum GameMenu: String {
+    case scissor = "1"
+    case rock = "2"
+    case paper = "3"
+    case endGame = "0"
+}
