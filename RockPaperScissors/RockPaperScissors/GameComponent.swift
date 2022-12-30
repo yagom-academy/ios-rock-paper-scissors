3409ba12a0e2185582d73ba4b2c87f88171c1c21
whk1140 <whk1140@gmail.com>
2022-12-30 09:59:14 +0900

    refactor: HandShape 파일 삭제, Game파일 GameComponent 이름 변경,  GameTurn 열거형 정의

---
+++ RockPaperScissors/RockPaperScissors/GameComponent.swift
@@ -0,0 +1,37 @@
+//
+//  GameComponent.swift
+//  RockPaperScissors
+//
+//  Created by Harry, Andrew on 2022/12/28.
+//
+
+enum GameFlow {
+    case keepPlaying
+    case gameOver
+}
+
+enum MatchResult {
+    case win
+    case same
+    case lose
+}
+
+enum HandShape: CaseIterable {
+    case rock
+    case paper
+    case scissors
+}
+
+enum GameTurn {
+    case user
+    case computer
+    
+    func turnMessage() -> String {
+        switch self {
+        case .user:
+            return "사용자의 턴입니다."
+        case .computer:
+            return "컴퓨터의 턴입니다."
+        }
+    }
+}
