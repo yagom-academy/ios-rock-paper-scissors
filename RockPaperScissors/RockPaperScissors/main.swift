4699f56f84491e684f1b1a70ed1bf9e3450be4a0
Zion <lion2212@naver.com>
2023-05-01 17:00:32 +0900

    feat: RockPaperScissorsGame 타입 추가 및 정의 start() 함수구현
    드라이버 : Zion
    네비게이터 : qoocrab

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,46 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
 
-import Foundation
+enum Choice: Int {
+    case Rock = 0
+    case Paper = 1
+    case Scissors = 2
+}
 
-print("Hello, World!")
 
+struct RockPaperScissorsGame {
+    
+    private
+    func getUserChoice(_ inputMenu: String?) -> Choice? {
+        var userChoice: Choice? = nil
+        
+        switch inputMenu {
+            case "1":
+                userChoice = Choice.Scissors
+            case "2":
+                userChoice = Choice.Rock
+            case "3":
+                userChoice = Choice.Paper
+            case "0":
+                break
+            default:
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+        }
+        
+        return userChoice
+    }
+    
+    func start() {
+        while true {
+            print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
+            
+            let inputMenu = readLine()
+            let computerChoice = Choice(rawValue: Int.random(in: 0...2))
+            
+            guard let userChoice = getUserChoice(inputMenu) else { continue }
+        }
+    }
+    
+}
