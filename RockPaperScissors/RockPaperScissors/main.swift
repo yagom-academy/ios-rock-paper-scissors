59391f291e81f482ce6bd850a6425626cc5b73e1
ysj <yyssjj9999@gmail.com>
2023-05-01 12:28:18 +0900

    feat: 유저와 컴퓨터 숫자 비교하는 함수추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -11,15 +11,32 @@ func showGameMune() {
     while isGameOn {
         print("가위(1), 바위(2), 보(3)! <종료 :0> : ", terminator: "")
         
-        let menuChoice: String = readLine() ?? ""
-        
-        if menuChoice == gameEnd {
+        guard let userNumber: String = readLine() else { return }
+        let computerNumber: String = String(Int.random(in: 1...3))
+
+        if userNumber == gameEnd {
             isGameOn = false
             print("게임종료")
-        } else if menuChoice == "1" || menuChoice == "2" || menuChoice == "3" {
-            //checkRockPaperScissors
+        } else if userNumber == "1" ||
+                    userNumber == "2" ||
+                    userNumber == "3" {
+            checkRockPaperScissors(with: userNumber, computerNumber)
         } else {
             print("잘못된 입력입니다. 다시 시도해주세요.")
         }
     }
 }
+
+func checkRockPaperScissors(with userInput: String, _ computerInput: String) {
+    if userInput == computerInput {
+        print("비겼습니다!")
+    } else if userInput == "1" && computerInput == "3" ||
+                userInput == "2" && computerInput == "1" ||
+                userInput == "3" && computerInput == "2" {
+        print("이겼습니다!")
+    } else {
+        print("졌습니다!")
+    }
+}
+
+showGameMune()
