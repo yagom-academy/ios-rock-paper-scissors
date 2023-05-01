75bb713cadf15a7b5dfbb5d9790053f3d9d59ccb
bubblecocoa <bubblecocoa@gmail.com>
2023-05-01 11:18:22 +0900

    feat: 사용자에게 입력받는 함수 일부 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,7 +4,16 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
-
-print("Hello, World!")
-
+struct RockPaperScissors {
+    func startGame() {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> :")
+    }
+    
+    func inputUser() -> Int {
+        guard let inputValue = readLine(), let intValue = Int(inputValue) else {
+            return
+        }
+        
+        return intValue
+    }
+}
