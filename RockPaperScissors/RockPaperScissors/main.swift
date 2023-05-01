025c7ce72d0f716553c602d58aa1fb00900650ee
Kobe <mskang@xingxingmobility.com>
2023-05-01 11:18:33 +0900

    feat: 사용자에게 입력값을 받는 함수 구현.

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,7 +4,17 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
+print("가위(1), 바위(2), 보(3)!<종료 : 0> :", terminator: " ")
 
-print("Hello, World!")
+func inputUserNumber() -> Int {
+	guard let input = readLine(), let input = Int(input) else {
+		return inputUserNumber()
+	}
+	
+	guard (0...3).contains(Int(input)) else {
+		return inputUserNumber()
+	}
+	
+	return input
+}
 
