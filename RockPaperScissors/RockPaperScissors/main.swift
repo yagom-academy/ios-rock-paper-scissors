85321d829d1b5ee948fba6381aa4c550acf724d3
MaryJo-github <jotjdal0215@gmail.com>
2023-05-01 13:32:28 +0900

    feat: 열거형 Menu 타입 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,13 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Menu: String {
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
+    case exit = "0"
+    
+    static let allMenu: Array<Menu> = [.scissors, .rock, .paper, .exit]
+    static let menuRawValues: Dictionary<String, Menu> = Menu.allMenu.reduce(into: Dictionary<String, Menu>()) { $0[$1.rawValue] = $1 }
+}
 
