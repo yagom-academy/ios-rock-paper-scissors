ec5383b72a956527aa9d96a15f79a235e88368ee
hemg2 <hemg2@naver.com>
2023-05-01 12:02:19 +0900

    feat: 게임메뉴출력 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,25 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by yy-ss99, Hemg.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
-
-print("Hello, World!")
-
+func showGameMune() {
+    var isGameOn: Bool = true
+    let gameEnd: String = "0"
+    
+    while isGameOn {
+        print("가위(1), 바위(2), 보(3)! <종료 :0> : ", terminator: "")
+        
+        let menuChoice: String = readLine() ?? ""
+        
+        if menuChoice == gameEnd {
+            isGameOn = false
+            print("게임종료")
+        } else if menuChoice == "1" || menuChoice == "2" || menuChoice == "3" {
+            //checkRockPaperScissors
+        } else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+        }
+    }
+}
