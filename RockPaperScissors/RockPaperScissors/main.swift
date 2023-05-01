5c4647886c73bdad0d88da2367db4af6a9faa03d
MaryJo-github <jotjdal0215@gmail.com>
2023-05-01 17:39:53 +0900

    feat: Result 타입 생성 및 게임 실행 기능 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,8 +1,8 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//  last modified by maxhyunm, Mary
 
 import Foundation
 
@@ -11,19 +11,35 @@ enum Menu: String {
     case rock = "2"
     case paper = "3"
     case exit = "0"
-    
-    static let allMenu: Array<Menu> = [.scissors, .rock, .paper, .exit]
-    static let menuRawValues: Dictionary<String, Menu> = Menu.allMenu.reduce(into: Dictionary<String, Menu>()) { $0[$1.rawValue] = $1 }
+}
+
+enum Result: String {
+    case win = "이겼습니다!"
+    case lose = "졌습니다!"
+    case draw = "비겼습니다!"
 }
 
 func startGame() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+    
     guard let inputValue = readLine(),
-          let inputValue = Menu.menuRawValues[inputValue],
-          Menu.allMenu.contains(inputValue)
+          let menu = Menu(rawValue: inputValue)
     else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
         return
     }
+    guard menu != .exit else {
+        print("게임 종료")
+        return
+    }
+    
+    let gameResult = playGame(menu)
+    
+    if gameResult == .draw {
+        startGame()
+    }
 }
 
+func playGame(_ users: Menu) -> Result {
+    return .win
+}
