a9d7b2d132d151f33c55bf846298dfcb46ce18d6
h-suo <phstoo1021@naver.com>
2023-05-01 18:04:42 +0900

    feat: getRockPaperScissors 함수 추가, readLine()으로 사용자 입력 받았을 때 오류 문구 구분

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,16 +2,13 @@
 //  RockPaperScissors - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
-
-import Foundation
+//
 
 enum RockPaperScissors: Int {
     case scissor = 1
     case rock = 2
     case paper = 3
 }
-
 
 func compareRockPaperScissors(between randomRockPaperScissors: RockPaperScissors, and playerRockPaperScissors: RockPaperScissors) -> String {
     if randomRockPaperScissors == playerRockPaperScissors {
@@ -25,5 +22,21 @@ func compareRockPaperScissors(between randomRockPaperScissors: RockPaperScissors
         return randomRockPaperScissors == RockPaperScissors.scissor ? "이겼습니다!" : "졌습니다!"
     case .paper:
         return randomRockPaperScissors == RockPaperScissors.rock ? "이겼습니다!" : "졌습니다!"
+    }
+}
+
+
+func getRockPaperScissors() {
+    while true {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+        if let input = readLine(), let choice = Int(input) {
+            if choice == 0 {
+                print("게임 종료")
+                break
+            } else if choice < 1 || choice > 3 {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                continue
+            }
+        }
     }
 }
