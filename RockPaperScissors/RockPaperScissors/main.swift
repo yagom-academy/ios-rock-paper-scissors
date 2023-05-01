28bb5689d6ddf2d0ed169b1953942dfa51845503
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-05-01 18:14:23 +0900

    feat: 메뉴 출력

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,7 +4,24 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
 
-print("Hello, World!")
+var programRunning = true
 
+while programRunning {
+    if let input: String = readLine() {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0>")
+        switch input {
+        case "1":
+            print("가위")
+        case "2":
+            print("바위")
+        case "3":
+            print("보")
+        case "0":
+            programRunning = false
+            print("게임 종료")
+        default:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+        }
+    }
+}
