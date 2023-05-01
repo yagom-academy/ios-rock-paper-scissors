024551dc70916b7e01957ec4abc4317ea6f515ba
Min Hyun <minnimida@gmail.com>
2023-05-01 13:49:21 +0900

    feat: 게임 시작 함수 startGame() 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -16,3 +16,14 @@ enum Menu: String {
     static let menuRawValues: Dictionary<String, Menu> = Menu.allMenu.reduce(into: Dictionary<String, Menu>()) { $0[$1.rawValue] = $1 }
 }
 
+func startGame() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+    guard let inputValue = readLine(),
+          let inputValue = Menu.menuRawValues[inputValue],
+          Menu.allMenu.contains(inputValue)
+    else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return
+    }
+}
+
