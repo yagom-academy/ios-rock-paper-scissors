6081fef81c9c593d64c3c665f8c5e4bbde7b338d
h-suo <phstoo1021@naver.com>
2023-05-01 18:56:17 +0900

    refactor: getUserInput내 while문을 startGame으로 분리하여 처리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -11,10 +11,11 @@ enum RockPaperScissors: Int {
 }
 
 func compareRockPaperScissors(between randomRockPaperScissors: RockPaperScissors, and playerRockPaperScissors: RockPaperScissors) -> String {
+    
     if randomRockPaperScissors == playerRockPaperScissors {
         return "비겼습니다!"
     }
-    
+
     switch playerRockPaperScissors {
     case .scissor:
         return randomRockPaperScissors == RockPaperScissors.paper ? "이겼습니다!" : "졌습니다!"
@@ -25,32 +26,33 @@ func compareRockPaperScissors(between randomRockPaperScissors: RockPaperScissors
     }
 }
 
-
-func getUserInput() {
-    while true {
-        print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
-        guard let input = readLine() else { return }
-     
-        switch input {
-        case "1", "2", "3":
-            guard let inputNumber = Int(input) else { return }
-            guard let playerRockPaperScissors = RockPaperScissors(rawValue: inputNumber) else { return }
-            playingGame(playerRockPaperScissors)
-        case "0":
-            break
-        default:
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            continue
-        }
-    }
-}
-
-func playingGame(_ playerInput: RockPaperScissors) {
-    guard let randomRockPaperScissors = RockPaperScissors(rawValue: Int.random(in: 1...3)) else { return }
+func getUserInput() -> Bool {
+    var isUserSelect: Bool = true
     
-    compareRockPaperScissors(between: randomRockPaperScissors, and: playerInput)
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+    guard let input = readLine() else { return isUserSelect }
+
+    switch input {
+    case "1", "2", "3":
+        guard let inputNumber = Int(input) else { return isUserSelect }
+        guard let playerRockPaperScissors = RockPaperScissors(rawValue: inputNumber) else { return isUserSelect }
+        guard let randomRockPaperScissors = RockPaperScissors(rawValue: Int.random(in: 1...3)) else { return isUserSelect }
+        print(compareRockPaperScissors(between: randomRockPaperScissors, and: playerRockPaperScissors))
+    case "0":
+        isUserSelect = false
+        print("게임 종료")
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+    return isUserSelect
 }
 
-getUserInput()
+func startGame() {
+    var isUserSelect: Bool = true
+    
+    repeat {
+        isUserSelect = getUserInput()
+    } while isUserSelect
+}
 
-
+startGame()
