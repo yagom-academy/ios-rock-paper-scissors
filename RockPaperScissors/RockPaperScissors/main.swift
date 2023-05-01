4ffb83079b185f8ee7943a4183ea6d032d36fa95
karen <karenrodanthe@icloud.com>
2023-05-01 17:41:17 +0900

    feat: RockPaperScissors 열거형 타입 추가, RockPaperScissors타입을 비교하고 결과를 출력하는 compareRockPaperScissors 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,24 @@
 
 import Foundation
 
-print("Hello, World!")
+enum RockPaperScissors: Int {
+    case scissor = 1
+    case rock = 2
+    case paper = 3
+}
 
+
+func compareRockPaperScissors(between randomRockPaperScissors: RockPaperScissors, and playerRockPaperScissors: RockPaperScissors) -> String {
+    if randomRockPaperScissors == playerRockPaperScissors {
+        return "비겼습니다!"
+    }
+    
+    switch playerRockPaperScissors {
+    case .scissor:
+        return randomRockPaperScissors == RockPaperScissors.paper ? "이겼습니다!" : "졌습니다!"
+    case .rock:
+        return randomRockPaperScissors == RockPaperScissors.scissor ? "이겼습니다!" : "졌습니다!"
+    case .paper:
+        return randomRockPaperScissors == RockPaperScissors.rock ? "이겼습니다!" : "졌습니다!"
+    }
+}
