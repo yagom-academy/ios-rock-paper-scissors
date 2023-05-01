c712b6d9ad919f52f52ef17f8bee0eabf31273c1
YaRkyungmin <kkgm94@naver.com>
2023-05-01 12:16:47 +0900

    feat:사용자의 입력 유효성검증 함수 구현 중, error case 열거형 선언

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -3,18 +3,37 @@
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
 // 
-
+enum UserError: Error {
+    case invalidConvertType
+    case invalidNumber
+    case nilValue
+}
 print("가위(1), 바위(2), 보(3)!<종료 : 0> :", terminator: " ")
 
-func inputUserNumber() -> Int {
+func inputUserNumber() throws -> Int {
 	guard let input = readLine(), let input = Int(input) else {
-		return inputUserNumber()
+        throw UserError.invalidConvertType
 	}
 	
-	guard (0...3).contains(Int(input)) else {
-		return inputUserNumber()
+	guard (0...3).contains(input) else {
+        throw UserError.invalidNumber
 	}
 	
 	return input
 }
 
+func validationNumber() -> Int? {
+    var input: Int?
+    
+    do {
+        input = try inputUserNumber()
+    } catch UserError.invalidConvertType {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    } catch UserError.invalidNumber {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    } catch {
+        print(error)
+    }
+    
+    return input
+}
