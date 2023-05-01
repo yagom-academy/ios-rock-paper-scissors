792c13ffbdfe8f95cdf870800520f7529f7c912c
bubblecocoa <bubblecocoa@gmail.com>
2023-05-01 12:24:59 +0900

    refactor: 승부 판별 함수 로직 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -9,12 +9,15 @@ struct RockPaperScissors {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
         let userNumber = inputUser()
         let computerNumber = Int.random(in: 1...3)
-        
+        print("computerNumber : \(computerNumber)")
+
         discrimination(user: userNumber, computer: computerNumber)
     }
     
     func inputUser() -> Int {
-        guard let inputValue = readLine(), let intValue = Int(inputValue) else {
+        guard let inputValue = readLine(), let intValue = Int(inputValue), 1...3 ~= intValue else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            
             return -1
         }
         
@@ -22,35 +25,31 @@ struct RockPaperScissors {
     }
     
     func discrimination(user: Int, computer: Int) {
-        switch user {
-        case 0:
-            print("게임 종료")
-        case 1:
-            if user == computer {
-                print("비겼습니다!")
-            } else if computer == 3 {
-                print("이겼습니다!")
-            } else {
-                print("졌습니다!")
-            }
-        case 2:
-            if user == computer {
-                print("비겼습니다!")
-            } else if computer == 1 {
-                print("이겼습니다!")
-            } else {
-                print("졌습니다!")
-            }
-        case 3:
-            if user == computer {
-                print("비겼습니다!")
-            } else if computer == 2 {
-                print("이겼습니다!")
-            } else {
-                print("졌습니다!")
-            }
-        default:
-            print("잘못된 입력입니다. 다시 시도해주세요.")
+        var userDefeat: Int
+        var userWin: Int
+        
+        if user == computer {
+            print("비겼습니다!")
+        }
+        
+        if user == 3 {
+            userDefeat = 1
+        } else {
+            userDefeat = user + 1
+        }
+        
+        if user == 1 {
+            userWin = 3
+        } else {
+            userWin = user - 1
+        }
+        
+        if userDefeat == computer {
+            print("졌습니다!")
+        }
+        
+        if userWin == computer {
+            print("이겼습니다!")
         }
     }
 }
