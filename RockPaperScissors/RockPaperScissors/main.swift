f37ad958214b38ca4e42c5727dbfc0b6864c88fc
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-05-01 17:45:14 +0900

    refactor: 불필요한 구조체 삭제

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,50 +4,47 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-struct RockPaperScissors {
-    func startGame() {
-        while true {
-            let computerNumber = Int.random(in: 1...3)
+func startGame() {
+    while true {
+        let computerNumber = Int.random(in: 1...3)
+        
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+        
+        guard let input = readLine(), let userNumber = Int(input), 0...3 ~= userNumber else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
             
-            print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-            
-            guard let input = readLine(), let userNumber = Int(input), 0...3 ~= userNumber else {
-                print("잘못된 입력입니다. 다시 시도해주세요.")
-                
-                continue
-            }
-            
-            if userNumber == 0 {
-                print("게임 종료")
-                
-                return
-            }
-            
-            guard compare(to: userNumber, and: computerNumber) else {
-                return
-            }
-        }
-    }
-    
-    func compare(to user: Int, and computer: Int) -> Bool {
-        if user == computer {
-            print("비겼습니다!!")
-            return true
-        } else if isUserWin(user, computer) {
-            print("이겼습니다!!")
-        } else {
-            print("졌습니다!!")
+            continue
         }
         
-        return false
-    }
-    
-    func isUserWin(_ user: Int, _ computer: Int) -> Bool {
-        return user == 1 && computer == 3
-            || user == 2 && computer == 1
-            || user == 3 && computer == 2
+        if userNumber == 0 {
+            print("게임 종료")
+            
+            return
+        }
+        
+        guard compare(to: userNumber, and: computerNumber) else {
+            return
+        }
     }
 }
 
-let rps = RockPaperScissors()
-rps.startGame()
+func compare(to user: Int, and computer: Int) -> Bool {
+    if user == computer {
+        print("비겼습니다!!")
+        return true
+    } else if isUserWin(user, computer) {
+        print("이겼습니다!!")
+    } else {
+        print("졌습니다!!")
+    }
+    
+    return false
+}
+
+func isUserWin(_ user: Int, _ computer: Int) -> Bool {
+    return user == 1 && computer == 3
+        || user == 2 && computer == 1
+        || user == 3 && computer == 2
+}
+
+startGame()
