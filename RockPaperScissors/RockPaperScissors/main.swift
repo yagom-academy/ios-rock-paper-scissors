d6b4c85af3a99c632b1b19813bf15c03762590ab
idinaloq <idinaloq@icloud.com>
2023-05-02 11:25:53 +0900

    feat: 메뉴입력 함수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -8,3 +8,10 @@ import Foundation
 
 print("Hello, World!")
 
+func inputMenu() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0>: ", terminator: "")
+    let userNumber = readLine()
+    if userNumber == 0 {
+        
+    } else 
+}
