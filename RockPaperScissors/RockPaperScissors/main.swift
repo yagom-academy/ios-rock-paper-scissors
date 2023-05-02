fe16378a6870cf9a8db704abcd8964b06b35c6ef
Whales <Whales.w.Jin@gmail.com>
2023-05-02 15:22:32 +0900

    fix: 가위바위보 비교함수 수정
    함수명을 showResult로 바꾸고 보가 가위를 이기는 문제 해결

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,28 +6,53 @@
 
 import Foundation
 
+enum HandShape: Int {
+    case scissors = 1, rock, paper
+}
+
 gameLoop: while true {
-    let computer = String(Int.random(in: 1...3))
-    print("computer: \(computer)")
+
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+    let computer = HandShape(rawValue: Int.random(in: 1...3))!
     let input = readLine()
+    
     switch input {
     case "0":
         print("게임 종료")
         break gameLoop
     case "1", "2", "3":
-        check(input: input, computer: computer)
+        print("computer: \(computer)")
+        let user = HandShape(rawValue: Int(input!)!)!
+        showResult(input: user, computer: computer)
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
     }
 }
 
-func check(input: String?, computer: String) {
-    if let input2 = input, input2 == computer {
-        print("비겼습니다!")
-    } else if let input2 = input, input2 > computer {
-        print("이겼습니다!")
+func showResult(input: HandShape, computer: HandShape) {
+    if input == .rock {
+        if computer == .rock {
+            print("비겼습니다!")
+        } else if computer == .scissors {
+            print("이겼습니다!")
+        } else {
+            print("졌습니다!")
+        }
+    } else if input == .scissors {
+        if computer == .scissors {
+            print("비겼습니다!")
+        } else if computer == .paper {
+            print("이겼습니다!")
+        } else {
+            print("졌습니다!")
+        }
     } else {
-        print("졌습니다!")
+        if computer == .paper {
+            print("비겼습니다!")
+        } else if computer == .rock {
+            print("이겼습니다!")
+        } else {
+            print("졌습니다!")
+        }
     }
 }
