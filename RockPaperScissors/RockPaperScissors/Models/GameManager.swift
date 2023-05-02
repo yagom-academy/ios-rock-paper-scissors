01ce391c6b464b56ab45b43803195e30ceac5081
myungsun <audtjs9987@naver.com>
2023-05-02 15:40:49 +0900

    feat: startGame 메서드 내에서 사용자 입력 값 받는 기능 구현

--- RockPaperScissors/RockPaperScissors/Models/GameManager.swift
+++ RockPaperScissors/RockPaperScissors/Models/GameManager.swift
@@ -6,9 +6,33 @@
 //
 
 class GameManager {
-    // 속성(특성)
-    
-    // 행위(메서드)
-    // 게임 시작 메서드
-    // 게임에 대한 결과를 알려주는 메서드
+    func startGame() {
+        let computerPlayer = Player(type: .computer)
+        let userPlayer = Player(type: .person)
+        let handShapes: [HandShape] = [.scissors, .rock, .paper]
+        var isGameOn: Bool = true
+        
+        while isGameOn {
+            print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+            guard let gameOption = readLine(),
+                  let gameOptionNumber = Int(gameOption) else {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                continue
+            }
+            
+            computerPlayer.makeRandomHandShape()
+            guard let computerHandShape = computerPlayer.getCurrentHandShape() else { continue }
+            
+            switch gameOptionNumber {
+            case 0:
+                isGameOn = false
+                print("게임 종료")
+            case 1...3:
+                let userHandShape = handShapes[gameOptionNumber-1]
+
+            default:
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+            }
+        }
+    }
 }
