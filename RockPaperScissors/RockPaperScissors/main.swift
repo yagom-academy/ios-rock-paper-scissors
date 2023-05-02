90ad2f174d313b24f9c64f7aebd4bcb10ec8d8fc
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-05-02 15:27:43 +0900

    refactor: 가독성 향상을 위해 변수명과 함수명 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -13,14 +13,17 @@ enum RockPaperScissors: Int {
 func startGame() {
     let computerNumber = Int.random(in: 1...3)
     let userNumber = inputUserNumber()
-    
+
     if userNumber == 0 {
         print("게임 종료")
         
         return
     }
     
-    compare(to: changeNumberToEnum(userNumber), and: changeNumberToEnum(computerNumber))
+    let userHand = convertEnum(of: userNumber)
+    let computerHand = convertEnum(of: computerNumber)
+    
+    compare(userHand, and: computerHand)
 }
 
 func inputUserNumber() -> Int {
@@ -37,28 +40,26 @@ func inputUserNumber() -> Int {
     return userNumber
 }
 
-func changeNumberToEnum(_ playerNumber: Int) -> RockPaperScissors {
-    var playerHand: RockPaperScissors = RockPaperScissors.paper
-    
-    switch playerNumber {
+func convertEnum(of number: Int) -> RockPaperScissors {
+    switch number {
     case 1:
-        playerHand = .scissors
+        return .scissors
     case 2:
-        playerHand = .rock
+        return .rock
     case 3:
-        playerHand = .paper
+        return .paper
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
     }
     
-    return playerHand
+    return .paper
 }
 
-func compare(to user: RockPaperScissors, and computer: RockPaperScissors) {
-    if user == computer {
+func compare(_ userHand: RockPaperScissors, and computerHand: RockPaperScissors) {
+    if userHand == computerHand {
         print("비겼습니다!!")
         startGame()
-    } else if isUserWin(user, computer) {
+    } else if isUserWin(userHand, computerHand) {
         print("이겼습니다!!")
     } else {
         print("졌습니다!!")
