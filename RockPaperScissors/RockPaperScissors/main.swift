8b8c47eae3d54c957dc9a41e79e8a4ac00f6010d
idinaloq <idinaloq@icloud.com>
2023-05-02 13:23:44 +0900

    feat: 메뉴입력함수, 비교함수 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,39 +10,44 @@ print("Hello, World!")
 
 func inputMenu() -> Int {
     print("가위(1), 바위(2), 보(3)! <종료 : 0>: ", terminator: "")
-    guard let inputNumber = readLine() else {
-        print("error2")
-        return 0
-    }
+    let inputNumber = readLine()
     
-            
-    if inputNumber == "0" {
+    switch inputNumber {
+    case "1":
+        compareRandomNumber(<#T##Int#>, to: <#T##Int#>)
+        inputMenu()
+    case "2":
+        compareRandomNumber(<#T##Int#>, to: <#T##Int#>)
+        inputMenu()
+    case "3":
+        compareRandomNumber(<#T##Int#>, to: <#T##Int#>)
+        inputMenu()
+    case "0":
         print("게임 종료")
-    }
-    if inputNumber != "1" || inputNumber != "2" || inputNumber != "3" {
+    default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
+        inputMenu()
     }
-    guard let userNumber = Int(inputNumber) else {
-        print("error3")
-        return 0
-    }
-    return userNumber
+    return Int(inputNumber)
 }
 
 func createRandomNumber() -> Int {
-    var randomNumber = Int.random(in: 1...3)
+    let randomNumber = Int.random(in: 1...3)
     return randomNumber
 }
 
-func compareRandomNumber(_ userNumber: Int, to randomNumber: Int) {
-    if userNumber == randomNumber {
+func compareRandomNumber(_ inputNumber: Int, to randomNumber: Int) {
+    if inputNumber == randomNumber {
         print("비겼습니다!")
-    } else if (userNumber - randomNumber).magnitude == 2 && userNumber < randomNumber {
+    } else if (inputNumber - randomNumber).magnitude == 2 && inputNumber < randomNumber {
         print("이겼습니다!")
-    } else if userNumber < randomNumber {
+    } else if inputNumber < randomNumber {
         print("졌습니다!")
-    } else {
-        print("error")
+    } else if inputNumber > randomNumber {
+        print("이겼습니다!")
     }
 }
 
+inputMenu()
+
+feat: 메뉴입력함수, 비교함수 수정
