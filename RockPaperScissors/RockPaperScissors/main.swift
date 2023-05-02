c574bc53e646526c88e676560147f601e796c964
agilestarskim <agilestars@kakao.com>
2023-05-02 14:44:59 +0900

    feat: 가위바위보 비교함수 생성
    switch문 case 병합

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -7,20 +7,27 @@
 import Foundation
 
 gameLoop: while true {
+    let computer = String(Int.random(in: 1...3))
+    print("computer: \(computer)")
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
     let input = readLine()
     switch input {
     case "0":
         print("게임 종료")
         break gameLoop
-    case "1":
-        print(1)
-    case "2":
-        print(2)
-    case "3":
-        print(3)
+    case "1", "2", "3":
+        check(input: input, computer: computer)
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
     }
 }
 
+func check(input: String?, computer: String) {
+    if let input2 = input, input2 == computer {
+        print("비겼습니다!")
+    } else if let input2 = input, input2 > computer {
+        print("이겼습니다!")
+    } else {
+        print("졌습니다!")
+    }
+}
