db052603fae36b2523db6e81e2eaa1c5b45065e3
MaryJo-github <jotjdal0215@gmail.com>
2023-05-02 10:47:08 +0900

    feat: inputMenu() 분리 및 오류처리 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -21,7 +21,7 @@ enum Menu: String {
             return .scissors
         case .paper:
             return .rock
-        default:
+        case .exit:
             return .exit
         }
     }
@@ -33,38 +33,59 @@ enum Result: String {
     case draw = "비겼습니다!"
 }
 
+enum GameError: Error {
+    case menuNotFound
+    case unknownError
+    
+    var errorMessage: String {
+        switch self {
+        case .menuNotFound:
+            return "잘못된 입력입니다. 다시 시도해주세요."
+        case .unknownError:
+            return "알 수 없는 오류입니다."
+        }
+    }
+}
+
 func startGame() {
+    do {
+        let menu = try inputMenu()
+        
+        guard menu != .exit else {
+            print("게임 종료")
+            return
+        }
+        let gameResult = try playGame(menu)
+        
+        print(gameResult.rawValue)
+        
+        if gameResult == .draw { startGame() }
+    } catch GameError.menuNotFound {
+        print("\(GameError.menuNotFound.errorMessage)")
+        startGame()
+    } catch {
+        print("\(GameError.unknownError.errorMessage)")
+    }
+}
+
+func inputMenu() throws -> Menu {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
     
     guard let inputValue = readLine(),
           let menu = Menu(rawValue: inputValue)
-    else {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        startGame()
-        return
-    }
-    guard menu != .exit else {
-        print("게임 종료")
-        return
-    }
-    
-    let gameResult = playGame(menu)
-    
-    print(gameResult.rawValue)
-    
-    if gameResult == .draw {
-        startGame()
-    }
+    else { throw GameError.menuNotFound }
+        
+    return menu
 }
 
-func playGame(_ users: Menu) -> Result {
-    guard let computer = Menu(rawValue: String(Int.random(in: 1...3))) else {
-        return .win
-    }
-    print("컴퓨터 : \(computer)")
-    if users == computer {
+func playGame(_ usersChoice: Menu) throws -> Result {
+    guard let computersChoice = Menu(rawValue: String(Int.random(in: 1...3)))
+    else { throw GameError.unknownError }
+    print("컴퓨터 : \(computersChoice)")
+
+    if usersChoice == computersChoice {
         return .draw
-    } else if users.winningOpponent == computer {
+    } else if usersChoice.winningOpponent == computersChoice {
         return .win
     } else {
         return .lose
