c09e1b77abd31c5b736a94fd91736e372d3df682
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-05-02 14:44:41 +0900

    refactor: while문에서 재귀함수로 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -11,65 +11,66 @@ enum RockPaperScissors: Int {
 }
 
 func startGame() {
-    while true {
-        let computerNumber = Int.random(in: 1...3)
+    
+    let computerNumber = Int.random(in: 1...3)
+    
+    printGameGuide()
+    
+    guard let input = readLine(), let userNumber = Int(input), 0...3 ~= userNumber else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
         
-        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+        startGame()
         
-        guard let input = readLine(), let userNumber = Int(input), 0...3 ~= userNumber else {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            
-            continue
-        }
-        
-        if userNumber == 0 {
-            print("게임 종료")
-            
-            return
-        }
-        
-        var userHand: RockPaperScissors
-        var computerhand: RockPaperScissors
-        
-        switch userNumber {
-        case 1:
-            userHand = .scissors
-        case 2:
-            userHand = .rock
-        case 3:
-            userHand = .paper
-        default:
-            return
-        }
-        
-        switch computerNumber {
-        case 1:
-            computerhand = .scissors
-        case 2:
-            computerhand = .rock
-        case 3:
-            computerhand = .paper
-        default:
-            return
-        }
-        
-        guard compare(to: userHand, and: computerhand) else {
-            return
-        }
+        return
     }
+    
+    if userNumber == 0 {
+        print("게임 종료")
+        
+        return
+    }
+    
+    var userHand: RockPaperScissors
+    var computerhand: RockPaperScissors
+    
+    switch userNumber {
+    case 1:
+        userHand = .scissors
+    case 2:
+        userHand = .rock
+    case 3:
+        userHand = .paper
+    default:
+        return
+    }
+    
+    switch computerNumber {
+    case 1:
+        computerhand = .scissors
+    case 2:
+        computerhand = .rock
+    case 3:
+        computerhand = .paper
+    default:
+        return
+    }
+    
+    compare(to: userHand, and: computerhand)
 }
 
-func compare(to user: RockPaperScissors, and computer: RockPaperScissors) -> Bool {
+func printGameGuide() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+}
+
+func compare(to user: RockPaperScissors, and computer: RockPaperScissors) {
     if user == computer {
         print("비겼습니다!!")
-        return true
+        startGame()
     } else if isUserWin(user, computer) {
         print("이겼습니다!!")
     } else {
         print("졌습니다!!")
     }
-    
-    return false
 }
 
 func isUserWin(_ user: RockPaperScissors, _ computer: RockPaperScissors) -> Bool {
