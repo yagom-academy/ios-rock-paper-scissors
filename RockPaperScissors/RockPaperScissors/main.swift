be857985b010006844d00ce34b4bc81dc0e5c532
JusBug <pksgwo123@gmail.com>
2023-05-02 11:41:35 +0900

    feat: 랜덤 숫자 생성 함수 추가, inputMenu()함수 내용 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -11,7 +11,27 @@ print("Hello, World!")
 func inputMenu() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0>: ", terminator: "")
     let userNumber = readLine()
-    if userNumber == 0 {
-        
-    } else 
+    
+    switch userNumber {
+    case "1":
+        print("case1")
+    case "2":
+        print("case2")
+    case "3":
+        print("case3")
+    case "0":
+        print("게임 종료")
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
 }
+
+func createRandomNumber() -> Int {
+    var randomNumber = Int.random(in: 1...3)
+    return randomNumber
+}
+
+func compareRandomNumber(_ userNumber: Int, to randomNumber: Int) {
+    
+}
+
