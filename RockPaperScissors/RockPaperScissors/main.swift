5c470b744bbe290be935d1feecbc991054174d07
Whales <Whales.w.Jin@gmail.com>
2023-05-02 14:19:49 +0900

    feat: 게임 실행 반복문 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,21 @@
 
 import Foundation
 
-print("Hello, World!")
+gameLoop: while true {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+    let input = readLine()
+    switch input {
+    case "0":
+        print("게임 종료")
+        break gameLoop
+    case "1":
+        print(1)
+    case "2":
+        print(2)
+    case "3":
+        print(3)
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
 
