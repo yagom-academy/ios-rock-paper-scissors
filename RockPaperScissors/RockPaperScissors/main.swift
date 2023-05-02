c716b50ff9530013dfc9fb40463e870615855031
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-05-02 20:57:21 +0900

    refactor: 변수, 함수 이름 수정 및 반복문 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,45 +4,51 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-
-enum Game: Int, CaseIterable {
-    case scissors = 1
-    case rock = 2
-    case paper = 3
+enum Menu: String, CaseIterable {
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
+    case exit = "0"
 }
 
-var programRunning = true
-
-while programRunning {
+func startGame() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-    if let input: String = readLine() {
-        switch input {
-        case "1":
-            computeGame(userInput: .scissors)
-        case "2":
-            computeGame(userInput: .rock)
-        case "3":
-            computeGame(userInput: .paper)
-        case "0":
-            programRunning = false
-            print("게임 종료")
-        default:
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-        }
-    }
-}
-
-func computeGame(userInput: Game) {
-    guard let computerNumber: Game = Game.allCases.randomElement() else {
+    guard let input: String = readLine() else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        startGame()
         return
     }
-    if userInput == computerNumber {
+    
+    switch input {
+    case Menu.scissors.rawValue:
+        playGame(with: .scissors)
+    case Menu.rock.rawValue:
+        playGame(with: .rock)
+    case Menu.paper.rawValue:
+        playGame(with: .paper)
+    case Menu.exit.rawValue:
+        print("게임 종료")
+    default:
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        startGame()
+    }
+}
+
+func playGame(with userHand: Menu) {
+    guard let rivalHand: Menu = Menu.allCases.randomElement() else {
+        return
+    }
+    
+    if userHand == rivalHand {
         print("비겼습니다!")
-    } else if (userInput == .scissors && computerNumber == .paper)
-                 || (userInput == .rock && computerNumber == .scissors)
-                 || (userInput == .paper && computerNumber == .rock) {
+        startGame()
+    } else if (userHand == .scissors && rivalHand == .paper)
+                 || (userHand == .rock && rivalHand == .scissors)
+                 || (userHand == .paper && rivalHand == .rock) {
         print("이겼습니다!")
     } else {
         print("졌습니다!")
     }
 }
+
+startGame()
