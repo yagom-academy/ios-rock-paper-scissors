5da463f8f9c6f5da954a46603756dda4fe400ac9
hemg2 <hemg2@naver.com>
2023-05-02 14:50:36 +0900

    feat: 사용자 메뉴 입력 기능 분리 함수 추가

---
+++ RockPaperScissors/RockPaperScissors/HandSign.swift
@@ -0,0 +1,12 @@
+//
+//  HandSign.swift
+//  RockPaperScissors
+//
+//  Created by hemg on 2023/05/02.
+//
+
+enum HandSign: String {
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
+}
--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,31 +1,32 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yy-ss99, Hemg.
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-func showGameMune() {
-    var isGameOn: Bool = true
+func showGameMenu() {
     let gameEnd: String = "0"
     
-    while isGameOn {
+    while true {
         print("가위(1), 바위(2), 보(3)! <종료 :0> : ", terminator: "")
         
-        guard let userNumber: String = readLine() else { return }
-        let computerNumber: String = String(Int.random(in: 1...3))
-
-        if userNumber == gameEnd {
-            isGameOn = false
-            print("게임종료")
-        } else if userNumber == "1" ||
-                    userNumber == "2" ||
-                    userNumber == "3" {
-            checkRockPaperScissors(with: userNumber, computerNumber)
-        } else {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-        }
+        guard let userInput: String = readLine() else { return }
+        let computerInput: String = String(Int.random(in: 1...3))
+        guard userInput != gameEnd else { return print("게임종료") }
+        getUserInput(userInput, computerInput)
     }
 }
+
+func getUserInput(_ userInput: String, _ computerInput: String) {
+    if userInput == HandSign.scissors.rawValue ||
+        userInput == HandSign.rock.rawValue ||
+        userInput == HandSign.paper.rawValue {
+//        showGameResult(checkRockPaperScissors(with: userInput, computerInput))
+    } else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+    }
+}
+
 
 func checkRockPaperScissors(with userInput: String, _ computerInput: String) {
     if userInput == computerInput {
@@ -39,4 +40,4 @@ func checkRockPaperScissors(with userInput: String, _ computerInput: String) {
     }
 }
 
-showGameMune()
+showGameMenu()
