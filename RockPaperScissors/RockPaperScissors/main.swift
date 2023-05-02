dffb93e90e8fe095610eaea014b041796cee8745
Whales <Whales.w.Jin@gmail.com>
2023-05-02 17:04:14 +0900

    refactor: 가위바위보 비교 함수 수정
    튜플을 이용한 비교 함수 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,13 +4,13 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-//TODO: OptionalBinding, 알고리즘 축약
+//TODO: 알고리즘 축약
 
 import Foundation
 
 gameLoop: while true {
-
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+    
     let computer = String(Int.random(in: 1...3))
     let input = readLine()
     
@@ -19,15 +19,27 @@ gameLoop: while true {
         print("게임 종료")
         break gameLoop
     case "1", "2", "3":
-        if let user = input {
-            print("computer: \(computer)")            
-            showResult(input: user, computer: computer)
+        if let user = input {            
+            if isOver(with: computer, by: user) {
+                break gameLoop
+            }
         }
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
     }
 }
 
-func showResult(input: String, computer: String ) {
-    
+func isOver(with computer: String, by user: String) -> Bool {
+    switch (user, computer) {
+    case ("1", "1"), ("2", "2"), ("3", "3"):
+        print("비겼습니다!")
+        return false
+    case ("1", "3"), ("3", "2"), ("2", "1"):
+        print("이겼습니다!")
+        return true
+    default:
+        print("졌습니다!")
+        return true
+    }
 }
+
