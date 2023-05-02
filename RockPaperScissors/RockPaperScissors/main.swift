61ae8d49901d6bcffc628a5933928bfb23bf853c
agilestarskim <agilestars@kakao.com>
2023-05-02 15:34:38 +0900

    refactor: 강제 언래핑에서 옵셔널 바인딩으로 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,16 +4,14 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
+//TODO: OptionalBinding, 알고리즘 축약
 
-enum HandShape: Int {
-    case scissors = 1, rock, paper
-}
+import Foundation
 
 gameLoop: while true {
 
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
-    let computer = HandShape(rawValue: Int.random(in: 1...3))!
+    let computer = String(Int.random(in: 1...3))
     let input = readLine()
     
     switch input {
@@ -21,38 +19,15 @@ gameLoop: while true {
         print("게임 종료")
         break gameLoop
     case "1", "2", "3":
-        print("computer: \(computer)")
-        let user = HandShape(rawValue: Int(input!)!)!
-        showResult(input: user, computer: computer)
+        if let user = input {
+            print("computer: \(computer)")            
+            showResult(input: user, computer: computer)
+        }
     default:
         print("잘못된 입력입니다. 다시 시도해주세요.")
     }
 }
 
-func showResult(input: HandShape, computer: HandShape) {
-    if input == .rock {
-        if computer == .rock {
-            print("비겼습니다!")
-        } else if computer == .scissors {
-            print("이겼습니다!")
-        } else {
-            print("졌습니다!")
-        }
-    } else if input == .scissors {
-        if computer == .scissors {
-            print("비겼습니다!")
-        } else if computer == .paper {
-            print("이겼습니다!")
-        } else {
-            print("졌습니다!")
-        }
-    } else {
-        if computer == .paper {
-            print("비겼습니다!")
-        } else if computer == .rock {
-            print("이겼습니다!")
-        } else {
-            print("졌습니다!")
-        }
-    }
+func showResult(input: String, computer: String ) {
+    
 }
