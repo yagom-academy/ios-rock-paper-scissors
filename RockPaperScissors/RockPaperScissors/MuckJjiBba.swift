aa18b8ee5898cb6e3254da8c7bd51467cc8f0661
hemg2 <hemg2@naver.com>
2023-05-03 23:28:03 +0900

    feat: 유저입력값 검증 함수 추가

--- RockPaperScissors/RockPaperScissors/MuckJjiBba.swift
+++ RockPaperScissors/RockPaperScissors/MuckJjiBba.swift
@@ -25,4 +25,19 @@ struct MuckJjiBba {
     private func makeComputerInput() -> String {
         return String(Int.random(in: 1...3))
     }
+    
+    private func checkVaildInput(with userInput: String, _ computerInput: String, _ gameResult: GameResult) {
+        switch userInput {
+        case MuckJjiBbaSign.jji.rawValue,
+            MuckJjiBbaSign.muck.rawValue,
+            MuckJjiBbaSign.bba.rawValue:
+            //검증 함수
+            break
+        default:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            getUserMuckJjiBbaInput(when: .lose)
+        }
+    }
+    
+    
 }
