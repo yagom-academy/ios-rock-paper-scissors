de4493b8ab8f0daedf5b4cdb4d859c5b83fc7710
Hyun A Song <hyuna@Hyunui-MacBookPro.local>
2023-05-03 20:38:46 +0900

    feat: 묵찌빠 게임 조건 함수 추가
    makeRival, comparhand 함수 생성

--- RockPaperScissors/RockPaperScissors/SecondGame.swift
+++ RockPaperScissors/RockPaperScissors/SecondGame.swift
@@ -10,27 +10,59 @@ struct SecondGame {
         print("[\(winner) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :", terminator: " ")
         choiceMukjjipaMenu()
     }
-
+    
     private func choiceMukjjipaMenu() {
         guard let input: String = readLine() else {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            startMukjjipa()
+            print("정상적인 접근이 아닙니다.")
             return
         }
         
         switch input {
         case MukjjipaOptions.muk.rawValue:
-            print("muk")
+            compareHand(with: .muk)
         case MukjjipaOptions.jji.rawValue:
-            print("jji")
+            compareHand(with: .jji)
         case MukjjipaOptions.pa.rawValue:
-            print("pa")
+            compareHand(with: .pa)
         case "0":
             print("게임 종료")
         default:
             print("잘못된 입력입니다. 다시 시도해주세요.")
+            winner = "컴퓨터"
             startMukjjipa()
         }
     }
     
+    private func makeRival() throws -> MukjjipaOptions {
+        guard let rivalHand: MukjjipaOptions = MukjjipaOptions.allCases.randomElement() else {
+            throw Errors.invalidRival
+        }
+        
+        return rivalHand
+    }
+    
+    private func compareHand(with userHand: MukjjipaOptions) {
+        let rivalHand: MukjjipaOptions
+        
+        do {
+            rivalHand = try makeRival()
+        } catch {
+            print("가위바위보가 성립하지 않습니다.")
+            return
+        }
+        
+        if userHand == rivalHand {
+            print("\(winner)의 승리!")
+        } else if (userHand == .jji && rivalHand == .pa)
+                    || (userHand == .muk && rivalHand == .jji)
+                    || (userHand == .pa && rivalHand == .muk) {
+            winner = "사용자"
+            print("\(winner)의 턴입니다.")
+            startMukjjipa()
+        } else {
+            winner = "컴퓨터"
+            print("\(winner)의 턴입니다.")
+            startMukjjipa()
+        }
+    }
 }
