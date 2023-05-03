646555f3f2edb21aab4b31b1ee3a02a1f8353241
JusBug <pksgwo123@gmail.com>
2023-05-03 15:02:16 +0900

    feat: enum 추가, 에러 및 게임종료 try catch구문 추가, 함수 네이밍 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,47 +6,64 @@
 
 import Foundation
 
-func inputMenu() {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0>: ", terminator: "")
-    guard let inputNumber = readLine() else { return }
-    guard let userCard = Int(inputNumber) else { return }
-    
-    switch userCard {
-    case 1:
-        compareRandomNumber(userCard, with: createRandomNumber())
-        inputMenu()
-    case 2:
-        compareRandomNumber(userCard, with: createRandomNumber())
-        inputMenu()
-    case 3:
-        compareRandomNumber(userCard, with: createRandomNumber())
-        inputMenu()
-    case 0:
-        print("게임 종료")
-    default:
+enum UserCards: Int {
+    case rock = 1
+    case scissors = 2
+    case paper = 3
+    case exit = 0
+}
+
+enum UserCardsError: Error {
+    case error
+    case exit
+}
+
+while true {
+    do {
+        try inputMenu()
+    } catch UserCardsError.error {
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        inputMenu()
+    } catch UserCardsError.exit {
+        break
     }
 }
 
+func inputMenu() throws {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0>: ", terminator: "")
+    guard let inputNumber = readLine(),
+          let user = Int(inputNumber),
+          let userCards: UserCards = UserCards(rawValue: user) else {
+        throw UserCardsError.error
+    }
+
+    switch userCards {
+    case .rock, .scissors, .paper:
+        compareCard(user, createRandomNumber())
+        try inputMenu()
+    case .exit:
+        print("게임 종료")
+        throw UserCardsError.exit
+    }
+
+}
 func createRandomNumber() -> Int {
     let randomNumber = Int.random(in: 1...3)
     print(randomNumber)
     return randomNumber
 }
 
-func compareRandomNumber(_ inputNumber: Int, with randomNumber: Int) {
-    if inputNumber == randomNumber {
+func compareCard(_ user: Int, _ computer: Int) {
+    let absolute = (user - computer).magnitude
+    
+    if user == computer {
         print("비겼습니다!")
-    } else if ((inputNumber - randomNumber).magnitude == 2) && (inputNumber == 1) {
+    } else if (absolute == 2) && (user == 1) {
         print("이겼습니다!")
-    } else if ((inputNumber - randomNumber).magnitude == 2) && (inputNumber == 3) {
+    } else if (absolute == 2) && (user == 3) {
         print("졌습니다!")
-    } else if (inputNumber > randomNumber) {
+    } else if (user > computer) {
         print("이겼습니다!")
     } else {
         print("졌습니다!")
     }
 }
-
-inputMenu()
