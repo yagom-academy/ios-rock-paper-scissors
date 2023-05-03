ca6922363de898417db461952e87416aa3af2ae1
agilestarskim <agilestars@kakao.com>
2023-05-03 17:37:39 +0900

    refactor: 피드백에 대한 전반적인 수정
    main에서 파일 분리 후 Game 파일에서 Game 클래스 생성, gameLoop 내용들을 여러 함수로 기능 분리, HandShape, GameResult 열거형 타입 생성

---
+++ RockPaperScissors/RockPaperScissors/Game.swift
@@ -0,0 +1,78 @@
+//
+//  HandShape.swift
+//  RockPaperScissors
+//
+//  Created by Minsup, Whales on 2023/05/03.
+//
+
+enum HandShape: Int {
+    case scissors = 1, rock, paper
+}
+
+enum GameResult {
+    case end, again
+}
+
+final class Game {
+    
+    private var computer: HandShape?
+    private var user: HandShape?
+    private var gameResult: GameResult?
+    
+    func start() {
+        while self.gameResult != .end {
+            printMenu()
+            self.computer = generateComputerHandShape()
+            let menu = getMenuFromUser()
+            executeMenu(by: menu)
+        }
+    }
+    
+    private func printMenu() {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+    }
+    
+    private func generateComputerHandShape() -> HandShape? {
+        return HandShape(rawValue: Int.random(in: 1...3))
+    }
+    
+    private func getMenuFromUser() -> Int? {
+        guard let menu = readLine(), let menuNumber = Int(menu) else {
+            return nil
+        }
+        return menuNumber
+    }
+    
+    private func executeMenu(by menuNumber: Int?) {
+        switch menuNumber {
+        case 0:
+            print("게임 종료")
+            self.gameResult = .end
+        case 1, 2, 3:
+            if let user = convertHandShape(from: menuNumber) {
+                self.gameResult = judgeGame(user, with: self.computer)
+            }
+        default:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+        }
+    }
+    
+    private func convertHandShape(from shapeNumber: Int?) -> HandShape? {
+        guard let shapeNumber = shapeNumber else { return nil }
+        return HandShape(rawValue: shapeNumber)
+    }
+    
+    private func judgeGame(_ user: HandShape?, with computer: HandShape?) -> GameResult {
+        switch (user, computer) {
+        case (.scissors, .scissors), (.rock, .rock), (.paper, .paper):
+            print("비겼습니다!")
+            return .again
+        case (.scissors, .paper), (.paper, .rock), (.rock, .scissors):
+            print("이겼습니다!")
+            return .end
+        default:
+            print("졌습니다!")
+            return .end
+        }
+    }
+}
--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,41 +2,7 @@
 //  RockPaperScissors - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
-//TODO: 함수이름
-
-import Foundation
-
-gameLoop: while true {
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
-    let computer = String(Int.random(in: 1...3))
-    let input = readLine()
-    
-    switch input {
-    case "0":
-        print("게임 종료")
-        break gameLoop
-    case "1", "2", "3":
-        if let user = input, judgeGame(user, with: computer) {
-            break gameLoop
-        }
-    default:
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-    }
-}
-
-func judgeGame(_ user: String, with computer: String) -> Bool {
-    switch (user, computer) {
-    case ("1", "1"), ("2", "2"), ("3", "3"):
-        print("비겼습니다!")
-        return false
-    case ("1", "3"), ("3", "2"), ("2", "1"):
-        print("이겼습니다!")
-        return true
-    default:
-        print("졌습니다!")
-        return true
-    }
-}
-
+let game = Game()
+game.start()
