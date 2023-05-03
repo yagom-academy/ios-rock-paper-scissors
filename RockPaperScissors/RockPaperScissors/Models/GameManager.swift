9768907a1658f4ac91da90ddaaaaa22fd30cc59c
myungsun <audtjs9987@naver.com>
2023-05-03 14:17:58 +0900

    refactor: startGame 메서드 내에서 사용자 입력 받는 기능 분리

--- RockPaperScissors/RockPaperScissors/Models/GameManager.swift
+++ RockPaperScissors/RockPaperScissors/Models/GameManager.swift
@@ -13,12 +13,7 @@ class GameManager {
         var isGameOn: Bool = true
         
         while isGameOn {
-            print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
-            guard let gameOption = readLine(),
-                  let gameOptionNumber = Int(gameOption) else {
-                print("잘못된 입력입니다. 다시 시도해주세요.")
-                continue
-            }
+            guard let gameOptionNumber = inputGameOptionNumber() else { continue }
             
             computer.makeRandomHandShape()
             guard let computerHandShape = computer.getCurrentHandShape() else { continue }
@@ -27,15 +22,13 @@ class GameManager {
             case 0:
                 isGameOn = false
                 print("게임 종료")
-            case 1...3:
+            default:
                 let userHandShape = handShapes[gameOptionNumber - 1]
                 let resultMessage = getGameResult(computerHandShape, userHandShape).resultMessage
                 isGameOn = getGameResult(computerHandShape, userHandShape).isGameOn
                 
                 user.changeHandShape(to: userHandShape)
                 print("\(resultMessage)")
-            default:
-                print("잘못된 입력입니다. 다시 시도해주세요.")
             }
         }
     }
@@ -44,12 +37,23 @@ class GameManager {
         switch (computerHandShape, userHandShape) {
         case let (computerHandShape, userHandShape) where computerHandShape == userHandShape:
             return (true, "비겼습니다.")
-            
         case (.rock, .paper), (.paper, .scissors), (.scissors, .rock):
             return (false, "이겼습니다!")
-            
         default:
             return (false, "졌습니다!")
         }
+    }
+    
+    private func inputGameOptionNumber() -> Int? {
+        let validOptionRange: ClosedRange<Int> = 0...3
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+        guard let gameOption = readLine(),
+              let gameOptionNumber = Int(gameOption),
+              validOptionRange.contains(gameOptionNumber) else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            return nil
+        }
+        
+        return gameOptionNumber
     }
 }
