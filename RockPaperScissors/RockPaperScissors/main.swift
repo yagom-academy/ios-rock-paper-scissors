1a930b21348f36dc1648f4e5ae5c2dd68551db12
YaRkyungmin <kkgm94@naver.com>
2023-05-03 10:44:49 +0900

    refector: 네이밍수정, 함수기능 분리및 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -20,59 +20,65 @@ func playRockScissorsPaper() {
 	var isRunningStart = true
 
 	while isRunningStart {
-		let inputUserMenu = inputUserNumber()
+		let userMenu = inputUserNumber()
 		
-		if inputUserMenu == 0 { break }
-		
-		let selectInputUserNumber = inputUserMenu-1
-		let selectRandomNumber = generateRandomNumber()-1
-		
-		let handShape = HandShape.allCases[selectInputUserNumber]
-		let randomHandShape = HandShape.allCases[selectRandomNumber]
-		
-		switch competeResult(user: handShape, computer: randomHandShape){
-		case .draw:
-			print("비겼습니다!")
-		case .lose:
-			print("졌습니다!")
-			isRunningStart = false
-		case .win:
-			print("이겼습니다!")
-			isRunningStart = false
+		if userMenu == 0 {
+			break
 		}
+		
+		let handShape = receiveHandShapeElement(index: userMenu)
+		
+		let gameResult = receiveGameResult(user: handShape, computer: generateRandomHandShape())
+		
+		isRunningStart = showGameResult(with: gameResult)
 	}
 	print("게임 종료")
 }
 
+func showGameResult(with gameResult: GameResult) -> Bool {
+	var isDraw = true
+	switch gameResult {
+	case .draw:
+		print("비겼습니다!")
+	case .lose:
+		print("졌습니다!")
+		isDraw = false
+	case .win:
+		print("이겼습니다!")
+		isDraw = false
+	}
+	return isDraw
+}
+
 func inputUserNumber() -> Int {
-	var outputNumber = 0
-	var isNotCorrectNumber = true
+	let isNotCorrectNumber = true
 
 	while isNotCorrectNumber {
 		print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
 
-		if let inputString = readLine(), let convertInteger = Int(inputString) {
-			outputNumber = convertInteger
-		} else {
+		guard let userInput = readLine(),
+			  let covertUserInput = Int(userInput) else {
 			print("잘못된 입력입니다. 다시 시도해주세요.")
 			continue
 		}
-
-		if (0...3).contains(outputNumber) {
-			isNotCorrectNumber = false
-		} else {
+		guard (0...3).contains(covertUserInput) else {
 			print("잘못된 입력입니다. 다시 시도해주세요.")
+			continue
 		}
+		return covertUserInput
 	}
-
-	return outputNumber
 }
 
-func generateRandomNumber() -> Int {
-	return Int.random(in: 1...3)
+func generateRandomHandShape() -> HandShape {
+	return HandShape.allCases[Int.random(in: 1...3) - 1]
 }
 
-func competeResult(user: HandShape, computer: HandShape) -> GameResult {
+func receiveHandShapeElement(index: Int) -> HandShape {
+	return HandShape.allCases[index - 1]
+}
+
+func receiveGameResult(user: HandShape, computer: HandShape) -> GameResult {
+	print(user,computer)
     switch (user, computer) {
     case (.scissors, .scissors), (.rock, .rock), (.paper, .paper):
         return .draw
