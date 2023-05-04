5695161dbf5c051c0e7e68995ec73045c70b252c
JusBug <pksgwo123@gmail.com>
2023-05-04 12:38:28 +0900

    feat: 출력 함수, 턴 변경 함수, 두 번째 비교 함수 수정 및 변수 추가

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -7,9 +7,10 @@
 
 struct RockPaperScissors {
     var isGameStart: Bool = true
-    var isPlayerTurn: Bool = true
     var userNumber: Int = 0
-    
+    var whosTurn = [Bool: String] = [true: "사용자", false: "컴퓨터"]
+    var isPlayerTurn = whosTurn[true] // "사용자"
+
     mutating func start() throws {
         while isGameStart {
             do {
@@ -26,7 +27,7 @@ struct RockPaperScissors {
 
         switch userCards {
         case .rock, .scissors, .paper:
-            compareCard(userNumber, createRandomNumber())
+            compareFirstCard(userNumber, createRandomNumber())
             try printfirstMenu()
         case .exit:
             print("게임 종료")
@@ -40,7 +41,7 @@ struct RockPaperScissors {
         return randomNumber
     }
 
-    mutating func compareCard(_ user: Int, _ computer: Int) {
+    mutating func compareFirstCard(_ user: Int, _ computer: Int) {
         let difference = user - computer
  
         switch difference {
@@ -49,10 +50,10 @@ struct RockPaperScissors {
             print("비김추가")
         case 1, -2:
             print("이김")
-            self.isPlayerTurn = true
+            self.isPlayerTurn = whosTurn[true]
         case -1, 2:
             print("패배")
-            self.isPlayerTurn = false
+            self.isPlayerTurn = whosTurn[false]
         default:
             print("오류1")
         }
@@ -62,6 +63,7 @@ struct RockPaperScissors {
         guard let inputNumber = readLine(),
               let user = Int(inputNumber),
               let userCards: UserCards = UserCards(rawValue: user) else {
+            changeTurn()
             throw UserCardsError.error
         }
         self.userNumber = user
@@ -69,55 +71,43 @@ struct RockPaperScissors {
     }
 
     mutating func printSecondMenu () throws {
-        var whosTurn: String
         
-        if isPlayerTurn == true {
-            whosTurn = "사용자"
-        } else {
-            whosTurn = "컴퓨터"
-        }
-        print("[\(whosTurn)] 턴 묵(1), 찌(2), 빠(3)! <종료 : 0>: ", terminator: "")
+        print("[\(isPlayerTurn)] 턴 묵(1), 찌(2), 빠(3)! <종료 : 0>: ", terminator: "")
         let userCards: UserCards = try input()
         
         switch userCards {
         case .rock, .scissors, .paper:
-            compareSecondCard(userCards, createRandomNumber(), whosTurn)
             print("패 비교")
+            compareSecondCard(userCards, createRandomNumber())
+            printTurn()
+            try printSecondMenu()
         case .exit:
             print("게임 종료")
-        default:
-            print("패 넘김")
+            self.isGameStart = false
         }
     }
     
-    func compareSecondCard(_ card: UserCards, _ randomNumber: Int, _ whosTurn: String) -> String {
+    mutating func compareSecondCard(_ card: UserCards, _ randomNumber: Int) {
         let randomNumber = UserCards(rawValue: randomNumber)
         
         if card == randomNumber {
-            print("\(whosTurn)의 승리!")
+            print("\(isPlayerTurn)의 승리!")
+            self.isGameStart = false
         } else {
-            changeTurn(<#T##isPlayerTurn: Bool##Bool#>)
+            changeTurn()
         }
     }
     
-    mutating func changeTurn(_ isPlayerTurn: Bool) {
-        if isPlayerTurn == true {
-            isPlayerTurn = false
+    mutating func changeTurn() {
+        if isPlayerTurn == whosTurn[true] {
+            isPlayerTurn = whosTurn[false]
         } else {
-            isPlayerTurn = true
+            isPlayerTurn = whosTurn[true]
         }
-        printTurn(<#T##card: UserCards##UserCards#>, <#T##whosTurn: String##String#>)
     }
     
-    mutating func printTurn(_ card: UserCards, _ whosTurn: String) {
-        switch card {
-        case .rock, .scissors, .paper:
-            print("\(whosTurn)의 턴입니다.")
-            printSecondMenu()
-        default:
-            printSecondMenu()
-        }
+    mutating func printTurn() {
+        print("\(isPlayerTurn)의 턴입니다.")
     }
 }
 
-feat: 턴 변경함수, 턴 출력함수 추가, 두 번째 비교함수 수정
