729f705ca8307be53bd884acaeed48f4e564f93b
ue-mu <ym14170@gmail.com>
2023-09-04 22:14:22 +0900

    refactor: 함수명 및 출력함수 수정

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -2,31 +2,31 @@
 //  RockPaperScissors - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
-
-import Foundation
+//
 
 func getRandomNumber() -> Int {
     return Int.random(in: 1...3)
 }
 
+
 func contendUserAndComputer(_ userChoice: Int, _ computerChoice: Int) -> Bool {
     if (userChoice == 1 && computerChoice == 3) || (userChoice == 2 && computerChoice == 1) || (userChoice == 3 && computerChoice == 2) {
-        print("이겼습니다.")
+        print("이겼습니다!")
         return true
     } else if userChoice == computerChoice {
-        print("비겼습니다.")
+        print("비겼습니다!")
         return false
     } else {
-        print("졌습니다..")
-        return true
+        print("졌습니다!")
+        return false
     }
 }
 
-func selectMenu() {
+
+func playRockPaperScissors() {
     while true {
         let computerChoice = getRandomNumber()
-        print("가위(1), 바위(2), 보(3)!<종료: 0> :", terminator: " ")
+        print("가위(1), 바위(2), 보(3)!<종료: 0> :", terminator: "")
 
         guard let input = readLine(), let userChoice = Int(input) else {
             print("잘못된 입력입니다. 다시 시도해주세요.")
@@ -49,4 +49,4 @@ func selectMenu() {
     }
 }
 
-selectMenu()
+playRockPaperScissors()
