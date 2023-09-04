650015e69b26105986968c9b2a56462c4bf3fbfe
Hisop <hisop@Hisopui-MacBookPro.local>
2023-09-04 20:30:42 +0900

    feat: 게임 메뉴 출력 및 사용자 입력 받는 함수 생성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,26 @@
 
 import Foundation
 
-print("Hello, World!")
+func startGame() {
+    var isPlaying: Bool = true
+    
+    while isPlaying {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+        
+        guard let userChoice = readLine() else {
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+            continue
+        }
+        switch userChoice {
+        case "1", "2", "3":
+            break
+        case "0":
+            print("게임을 종료합니다.")
+            return
+        default:
+            print("잘못된 입력입니다. 다시 시도해주세요.")
+        }
+    }
+}
 
+startGame()
