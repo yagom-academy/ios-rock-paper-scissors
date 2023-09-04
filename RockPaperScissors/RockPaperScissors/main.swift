d6f4ee99e7f0a8779d43732e3f3717a2221f8fd2
ue-mu <ym14170@gmail.com>
2023-09-04 19:47:45 +0900

    feat: 사용자 입력과 컴퓨터의 패 비교 함수 구현

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,4 +10,16 @@ func getRandomNumber() -> Int {
     return Int.random(in: 1...3)
 }
 
+func contendUserAndComputer(_ userChoice: Int, _ computerChoice: Int) -> Bool {
+    if (userChoice == 1 && computerChoice == 3) || (userChoice == 2 && computerChoice == 1) || (userChoice == 3 && computerChoice == 2) {
+        print("이겼습니다.")
+        return true
+    } else if userChoice == computerChoice {
+        print("비겼습니다.")
+        return false
+    } else {
+        print("졌습니다..")
+        return true
+    }
+}
 
