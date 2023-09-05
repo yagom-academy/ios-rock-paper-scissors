ce03ba0e04f01e6a073bdd9cc2e95a6bfc2d8e12
woohyeon3382 <woohyeon3382@naver.com>
2023-09-05 11:55:24 +0900

    Feat: 메뉴 출력 함수와 입력값 받는 함수
    유저에게 보여줄 메뉴를 출력하는 함수를 구현
    유저에게 받은 입력값의 옵셔널을 바인딩 하는 함수 구현

--- RockPaperScissors/RockPaperScissors/RPSManager.swift
+++ RockPaperScissors/RockPaperScissors/RPSManager.swift
@@ -10,4 +10,12 @@ import Foundation
 struct RPSManager {
     private var computerCard = RockPaperScissors(rawValue: Int.random(in: 1...3))
     
+    func inputUserNumber(number: String?) -> Int {
+        guard let number = number, let number = Int(number) else { return 4 }
+        return number
+    }
+    
+    func printMenu() {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0>:", terminator: " ")
+    }
 }
