6aa31bb2e0c6c196c6bab50f23f5f518ea17eebe
charles <kch1759@naver.com>
2023-09-05 10:56:09 +0900

    Feat: 열거형, 구조체 정의
    가위바위보의 정보를 담을 수 있는 enum 타입을 선언.
    가위바위보의 게임을 관리하는 struct 타입 선언.

---
+++ RockPaperScissors/RockPaperScissors/RPSManager.swift
@@ -0,0 +1,13 @@
+//
+//  RPSManger.swift
+//  RockPaperScissors
+//
+//  Created by 김형철 on 2023/09/05.
+//
+
+import Foundation
+
+struct RPSManager {
+    private var computerCard = RockPaperScissors(rawValue: Int.random(in: 1...3))
+    
+}
---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -0,0 +1,14 @@
+//
+//  RockPaperScissors.swift
+//  RockPaperScissors
+//
+//  Created by 김형철 on 2023/09/05.
+//
+
+import Darwin
+
+enum RockPaperScissors: Int {
+    case scissors = 1
+    case rock
+    case paper
+}
