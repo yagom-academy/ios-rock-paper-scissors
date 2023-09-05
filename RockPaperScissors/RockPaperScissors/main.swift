baffb58e89c2abb2d1b1562a82696f010946af8e
kiseok <carti1108@naver.com>
2023-09-05 10:57:47 +0900

    feat: 사용자의 입력값과 컴퓨터의 입력값을 비교하여 결과를 출력해주는 함수 작성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,6 +6,12 @@
 
 import Foundation
 
+enum RockPaperScissors {
+    case rock, paper, scissors
+}
+
+var userHand: RockPaperScissors = .rock
+
 enum Result {
     case win, draw, lose, exit
     
@@ -34,19 +40,64 @@ enum InputError: LocalizedError {
     }
 }
 
-struct RockScissorsPaperGame {
-    func readUserInput() throws {
-        print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
-        
-        guard let userInput = readLine() else {
-            throw InputError.wrongInput
-        }
-        guard let user = Int(userInput) else {
-            throw InputError.wrongInput
-        }
-        
-        guard 0...3 ~= user else {
-            throw InputError.wrongInput
-        }
+func readUserInput() throws {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
+    
+    guard let userInput = readLine() else {
+        throw InputError.wrongInput
+    }
+    guard let user = Int(userInput) else {
+        throw InputError.wrongInput
+    }
+    
+    guard 0...3 ~= user else {
+        throw InputError.wrongInput
+    }
+    
+    switch user {
+    case 0:
+        print("게임종료")
+        break
+    case 1:
+        userHand = .scissors
+        playRockPaperScissors(userHand: userHand)
+        break
+    case 2:
+        userHand = .rock
+        playRockPaperScissors(userHand: userHand)
+        break
+    case 3:
+        userHand = .paper
+        playRockPaperScissors(userHand: userHand)
+        break
+    default:
+        print("잘못된 입력입니다.")
+        break
     }
 }
+
+func playRockPaperScissors(userHand: RockPaperScissors) {
+    let selectComputerHand = Int.random(in: 1...3)
+    var computerHand: RockPaperScissors = .rock
+    
+    switch selectComputerHand {
+    case 1:
+        computerHand = .scissors
+    case 2:
+        computerHand = .rock
+    case 3:
+        computerHand = .paper
+    default:
+        break
+    }
+    
+    if computerHand == userHand {
+        print("비겼습니다!")
+    } else if computerHand == .rock && userHand == .paper || computerHand == .scissors && userHand == .rock || computerHand == .paper && userHand == .scissors {
+        print("이겼습니다!")
+    } else {
+        print("졌습니다!")
+    }
+}
+
+try readUserInput()
