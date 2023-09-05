8c4b251784f729efe2ec000127dbed9685020392
Morgan <101351216+devjoon@users.noreply.github.com>
2023-09-05 10:20:58 +0900

    feat: 결과,에러 에 대한 열거형 정의, 가위바위보 게임 구조체 정의

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,52 @@
 //
 //  RockPaperScissors - main.swift
-//  Created by yagom. 
+//  Created by Kiseok, Morgan.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import Foundation
 
-print("Hello, World!")
+enum Result {
+    case win, draw, lose, exit
+    
+    var result: String {
+        switch self {
+        case .win:
+            return "이겼습니다!"
+        case .lose:
+            return "졌습니다!"
+        case .draw:
+            return "비겼습니다!"
+        case .exit:
+            return "게임종료"
+        }
+    }
+}
 
+enum InputError: LocalizedError {
+    case wrongInput
+    
+    var errorDescription: String? {
+        switch self {
+        case .wrongInput:
+            return "잘못된 입력입니다. 다시 시도해주세요."
+        }
+    }
+}
+
+struct RockScissorsPaperGame {
+    func readUserInput() throws {
+        print("가위(1), 바위(2), 보(3)! <종료 : 0>", terminator: " : ")
+        
+        guard let userInput = readLine() else {
+            throw InputError.wrongInput
+        }
+        guard let user = Int(userInput) else {
+            throw InputError.wrongInput
+        }
+        
+        guard 0...3 ~= user else {
+            throw InputError.wrongInput
+        }
+    }
+}
