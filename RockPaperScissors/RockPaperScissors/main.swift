45919851f2a8b0b502e4ef4e1eaa7867291d87ce
jyubong <nane030@naver.com>
2023-09-07 14:46:54 +0900

    refactor: 승자 결정 함수 수정
    compareWithComputer -> deciveVictory
    computerChoice 랜덤값 생성: Int.random -> allCases.randomElement
    computer와 user choice 비교 수정 : String -> enum

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -51,13 +51,29 @@ func displayRoundTwoResult(result: RoundResult, turn: RoundResult) {
     print("[\(turn.rawValue) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
 }
 
+func decideVictory(userChoice: Choice) -> RoundResult {
+    guard let computerChoice = Choice.allCases.randomElement() else {
+        return .invalidInput
+    }
+    
+    guard computerChoice != userChoice else {
+        return .draw
+    }
+    
+    guard (computerChoice == .scissors && userChoice == .paper) ||
+            (computerChoice == .rock && userChoice == .scissors) ||
+            (computerChoice == .paper && userChoice == .rock) else {
+        return .userWin
+    }
+    
+    return .computerWin
+}
+
 @discardableResult
 func startGame(isContinous: Bool) -> Bool {
     guard isContinous else {
         return false
     }
-
-    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
 
     guard let userChoice = readLine() else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
@@ -66,32 +82,14 @@ func startGame(isContinous: Bool) -> Bool {
     
     switch userChoice {
     case "1", "2", "3":
-        startGame(isContinous: compareWithComputer(userChoice: userChoice))
+        startGame(isContinous: true)
+//        startGame(isContinous: decideVictory(userChoice: userChoice))
     case "0":
         print("게임 종료")
         break
     default:
         startGame(isContinous: true)
     }
-    return false
-}
-
-func compareWithComputer(userChoice: String) -> Bool {
-    let computerChoice: String = String(Int.random(in: 1...3))
-    
-    guard computerChoice != userChoice else {
-        print("비겼습니다!")
-        return true
-    }
-    
-    guard (computerChoice == "1" && userChoice == "3") ||
-            (computerChoice == "2" && userChoice == "1") ||
-            (computerChoice == "3" && userChoice == "2") else {
-        print("이겼습니다!")
-        return false
-    }
-    
-    print("졌습니다!")
     return false
 }
 
