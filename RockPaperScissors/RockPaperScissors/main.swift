22075756d0211bdfd3003ccf24570a6281786f79
Hisop <hisop@Hisopui-MacBookPro.local>
2023-09-07 15:02:15 +0900

    feat: mappingUserChoice 추가
    userInput을 라운드에 따른 enum case로 mapping하는 함수 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -18,6 +18,74 @@ enum Choice: String, CaseIterable {
     case scissors
 }
 
+@discardableResult
+func startGame(isContinous: Bool) -> Bool {
+    guard isContinous else {
+        return false
+    }
+
+    guard let userChoice = readLine() else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return startGame(isContinous: true)
+    }
+    
+    switch userChoice {
+    case "1", "2", "3":
+        startGame(isContinous: true)
+//        startGame(isContinous: decideVictory(userChoice: userChoice))
+    case "0":
+        print("게임 종료")
+        break
+    default:
+        startGame(isContinous: true)
+    }
+    return false
+}
+
+func mappingUserChoice(userInput: String, round: Int) -> Choice?  {
+    if round == 1 {
+        switch userInput {
+        case "1":
+            return .scissors
+        case "2":
+            return .rock
+        case "3":
+            return .paper
+        default:
+            return nil
+        }
+    } else {
+        switch userInput {
+        case "1":
+            return .rock
+        case "2":
+            return .scissors
+        case "3":
+            return .paper
+        default:
+            return nil
+        }
+    }
+}
+
+func decideVictory(userChoice: Choice) -> RoundResult {
+    guard let computerChoice = Choice.allCases.randomElement() else {
+        return .invalidInput
+    }
+    
+    guard computerChoice != userChoice else {
+        return .draw
+    }
+    
+    guard (computerChoice == .scissors && userChoice == .paper) ||
+            (computerChoice == .rock && userChoice == .scissors) ||
+            (computerChoice == .paper && userChoice == .rock) else {
+        return .userWin
+    }
+    
+    return .computerWin
+}
+
 func displayRoundOneResult(result: RoundResult) {
     switch result {
     case .userWin:
@@ -49,48 +117,6 @@ func displayRoundTwoResult(result: RoundResult, turn: RoundResult) {
         break
     }
     print("[\(turn.rawValue) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
-}
-
-func decideVictory(userChoice: Choice) -> RoundResult {
-    guard let computerChoice = Choice.allCases.randomElement() else {
-        return .invalidInput
-    }
-    
-    guard computerChoice != userChoice else {
-        return .draw
-    }
-    
-    guard (computerChoice == .scissors && userChoice == .paper) ||
-            (computerChoice == .rock && userChoice == .scissors) ||
-            (computerChoice == .paper && userChoice == .rock) else {
-        return .userWin
-    }
-    
-    return .computerWin
-}
-
-@discardableResult
-func startGame(isContinous: Bool) -> Bool {
-    guard isContinous else {
-        return false
-    }
-
-    guard let userChoice = readLine() else {
-        print("잘못된 입력입니다. 다시 시도해주세요.")
-        return startGame(isContinous: true)
-    }
-    
-    switch userChoice {
-    case "1", "2", "3":
-        startGame(isContinous: true)
-//        startGame(isContinous: decideVictory(userChoice: userChoice))
-    case "0":
-        print("게임 종료")
-        break
-    default:
-        startGame(isContinous: true)
-    }
-    return false
 }
 
 startGame(isContinous: true)
