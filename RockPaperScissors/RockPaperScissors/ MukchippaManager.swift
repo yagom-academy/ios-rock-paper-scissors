ae70a62049c3e16efabe564b17f8d8fc4474500d
woohyeon3382 <woohyeon3382@naver.com>
2023-09-07 22:24:26 +0900

    Feat: Mukchippa  타입 설계 및 MukchippaManager 구조체 설계

---
+++ RockPaperScissors/RockPaperScissors/ MukchippaManager.swift
@@ -0,0 +1,19 @@
+//
+//   MukchippaManager.swift
+//  RockPaperScissors
+//
+//  Created by Charles, Toy on 2023/09/07.
+//
+
+struct MukchippaManager {
+    
+    private func printMenu(turn: String) {
+        print("[\(turn)턴]묵(1), 찌(2), 빠(3)! <종료 : 0> :", terminator: " ")
+    }
+    
+    
+    
+    
+    
+    
+}
