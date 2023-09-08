0a7fb151646cf2b585437b8afd2fbf35a4aec934
charles <kch1759@naver.com>
2023-09-08 10:46:01 +0900

    Feat: inputUserNumber 함수 추가

--- RockPaperScissors/RockPaperScissors/MukchippaManager.swift
+++ RockPaperScissors/RockPaperScissors/MukchippaManager.swift
@@ -6,15 +6,23 @@
 //
 
 struct MukchippaManager {
-    private var isTurn = true
+    private var isUserTurn = true
     var isWinOrLose = true
     
     private func printMenu(turn: String) {
         print("[\(turn)턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :", terminator: " ")
     }
 
-    
-    
-    
-    
+    private mutating func inputUserNumber() -> Int {
+        while true {
+            let stringNumber = readLine()
+            
+            if let stringNumber = stringNumber, let number = Int(stringNumber) {
+                return number
+            } else {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                isWinOrLose = false
+            }
+        }
+    }
 }
