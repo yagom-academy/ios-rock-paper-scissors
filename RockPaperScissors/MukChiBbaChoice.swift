c9a26a733b28243038925f949d00b70bdedb5c4a
ue-mu <ym14170@gmail.com>
2023-09-08 14:18:16 +0900

    feat: enum타입 생성
    RockPaperScissorsChoice, MukChiBbaChoice 가위 바위 보와 묵 찌 빠 생성

---
+++ RockPaperScissors/MukChiBbaChoice.swift
@@ -0,0 +1,12 @@
+//
+//  MukChiBbaChoice.swift
+//  Created by uemu, mireu.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+enum MukJjiBbaChoice: Int {
+    case muk = 1
+    case jji = 2
+    case bba = 3
+    case quit = 0
+}
---
+++ RockPaperScissors/RockPaperScissorsChoice.swift
@@ -0,0 +1,12 @@
+//
+//  RockPaperScissorsChoice.swift
+//  Created by uemu, mireu.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+enum RockPaperScissorsChoice: Int {
+    case rock = 1
+    case paper = 2
+    case scissors = 3
+    case quit = 0
+}
