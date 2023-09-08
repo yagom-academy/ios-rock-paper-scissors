ea02921576bc65746542e27be9ae79d22caee532
mireu79 <mireu79@github.com>
2023-09-08 14:28:55 +0900

    feat: 가위바위보게임 구조체 정의
    기존의 가위바위보게임을 구조체로 정의

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -0,0 +1,53 @@
+//
+//  RockPaperScissors.swift
+//  Created by uemu, mireu.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+struct RockPaperScissors {
+    private var nextGame = MukJjiBbaGame()
+    
+    mutating func compareUserPickAndComputerPick(_ userChoice: RockPaperScissorsChoice, _ computerChoice: RockPaperScissorsChoice) {
+        switch (userChoice, computerChoice) {
+        case (.rock, .scissors), (.paper, .rock), (.scissors, .paper):
+            print("이겼습니다!")
+            turn = "사용자"
+            nextGame.playMukChiBbaGame()
+        case (.rock, .rock), (.paper, .paper), (.scissors, .scissors):
+            print("비겼습니다!")
+            playRockPaperScissors()
+        default:
+            turn = "컴퓨터"
+            print("졌습니다!")
+            nextGame.playMukChiBbaGame()
+        }
+    }
+    
+    private func selectMenu() -> RockPaperScissorsChoice {
+        while true {
+            print("가위(1), 바위(2), 보(3)!<종료: 0> :", terminator: " ")
+            guard let input = readLine(), let userChoice =  RockPaperScissorsChoice(rawValue: Int(input) ?? -1)  else {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                continue
+            }
+            return userChoice
+        }
+    }
+    
+    mutating func playRockPaperScissors() {
+        let isRunning = true
+        
+        while isRunning {
+            let computerChoice = RockPaperScissorsChoice(rawValue: getRandomNumber()) ?? .rock
+            let userChoice = selectMenu()
+            
+            if userChoice == .quit {
+                print("게임종료")
+                break
+            }
+            
+            compareUserPickAndComputerPick(userChoice, computerChoice)
+            break
+        }
+    }
+}
