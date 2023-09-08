86407d2c0fb6e2b479594c01756d342431dca95d
charles <kch1759@naver.com>
2023-09-08 11:10:43 +0900

    Feat: 변수 2개 선언 및 compareValues 함수 구현
    컴퓨터패와 사용자패를 담는 변수 선언
    컴퓨터패와 사용자패를 비교하여 승패 여부를 확인하는 함수 구현

--- RockPaperScissors/RockPaperScissors/MukchippaManager.swift
+++ RockPaperScissors/RockPaperScissors/MukchippaManager.swift
@@ -7,11 +7,13 @@
 
 struct MukchippaManager {
     var isUserTurn = true
+    private var computerCard: Mukchippa?
+    private var userCard: Mukchippa?
     
     private func printMenu(turn: String) {
         print("[\(turn)턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :", terminator: " ")
     }
-
+    
     private mutating func inputUserNumber() -> Int {
         while true {
             let stringNumber = readLine()
@@ -31,6 +33,21 @@ struct MukchippaManager {
             printMenu(turn: "사용자")
         } else {
             printMenu(turn: "컴퓨터")
+        }
+    }
+    
+    private mutating func compareValues() {
+        if computerCard == userCard {
+            isUserTurn ? print("사용자의 승리!") : print("컴퓨터의 승리!")
+            return
+        } else if computerCard == .ppa && userCard == .muk ||
+                  computerCard == .muk && userCard == .chi ||
+                  computerCard == .chi && userCard == .ppa {
+            print("컴퓨터의 터입니다.")
+            isUserTurn = false
+        } else {
+            print("사용자의 턴입니다.")
+            isUserTurn = true
         }
     }
 }
