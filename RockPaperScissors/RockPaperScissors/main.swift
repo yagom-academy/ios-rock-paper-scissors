b194b7261c4a8c04d737e7b410b13bb1e0febadc
KKOMGI <kkomgi90@gmail.com>
2024-01-08 23:20:50 +0900

    refactor: 묵찌빠 진행 로직 함수 분리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,22 +10,11 @@ func run() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
     
     let userInput = readLine()
-    let computerChoice = Int.random(in: 1...3)
     
     if let safeUserInput = userInput {
         switch safeUserInput {
         case "1", "2", "3":
-            print("컴퓨터 : \(computerChoice)")
-            
-            if safeUserInput == String(computerChoice) {
-                print("비겼습니다!")
-            } else if (safeUserInput == "1" && String(computerChoice) == "3") ||
-                        (safeUserInput == "2" && String(computerChoice) == "1") ||
-                        (safeUserInput == "3" && String(computerChoice) == "2") {
-                print("이겼습니다!")
-            } else {
-                print("졌습니다.")
-            }
+            showResult(userInput: safeUserInput)
         case "0":
             print("프린트 종료")
         default:
@@ -33,6 +22,20 @@ func run() {
         }
     } else{
         print("잘못된 입력입니다. 다시 시도하여 주세요.")
+    }
+}
+
+func showResult(userInput: String){
+    let computerChoice = String(Int.random(in: 1...3))
+    
+    print("컴퓨터 : \(computerChoice)")
+    
+    if userInput == computerChoice {
+        print("비겼습니다!")
+    } else if (userInput == "1" && computerChoice == "3") || (userInput == "2" && computerChoice == "1") || (userInput == "3" && computerChoice == "2") {
+        print("이겼습니다!")
+    } else {
+        print("졌습니다.")
     }
 }
 
