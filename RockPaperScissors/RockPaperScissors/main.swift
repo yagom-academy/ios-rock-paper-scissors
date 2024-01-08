7956243ad5f63fd43963c99d4dff463283bb60f6
KKOMGI <kkomgi90@gmail.com>
2024-01-08 22:40:05 +0900

    feat: 최초 실행 시 출력

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,5 +6,22 @@
 
 import Foundation
 
-print("Hello, World!")
+func run() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
+    
+    let userInput = readLine()
+    if let safeUserInput = userInput {
+        switch userInput {
+        case "1", "2", "3":
+            print("실행결과")
+        case "0":
+            print("프린트 종료")
+        default:
+            print("잘못된 입력입니다. 다시 시도하여 주세요.")
+        }
+    } else{
+        print("잘못된 입력입니다. 다시 시도하여 주세요.")
+    }
+}
 
+run()
