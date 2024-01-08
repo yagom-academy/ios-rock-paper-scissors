e45884ea7fc3886394d273c65cf4688af05f1c75
forseaest <u3z@naver.com>
2024-01-08 22:56:51 +0900

    feat: 가위, 바위, 보 결과 도출하는 로직 추가

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -10,10 +10,22 @@ func run() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
     
     let userInput = readLine()
+    let computerChoice = Int.random(in: 1...3)
+    
     if let safeUserInput = userInput {
-        switch userInput {
+        switch safeUserInput {
         case "1", "2", "3":
-            print("실행결과")
+            print("컴퓨터 : \(computerChoice)")
+            
+            if safeUserInput == String(computerChoice) {
+                print("비겼습니다!")
+            } else if (safeUserInput == "1" && String(computerChoice) == "3") ||
+                        (safeUserInput == "2" && String(computerChoice) == "1") ||
+                        (safeUserInput == "3" && String(computerChoice) == "2") {
+                print("이겼습니다!")
+            } else {
+                print("졌습니다.")
+            }
         case "0":
             print("프린트 종료")
         default:
