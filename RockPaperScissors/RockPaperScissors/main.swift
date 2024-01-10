49de2990e21d10b5c6dacdf323fc828eaffe151f
forseaest <u3z@naver.com>
2024-01-10 11:12:52 +0900

    refactor: showResult 함수에서 결과 도출하는 로직 분리

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,6 +6,12 @@
 
 import Foundation
 
+enum GameResult {
+    case draw
+    case win
+    case fail
+}
+
 func run() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
     
@@ -26,17 +32,31 @@ func run() {
 }
 
 func showResult(userInput: String) {
+    let gameResult = makeComputerChoice(userInput: userInput)
+    switch gameResult {
+    case .draw:
+        print("비겼습니다!")
+    case .win:
+        print("이겼습니다!")
+    case .fail:
+        print("졌습니다.")
+    default:
+        return
+    }
+}
+
+func makeComputerChoice(userInput: String) -> GameResult {
     let computerChoice = String(Int.random(in: 1...3))
-    let isWin: Bool = (userInput == "1" && computerChoice == "3") || (userInput == "2" && computerChoice == "1") || (userInput == "3" && computerChoice == "2")
-    
     print("컴퓨터 : \(computerChoice)")
     
+    let isWin: Bool = (userInput == "1" && computerChoice == "3") || (userInput == "2" && computerChoice == "1") || (userInput == "3" && computerChoice == "2")
+    
     if userInput == computerChoice {
-        print("비겼습니다!")
+        return .draw
     } else if isWin {
-        print("이겼습니다!")
+        return .win
     } else {
-        print("졌습니다.")
+        return .fail
     }
 }
 
