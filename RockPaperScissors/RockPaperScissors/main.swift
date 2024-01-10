ba8cc2836ec280faa1bc1f7d0e9fe49a5a99715b
KKOMGI <kkomgi90@gmail.com>
2024-01-10 13:43:14 +0900

    refactor: 튜플로 가독성 향상 및 코드 간결화

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -9,55 +9,54 @@ import Foundation
 enum GameResult {
     case draw
     case win
-    case fail
+    case lose
 }
 
-func run() {
+func gameRun() {
     print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
     
     let userInput = readLine()
     
     if let safeUserInput = userInput {
-        switch safeUserInput {
-        case "1", "2", "3":
-            showResult(userInput: safeUserInput)
-        case "0":
+        //
+        if(safeUserInput == "0") {
             print("게임 종료")
+            return
+        }
+        
+        let computerChoice = makeComputerChoice()
+        
+        switch (safeUserInput, computerChoice) {
+        case (_, _) where safeUserInput == computerChoice:
+            print("비겼습니다!")
+        case ("1", "3"), ("2", "1"), ("3", "2"):
+            print("이겼습니다")
         default:
-            print("잘못된 입력입니다. 다시 시도하여 주세요.")
+            print("졌습니다.")
         }
     } else{
         print("잘못된 입력입니다. 다시 시도하여 주세요.")
     }
 }
 
-func showResult(userInput: String) {
-    let gameResult = makeComputerChoice(userInput: userInput)
-    switch gameResult {
-    case .draw:
-        print("비겼습니다!")
-    case .win:
-        print("이겼습니다!")
-    case .fail:
-        print("졌습니다.")
-    default:
-        return
-    }
-}
+//func showResult(userInput: String) {
+//    let gameResult = makeComputerChoice(userInput: userInput)
+//    switch gameResult {
+//    case .draw:
+//        print("비겼습니다!")
+//    case .win:
+//        print("이겼습니다!")
+//    case .lose:
+//        print("졌습니다.")
+//    default:
+//        return
+//    }
+//}
 
-func makeComputerChoice(userInput: String) -> GameResult {
+func makeComputerChoice() -> String {
     let computerChoice = String(Int.random(in: 1...3))
-    print("컴퓨터 : \(computerChoice)")
-    
-    let isWin: Bool = (userInput == "1" && computerChoice == "3") || (userInput == "2" && computerChoice == "1") || (userInput == "3" && computerChoice == "2")
-    
-    if userInput == computerChoice {
-        return .draw
-    } else if isWin {
-        return .win
-    } else {
-        return .fail
-    }
+    return String(computerChoice)
 }
 
-run()
+
+gameRun()
