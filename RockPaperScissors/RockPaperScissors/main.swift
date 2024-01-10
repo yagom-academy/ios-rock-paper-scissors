da011a885e7b98cdc20554cd61fa832c2bcbddd1
yawoong2 <adamzoong@naver.com>
2024-01-10 14:48:07 +0900

    refactor: 매직넘버 생성 및 변수명 변경

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -6,7 +6,12 @@
 
 let inputList: Array<Int> = [0, 1, 2, 3]
 
-enum RPS: Int {
+struct errorCode {
+    static let outOfRange = -1
+    static let wrongInput = -2
+}
+
+enum Gesture: Int {
     case scissors = 1
     case rock = 2
     case paper = 3
@@ -16,7 +21,7 @@ func computerPlay() -> Int {
     let menuList = inputList[1...3]
     
     guard let computerSelect = menuList.randomElement() else {
-        return -1
+        return errorCode.outOfRange
     }
     return computerSelect
 }
@@ -24,11 +29,11 @@ func computerPlay() -> Int {
 func userInput() -> Int {
     guard let input = readLine(), !input.isEmpty else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        return -1
+        return errorCode.wrongInput
     }
     guard let inputInt = Int(input), inputList.contains(inputInt) else {
         print("잘못된 입력입니다. 다시 시도해주세요.")
-        return -1
+        return errorCode.wrongInput
     }
     
     return inputInt
@@ -36,23 +41,26 @@ func userInput() -> Int {
 
 func rockScissorsPaper() {
     var repeatFlag: Bool = false
+    let scissors:Int = Gesture.scissors.rawValue
+    let rock:Int = Gesture.rock.rawValue
+    let paper:Int = Gesture.paper.rawValue
     
     repeat {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
         let computerNumber = computerPlay()
         let userInput = userInput()
         
-        guard let rpsValue = RPS(rawValue: computerNumber) else {
+        guard let rpsValue = Gesture(rawValue: computerNumber) else {
             return
         }
         
         switch rpsValue {
         case .scissors:
-            repeatFlag = gameResult(userInput: userInput, gameCase: [2, 3])
+            repeatFlag = gameResult(userInput: userInput, gameCase: [scissors, paper])
         case .rock:
-            repeatFlag = gameResult(userInput: userInput, gameCase: [3, 1])
+            repeatFlag = gameResult(userInput: userInput, gameCase: [paper, scissors])
         case .paper:
-            repeatFlag = gameResult(userInput: userInput, gameCase: [1, 2])
+            repeatFlag = gameResult(userInput: userInput, gameCase: [scissors, rock])
         }
     } while repeatFlag
 }
