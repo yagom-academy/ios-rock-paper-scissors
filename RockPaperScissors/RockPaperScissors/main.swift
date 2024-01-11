ee30aebb6fe0a438e6858f85ba3bd0a9a6d16c4b
KKOMGI <kkomgi90@gmail.com>
2024-01-12 00:41:07 +0900

    rename: 네이밍 수정 및 step1 과 step2 게임 분할

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -18,11 +18,9 @@ enum GameState {
 }
 
 var gameState: GameState = .rockScissorPaper
+var whoseTurn = "computer"
 
 let regex = #"^[0-3]$"#
-func isNumber(_ input: String) -> Bool {
-    return input.range(of: regex, options: .regularExpression) != nil
-}
 
 func gameRun() {
     var loopFlag = true
@@ -30,26 +28,26 @@ func gameRun() {
     while loopFlag {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> :", terminator: " ")
         
-        let userInput = readLine()
+        let playerInput = readLine()
         
-        guard let safeUserInput = userInput else {
+        guard let safePlayerInput = playerInput else {
             print("잘못된 입력입니다. 다시 시도하여 주세요.")
             loopFlag = false
             return
         }
         
-        if (!isNumber(safeUserInput)) {
+        if (!isValidInput(safePlayerInput)) {
             print("0-3까지의 수를 입력해주세요.")
             continue
         }
         
-        if (safeUserInput == "0") {
+        if (safePlayerInput == "0") {
             print("게임 종료")
             return
         }
         
         let computerChoice = makeComputerChoice()
-        let gameResult = judgeGame(userChoice: safeUserInput, computerChoice: computerChoice)
+        let gameResult = judgeRockScissorPaper(playerChoice: safePlayerInput, computerChoice: computerChoice)
         
         print("컴퓨터: \(computerChoice)")
         printResult(gameResult: gameResult)
@@ -59,9 +57,83 @@ func gameRun() {
         } else {
             loopFlag = false
             gameState = .mukChiba
-            whoseTurn = gameResult == .win ? "user" : "computer"
+            whoseTurn = gameResult == .win ? "player" : "computer"
             runGameStep2()
         }
+    }
+}
+
+func runGameStep2() {
+    var loopFlag = true
+    
+    while loopFlag {
+        print("[\(whoseTurn)의 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :", terminator: " ")
+        
+        let playerSecondInput = readLine()
+        
+        guard let playerSecondChoice = playerSecondInput else {
+            print("잘못된 입력입니다. 다시 시도하여 주세요.")
+            loopFlag = false
+            return
+        }
+        
+        if (!isValidInput(playerSecondChoice)) {
+            print("0-3까지의 수를 입력해주세요.")
+            continue
+        }
+        
+        if (playerSecondChoice == "0") {
+            print("게임 종료")
+            return
+        }
+        
+        let computerSecondChoice = makeComputerChoice()
+        let gameResult = judgeMukChiBa(playerChoice: playerSecondChoice, computerChoice: computerSecondChoice)
+        
+        print("컴퓨터: \(computerSecondChoice)")
+        
+        switch gameResult {
+        case .win:
+            whoseTurn = "player"
+            print("\(whoseTurn)의 턴입니다.")
+        case .lose:
+            whoseTurn = "computer"
+            print("\(whoseTurn)의 턴입니다.")
+        default:
+            print(whoseTurn == "computer" ? "컴퓨터의 승리!" : "player(사용자)의 승리!")
+            loopFlag = false
+        }
+    }
+}
+
+func isValidInput(_ input: String) -> Bool {
+    return input.range(of: regex, options: .regularExpression) != nil
+}
+
+func makeComputerChoice() -> String {
+    let computerChoice = String(Int.random(in: 1...3))
+    return String(computerChoice)
+}
+
+func judgeRockScissorPaper(playerChoice: String, computerChoice: String) -> GameResult {
+    switch (playerChoice, computerChoice) {
+    case (_, _) where playerChoice == computerChoice:
+        return .draw
+    case ("1", "3"), ("2", "1"), ("3", "2") :
+        return .win
+    default:
+        return .lose
+    }
+}
+
+func judgeMukChiBa(playerChoice: String, computerChoice: String) -> GameResult {
+    switch (playerChoice, computerChoice) {
+    case (_, _) where playerChoice == computerChoice:
+        return .draw
+    case ("1", "3"), ("2", "1"), ("3", "2") :
+        return .lose
+    default:
+        return .win
     }
 }
 
@@ -73,75 +145,6 @@ func printResult(gameResult: GameResult) {
         print("이겼습니다!")
     case .lose:
         print("졌습니다!")
-    }
-}
-
-func judgeGame(userChoice: String, computerChoice: String) -> GameResult {
-    switch (userChoice, computerChoice) {
-    case (_, _) where userChoice == computerChoice:
-        return .draw
-    case ("1", "3"), ("2", "1"), ("3", "2") :
-        if gameState == .rockScissorPaper {
-            return .win
-        } else {
-            return .lose
-        }
-    default:
-        if gameState == .rockScissorPaper {
-            return .lose
-        } else {
-            return .win
-        }
-    }
-}
-
-func makeComputerChoice() -> String {
-    let computerChoice = String(Int.random(in: 1...3))
-    return String(computerChoice)
-}
-
-var whoseTurn = "computer"
-
-func runGameStep2() {
-    var loopFlag = true
-    
-    while loopFlag {
-        print("[\(whoseTurn)의 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :", terminator: " ")
-        
-        let userInput = readLine()
-        
-        guard let userChoice = userInput else {
-            print("잘못된 입력입니다. 다시 시도하여 주세요.")
-            loopFlag = false
-            return
-        }
-        
-        if (!isNumber(userChoice)) {
-            print("0-3까지의 수를 입력해주세요.")
-            continue
-        }
-        
-        if (userChoice == "0") {
-            print("게임 종료")
-            return
-        }
-        
-        let computerChoice = makeComputerChoice()
-        let gameResult = judgeGame(userChoice: userChoice, computerChoice: computerChoice)
-        
-        print("컴퓨터: \(computerChoice)")
-        
-        switch gameResult {
-        case .win:
-            whoseTurn = "user"
-            print("\(whoseTurn)의 턴입니다.")
-        case .lose:
-            whoseTurn = "computer"
-            print("\(whoseTurn)의 턴입니다.")
-        default:
-            print(whoseTurn == "computer" ? "컴퓨터의 승리!" : "사용자의 승리!")
-            loopFlag = false
-        }
     }
 }
 
