df8577e2c04f1056d04c6ffbf9d7db9287e739e5
forseaest <u3z@naver.com>
2024-01-11 21:52:28 +0900

    feat: 묵찌빠 게임 함수 작성

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -59,4 +59,71 @@ func makeComputerChoice() -> String {
     return String(computerChoice)
 }
 
+var whoseTurn = "computer"
+
+func runGameStep2() {
+    // 게임 진행
+    // 컴퓨터의 임의의 수를 뽑는다
+    let computerChoice = makeComputerChoice()
+    
+    var loopFlag = true
+    
+    while loopFlag {
+        print("[\(whoseTurn)의 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> :", terminator: " ")
+        
+        let userInput = readLine()
+        
+        guard let userChoice = userInput else {
+            print("잘못된 입력입니다. 다시 시도하여 주세요.")
+            loopFlag = false
+            return
+        }
+        
+        if (!isNumber(userChoice)) {
+            print("0-3까지의 수를 입력해주세요.")
+            continue
+        }
+        
+        if (userChoice == "0") {
+            print("게임 종료")
+            return
+        }
+        
+        // if 컴퓨터가 이겼다면
+        // 컴퓨터의 턴이며 값이 같을 경우, whoseTurn이 컴퓨터일 때,
+//        if (whoseTurn == "computer" && userChoice == computerChoice) {
+//            
+//        }
+        if (userChoice == computerChoice) {
+            if whoseTurn == "computer" {
+                print("컴퓨터의 승리!")
+            } else {
+                print("사용자의 승리!")
+            }
+        } else {
+            
+        }
+        
+        // else if 사용자가 이겼다면
+        
+        // else 잘못된 입력인 경우
+        // 컴퓨터에게 턴을 넘긴다
+        
+        // 사용자에게 0부터 3까지 입력 받는다
+        // 결과를 출력한다
+        
+        // if 사용자 == 컴퓨터
+        // 현재 턴을 차지한 자가 승리한다
+        
+        // else if 사용자 != 컴퓨터
+        // if 사용자 패랑 컴퓨터 패를 비교한다
+        // 묵찌빠 다시 실행한다
+        
+        // else if 입력이 0인 경우
+        // 게임 종료
+        
+        loopFlag = false
+    }
+}
+
 gameRun()
